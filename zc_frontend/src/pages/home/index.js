71e5571753cb6bda1b476bdb6cd51239bdbd7b07
EmmanuelOlwafemi <seek9ja@gmail.com>
2021-08-30 17:05:17 +0100

    added profile modal component close #554
    Former-commit-id: 4a13d9d4397fa4a822d2f865b500cbb5d874910c

--- zc_frontend/src/pages/home/index.js
+++ zc_frontend/src/pages/home/index.js
@@ -3,10 +3,18 @@ import { Sidebar } from '../../components/Sidebar'
 import { Topbar } from '../../components/Topbar'
 import styles from '../../styles/Home.module.css'
 import { UrlProvider } from '../../contexts/Url'
+import ProfileModal from '../../components/ProfileModal'
+import { useState } from 'react'
 
 const Home = () => {
+
+  const [isShow, setShow] = useState(true);
+
   return (
     <UrlProvider>
+      <ProfileModal isShow={isShow} setShow={setShow} title="Edit your profile">
+        Testing
+      </ProfileModal>
       <div className={styles.container}>
         <Sidebar />
         <div className={styles.room}>
