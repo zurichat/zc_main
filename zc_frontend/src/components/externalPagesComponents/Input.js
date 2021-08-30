dba687ffded4811a0d2f7b39cfaac5171ffb27ea
Tunde Alabi <tundealabi4780@gmail.com>
2021-08-30 14:32:49 +0100

    update to show login page by pidoxy
    Former-commit-id: 6da864d97ef21300df49763eec9b3e6196899ca7

---
+++ zc_frontend/src/components/externalPagesComponents/Input.js
@@ -0,0 +1,9 @@
+const Input = () => { 
+    return (
+        <div>
+            This is input component
+        </div>
+    )
+}
+
+export default Input
---
+++ zc_frontend/src/pages/signup/index.js
@@ -0,0 +1,6 @@
+const Login = () => {
+    return <div>This is signup page</div>
+  }
+  
+  export default Login
+  
