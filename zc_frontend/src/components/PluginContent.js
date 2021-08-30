0bde1c0f8672d45c7fd832fd2d0e6888cebca934
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-08-30 15:05:43 +0100

    Added Proxy Route, now all plugin resource request will be routed through the backend... bye, bye CORS
    Former-commit-id: 3948bf6c9353617f935d660e733ab3018b1eebb3

--- zc_frontend/src/components/PluginContent.js
+++ zc_frontend/src/components/PluginContent.js
@@ -4,10 +4,7 @@ import { URLContext } from '../contexts/Url'
 import cheerio from 'cheerio'
 
 import styles from '../styles/PluginContent.module.css'
-
-const proxyUrl = (url, baseUrl = '', mimeType = 'text/html') => {
-  return `/proxy?url=${encodeURI(url)}&mimeType=${encodeURI(mimeType)}`
-}
+import Welcome from './Welcome'
 
 export const PluginContent = () => {
   // const pluginUrl = '/apps/default';
@@ -19,39 +16,54 @@ export const PluginContent = () => {
     const elRoot = document.getElementById('zc-plugin-root')
     const reProtocol = /^https?:\/\//
     const oURL = new URL(reProtocol.test(url) ? url : 'http://' + url)
-    const prefixLink = (url, oURL) =>
-      reProtocol.test(url) ? url : `${oURL.origin}${url}`
+    const prefixLink = (url, oURL, mimeType = 'text/html') => {
+      let ret = reProtocol.test(url) ? url : `${oURL.origin}${url}`
+      return `/proxy?url=${ret}&mimeType=${mimeType}`
+    }
 
-    axios.get(oURL).then((res) => {
-      const $ = cheerio.load(res.data)
+    axios
+      .get(prefixLink(oURL.toString()))
+      .then((res) => {
+        const $ = cheerio.load(res.data)
 
-      $(`link[rel="stylesheet"]`).each(function () {
-        const link = document.createElement('link')
-        Object.keys(this.attribs).forEach((attr) =>
-          link.setAttribute(attr, this.attribs[attr])
-        )
-        link.setAttribute('href', prefixLink(this.attribs.href, oURL))
-        link.setAttribute('data-plugin-res', true)
-        $(this).remove()
-        document.head.prepend(link)
+        // append stylesheet
+        $(`link[rel="stylesheet"]`).each(function () {
+          const link = document.createElement('link')
+          Object.keys(this.attribs).forEach((attr) =>
+            link.setAttribute(attr, this.attribs[attr])
+          )
+          link.setAttribute(
+            'href',
+            prefixLink(this.attribs.href, oURL, 'text/css')
+          )
+          link.setAttribute('data-plugin-res', true)
+          $(this).remove()
+          document.head.prepend(link)
+        })
+
+        // append scripts
+        $('script').each(function () {
+          const script = document.createElement('script')
+          Object.keys(this.attribs).forEach((attr) =>
+            script.setAttribute(attr, this.attribs[attr])
+          )
+          if (script.src) {
+            script.setAttribute(
+              'src',
+              prefixLink(this.attribs.src, oURL, 'application/javascript')
+            )
+          } else {
+            script.innerText = $(this).html()
+          }
+          $(this).remove()
+          script.setAttribute('data-plugin-res', true)
+          document.body.appendChild(script)
+        })
+        elRoot.innerHTML = $('body').html()
       })
-
-      $('script').each(function () {
-        const script = document.createElement('script')
-        Object.keys(this.attribs).forEach((attr) =>
-          script.setAttribute(attr, this.attribs[attr])
-        )
-        if (script.src) {
-          script.setAttribute('src', prefixLink(this.attribs.src, oURL))
-        } else {
-          script.innerText = $(this).html()
-        }
-        $(this).remove()
-        script.setAttribute('data-plugin-res', true)
-        document.body.appendChild(script)
+      .catch((e) => {
+        elRoot.innerHTML = `Failed to Load ${url} Plugin: ${e.message}`
       })
-      elRoot.innerHTML = $('body').html()
-    })
     return () => {
       elRoot.innerHTML = ''
       document
@@ -61,10 +73,11 @@ export const PluginContent = () => {
   }, [url])
 
   return (
-    <section className={styles.container}>
-      <div id="zc-plugin-root" className={styles.content}>
-        Loading...
-      </div>
-    </section>
+    <>
+      <section className={styles.container}>
+        <div id="zc-plugin-root">Loading...</div>
+      </section>
+      {!url && <Welcome />}
+    </>
   )
 }
--- zc_frontend/src/contexts/Url.js
+++ zc_frontend/src/contexts/Url.js
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react'
 
 export const URLContext = createContext(null)
 export const UrlProvider = ({ children }) => {
-  const [url, setUrl] = useState('http://zc-react-todolist-plugin.netlify.app')
+  const [url, setUrl] = useState(null)
 
   return (
     <URLContext.Provider value={{ setUrl, url }}>
