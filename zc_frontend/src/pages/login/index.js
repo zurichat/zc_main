839934b4b201e128ad00a0482f7981e87d4a3627
sammiearchie77 <chinonsosamuel29@gmail.com>
2021-08-31 06:19:15 +0100

    issue #325 interactive login

--- zc_frontend/src/pages/login/index.js
+++ zc_frontend/src/pages/login/index.js
@@ -1,13 +1,29 @@
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable react/jsx-no-duplicate-props */
 import styles from '../../styles/Login.module.css';
 import React, { useState } from 'react';
 
 const Login = () => {
 
 const [email, setEmail] = useState("");
-const [pass, setPass] = useState("");
+const [pass, setPass] = useState({
+  password: "",
+  showPassword: false,
+});
+const handleClickShowPassword = () => {
+  setPass({ ...pass, showPassword: !pass.showPassword });
+};
+
+const handleMouseDownPassword = (event) => {
+  event.preventDefault();
+};
+
+const handlePasswordChange = (prop) => (event) => {
+  setPass({ ...pass, [prop]: event.target.value });
+};
 
 return (
-<div className={`container`}>
+<div className={`container ${styles.body}`}>
   <div class={`row`}>
     <div class={`col-md-3`}></div>
     <div class={`col-md-6`}>
@@ -18,32 +34,49 @@ return (
       </div>
       <div className={`pt-1 mt-3`}>
         
-        <h2 className={`pt-3 `}>Login in</h2>
+        <h2 className={`pt-3 `}>Log in</h2>
         <p className={styles.subtext}>Login with the data you entered during your</p>
         <div className={`d-flex ${styles.email_input}`}>
           <div className={`mb-3 col-12 col-md-6 w-100`}>
             <input type="email" className={`py-2 form-control`} value={email} onChange={(e)=> {
             console.log(e.target.value)
             setEmail(e.target.value)
-            }} placeholder="codinggee@teameinstein.zuri.com" />
+            }} placeholder="Enter an email" />
           </div>
         </div>
         <div className={`d-flex ${styles.email_input}`}>
-          <div className={`mb-3 col-12 col-md-6 w-100`}>
-            <input type="email" className={`py-2 form-control`} value={pass} onChange={(e)=> {
-            console.log(e.target.value)
-            setPass(e.target.value)
-            }} placeholder="********" />
+          <div className={`mb-3 col-12 col-md-6 w-100 d-flex`}>
+            <input 
+              type={pass.showPassword ? "text" : "password"} 
+              onChange={handlePasswordChange("password")}
+              className={`py-2 form-control`} 
+              value={pass.password} 
+              onChange={(e)=> {
+              console.log(e.target.value)
+              setPass(e.target.value)
+            }} placeholder="Enter a password" 
+            id="id_password"
+            />
           </div>
+          <i className={`far fa-eye ${styles.far}`} 
+            id="togglePassword"
+            onClick={handleClickShowPassword}
+            onMouseDown={handleMouseDownPassword}></i>
+            {pass.password ? <i className={`far far-eye ${styles.far}`}></i>: <i className={`fa-eye-slash ${styles.far}`}></i>}
         </div>
-        <div class="mb-3 form-check d-flex justify-space-between">
+        <div class="mb-3 form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          <label className={'mx-4'}>Forgot Password? get help siging in</label>
+          <label className={`form-check-label text-secondary ${styles.checktext}`} for="exampleCheck1">Remember me</label>
+          <label className={`float-end text-secondary  ${styles.checktext}`}>Forgot password? <a href="/" className={`${styles.checklink}`}>Get help signing in</a></label>
         </div>
         <div>
-          <button className={` ${styles.button} btn mb-3 col-12 col-md-6 px-5 px-md-5 py-2 btn-success`}>Sign
+          <button className={` ${styles.button} btn mb-3 col-12 col-md-6 px-5 px-md-5 py-2 btn-success`}>Log
             In</button>
+        </div>
+        <div className={`my-2 d-flex justify-content-around`}>
+            <img className={`w-50 p-3`} src="Line.svg" alt="line" />
+            <p className={`${styles.or}`}>OR</p>
+            <img className={`w-50 p-3`} src="Line.svg" alt="line" srcset="" />
         </div>
         <div className={`my-3`}>
           <button className={`${styles.button_outline} m-0 col-12 col-md-6 btn px-2 px-md-5 py-2
@@ -51,10 +84,8 @@ return (
               alt="google icon" />Log in with Google</button>
         </div>
         <div className={`my-3`}>
-          <button className={`${styles.button_outline} m-0 col-12 col-md-6 px-2 px-md-5 py-2 btn btn-outline-dark`}><img
-              className={`text-align-center px-2`}
-              src={`https://media.idownloadblog.com/wp-content/uploads/2018/07/Apple-logo-black-and-white.png`}
-              alt="Apple icon" />Log in with Apple</button>
+          <button className={`${styles.button_outline} m-0 col-12 col-md-6 px-2 px-md-5 py-2 btn btn-outline-dark`}>
+             <i class="fab fa-apple"></i> Log in with Apple</button>
         </div>
         <h3>New to Us?</h3>
         <div className={`my-3`}>
@@ -62,10 +93,10 @@ return (
             btn-outline-primary`}><img className={`text-align-center px-2`} alt="" />Create an Account</button>
         </div>
       </div>
-      <div class={`d-inline-flex my-5 mx-5`}>
-        <a href="#" class={`mx-md-4 text-secondary`}>Contact Us</a>
-        <a href="#" class={`mx-md-4 text-secondary`}>Legal Policy</a>
-        <a href="#" class={`mx-md-4 text-secondary`}>Change Region</a>
+      <div class={`d-flex justify-content-around my-5  ${styles.footer}`}>
+        <a href="/" class={`mx-md-4 text-secondary`}>Contact Us</a>
+        <a href="/" class={`mx-md-4 text-secondary`}>Legal Policy</a>
+        <a href="/" class={`mx-md-4 text-secondary`}><i class="fas fa-globe"></i> Change Region <i class="fas fa-caret-down"></i></a>
       </div>
     </div>
     <div class={`col-md-3`}></div>
