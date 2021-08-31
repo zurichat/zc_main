2ef6e9d960bd2c387ba8cdabf1a3cb392a36112e
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-08-31 11:26:09 +0100

    Updates Button component

--- zc_frontend/src/components/externalPagesComponents/Button.js
+++ zc_frontend/src/components/externalPagesComponents/Button.js
@@ -1,9 +1,28 @@
-const Button = () => {
-    return (
-        <div>
-            This is button component
-        </div>
-    )
-}
+import styles from "../../styles/Button.module.css";
 
-export default Button
+/**
+ *
+ * @param {Object} prop - button props consisting of ;
+ * 1) variant - indicating what styles to add to the default style. By default it will add the 'primary' styles. If the value is 'accent', it will not add any additional styles and you can style yourself using className or styles prop.
+ * 2) all the regular button attributes.
+ * @returns
+ */
+
+const Button = ({ variant = "primary", className = "", ...rest }) => {
+  return (
+    <button
+      className={`${styles.general_button} ${className} ${
+        variant === "secondary"
+          ? styles.general_button__secondary
+          : variant === "accent"
+          ? ""
+          : styles.general_button__primary
+      }`}
+      {...rest}
+    >
+      {rest.children}
+    </button>
+  );
+};
+
+export default Button;
