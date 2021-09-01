6f2ad323807e290193945b705aafe172729e7228
Yasir Salis <dantaneecodes@gmail.com>
2021-09-01 14:36:11 +0000

    Footer component with optional message display component

--- zc_frontend/src/pages/login/index.js
+++ zc_frontend/src/pages/login/index.js
@@ -1,6 +1,7 @@
 import styles from './styles/Login.module.css'
 import React, { useState } from 'react'
 import LoginLoading from '../../components/LoginLoading'
+import Footer from '../../components/externalPagesComponents/Footer'
 
 const Login = () => {
   const [email, setEmail] = useState('')
--- zc_frontend/src/pages/news/index.js
+++ zc_frontend/src/pages/news/index.js
@@ -1,5 +1,10 @@
+import Footer from "../../components/externalPagesComponents/Footer"
+
 const Login = () => {
-  return <div>This is news page</div>
+  return <div>This is news page
+     
+  </div>
+ 
 }
 
 export default Login
