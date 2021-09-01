4348e237c768e203de19b6afab0660732245a9c1
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-01 14:16:30 +0100

    removed files for pages that were unapproved | restructured features folder

--- zc_frontend/src/pages/features/components/Section4.js
+++ zc_frontend/src/pages/features/components/Section4.js
@@ -1,12 +1,10 @@
-import app from '../../assets/app.png';
-import fun from '../../assets/Fun.png';
-import styles from '../../styles/Section4.module.css';
+import styles from '../styles/Section4.module.css';
 const Section4 = () => {
     return(
         <div className={styles.section4}>
-            <img src={app} alt='app' className={styles.appImage} />
+            <img src={`/app.png`} alt='app' className={styles.appImage} />
             <div className={styles.funSection}>
-                <img src={fun} alt='fun' className={styles.funImage} />
+                <img src={`/Fun.png`} alt='fun' className={styles.funImage} />
                 <div className={styles.funText}>
                     <h2>Fun at is Peak</h2>
                     <p>With zuri Chat, it’s not just all 
