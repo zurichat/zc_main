ef3ba2b4b418a4b1072643776182521c3f5c82df
Akindeji <akindejioluwaseun015@gmail.com>
2021-09-01 15:20:02 +0100

    build and style status badge component close #422
    Former-commit-id: 681863716c48357614a624a0c92c4e7dc9d78bb0 [formerly c2ceb878f660cd0fd5fabadcc0378c1f16117d94]
    Former-commit-id: 4a358060433a25c68449cfac95fbde256b41a89e

--- zc_frontend/src/components/Topbar.js
+++ zc_frontend/src/components/Topbar.js
@@ -1,22 +1,24 @@
-import React, { useContext } from 'react'
-import { TopbarContext } from '../contexts/Topbar'
+import React, { useContext } from "react";
+import { TopbarContext } from "../contexts/Topbar";
 
-import styles from '../styles/Topbar.module.css'
-import SearchBar from './externalPagesComponents/SearchBar'
+import styles from "../styles/Topbar.module.css";
+import SearchBar from "./externalPagesComponents/SearchBar";
+import StatusBadge from "./StatusBadge";
 
 export const Topbar = () => {
-  const state = useContext(TopbarContext)
-  const { openModal } = state
+  const state = useContext(TopbarContext);
+  const { openModal } = state;
 
   return (
     <div className={styles.container}>
       <div className={styles.topbar}>
         <SearchBar />
         <img src="/settings.svg" alt="settings" />
+        <StatusBadge />
         <div className={styles.profile} onClick={openModal}>
           <img src="/profile.png" alt="Profile" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
