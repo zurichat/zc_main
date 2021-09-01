5ce96377dd225bbd73c578ff30ee3124d8c1a86f
Ayodejioladimeji <layobright11@gmail.com>
2021-09-01 13:28:03 -0700

    feat/set status tag feature
    Former-commit-id: a4b5574dceddf6a364f43bac93e58793e921f78e [formerly e34b857ded95903746c44bb7c0dcd67dfafd8e55]
    Former-commit-id: 5a053fb5a8ac5081a241bd793928f180067f71b1

--- zc_frontend/src/components/Topbar.js
+++ zc_frontend/src/components/Topbar.js
@@ -8,22 +8,12 @@ import SearchBar from './externalPagesComponents/SearchBar'
 export const Topbar = () => {
   const state = useContext(URLContext)
   const { openModal } = state
-<<<<<<< HEAD
-
-=======
->>>>>>> 216e38aad34079b5acb93e1043f9a4c6cbedd999
 
   return (
     <div className={styles.container}>
       <div className={styles.topbar}>
         <SearchBar />
-<<<<<<< HEAD
-        <Link to="/account_settings">
-          <img src="/settings.svg" alt="settings" />
-        </Link>
-=======
         <img src="/settings.svg" alt="settings" />
->>>>>>> 216e38aad34079b5acb93e1043f9a4c6cbedd999
         <div className={styles.profile} onClick={openModal}>
           <img src="/profile.png" alt="Profile" />
         </div>
