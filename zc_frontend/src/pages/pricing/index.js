cd25e714efe6994d1b25c97c2b70775aafa7be05
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-02 17:31:30 +0100

    chore
    Former-commit-id: 35cf2a93523517396767e79fee8b85360343f0d0 [formerly 790a140160a71ef525fd6d6cc2ee448a3a9d3647 [formerly e8ef931cbaab7a604a99550c4c1eb72b578ee758]]
    Former-commit-id: 0ed6a277e00571979e74676302708a6530f127c7
    Former-commit-id: e02074e4121d049df427d4f79d109c1c0da18073

--- zc_frontend/src/pages/pricing/index.js
+++ zc_frontend/src/pages/pricing/index.js
@@ -1,11 +1,13 @@
 import Header from '../../components/externalPagesComponents/Header'
 import PricingTable from './components/PricingTable'
+// import Section4PricingPage from './components/Section4_pricing_page'
 
 function Pricing() {
   return (
     <div>
       <Header />
       <PricingTable />
+      {/* <Section4PricingPage /> */}
     </div>
   )
 }
