1302ef0c8d25243152b619062ebf2d612f2f6844
Williams Samuel <samwill300@gmail.com>
2021-09-02 13:05:13 +0100

    added workspace loading screen close #429

--- zc_frontend/src/App.js
+++ zc_frontend/src/App.js
@@ -1,31 +1,28 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import WorkspaceLoader from './components/WorkspaceLoading';
 import Home from './pages/home'
 import Login from './pages/login'
 import SignUp from './pages/signup'
-import Blog from './pages/blog'
-import News from './pages/news'
+// import Blog from './pages/blog'
+// import News from './pages/news'
 import './styles/globals.css'
+// import 'tailwindcss/tailwind.css'
 
 const App = () => (
-	<BrowserRouter>
-		<Switch>
-			<Route path='/' exact>
-				<Home />
-			</Route>
-			<Route path='/login'>
-				<Login />
-			</Route>
-			<Route path='/signup'>
-				<SignUp />
-			</Route>
-			<Route path='/blog'>
-				<Blog />
-			</Route>
-			<Route path='/news'>
-				<News />
-			</Route>
-		</Switch>
-	</BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact>
+      	<Home />
+      </Route>
+      <Route path="/login">
+        	<Login />
+      </Route>
+      <Route path="/signup">
+        	<SignUp />
+      </Route>
+    </Switch>
+	 <WorkspaceLoader />
+  </BrowserRouter>
 )
 
 export default App
--- zc_frontend/src/pages/login/index.js
+++ zc_frontend/src/pages/login/index.js
@@ -1,82 +1,176 @@
-import styles from './styles/Login.module.css'
+/* eslint-disable no-template-curly-in-string */
+/* eslint-disable react/jsx-no-duplicate-props */
+import styles from '../../styles/Login.module.css'
 import React, { useState } from 'react'
-import LoginLoading from '../../components/LoginLoading'
 
 const Login = () => {
-	const [email, setEmail] = useState('')
-	const [loggingIn, setLoggingIn] = useState(false)
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState({
+    password: '',
+    showPassword: false
+  })
+  const handleClickShowPassword = () => {
+    setPass({ ...pass, showPassword: !pass.showPassword })
+  }
 
-	return (
-		<div className={`container text-center m-auto`}>
-			{email && loggingIn && <LoginLoading />}
-			<div className={`pt-5 px-3 mt-5`}>
-				<span>
-					<img src='/zurichatlogo.svg' alt='logo' />
-					<p className={`d-inline  p-2`}>Zuri chat</p>
-				</span>
-				<h2 className={` pt-2`}>Sign in to Zuri Chat</h2>
-				<p className={styles.subtext}>Enter your email to sign In</p>
-				<div className={`my-3`}>
-					<button
-						className={`${styles.button_outline} m-0 col-12 col-md-6 px-2 px-md-5 py-2 btn btn-outline-dark`}>
-						<img
-							className={`text-align-center px-2`}
-							src={`/googleicon.svg`}
-							alt='google icon'
-						/>
-						Sign in with Google
-					</button>
-				</div>
-				<div className={`my-3`}>
-					<button
-						className={`${styles.button_outline} m-0 col-12 col-md-6 btn px-2 px-md-5 py-2 btn-outline-primary`}>
-						<img
-							className={`text-align-center px-2`}
-							src={`/facebookicon.svg`}
-							alt='google icon'
-						/>
-						Sign in with Facebook
-					</button>
-				</div>
-				<div className={`d-flex`}>
-					<div className={`col-12 col-md-6 m-auto mt-5`}>
-						<p
-							className={`mb-0 text-start ${styles.email_subheading}`}>
-							Or sign up with Email
-						</p>
-					</div>
-				</div>
-				<div className={`d-flex ${styles.email_input}`}>
-					<div className={`mb-3 col-12 col-md-6 m-auto`}>
-						<input
-							type='email'
-							className={`py-2 form-control`}
-							value={email}
-							onChange={(e) => {
-								console.log(e.target.value)
-								setEmail(e.target.value)
-							}}
-							placeholder='aristotle@gmail.com'
-						/>
-					</div>
-				</div>
+  const handleMouseDownPassword = event => {
+    event.preventDefault()
+  }
 
-				<div>
-					<button
-						className={` ${styles.button} btn mb-3 col-12 col-md-6 px-5 px-md-5 py-2 btn-primary`}
-						onClick={() => setLoggingIn(true)}>
-						Sign In
-					</button>
-				</div>
-				<span className={``}>
-					Don't have an account?{' '}
-					<a className={styles.link} href='/signup'>
-						Create an account
-					</a>
-				</span>
-			</div>
-		</div>
-	)
+  const handlePasswordChange = prop => event => {
+    setPass({ ...pass, [prop]: event.target.value })
+  }
+
+  return (
+    <div className={`container-fluid ${styles.body}`}>
+      <div class={`row`}>
+        <div class={`col-md-5 ${styles.side}`}>
+          <div className={`row`}>
+            <img src="/Group3155.png" alt="" />
+            <div className={`col-md-12 mb-5`}>
+              <img
+                className={`float-end mt-4 ${styles.headimg}`}
+                src="/Group3150.svg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className={`col-md-12 text-center text-white ${styles.header}`}>
+            <h3 className={` ${styles.headertext}`}>
+              Have fun while working <br /> as a team
+            </h3>
+            <p className={`${styles.text}`}>
+              Unlimited team collaboration platform
+            </p>
+            <img src="/Group3149.svg" alt="" />
+          </div>
+          <div class={` col-md-6 d-flex ${styles.person}`}>
+            <img class={`${styles.flower}`} src="/Group3151.png" alt="" />
+            <img class={`${styles.person1}`} src="person1.svg" alt="" />
+            <img class={`${styles.person2}`} src="person2.svg" alt="" />
+          </div>
+        </div>
+        <div class={`col-md-7 ${styles.login}`}>
+          <div className={`pt-4 mt-3 text-center`}>
+            <span>
+              <img src="zurichatlogo.svg" alt="logo" />
+              <p className={`d-inline p-2`}>Zuri chat</p>
+            </span>
+          </div>
+          <div className={`pt-1 mt-3`}>
+            <h2 className={`pt-2 `}>Log in</h2>
+            <p className={styles.subtext}>
+              Login with the data you entered during your
+            </p>
+            <div className={`d-flex ${styles.email_input}`}>
+              <div className={`mb-3 col-12 col-md-6 w-100`}>
+                <input
+                  type="email"
+                  className={`py-2 form-control`}
+                  value={email}
+                  onChange={e => {
+                    console.log(e.target.value)
+                    setEmail(e.target.value)
+                  }}
+                  placeholder="Enter your email address"
+                  required
+                />
+              </div>
+            </div>
+            <div className={`d-flex ${styles.email_input}`}>
+              <div className={`mb-3 col-12 col-md-6 w-100 d-flex`}>
+                <input
+                  type={pass.showPassword ? 'text' : 'password'}
+                  onChange={handlePasswordChange('password')}
+                  className={`py-2 form-control`}
+                  value={pass.password}
+                  onChange={e => {
+                    console.log(e.target.value)
+                    setPass(e.target.value)
+                  }}
+                  placeholder="Enter a password"
+                  id="id_password"
+                />
+              </div>
+              <i
+                className={`far fa-eye-slash ${styles.far}`}
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              ></i>
+              {/* {pass.password ? <i className={`far far-eye ${styles.far}`}></i>: <i className={`far fa-eye-slash ${styles.far}`}></i>} */}
+            </div>
+            <div class="mb-3 form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+              />
+              <label
+                className={`form-check-label text-secondary ${styles.checktext}`}
+                for="exampleCheck1"
+              >
+                Remember me
+              </label>
+              <label
+                className={`float-md-end text-secondary  ${styles.checktext}`}
+              >
+                Forgot password?{' '}
+                <a href="/" className={`${styles.checklink}`}>
+                  Get help signing in
+                </a>
+              </label>
+            </div>
+            <div>
+              <button
+                className={`${styles.button} btn mb-3 col-12 col-md-6 px-5 px-md-5 py-2`}
+              >
+                Log In
+              </button>
+            </div>
+            <div className={`my-2 d-flex justify-content-around`}>
+              <img className={`w-50 p-3`} src="Line.svg" alt="line" />
+              <p className={`${styles.or}`}>OR</p>
+              <img className={`w-50 p-3`} src="Line.svg" alt="line" srcset="" />
+            </div>
+            <div className={`my-3 text-center`}>
+              <img
+                className={`mx-3 ${styles.icon}`}
+                src={`/google.png`}
+                alt="google icon"
+              />
+              <img
+                className={`mx-3 ${styles.icon}`}
+                src={`/apple.png`}
+                alt="apple icon"
+              />
+            </div>
+            <div className={`my-3`}>
+              <p
+                className={`text-center py-2 text-secondary ${styles.checktext}`}
+              >
+                New to us?{' '}
+                <a href="/" className={`${styles.checklink}`}>
+                  Create an Account
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class={`d-flex justify-content-around my-5  ${styles.footer}`}>
+            <a href="/" class={`mx-md-4 text-secondary`}>
+              Contact Us
+            </a>
+            <a href="/" class={`mx-md-4 text-secondary`}>
+              Legal Policy
+            </a>
+            <a href="/" class={`mx-md-4 text-secondary`}>
+              <i class="fas fa-globe"></i> Change Region{' '}
+              <i class="fas fa-caret-down"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
