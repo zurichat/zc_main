9100d4045c36064083ac9a93137f5bb01d9d036d
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-09-02 01:58:35 +0100

    Disabled Content Security from helmet

--- zc_frontend/tailwind.config.js
+++ zc_frontend/tailwind.config.js
@@ -2,12 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+  plugins: [require('@tailwindcss/forms')]
 }
