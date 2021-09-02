3bac95f1ca04015a48c37beb58d87b41332080dd
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2021-09-02 01:47:39 +0100

    Disabled Content Security from helmet
    Former-commit-id: 05877168f8db8ddf173ae9fd80533f98d7b457a2 [formerly fe99772e55b574fb1bee7c22c8c3e0f8da4f1433]
    Former-commit-id: 9648a536baefa40525437cc6a91a4981223de7ba

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 // activate helmet--server security
-app.use(helmet())
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+)
 app.set('x-powered-by', false)
 
 routes(app)
