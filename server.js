7c8c563c97b0a8bb656612b088ac7551a423ac00
Akanowo <ukoakanowo98@gmail.com>
2021-09-02 08:49:21 +0100

    added proof for winston logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const loadFrontend = require('./middlewares/load-frontend')
+const logger = require('./lib/logger');
 
 const PORT = process.env.PORT || 3000
 
@@ -10,7 +11,5 @@ routes(app)
 loadFrontend(app)
 
 app.listen(PORT, () => {
-  console.log(
-    `Server Started on Port: ${PORT}\nMode: ${process.env.NODE_ENV}\nURI: http://127.0.0.1:${PORT}`
-  )
+  logger.info(`Server Started on Port: ${PORT}\nMode: ${process.env.NODE_ENV}\nURI: http://127.0.0.1:${PORT}`);
 })
