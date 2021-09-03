f0e00af85731948b23ddd74e55dcd1e962e846a3
solintin <soliualley@gmail.com>
2021-09-03 17:55:50 +0100

    feat/Edit User Profile close #258

---
+++ zc_frontend/src/components/Preferences.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfileModal from './ProfileModal'
+
+const Preferences = () => {
+    return (
+        <ProfileModal title="Preference">
+            Hello
+        </ProfileModal>
+    )
+}
+
+export default Preferences
