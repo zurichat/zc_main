c7fd5a1b241ceedc7a35a0e6151a87fd288e7cce
deku <adedokunoluwaferanmi@gmail.com>
2021-09-03 19:16:45 +0100

    feat: marketplace page, data flow and responsiveness
    Former-commit-id: 7fa5444bea79e0c90ff9d0c26e42f89c058474d7

--- zc_frontend/src/contexts/MarketPlace.context.js
+++ zc_frontend/src/contexts/MarketPlace.context.js
@@ -1,17 +1,23 @@
-import { createContext, useContext, useEffect, useState, useCallback } from 'react'
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useCallback
+} from 'react'
 
 export const MarketPlaceContext = createContext(null)
 
 export const MarketPlaceProvider = ({ children }) => {
   const [modal, setModal] = useState(false)
-  const [ pluginId, setPluginId ] = useState(null)
+  const [pluginId, setPluginId] = useState(null)
 
   const toggleModalState = useCallback(() => {
     if (pluginId !== null) {
       setModal(true)
     } else {
       setModal(false)
-    }  
+    }
   }, [pluginId])
 
   useEffect(() => {
@@ -19,12 +25,13 @@ export const MarketPlaceProvider = ({ children }) => {
   }, [pluginId, modal, toggleModalState])
 
   return (
-    <MarketPlaceContext.Provider value={{ modal, pluginId, setPluginId, toggleModalState }}>
+    <MarketPlaceContext.Provider
+      value={{ modal, pluginId, setPluginId, toggleModalState }}
+    >
       {children}
     </MarketPlaceContext.Provider>
   )
 }
-
 
 export const useMarketPlaceContext = () => {
   return useContext(MarketPlaceContext)
