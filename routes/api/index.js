5c5039aa19bb8a8a786c17ce0be2dd64807fee6e
temilade05 <temioladugba@gmail.com>
2021-09-03 17:33:05 +0100

    add logic and route to get user details close issue #762
    Former-commit-id: 37fb42d59ada494c734ee9e7f9b2ccbf984cb0cf [formerly 4df95665d78cba4aadc9b5b01e52d7b6d77a7074 [formerly 375d1e85c75dc095845fb0e3f9d9572b35b41b6c]]
    Former-commit-id: f6ad50dd2499b4be409dea9667aa00429f071fff
    Former-commit-id: 4569885d2f3530b456b82f7a6a590a9b20a11d60

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,3 @@
-module.exports = [require('./plugin'), require('../../middlewares/getDevice')]
+module.exports = [require('./plugin'),
+    require('../../middlewares/getDevice'),
+    require('../../middlewares/getUser')]
