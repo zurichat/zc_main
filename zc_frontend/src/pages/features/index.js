853a85e25e153da53de926171f739574959c03a8
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-03 16:33:14 +0100

    chore

--- zc_frontend/src/pages/features/index.js
+++ zc_frontend/src/pages/features/index.js
@@ -2,12 +2,14 @@ import Header from '../../components/externalPagesComponents/Header'
 import Section4 from './components/Section4'
 import Section6 from './components/Section6'
 import Section3 from './components/Section3'
+import Section2 from './components/Section2'
 import Footer from '../../components/externalPagesComponents/Footer/Footer'
 import SpecialFeatures from './components/SpecialFeatures'
 const Features = () => {
   return (
     <>
       <Header />
+      <Section2 />
       <Section3 />
       <Section4 />
       <SpecialFeatures />
