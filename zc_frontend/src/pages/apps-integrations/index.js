1462dc12595890de9248f792625849644fbf2a9a
ddarkCode <royal.ugoh@gmail.com>
2021-09-03 16:40:30 +0100

    Build the apps/integration  section one

--- zc_frontend/src/pages/apps-integrations/index.js
+++ zc_frontend/src/pages/apps-integrations/index.js
@@ -1,5 +1,12 @@
+import SectionOne from './SectionOne'
+
 function AppsAndIntegrations() {
-  return <div>This is Apps and Integrations Page</div>
+  return (
+    <div>
+      <h2>This is Apps and Integrations Page</h2>
+      <SectionOne />
+    </div>
+  )
 }
 
 export default AppsAndIntegrations
