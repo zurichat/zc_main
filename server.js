0c27b3dc04c31eeff822e09ab56c8f6e2d1c23f8
davidnwogucodes <caprisonepapi123@gmail.com>
2021-09-03 14:10:04 +0100

    added global error handler

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express')
 const routes = require('./routes')
 const helmet = require('helmet')
@@ -6,9 +7,18 @@ const useragent = require('express-useragent')
 
 const loadFrontend = require('./middlewares/load-frontend')
 const PORT = process.env.PORT || 3000
+=======
+const express = require("express");
+const routes = require("./routes");
+const loadFrontend = require("./middlewares/load-frontend");
+const { ApiError } = require("./lib/ApiError");
 
-const app = express()
+const PORT = process.env.PORT || 3000;
+>>>>>>> 56a694b42610c2d7aef7121e995614564a06c304
 
+const app = express();
+
+<<<<<<< HEAD
 // activate helmet--server security
 app.use(
   helmet({
@@ -23,9 +33,21 @@ app.use(useragent.express())
 
 routes(app)
 loadFrontend(app)
+=======
+routes(app);
+loadFrontend(app);
+app.use((err, req, res, next) => {
+  if (err) {
+    if (err instanceof ApiError) {
+      return res.status(err.code).send(err.message);
+    }
+    res.status(500).send("Something Went Wrong");
+  }
+});
+>>>>>>> 56a694b42610c2d7aef7121e995614564a06c304
 
 app.listen(PORT, () => {
   console.log(
     `Server Started on Port: ${PORT}\nMode: ${process.env.NODE_ENV}\nURI: http://127.0.0.1:${PORT}`
-  )
-})
+  );
+});
