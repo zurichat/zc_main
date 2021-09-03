389b89d9ad1ba1ca5ea388f3ed93d66832e0051b
tuasegun <tuase360@gmail.com>
2021-09-03 17:20:04 +0000

    Notification Preference and Modal

---
+++ zc_frontend/src/components/EditProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfileModal from './ProfileModal'
+
+const Preferences = () => {
+    return (
+        <ProfileModal full title="Edit Profile">
+            Edit Profile
+        </ProfileModal>
+    )
+}
+
+export default Preferences
