fac4a7505bd025eca5bc6ce5839e9246013d3f5f
vikmaamaa <maamaavictor@yahoo.com>
2021-09-03 21:50:39 +0100

    added routes access controller
    Former-commit-id: 378696ad97bf43320b404e56077e3e4f328806f3

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 module.exports = [
   require('./plugin'),
   require('../../middlewares/getDevice'),
-  require('../../middlewares/getSession')
+  require('../../middlewares/getSession'),
+  require('../../middlewares/routeAccess')
 ]
