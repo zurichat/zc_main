84d31aec18a2f6ccd0c1e8d0f14fe827b5a0bd72
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-04 05:44:10 +0100

    maintenance
    Former-commit-id: 33ed427fee7cef4c5eb5e8cbb8b00ccee46bd81e [formerly 7243487139c69deff79affcb9abbd4b6cb7b761f [formerly 8fd71a13201eefdfc430ec11edec8bf36bbdc0b0]]
    Former-commit-id: 1dceb7bb30cbe00d8a1bd948526a4f18cd27b9ac
    Former-commit-id: 7194326787d372b0448b24e3554b8d8be329327d

--- zc_frontend/src/pages/security/index.js
+++ zc_frontend/src/pages/security/index.js
@@ -1,25 +1,66 @@
 import ComplianceCertification from './components/ComplianceCertification'
 import securityStyles from './styles/security.module.css'
-import Header from '../../components/externalPagesComponents/Header'
+// import Header from '../../components/externalPagesComponents/Header'
 import Footer from '../../components/externalPagesComponents/Footer/Footer'
 
 const Security = () => {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <section className={securityStyles.compliancesection}>
-        <h2 className={securityStyles.complianceheading}>The security program at Zuri protects our organization and your data at every layer</h2>
-        <p className={securityStyles.compliancetext}>Compliance certifications and attestations</p>
+        <h2 className={securityStyles.complianceheading}>
+          The security program at Zuri protects our organization and your data
+          at every layer
+        </h2>
+        <p className={securityStyles.compliancetext}>
+          Compliance certifications and attestations
+        </p>
         <div className={securityStyles.compliancewrapper}>
-          <ComplianceCertification img="/iso_27001.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
-          <ComplianceCertification img="/iso_27017.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
-          <ComplianceCertification img="/iso_27018.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
-          <ComplianceCertification img="/security-badge-green.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
-          <ComplianceCertification img="/logo_soc3.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
-          <ComplianceCertification img="/logo_soc2.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
-          <ComplianceCertification img="/Schellman-APEC-Processor.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
-          <ComplianceCertification img="/Schellman-APEC-Processor2.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
-          <ComplianceCertification img="/logo_csa.png" title="ISO/IEC 27001" description="Information Security Management System (ISMS)" />
+          <ComplianceCertification
+            img="/iso_27001.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
+          <ComplianceCertification
+            img="/iso_27017.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
+          <ComplianceCertification
+            img="/iso_27018.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
+          <ComplianceCertification
+            img="/security-badge-green.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
+          <ComplianceCertification
+            img="/logo_soc3.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
+          <ComplianceCertification
+            img="/logo_soc2.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
+          <ComplianceCertification
+            img="/Schellman-APEC-Processor.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
+          <ComplianceCertification
+            img="/Schellman-APEC-Processor2.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
+          <ComplianceCertification
+            img="/logo_csa.png"
+            title="ISO/IEC 27001"
+            description="Information Security Management System (ISMS)"
+          />
         </div>
       </section>
       <Footer />
