f055b915d87d16c1643b4bed73641fd3aaf2cc5e
Obafemi Joseph <obafedance@gmail.com>
2021-09-05 00:40:45 +0100

    adds registration route
    Former-commit-id: 3a7596230c3017224610f362f0cb9eeb3d958065 [formerly 4257daa5c7b3bf6e3843eeb9678a5168be3bcef5 [formerly b77ba1aea5add20303db29fd86b1dc950ac395b8]]
    Former-commit-id: a2e58b6aad497269b78e37e23e39349804f08c1b
    Former-commit-id: 8890797f11b9a4234060ad033e7add15967790cc

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -10,7 +10,8 @@ const dummyDB = [
     email: 'markessien@gmail.com',
     password: 'Testpwd11@@',
     verified: false,
-    profileImageUrl: 'https://techpoint.africa/wp-content/uploads/2020/01/mark-essien-e1580122566517.jpg',
+    profileImageUrl:
+      'https://techpoint.africa/wp-content/uploads/2020/01/mark-essien-e1580122566517.jpg',
     status: 'Hi, I am new here',
     phoneNumber: '',
     occupation: 'CEO',
@@ -20,34 +21,34 @@ const dummyDB = [
   }
 ]
 
-module.exports.dummyDB = dummyDB;
+module.exports.dummyDB = dummyDB
 
 const controllers = () => {
-
   const handleLogin = (req, res) => {
-    const { email, password } = req.body;
+    const { email, password } = req.body
     // find user in db
-    const user = dummyDB.find((x) => x.email === email);
+    const user = dummyDB.find(x => x.email === email)
     if (!user) {
       return res.status(401).json({
         status: false,
         message: 'invalid username or password'
-      });
+      })
     }
 
     // compare passwords
-    if (user && (user.password !== password)) {
+    if (user && user.password !== password) {
       return res.status(401).json({
         status: false,
         message: 'invalid username or password'
-      });
+      })
     }
 
     // generate the tokens
-    const token = createJwt(user.id);
+    const token = createJwt(user.id)
 
     // delete password from user object
-    delete user.password;
+    const userCopy = { ...user }
+    delete userCopy.password
 
     // send response back to user
     return res.status(200).json({
@@ -55,11 +56,10 @@ const controllers = () => {
       message: 'login sucessful',
       data: {
         token,
-        user,
+        userCopy
       }
     })
-  };
-
+  }
 
   return {
     handleLogin
