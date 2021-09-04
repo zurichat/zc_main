21b04d834145eb277c533c9e7bb825ed20420abe
“zyhon96” <adedoyin27@gmail.com>
2021-09-04 14:59:03 +0100

    added invite modal and fixed conflict 4th try #495

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const routes = require('./routes')
 const helmet = require('helmet')
 const device = require('express-device')
 const useragent = require('express-useragent')
+const sessions = require('./lib/user_session')
 
 const loadFrontend = require('./middlewares/load-frontend')
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+app.use(express.json())
 
 // activate helmet--server security
 app.use(
@@ -16,6 +19,9 @@ app.use(
   })
 )
 app.set('x-powered-by', false)
+
+//load the session middleware
+sessions.startSession(app)
 
 // Retrieve device information for logging purposes.
 app.use(device.capture())
--- zc_frontend/src/App.js
+++ zc_frontend/src/App.js
@@ -14,6 +14,12 @@ import Download from './pages/download'
 import ContactUs from './pages/contact-us'
 import Pricing from './pages/pricing'
 import Header from './components/externalPagesComponents/Header'
+import './styles/globals.css'
+import MarketPlace from './pages/marketplace/marketplace'
+import Input from './components/externalPagesComponents/Input'
+import FAQ from './pages/FAQ'
+import SearchResult from './components/SearchResults3'
+import LinkComponent from './components/externalPagesComponents/Link'
 
 const App = () => (
   <BrowserRouter>
@@ -33,9 +39,6 @@ const App = () => (
       <Route path="/features">
         <Features />
       </Route>
-      <Route path="/pricing">
-        <Pricing />
-      </Route>
       <Route path="/security">
         <Security />
       </Route>
@@ -54,11 +57,28 @@ const App = () => (
       <Route path="/contact-us">
         <ContactUs />
       </Route>
-      <Route path="/pricing">
-        <Pricing />
-      </Route>
       <Route path="/header">
         <Header />
+      </Route>
+      <Route path="/marketplace" exact>
+        <MarketPlace />
+      </Route>
+      <Route path="/faq">
+        <FAQ />
+      </Route>
+      <Route path="/input">
+        <Input />
+      </Route>
+      <Route path="/search">
+        <SearchResult />
+      </Route>
+      <Route path="/link">
+        <LinkComponent
+          title={`test-to-home`}
+          to={`/home`}
+          color={`black`}
+          underline
+        />
       </Route>
     </Switch>
   </BrowserRouter>
