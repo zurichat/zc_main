08839bee0350d054cdde532f3e57656944c04a36
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-04 12:16:14 +0100

    Revert "Integration section one page (#831)"
    This reverts commit f772bb0e09bf2d47fa5ec2231a2ff88c9bf99ca7.

--- zc_frontend/src/pages/apps-integrations/index.js
+++ zc_frontend/src/pages/apps-integrations/index.js
@@ -1,18 +1,7 @@
- integration-section-one
-import SectionOne from './SectionOne'
-
-function AppsAndIntegrations() {
-  return (
-    <div>
-      <h2>This is Apps and Integrations Page</h2>
-      <SectionOne />
-    </div>
-  )
 import Categories from './Categories'
 
 function AppsAndIntegrations() {
   return <Categories />
- develop
 }
 
 export default AppsAndIntegrations
