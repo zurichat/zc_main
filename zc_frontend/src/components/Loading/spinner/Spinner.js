0324b277724d2040b79d19274ee1748265fab07f
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-04 20:35:59 +0100

    chore
    Former-commit-id: b6cbcbd582745a901f525c1c90bee8074144a577 [formerly fd90fda2ead4bf3892c89f6c62e1610522c3497d [formerly 747d2b138c0f9dcf239dec96435b643c481a7f57]]
    Former-commit-id: 702d06d5166fd9464c87ba4c6dbd8a6498a7484a
    Former-commit-id: 3936f24aa4a641b35b88b96748b99fdbca88d007

--- zc_frontend/src/components/Loading/spinner/Spinner.js
+++ zc_frontend/src/components/Loading/spinner/Spinner.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './Spinner.css';
-import zuriicon from '../../images/zuriicon.png';
+import React from 'react'
+import './Spinner.css'
+import zuriicon from '../../images/zuriicon.png'
 
-const Spinner = () =>{
-    return <div className="spinner-1"> 
-    <div>
+const Spinner = () => {
+  return (
+    <div className="spinner-1">
+      <div>
         <img src={zuriicon} className="zuri-icon" alt="zuri icon" />
+      </div>
     </div>
-    </div>
-};
-export default Spinner;
+  )
+}
+export default Spinner
