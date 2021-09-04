8febe4ac8604e9c3d6f66d2a48e2f08329a46dd0
Ayodejioladimeji <layobright11@gmail.com>
2021-09-04 00:41:47 -0700

    frontend settings page fixes #749
    Former-commit-id: d71edfd7580a7972c2f4b898024e82963d816875 [formerly 94c00a90388339c84d56ed5365d3bbaf76b648f5 [formerly fa2e3accd8e2deeb8b07fde0ca62c0a5771116f0]]
    Former-commit-id: 38bb528081508c18e1e9fac8396dc557c377639c
    Former-commit-id: 3326c1168093778d9a11e737e0ebf0d0b5aebc7d

---
+++ zc_frontend/src/pages/settings/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SettingsNav from './components/SettingsNav'
+import Tab from './components/Tab'
+
+const index = () => {
+  return (
+    <div>
+      <SettingsNav />
+      <Tab />
+    </div>
+  )
+}
+
+export default index
