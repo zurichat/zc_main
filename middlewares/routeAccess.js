cb5f94d3c42a5974637a82d7f018830920ba19ce
Maamaa Victor <73889664+VikMaamaa@users.noreply.github.com>
2021-09-05 12:23:00 +0100

    Update routeAccess.js

--- middlewares/routeAccess.js
+++ middlewares/routeAccess.js
@@ -1,39 +1,16 @@
 const express = require('express')
 const restrictToTest = express.Router()
 
-class AppError extends Error {
-  constructor(message, statusCode) {
-    super(message)
 
-    this.statusCode = statusCode
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
-    this.isOperational = true
-
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
 
 //This can be attached to any route
 //This controller can also be imported from 'lib' folder.
-exports.restrictToMain = (...roles) => {
-  return (req, res, next) => {
-    //roles ['admin', 'staff'], role ='user'
-    // console.log(req.User.role);
-    if (!roles.includes(req.User.role)) {
-      return next(
-        new AppError('You do not have permission to perform this action', 403)
-      )
-    }
 
-    next()
-  }
-}
 
 //For testing purpose
-restrictToTest.route('/test/route-access-controller').get((req, res, next) => {
+restrictToTest.get('/vik/route-access-controller',(req, res, next) => {
   const ra = (...roles) => {
-    //roles ['admin', 'lead-guide'], role ='user'
-    // console.log(roles);
+   
     let role = 'admin, user'
     if (!roles.includes(role)) {
       res.json({
