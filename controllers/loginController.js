9038db30deb2d3607fe4faac53cb76f317e83724
Obafemi Joseph <obafedance@gmail.com>
2021-09-05 09:08:09 +0100

    adds get handler for api login and register endpoint
    Former-commit-id: 9d3893cfe6b2894d408d4fb6b4979782bd742f19

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -60,9 +60,24 @@ const controllers = () => {
       }
     })
   }
+  const login = (_, res) => {
+    const user = dummyDB[0]
+    const token = createJwt(user.id)
+    const userCopy = { ...user }
+    delete userCopy.password
+    return res.status(200).json({
+      status: true,
+      message: 'login sucessful',
+      data: {
+        token,
+        user: userCopy
+      }
+    })
+  }
 
   return {
-    handleLogin
+    handleLogin,
+    login
   }
 }
 
