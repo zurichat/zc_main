d5d9e91c067f24053674b41597350e8e804ade6e
sammiearchie77 <chinonsosamuel29@gmail.com>
2021-09-05 09:23:24 +0100

    debugging

--- zc_frontend/src/pages/careers/index.js
+++ zc_frontend/src/pages/careers/index.js
@@ -2,13 +2,13 @@ import Footer from '../../components/externalPagesComponents/Footer/Footer'
 import Message from '../../components/externalPagesComponents/Footer/Message'
 import CareersSection2 from './CareersSection2'
 import Section3 from './components/Section3/Section3'
-import SectionOne from './components/section1/sectionOne';
+import SectionOne from './components/section1/sectionOne'
 // import Header from "../../components/externalPagesComponents/Header";
 
 function Careers() {
   return (
     <div>
-      {/* <Header /> */ }
+      {/* <Header /> */}
       <SectionOne />
       <CareersSection2 />
       <Section3 />
