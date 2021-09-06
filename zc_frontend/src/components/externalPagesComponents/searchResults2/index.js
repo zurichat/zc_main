c056c16b8d6f9a0d011e8bb99cdc6ef70a4be0f0
Joshua Solomon <akpanjoshuasolomon@gmail.com>
2021-09-06 12:22:26 +0100

    Feat/searchresults2 (#971)
    * Email Notification Commit
    
    * Email Notification
    
    * add search results 2 page close #928
    
    * Revert "Feat/emailnotification" (#960)
    
    Co-authored-by: walemust <52992651+walemust@users.noreply.github.com>
    
    * Review (#967)
    
    * add logic and route for profile picture upload closes #473
    
    * SignUp ZuriChat Page
    
    * Zuri chat SignUp
    
    * ZuriChat signup
    
    * ZuriChat signup
    
    * updating repo
    
    * ZuriChat signup
    
    * Revert "add logic and route for profile picture upload closes #473"
    
    * ZuriChat signup
    
    * add logic and route to get user details close issue #762
    
    * recent UI updates after the server crashed
    
    * recent UI updates after the server crashed
    
    * recent UI updates after the server crashed
    
    * responsive table view
    
    * login ui responsiveness
    
    * Cookies banner view
    
    * add logic and route to get user details close issue #762
    
    * chore
    
    * Delete modules.xml
    
    * Delete .gitignore
    
    * Delete zuri.iml
    
    * Delete vcs.xml
    
    * fixed auto deploy blocks
    
    * incoming update
    
    * cookies banner view
    
    * Cookies Banner View
    
    * First fix
    
    * refactor
    
    * fix3
    
    * fixes
    
    * fixed security page
    
    * Revert "Feat/get user details"
    
    * Bug/security page (#962)
    
    * fixed security page
    
    * fixed security page
    
    * maintenance
    
    Co-authored-by: temilade05 <temioladugba@gmail.com>
    Co-authored-by: unknown <obediobadiah05@gmail.com>
    Co-authored-by: Christian Ndu <christiannduh@gmail.com>
    Co-authored-by: Samuel Archibong <chinonsosamuel29@gmail.com>
    Co-authored-by: Temiladeola Oladugba <43822642+Temilade05@users.noreply.github.com>
    Co-authored-by: dantanee <77518719+dantanee@users.noreply.github.com>
    Co-authored-by: Yasir Salis <dantaneecodes@gmail.com>
    
    * Revert "Review (#967)" (#969)
    
    This reverts commit b646b9ef6d82606e36954a1323b1ba071677bb9e [formerly b14a1ac0823834ebc1bc4f9835de3a0c4b36e260].
    
    * chore
    
    Co-authored-by: siryus95 <salaudeenyusuf95@gmail.com>
    Co-authored-by: Ajsolmon <Ajsolmon@users.noreply.github.com>
    Co-authored-by: walemust <52992651+walemust@users.noreply.github.com>
    Co-authored-by: IDOKO EMMANUEL <pidox31102002@gmail.com>
    Co-authored-by: temilade05 <temioladugba@gmail.com>
    Co-authored-by: unknown <obediobadiah05@gmail.com>
    Co-authored-by: Christian Ndu <christiannduh@gmail.com>
    Co-authored-by: Samuel Archibong <chinonsosamuel29@gmail.com>
    Co-authored-by: Temiladeola Oladugba <43822642+Temilade05@users.noreply.github.com>
    Co-authored-by: dantanee <77518719+dantanee@users.noreply.github.com>
    Co-authored-by: Yasir Salis <dantaneecodes@gmail.com>
    Former-commit-id: c2664bac582c91aae7e7a7d2a41179fff5ea3b25

---
+++ zc_frontend/src/components/externalPagesComponents/searchResults2/index.js
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./searchResults2.js";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
