33d1ea8ebdd60cba10b7b1492e9685f34e5cdba0
Christian Ndu <christiannduh@gmail.com>
2021-09-06 05:57:26 +0100

    Revert "Feat/get user details"

--- middlewares/getUser.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const getUser = express.Router()
-const getUserInfo = require('../lib/get_user_details')
-
-getUser.get('/test/get-user', getUserInfo.getUserInfo)
-
-module.exports = getUser
