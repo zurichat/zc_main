56586df65b640f53764d1ee9e0709f7f94cf30c5
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-06 20:29:48 +0100

    Revert "Feat/download (#940)"
    This reverts commit 36e31f75b64bc2ef36dcfb683737b759b0275a5d.

--- zc_frontend/src/pages/download/index.js
+++ zc_frontend/src/pages/download/index.js
@@ -1,15 +1,9 @@
-
-import { ReactComponent as Googleplay} from '../images/googleplay.svg';
-import { ReactComponent as Apple} from '../images/apple.svg';
-
 import React from 'react'
-
 import Footer from '../../components/externalPagesComponents/Footer/Footer'
 import styles from '../download/styles/download.module.css'
 import SearchSuggestion from '../searchSuggestion'
 
 const Download = () => {
-
   return (
     <>
       <SearchSuggestion />
@@ -23,88 +17,54 @@ const Download = () => {
         <div className={styles.images}>
           <img
             className={styles.circle}
-            src={Circle}
+            src="/Ellipse159.svg"
             alt="circle"
           ></img>
           <img
             className={`${styles.screenshot}`}
-            src={RectangleOverlay}
+            src="/Rectangle.svg"
             alt="app screenshot"
           ></img>
-          
           <img
             className={styles.circle2}
-            src={Circle}
+            src="/Ellipse160.svg"
             alt="circle2"
           ></img>
         </div>
         <div className={styles.buttondiv}>
-          <p className={styles.windows}>Available on windows 8 and higher</p>
           <button className={styles.button1} type="submit">
-            Download for Windows (64 bits)
+            Download for Windows (64bits)
           </button>
           <button className={styles.button2} type="submit">
             Download for Mac OS X 10.10 and higher
           </button>
-          
+          <p className={styles.windows}>Available on windows 8 and higher</p>
         </div>
       </section>
-      <section className={styles.store}>
-
-
-        <div className={styles.google_apple_Box}>
-          <h1 className={styles.h1}>Zuri Chat for Mobile</h1>
+      <section className={`container`}>
+        <div className={styles.store}>
+          <div className="{}">
+            <h1 className={styles.h1}>Zuri Chat for Mobile</h1>
             <p className={styles.storep}>
               Keep up with your team and hold meetings from your device
             </p>
-              <div  className={styles.googleBox}>
-          <Googleplay className={styles.googleplay}/>
-          <div className={styles.googleBox_text}>
-          <small className={styles.googleText1}>GET IT ON</small>
-          <br/>
-         <span className={styles.googleText}>Google Play</span>
-         </div>
-         </div>
-       
-        <div  className={styles.appleBox}>
-        <Apple />
-          <div className={styles.appleBox_text}>
-          <small className={styles.appleText1}>Download on the</small>
-          <br/>
-         <span className={styles.appleText}>Apple Store</span>
-         </div>
-         </div>
+          </div>
+          <div className={styles.storelink}>
+            <a className={styles.imgLink} href="https://play.google.com/">
+              <img src="/Play_Store_badge.svg" alt="Google Play Store"></img>
+            </a>
+
+            <a
+              className={styles.imgLink}
+              href="https://www.apple.com/app-store/"
+            >
+              <img src="/App_Store_badge.svg" alt="Apple App Store"></img>
+            </a>
+          </div>
         </div>
       </section>
-      <div className={styles.zurilogo}>
-        <span><img src={Logo} alt="zurilogo" /><span className={styles.zuri_logo_text}>Zuri Chat</span></span>
-      </div>
-      <footer>
-        <div className={styles.footertext}><p>WHY ZURI CHAT</p> <img src={Downarow} alt="down arrow" /></div>
-        <div className={styles.footertext}><p>PRODUCTS</p><img src={Downarow} alt="down arrow" /></div>
-        <div className={styles.footertext}><p>PRICING</p><img src={Downarow} alt="down arrow" /></div>
-        <div className={styles.footertext}><p>RESOURCES</p><img src={Downarow} alt="down arrow" /></div>
-
-        <div className={styles.footerlinks}>
-          <a href="/">Privacy</a>
-          <a href="/">Terms</a>
-          <a href="/">Help Center</a>
-          <a href="/">Contact US</a>
-        </div>
-            <div className={styles.socio}>
-              <p><img src={Facebook} alt="facebook" /></p>
-              <p><img src={Instagram} alt="instagram" /></p>
-              <p><img src={Twitter} alt="twitter" /></p>
-              <p><img src={Linkedin} alt="linkedin" /></p>
-            </div>
-      </footer>
-      <p className={styles.copyright}><span dangerouslySetInnerHTML={{ "__html": "&copy;" }} />2021 zuri,Team Aristotle. All Rights Reserved.</p>
-  </div>
+      <Footer />
+    </>
   )
 }
-
 export default Download
-
-
-    
-  
