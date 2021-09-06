20c61c9fab97ebbf957fa685965bd002bdbe8bcb
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-06 09:52:30 +0100

    Revert "- I fix section page overflow and css error, (#957)" (#965)
    This reverts commit 0ff663b53d821e51f21bbb087efbd143b76904ca [formerly 4f8d7985a9a901e7bcdb07f62f218ca917344e92] [formerly 11375657cbe6cdedabbb6fac29f4db7c5f2eab5e] [formerly bc1809603c27f6605cb9f13a1ddfc3527ca927ec [formerly 8cef6a8ce1af5132db8098d677609eb82babeaf2 [formerly f21123797dca409ae86732008386a9fe676eba2f]]].
    
    Former-commit-id: 063a5651b83c94ade7d49158ed028b2a3bc133e9 [formerly 9222396d7fbfa204dd59e190487726b68d8f48e0 [formerly d70ac6959b397fee58cbba0011cae9dd787af03b]]
    Former-commit-id: fdafa8caddf0a217d9d9b03d709fcc5e14dfb383
    Former-commit-id: 4b0c9e419acb03aafa07874b3b805b4b43f11df9

--- zc_frontend/src/pages/resources/index.js
+++ zc_frontend/src/pages/resources/index.js
@@ -1,17 +1,17 @@
 import Footer from '../../components/externalPagesComponents/Footer/Footer.js'
 import Header from '../../components/externalPagesComponents/Header.js'
 import SectionFour from '../../components/externalPagesComponents/Resources/SectionFour/SectionFour.js'
-import Section1 from './components/section1'
+// import Section1 from './components/section1'
 import Section4 from './components/section4'
 
 function Resources() {
   return (
-    <div style={{"overflow":"hidden"}}>
-      <Header /> 
-      <Section1 />
+    <div>
+      <Header />
+      {/* <Section1 /> */}
       <Section4 />
       <SectionFour />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
