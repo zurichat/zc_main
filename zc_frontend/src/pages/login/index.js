f74b90703700be10840d92ba83dcaafbcddc0596
sammiearchie77 <chinonsosamuel29@gmail.com>
2021-09-07 04:06:39 +0100

    still fixing

--- zc_frontend/src/pages/login/index.js
+++ zc_frontend/src/pages/login/index.js
@@ -10,20 +10,11 @@ import axios from 'axios'
 import Illustration from '../../components/Illustration'
 
 const Login = () => {
-<<<<<<< HEAD
-  const [email, setEmail] = useState('');
-=======
   const [email, setEmail] = useState('')
->>>>>>> b6b47ff4830215ce7f5e7af379ebf56e3b6f4fad
   const [password, setPassword] = useState('')
   const [showPassword, setShowPassword] = useState(false)
   const [showLoader, setShowLoader] = useState(false)
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> b6b47ff4830215ce7f5e7af379ebf56e3b6f4fad
   const handleEmailChange = e => {
     setEmail(e.target.value)
   }
@@ -107,70 +98,6 @@ const Login = () => {
               alt="apple icon"
             />
           </div>
-<<<<<<< HEAD
-          <div className={`pt-1 mt-3`}>
-            <h2 className={`pt-2 ${styles.loginheader} text-dark`}>Log in</h2>
-            <p className={styles.subtext}>
-              Login with the data you entered during your registration
-            </p>
-            <div className={`my-lg-3 my-sm-3 text-center`}>
-              <GoogleLogin
-                clientId="78755437309-27q9m2toval9c439d2r7q5gj28h0pqcc.apps.googleusercontent.com"
-                render={renderProps => (
-                  <img
-                    onClick={renderProps.onClick}
-                    className={`mx-3 ${styles.icon}`}
-                    src={`/google.png`}
-                    alt="google icon"
-                  />
-                )}
-                buttonText=""
-                onSuccess={successResponseGoogle}
-                onFailure={failureResponseGoogle}
-                cookiePolicy={'single_host_origin'}
-              />
-              <img
-                className={`mx-lg-3 mx-sm-3 ${styles.icon}`}
-                src={`/apple.png`}
-                alt="apple icon"
-              />
-            </div>
-            <div className={`d-flex ${styles.email_input}`}>
-              <div className={`mb-2 col-12 col-md-6 w-100`}>
-            </div>
-            <form className={`mb-sm-3`} onSubmit={handleSubmit}>
-              <div className={` ${styles.email_input}`}>
-                <div className={`mb-lg-3 mb-md-3 mb-sm-3 col-sm-12 col-md-6 w-100`}>
-                  <label for="Email1" class="form-label">
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    className={`py-lg-3 py-md-3 py-sm-3 form-control`}
-                    value={email}
-                    onChange={handleEmailChange}
-                    placeholder="Enter your email address"
-                   
-                  />
-                </div>
-              </div>
-              <div className={`d-flex ${styles.email_input}`}>
-                <div className={`mb-lg-3 mb-md-3 mb-sm-3  col-sm-12 col-md-6 w-100`}>
-                  <label for="Password" class="form-label">
-                    Password
-                  </label>
-                  <input
-                     type={showPassword ? 'text' : 'password'}
-                    className={`py-lg-3 py-md-3 py-sm-3 form-control`}
-                    value={password}
-                    onChange={e => {
-                      setPassword(e.target.value)
-                    }}
-                    placeholder="Enter a password"
-                  />
-                </div>
-                <i
-=======
           <div className={` d-flex justify-content-between`}>
             <img className={`${styles.line}`} src="Line.svg" alt="line" />
             <p className={`${styles.or}`}>Or log in with</p>
@@ -216,66 +143,11 @@ const Login = () => {
                 />
               </div>
               <i
->>>>>>> b6b47ff4830215ce7f5e7af379ebf56e3b6f4fad
                 className={`cursor-pointer far fa-eye${
                   showPassword ? '' : '-slash'
                 } ${styles.far}`}
                 onClick={handleClickShowPassword}
               ></i>
-<<<<<<< HEAD
-              </div>
-              <div class="mb-lg-2 mb-md-2 mb-sm-3 my-sm-3 form-check">
-                <input type="checkbox" class="form-check-input" id="Check" />
-                <label
-                  className={`form-check-label text-secondary ${styles.check}`}
-                  for="Check"
-                >
-                  Remember me
-                </label>
-                <label
-                  className={`float-md-end text-secondary  ${styles.checktext}`}
-                >
-                  Forgot password?{' '}
-                  <a href="/" className={`${styles.checklink}`}>
-                    Get help signing in
-                  </a>
-                </label>
-              </div>
-            </form>
-            <div>
-              <Button
-                 className={`${styles.button} btn mb-3 col-12 col-md-6 px-lg-5 px-md-5 py-lg-3 py-md-3`}
-                 type="submit"
-                 disabled={!email || !password}
-                 onClick={() => setShowLoader(true)}
-               >
-                 Log in
-              </Button>
-            </div>
-            <div className={`my-2`}>
-              <p
-              >
-                New to us?{' '}
-                <a href="/" className={`${styles.checklink}`}>
-                  Create an Account
-                </a>
-              </p>
-            </div>
-          </div>
-          <div
-            class={`d-flex justify-content-around ${styles.footer}`}
-          >
-            <a href="/" class={`mx-md-4 text-secondary`}>
-              Contact Us
-            </a>
-            <a href="/" class={`mx-md-4 text-secondary`}>
-              Legal Policy
-            </a>
-            <a href="/" class={`mx-md-4 text-secondary`}>
-              <i class="fas fa-globe"></i> Change Region{' '}
-              <i class="fas fa-caret-down"></i>
-            </a>
-=======
             </div>
             <div class="mb-lg-2 mb-md-2 mb-sm-3 my-sm-3 form-check">
               <input type="checkbox" class="form-check-input" id="Check" />
@@ -314,7 +186,6 @@ const Login = () => {
                 Create an Account
               </a>
             </p>
->>>>>>> b6b47ff4830215ce7f5e7af379ebf56e3b6f4fad
           </div>
         </div>
         <div class={`d-flex justify-content-around ${styles.footer}`}>
@@ -331,7 +202,6 @@ const Login = () => {
         </div>
       </div>
     </div>
-  </div>
   )
 }
 
