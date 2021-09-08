4545a41953250dc2871168979fa9f7ef8c9f9525
Obi Fortune <gabrielobi.of@gmail.com>
2021-09-08 21:29:53 +0100

    added password support to the inputbox
    Former-commit-id: 0b39d210d2941ae73fc4ad903ce9efd086b52e79 [formerly 6b064e87693eb36a12dca238f8464c6dc8051566 [formerly b0897244a462219adff19c5dee2f7227c28c7380]]
    Former-commit-id: 86e55c38e0005baa32f92f3b4bd2a3e1f0427c17
    Former-commit-id: a2b9da2b515f356a04df496d12c2b602e01c548f

--- zc_frontend/src/components/AuthInputBox.js
+++ zc_frontend/src/components/AuthInputBox.js
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'react'
+
 import styles from '../styles/InputBox.module.css'
 
 const InputBox = ({
@@ -10,22 +12,50 @@ const InputBox = ({
   placeholder,
   name
 }) => {
+  const ref = useRef(null)
+  const [passwordVisible, setpasswordVisible] = useState(
+    type === 'password' ? true : false
+  )
+
+  const passwordToggle = e => {
+    e.preventDefault()
+    setpasswordVisible(!passwordVisible)
+
+    ref.current.type = passwordVisible ? 'text' : 'password'
+  }
+
   return (
     <>
-      <div className={`${styles.InputWrapper} ${className}`}>
+      <div className={`${styles.InputContainer} ${className}`}>
         <label htmlFor={id} className={`${styles.InputLabel}`}>
           {name}
         </label>
 
-        <input
-          id={id}
-          className={`${styles.InputElement} ${error && styles.is_invalid}`}
-          name={name}
-          type={type}
-          placeholder={placeholder}
-          value={value}
-          onChange={e => setValue(e.target.value)}
-        />
+        <div
+          className={`${styles.InputWrapper} ${
+            type === 'password' ? styles.InputWrapperWithPassword : ''
+          }`}
+        >
+          <input
+            id={id}
+            ref={ref}
+            className={`${styles.InputElement} ${error && styles.is_invalid}`}
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+
+          {/password/.test(name) && (
+            <div
+              className={`${styles.ToggleVisibility}`}
+              onClick={passwordToggle}
+            >
+              <i className={`far ${passwordVisible ? 'eye-slash' : 'eye'}`}></i>
+            </div>
+          )}
+        </div>
 
         <span className={`${styles.InputError}`}>{error}</span>
       </div>
