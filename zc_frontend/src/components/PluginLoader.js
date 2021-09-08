770dd6af04595c138ef07455301d9119b128ee53
Akindeji <akindejioluwaseun015@gmail.com>
2021-09-08 08:05:22 +0100

    write logic for loader transition
    Former-commit-id: e3a75b483262d3f32b036e767755db35ae9b0946 [formerly 16d6db9b570def68654aa1b0cb3590f3c7657b9e [formerly fe7e2b6aa815a417955c9cc37edf3fcbe7212b49]]
    Former-commit-id: b8429c2e213d77a8327097ce97369e90e8b46b96
    Former-commit-id: cbb67892443f439acbcd6360e1f0a9412344130b

--- zc_frontend/src/components/PluginLoader.js
+++ zc_frontend/src/components/PluginLoader.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+import { PluginLoaderContext } from '../contexts/Url'
 import styles from '../styles/PluginLoader.module.css'
 
 const PluginLoader = () => {
 
+  const { loader } = useContext(PluginLoaderContext);
+  const loaderEl = useRef(null);
+
+  useEffect(() => {
+    if (loader) {
+      setTimeout(() => {
+        loaderEl.current.animation = "reduce 1.2s cubic-bezier(0.175, 0.885, 0.32, 1.275);"
+      }, 1000)
+    }
+  })
+
   return (
-    <div class={styles.loader}>
+    <div ref={loaderEl} class={styles.loader}>
       <svg xmlns="http://www.w3.org/2000/svg" width="67" height="72" viewBox="0 0 47 52" fill="none">
         <rect class={styles.topLeft} x="6.30078" width="18.9016" height="19.8545" rx="1.0991" fill="#00B87C" />
         <rect class={styles.bottomLeft} y="22.6904" width="18.9016" height="19.8545" rx="1.0991" fill="#FEA162" />
--- zc_frontend/src/contexts/PluginLoaderState.js
+++ zc_frontend/src/contexts/PluginLoaderState.js
@@ -1,12 +1,12 @@
 import { createContext, useState } from 'react'
 
-export const URLContext = createContext(false)
+export const PluginLoaderContext = createContext(null)
 export const PluginLoaderProvider = ({ children }) => {
   const [loader, setLoader] = useState(false)
 
   return (
-    <URLContext.Provider value={{ loader, setLoader }}>
+    <PluginLoaderContext.Provider value={{ loader, setLoader }}>
       {children}
-    </URLContext.Provider>
+    </PluginLoaderContext.Provider>
   )
 }
