91bd70d3d478c59c4205da7630f4434fcde06d33
Akindeji <akindejioluwaseun015@gmail.com>
2021-09-08 07:48:29 +0100

    clean plugin loader component

--- zc_frontend/src/components/PluginLoader.js
+++ zc_frontend/src/components/PluginLoader.js
@@ -1,16 +1,17 @@
-import React, { useContext } from 'react'
-import { TopbarContext } from '../contexts/Topbar'
+import React from 'react'
 import styles from '../styles/PluginLoader.module.css'
 
 const PluginLoader = () => {
-  const {
-    emoji: [chosenEmoji],
-    openStatus
-  } = useContext(TopbarContext)
+
   return (
-    <button className={styles.statusBadge} onClick={openStatus}>
-      {chosenEmoji ? chosenEmoji.emoji : null}
-    </button>
+    <div class={styles.oader}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="67" height="72" viewBox="0 0 47 52" fill="none">
+        <rect class={styles.topLeft} x="6.30078" width="18.9016" height="19.8545" rx="1.0991" fill="#00B87C" />
+        <rect class={styles.bottomLeft} y="22.6904" width="18.9016" height="19.8545" rx="1.0991" fill="#FEA162" />
+        <rect class={styles.topRight} x="27.9033" y="9.45508" width="18.9016" height="19.8545" rx="1.0991" fill="#1A61DB" />
+        <rect class={styles.bottomRight} x="21.6025" y="32.1455" width="18.9016" height="19.8545" rx="1.0991" fill="#DC1AA3" />
+      </svg>
+    </div>
   )
 }
 
