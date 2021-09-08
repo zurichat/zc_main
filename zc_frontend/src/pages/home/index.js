bb1102b792adebe436cc3ca2dfe9ec7ea057503a
Akindeji <akindejioluwaseun015@gmail.com>
2021-09-08 07:56:19 +0100

    provide state to plugin component
    Former-commit-id: 47220a2935b1c0d1eb9ce74679d53f569f4e75d9

--- zc_frontend/src/pages/home/index.js
+++ zc_frontend/src/pages/home/index.js
@@ -5,6 +5,7 @@ import styles from '../../styles/Home.module.css'
 import { UrlProvider } from '../../contexts/Url'
 import { ProfileProvider } from '../../contexts/ProfileModal'
 import { TopbarProvider } from '../../contexts/Topbar'
+import { PluginLoaderProvider } from '../../contexts/PluginLoaderState'
 import Profile from '../../components/Profile'
 
 const Home = () => {
@@ -19,7 +20,9 @@ const Home = () => {
             </TopbarProvider>
             <div className={styles.spaceWrapper}>
               <div className={styles.pluginContent}>
-                <PluginContent />
+                <PluginLoaderProvider>
+                  <PluginContent />
+                </PluginLoaderProvider>
               </div>
               <Profile />
             </div>
