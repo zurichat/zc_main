243a1b9e2d3f0ab597f7f2c1e4b4b1cf47635473
AndrewGlago <andrewglago1@gmail.com>
2021-09-08 14:13:09 +0000

    list view

--- zc_frontend/src/components/MembersModal.js
+++ zc_frontend/src/components/MembersModal.js
@@ -1,27 +1,76 @@
 import React, { useContext } from 'react'
+import faker from 'faker'
+
+import { IoMdClose, IoMdSearch } from 'react-icons/io'
 import { TopbarContext } from '../contexts/Topbar'
 import styles from '../styles/MembersModal.module.css'
 
-const Header = () => {
+const MembersModal = ({ channelTitle }) => {
   return (
-    <div id="header">
-      <div style={styles}>Header</div>
+    <div className={styles.modalWrapper}>
+      <Header channelTitle={channelTitle || 'announcement'} />
+      <Search />
+      <MemberList />
+    </div>
+  )
+}
+
+const Header = ({ channelTitle }) => {
+  return (
+    <div className={styles.headerWrapper}>
+      <div className={styles.headerTop}>
+        <div className={styles.headerText}>{`#${channelTitle}`}</div>
+        <div>
+          <IoMdClose />
+        </div>
+      </div>
+      <p>Members - 1470</p>
     </div>
   )
 }
 
 const Search = () => {
   return (
-    <div id="search">
-      <div style={styles}>Search</div>
+    <div id="search" className={styles.searchWrapper}>
+      <div className={styles.searchBox}>
+        <IoMdSearch fontSize={20} />
+        <input placeholder="Find members" />
+      </div>
     </div>
   )
 }
 
 const MemberList = () => {
+  const n = 100
+
   return (
-    <div id="member-list">
-      <div style={styles}>Member List</div>
+    <div id="member-list" className={styles.memberlist}>
+      {[...Array(n)].map((val, idx) => {
+        return <MemberCard key={idx} />
+      })}
+    </div>
+  )
+}
+
+const MemberCard = ({ username, fullname, status, avatar }) => {
+  // faker for placeholder data
+  const _username = username || `${faker.internet.userName().toLowerCase()}`
+  const _fullname =
+    fullname || `${faker.name.firstName()} ${faker.name.lastName()}`
+  const _status = status || `${faker.lorem.sentence()}`
+  const _avatar = avatar || faker.internet.avatar()
+
+  return (
+    <div id="member-card" className={styles.membercardWrapper}>
+      <img src={_avatar} alt="user avatar" />
+      <div className={styles.membercardTextBlock}>
+        <div className={styles.membercardUserDetails}>
+          <b>{_username}</b>
+          {'-'}
+          <p>{_fullname}</p>
+        </div>
+        <div className={styles.membercardStatus}>{_status}</div>
+      </div>
     </div>
   )
 }
@@ -33,16 +82,6 @@ export const MembersModalButton = () => {
       <button onClick={openMembersModal} className={styles.modalButton}>
         Show Modal
       </button>
-    </div>
-  )
-}
-
-const MembersModal = () => {
-  return (
-    <div>
-      <Header />
-      <Search />
-      <MemberList />
     </div>
   )
 }
