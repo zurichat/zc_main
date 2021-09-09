2adc115795bba9664dd819950e2b1bb724b8b69d
ehindero israel <iehindero419@stu.ui.edu.ng>
2021-09-09 07:04:16 +0100

    'lastRefactor'

--- zc_frontend/src/pages/signup/index.js
+++ zc_frontend/src/pages/signup/index.js
@@ -32,100 +32,96 @@ import styles from './styles/SignUp.module.css'
 // }
 
 const Signup = () => {
-
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [cpassword, setCPassword] = useState('')
   const [tos, setTos] = useState(false)
 
-
   const handleSubmit = e => {
     e.preventDefault()
   }
-
 
   return (
     <main id={styles.authPageWrapper}>
       <aside id={styles.authAsideContainer} className={styles.display_none}>
         <div id={styles.authImageWrapper}>
           <img src={authBg} alt="backgroundImage" />
-          <div id={styles.aside_txt}>
-          
-          </div>
+          <div id={styles.aside_txt}></div>
         </div>
       </aside>
       <section id={styles.authFormContainer}>
-      <FormWrapper
-            header="Create Account"
-            googleHeader="Sign up with Google"
-            topLineText="or"
-            submitButtonName="Sign up"
-            name={name}
-            email={email}
-            password={password}
-            check={tos}
-            handleSubmit={handleSubmit}
-            bottomLine="Already have an account?"
-            bottomLink= "Log in"
-            >
-            <AuthInputBox
-                className={`${styles.inputElement}`}
-                id="name"
-                name="Full Name"
-                type="text"
-                placeholder="John Doe"
-                value={name}
-                setValue={setName}
-                error=''
-              />
-              <AuthInputBox
-                className={`${styles.inputElement}`}
-                id="email"
-                name="Email"
-                type="email"
-                placeholder="johnDoe@example.com"
-                value={email}
-                setValue={setEmail}
-                error=''
-              />
-              <AuthInputBox
-                className={`${styles.inputElement}`}
-                id="password"
-                name="Password"
-                type="password"
-                placeholder="Enter your Password"
-                value={password}
-                setValue={setPassword}
-                error=''
-              />
-              <AuthInputBox
-                className={`${styles.inputElement}`}
-                id="cpassword"
-                name="confirmPassword"
-                type="password"
-                placeholder="Enter your password"
-                value={cpassword}
-                setValue={setCPassword}
-                error={false}
-            />
-              <div className={`${styles.tos}`}>
-              <input
+        <FormWrapper
+          header="Create Account"
+          googleHeader="Sign up with Google"
+          topLineText="or"
+          submitButtonName="Sign up"
+          name={name}
+          email={email}
+          password={password}
+          check={tos}
+          handleSubmit={handleSubmit}
+          bottomLine="Already have an account?"
+          bottomLink="Log in"
+        >
+          <AuthInputBox
+            className={`${styles.inputElement}`}
+            id="name"
+            name="Full Name"
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            setValue={setName}
+            error=""
+          />
+          <AuthInputBox
+            className={`${styles.inputElement}`}
+            id="email"
+            name="Email"
+            type="email"
+            placeholder="johnDoe@example.com"
+            value={email}
+            setValue={setEmail}
+            error=""
+          />
+          <AuthInputBox
+            className={`${styles.inputElement}`}
+            id="password"
+            name="Password"
+            type="password"
+            placeholder="Enter your Password"
+            value={password}
+            setValue={setPassword}
+            error=""
+          />
+          <AuthInputBox
+            className={`${styles.inputElement}`}
+            id="cpassword"
+            name="confirmPassword"
+            type="password"
+            placeholder="Enter your password"
+            value={cpassword}
+            setValue={setCPassword}
+            error={false}
+          />
+          <div className={`${styles.tos}`}>
+            <input
               className={`${styles.checkBox}`}
               name="tos"
               type="checkbox"
               value={tos}
-              onClick={()=>{setTos(!tos)}}
-              /> 
-              <span 
-              className={`${styles.tosText}`}> 
+              onClick={() => {
+                setTos(!tos)
+              }}
+            />
+            <span className={`${styles.tosText}`}>
               I agree to Zurichat's {''}
-                <a href="">terms of services{''} </a>
-                and
-                <a href=""> {''}privacy</a>
-                </span>
-              </div>
-            </FormWrapper>
+              <a href="">terms of services{''} </a>
+              and
+              <a href=""> {''}privacy</a>
+            </span>
+          </div>
+        </FormWrapper>
       </section>
     </main>
   )
