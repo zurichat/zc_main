e275e068c11efc2ed291b01cf9f28a698142361d
ehindero israel <iehindero419@stu.ui.edu.ng>
2021-09-09 07:04:16 +0100

    'lastRefactor'
    Former-commit-id: 45f819bf3ec77624d562ba56a80a1a33a18e4e0b [formerly 1e285be626383ef1b9e8f08a025ca6a2f4baf685 [formerly 2adc115795bba9664dd819950e2b1bb724b8b69d]]
    Former-commit-id: f000d0cc5f1845909b174f8403751b5205561c19
    Former-commit-id: ea8eca61a831e678735cd2aa78a1a4571d9e293b

--- zc_frontend/src/pages/login/index.js
+++ zc_frontend/src/pages/login/index.js
@@ -8,78 +8,74 @@ import AuthInputBox from '../../components/AuthInputBox'
 import FormWrapper from '../../components/AuthFormWrapper'
 //import GoogleLogin from 'react-google-login'
 
-
 const Login = () => {
-
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [rememberMe, setRememberMe] = useState('')
 
-
   const handleSubmit = e => {
     e.preventDefault()
   }
-
 
   return (
     <main id={styles.authPageWrapper}>
       <aside id={styles.authAsideContainer} className={styles.display_none}>
         <div id={styles.authImageWrapper}>
           <img src={authBg} alt="backgroundImage" />
-          <div id={styles.aside_txt}>
-          
-          </div>  
+          <div id={styles.aside_txt}></div>
         </div>
       </aside>
       <section id={styles.authFormContainer}>
-      <FormWrapper
-            header="Login"
-            googleHeader="Login with Google"
-            topLineText="OR"
-            submitButtonName="Log in"
-            email={email}
-            password={password}
-            check={rememberMe}
-            handleSubmit={handleSubmit}
-            bottomLine="New to us? Create an Account"
-            bottomLink= "Log in"
-            >
-            <AuthInputBox
-                className={`${styles.inputElement}`}
-                id="email"
-                name="Email adress"
-                type="eemail"
-                placeholder="Johndoe@example.com"
-                value={email}
-                setValue={setEmail}
-                error=''
-              />
-              <AuthInputBox
-                className={`${styles.inputElement}`}
-                id="password"
-                name="Password"
-                type="password"
-                placeholder="Enter a password"
-                value={email}
-                setValue={setEmail}
-                error=''
-              />
+        <FormWrapper
+          header="Login"
+          googleHeader="Login with Google"
+          topLineText="OR"
+          submitButtonName="Log in"
+          email={email}
+          password={password}
+          check={rememberMe}
+          handleSubmit={handleSubmit}
+          bottomLine="New to us? Create an Account"
+          bottomLink="Log in"
+        >
+          <AuthInputBox
+            className={`${styles.inputElement}`}
+            id="email"
+            name="Email adress"
+            type="eemail"
+            placeholder="Johndoe@example.com"
+            value={email}
+            setValue={setEmail}
+            error=""
+          />
+          <AuthInputBox
+            className={`${styles.inputElement}`}
+            id="password"
+            name="Password"
+            type="password"
+            placeholder="Enter a password"
+            value={email}
+            setValue={setEmail}
+            error=""
+          />
 
-              <div className={`${styles.rememberMe}`}>
-              <input
+          <div className={`${styles.rememberMe}`}>
+            <input
               className={`${styles.checkBox}`}
               name="RememeberMe"
               type="checkbox"
               value={rememberMe}
-              onClick={()=>{setRememberMe(!rememberMe)}}
-              /> 
-              {/* <span 
+              onClick={() => {
+                setRememberMe(!rememberMe)
+              }}
+            />
+            {/* <span 
               className={`${styles.rememberMe}`}> 
               
                 
                 </span> */}
-              </div>
-            </FormWrapper>
+          </div>
+        </FormWrapper>
       </section>
     </main>
   )
