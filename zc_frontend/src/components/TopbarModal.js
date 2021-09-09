93603e5122500729427bbfc7465f5ea54adbc7fc
DevSaalih <soliualley@gmail.com>
2021-09-09 18:42:05 +0100

    Refactoring and More implement more dropdown
    Former-commit-id: 44c21becf5fbb2429a3ca58e6983a590f1e0d24f [formerly ccc2b713ed6a7e6289265078b3963a11fd12f350 [formerly 9dfabacb7d3b77a6b6ba0c647885ed90e8f32b69]]
    Former-commit-id: 53a3b83b7e34c64f29302e9586c4f494d633b45e
    Former-commit-id: 0e450d16e93e33472ec378aca3efeb76b025b74d

--- zc_frontend/src/components/TopbarModal.js
+++ zc_frontend/src/components/TopbarModal.js
@@ -1,7 +1,7 @@
 import { useContext, useState } from 'react'
 
 // react icons
-import { FaChevronRight, FaTimes, FaCircle } from 'react-icons/fa'
+import { FaChevronRight, FaTimes,  } from 'react-icons/fa'
 
 import Picker, { SKIN_TONE_MEDIUM_DARK } from 'emoji-picker-react'
 
@@ -58,13 +58,13 @@ const TopbarModal = () => {
               <h4>Praise.A</h4>
               {active ? (
                 <div className={styles.online}>
-                  <FaCircle className={styles.circle} />
-                  <p>Active</p>
+                  <div className={styles.circle} />
+                  <p >Active</p>
                 </div>
               ) : (
                 <div className={styles.online}>
-                  <FaCircle className={styles.circlegrey} />
-                  <p>Away</p>
+                  <div className={styles.circlegrey} />
+                  <p style={{marginLeft: '14px'}} >Away</p>
                 </div>
               )}
             </div>
@@ -90,8 +90,8 @@ const TopbarModal = () => {
           <div className={styles.sectionFour}>
             <p
               onClick={() => {
-                toggleModalState()
                 setModal('edit profile')
+                toggleModalState()
               }}
             >
               Edit profile
@@ -99,8 +99,8 @@ const TopbarModal = () => {
             <p onClick={toggleProfileState}>View profile</p>
             <p
               onClick={() => {
-                toggleModalState()
                 setModal('preference')
+                toggleModalState()
               }}
             >
               Preferences
