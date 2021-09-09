0aa89df48a05306ae1ef3749cee6ed0337f0e3a3
Ejike <41432574+agkayster@users.noreply.github.com>
2021-09-09 09:47:46 +0100

    Fixed the drop menu on the user icon, so it is positioned properly (#1001)
    * I worked on the drop menu of the user icon, I added downloads link and did the css polishing to meet up the figma design
    
    * finished the polishing of the drop menu on the user icon
    
    * I removed downloads
    
    * I added the downloads code and did a yarn run lint
    
    Former-commit-id: 9a4ef12ec89a5635bdffa3395a0a9bf203b60299

--- zc_frontend/src/components/TopbarModal.js
+++ zc_frontend/src/components/TopbarModal.js
@@ -85,7 +85,7 @@ const TopbarModal = () => {
             </div>
           </div>
 
-          <hr />
+          <hr className={styles.hr} />
 
           <div className={styles.sectionFour}>
             <p
@@ -103,15 +103,21 @@ const TopbarModal = () => {
                 setModal('preference')
               }}
             >
-              Preference
+              Preferences
             </p>
+          </div>
+
+          <hr className={styles.hr} />
+
+          <div className={styles.sectionSix}>
+            <p>Downloads</p>
           </div>
 
           {modal === 'edit profile' && <EditProfile />}
 
           {modal === 'preference' && <Preferences />}
 
-          <hr />
+          <hr className={styles.hr} />
 
           <div className={styles.sectionFive}>
             <p>Sign out of Team Einstein workspace</p>
