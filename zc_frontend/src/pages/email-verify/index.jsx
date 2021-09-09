6905b704fff27db1d744d7c092cf3f7292a7965c
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-09 14:21:00 +0100

    fixed errors and returned
    Former-commit-id: 264d58a697af68cd430e152a4875464909a27509 [formerly a3716b80b9fb80d58a7f09d1b5c579f0e30d4ee2 [formerly ec93eb8fea7dfde44dc85f7e4868192c6462f10e]]
    Former-commit-id: 81a849d20a9796d3d49e111dad3be8c2a09a021c
    Former-commit-id: 8dfdcece37f2b4e7b8849def220e1b69963d0672

--- zc_frontend/src/pages/email-verify/index.jsx
+++ zc_frontend/src/pages/email-verify/index.jsx
@@ -1,13 +1,15 @@
 import ReactCodeInput from 'react-code-input'
-import styles from 'EmailVerification.module.css'
+import styles from './styles/EmailVerification.module.css'
 
 export default function EmailVerififcation() {
   return (
-    <div className={styles.main}>
-      <img src="" alt="zurichat logo" />
-      <div className={styles.input}>
-        <ReactCodeInput type="number" fields={6} />
+    <>
+      <div className={styles.main}>
+        <img src="" alt="zurichat logo" />
+        <div className={styles.input}>
+          <ReactCodeInput type="number" fields={6} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
