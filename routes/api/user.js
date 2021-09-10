6b7052e8d80d46ad9bfc5872eec5e3edb368b3f9
Olawale Mustapha <walemust28@gmail.com>
2021-09-10 10:41:08 +0100

    Conflict resolution
    Former-commit-id: 45edabd1058d75000b2c6898a6f663d6c131f161

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const userRoute = express.Router()
- review
 const {
   register,
   handleRegister
@@ -13,14 +12,4 @@ userRoute.post('/register', validateRegistration, register)
 userRoute.post('/login', validateLogin, handleLogin)
 userRoute.get('/register', handleRegister)
 userRoute.get('/login', login)
-=======
-const { register } = require('../../controllers/registrationController')
-const { validateRegistration } = require('../../lib/authValidator')
-const { validateLogin } = require('../../lib/authValidator')
-const { handleLogin } = require('../../controllers/loginController.js')
-
-userRoute.post('/register', validateRegistration, register)
-userRoute.post('/login', validateLogin, handleLogin)
-
- main
 module.exports = userRoute
--- zc_frontend/src/pages/apps-integrations/index.js
+++ zc_frontend/src/pages/apps-integrations/index.js
@@ -1,11 +1,15 @@
-import Categories from './Categories'
-import SectionOne from './SectionOne'
+// import Categories from './components/Categories'
+import Footer from '../../components/externalPagesComponents/Footer/Footer'
+import SearchSuggestion from '../searchSuggestion'
+import SectionOne from './components/SectionOne'
 
 function AppsAndIntegrations() {
   return (
     <div>
+      <SearchSuggestion />
       <SectionOne />
-      <Categories />
+      {/* <Categories /> */}
+      <Footer />
     </div>
   )
 }
