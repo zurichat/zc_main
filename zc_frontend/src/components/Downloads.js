18f3c5ba114db73a051a1857fdf71195cd283ecb
Akindeji <akindejioluwaseun015@gmail.com>
2021-09-10 18:00:13 +0100

    update button to close state

--- zc_frontend/src/components/Downloads.js
+++ zc_frontend/src/components/Downloads.js
@@ -5,7 +5,7 @@ import open from '../assets/file-open.svg'
 import close from '../assets/file-close.svg'
 import pointy from '../assets/pointy.png'
 
-const Downloads = () => {
+const Downloads = ({setModal}) => {
   const [files] = useState([
     {
       name: 'HNG Design Rules Book.pdf',
@@ -21,7 +21,7 @@ const Downloads = () => {
     <section
       className={styles.downloadsContainer}
     >
-      <div className={styles.overlay}></div>
+      <div className={styles.overlay}  onClick={() => {setModal("no")}}></div>
       <div className={styles.downloadsModal} >
         <header className={styles.downloadsHeader}>
         <h3 className={styles.downloadsHeading}>Downloads</h3>
@@ -39,7 +39,7 @@ const Downloads = () => {
             </div>
             </div>
             }
-            <svg className={styles.closeIcon} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <svg onClick={() => {setModal("no")}} className={styles.closeIcon} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
               <path d="M17 7L7 17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M7 7L17 17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
