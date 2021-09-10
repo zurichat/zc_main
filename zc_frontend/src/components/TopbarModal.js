5e7054040d26cc93d0de679aec780a1390738530
Akindeji <akindejioluwaseun015@gmail.com>
2021-09-10 15:51:19 +0100

    Building download modal
    Former-commit-id: ebeb332f86cafe0116b22864dc41d093281a4097 [formerly f18e75c21f858cb863042ab1ecfeee8730ff0437] [formerly 31276507c3863bff91a7c223e3951d085853e2ca [formerly ab5d84a2e1a0a4de4a0a2163c093687a44c3947a [formerly ca9e78895ef566a1f6b96b9b7e1adfd64d72dd8c] [formerly a1a1c10eab14c82be0f1297da660016a4aa10fb5]]]
    Former-commit-id: 45bec2d9081f84564675e22870ad77b83c742dd1 [formerly ab5d84a2e1a0a4de4a0a2163c093687a44c3947a [formerly ca9e78895ef566a1f6b96b9b7e1adfd64d72dd8c]]
    Former-commit-id: 5da9163b9deab4f4642d993f6b3f81b78e247225
    Former-commit-id: 80de5d04aa76d973aab7c2ef87f1cac103afedf2
    Former-commit-id: 6c3c290497d7ab33c9a620a32528fe5206298661

--- zc_frontend/src/components/TopbarModal.js
+++ zc_frontend/src/components/TopbarModal.js
@@ -12,6 +12,7 @@ import { ProfileContext } from '../context/ProfileModal'
 import Preferences from './Preferences'
 import EditProfile from './EditProfile'
 import MembersModal from './MembersModal'
+import Downloads from './Downloads'
 
 const TopbarModal = () => {
   const { toggleModalState, toggleProfileState } = useContext(ProfileContext)
@@ -123,12 +124,17 @@ const TopbarModal = () => {
           <hr className={styles.hr} />
 
           <div className={styles.sectionSix}>
-            <p>Downloads</p>
+            <p
+              onClick={() => {
+                setModal('downloads')
+              }}>Downloads</p>
           </div>
 
           {modal === 'edit profile' && <EditProfile />}
 
           {modal === 'preference' && <Preferences />}
+
+          {modal === 'downloads' && <Downloads />}
 
           <hr className={styles.hr} />
 
