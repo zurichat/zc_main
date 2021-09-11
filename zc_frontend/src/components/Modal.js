e3e03131e36863b925888ecda8f032e270fd5c4d
“zyhon96” <adedoyin27@gmail.com>
2021-09-11 12:15:09 +0100

    fixed issues

--- zc_frontend/src/components/Modal.js
+++ zc_frontend/src/components/Modal.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import './Modal.css';
-
+import React from 'react'
+import './Modal.css'
 
 function Modal({ closeModal }) {
-    return (
-        <div className="modalBackground">
-        <div className="modalContainer">
-            <h5 onClick={() => closeModal(false)} id="exitBtn"> X </h5>
-            <div className="Header">
-                 <h3>X</h3>
-                </div>            
-                <div className="title">
-                <h1>Unable to send invite</h1>
-            </div>
-            <div className="body">
-                <p>dedi@teamvolitaire.com is already in your workspace</p>
-            </div>
-
-
-            <div className="footer">
-            <a href="#"> <img src="arrow.png" /> </a>  <h4 className="see">   See previous invites </h4>
-            
-                <button id="sendBtn">Send more invites</button>
-                <button onClick={() => closeModal(false)} id="cancelBtn">Done</button>
-            </div>
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <h5 onClick={() => closeModal(false)} id="exitBtn">
+          {' '}
+          X{' '}
+        </h5>
+        <div className="Header">
+          <h3>X</h3>
         </div>
+        <div className="title">
+          <h1>Unable to send invite</h1>
         </div>
-    );
+        <div className="body">
+          <p>dedi@teamvolitaire.com is already in your workspace</p>
+        </div>
+
+        <div className="footer">
+          <a href="#">
+            {' '}
+            <img src="arrow.png" />{' '}
+          </a>{' '}
+          <h4 className="see"> See previous invites </h4>
+          <button id="sendBtn">Send more invites</button>
+          <button onClick={() => closeModal(false)} id="cancelBtn">
+            Done
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
- export default Modal
+export default Modal
