fdf9b30c59cf43946c94a976370dec80b5336199
sammiearchie77 <chinonsosamuel29@gmail.com>
2021-09-11 07:01:43 +0100

    password recovery && password reset page in progress
    Former-commit-id: 488675f86e42c50bf48e3c73c74a5e693ed34019 [formerly 6bc1ac29c12e0fba4b681f1a120df361b30e75bf] [formerly 89eea61aa9fb189a422416954c8e25c41b96be42 [formerly cd342592b1755419b232e5276a459f180b70e5f1 [formerly b814d7d772ecfc8351f11b1e6382a45819c9dc36] [formerly e387924db2270c13d4c9c8fb32c5833b67c446c6]]]
    Former-commit-id: 833a41dcc278e77515cad6b3ea88d4a86c1808a7 [formerly cd342592b1755419b232e5276a459f180b70e5f1 [formerly b814d7d772ecfc8351f11b1e6382a45819c9dc36]]
    Former-commit-id: c2a4c8c28546d7cdc0ee79a24a6ca547832045c1
    Former-commit-id: 914d95e668d8ce57eae228cc5f7ed1ab979b2afd
    Former-commit-id: 274bec837eaaf3727d41ce383902c5fb63d2a834

---
+++ zc_frontend/src/pages/passwordRecovery/index.js
@@ -0,0 +1,56 @@
+import React, { useState} from 'react'
+import authBg from '../../pages/images/backg.svg'
+import { withRouter } from 'react-router-dom'
+import AuthInputBox from '../../components/AuthInputBox'
+import FormWrapper from '../../components/AuthFormWrapper'
+import styles from '../../styles/AuthFormElements.module.css'
+// import axios from 'axios'
+
+
+const Index = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    return (
+        <main id={styles.authPageWrapper}>
+        <aside id={styles.authAsideContainer} className={styles.display_none}>
+          <div id={styles.authImageWrapper}>
+            <img src={authBg} alt="backgroundImage" />
+            <div id={styles.aside_txt}></div>
+          </div>
+        </aside>
+        <section id={styles.authFormContainer}>
+          <FormWrapper
+            header="Recover Password"
+            subHeader="Don't fret!Fill in the details, and your account wil be all yours again."
+            email={email}
+            username={username}
+            submitButtonName="Continue"
+            // handleSubmit={handleSubmit}
+          >
+            <AuthInputBox
+              className={`${styles.inputElement}`}
+              id="email"
+              name="Email address"
+              type="email"
+              placeholder="Johndoe@example.com"
+              value={email}
+              setValue={setEmail}
+              error=""
+            />
+            <AuthInputBox
+              className={`${styles.inputElement}`}
+              id="username"
+              name="username"
+              type="username"
+              placeholder="Johny12345"
+              value={username}
+              setValue={setUsername}
+              error=""
+            />
+          </FormWrapper>
+        </section>
+      </main>
+    )
+}
+
+export default withRouter(Index)
