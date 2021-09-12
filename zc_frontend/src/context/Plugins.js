da6756923e69eea596819422cf919ce7d6d3536d
Elijah Soladoye <shodown96@gmail.com>
2021-09-12 20:47:18 +0100

    live-session
    Former-commit-id: aa333b35a061437dd9de3853e3c9afa9d980fa21 [formerly bc295b7799371b1566732a13392a723d60037385] [formerly 8d4864f6911be4611ba6de764cb898b3f2188ac2 [formerly b6666c2f60104fd809ee4cd1e44731b9b17193e1 [formerly a2bae02d47d065d58c59912b8046a752d23d0f50] [formerly 98555604c313048ebb14a271b4f12f106b0f0bdb]]]
    Former-commit-id: 190c61698cda4284658ea3a85449813c626b2c13 [formerly b6666c2f60104fd809ee4cd1e44731b9b17193e1 [formerly a2bae02d47d065d58c59912b8046a752d23d0f50]]
    Former-commit-id: b510d782bd44cffce120093543dd3a3c3fde81bd
    Former-commit-id: ec819ae138a0cfac449e1a2ca1d54474ead58df5
    Former-commit-id: 68765014bcb2b785f8a07eda6715b63af4936299

--- zc_frontend/src/context/Plugins.js
+++ zc_frontend/src/context/Plugins.js
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react'
 
 export const PluginContext = createContext(null)
 export const PluginProvider = ({ children }) => {
-  const [plugins, setPlugins] = useState(null)
+  const [plugins, setPlugins] = useState([])
 
   return (
     <PluginContext.Provider value={{ plugins, setPlugins }}>
