ee642f001f27ca5639335c8a69591fb72e36d22f
Obi Fortune <gabrielobi.of@gmail.com>
2021-09-12 12:11:24 +0100

    fixed react useeffect rerender on review branch
    Former-commit-id: 0847ae27c01f7367cb860ecb139c365f408bb38f

--- zc_frontend/src/components/Sidebar.js
+++ zc_frontend/src/components/Sidebar.js
@@ -27,15 +27,9 @@ export const Sidebar = () => {
   const close = () => setShowDialog(false)
   const [rooms, setRooms] = useState({})
   const [query, setQuery] = useState('')
+  // const [sort,setSort] = useState('')
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
-
-  // const [sort,setSort] = useState('');
-
-  useEffect(() => {
-    sidebarApi()
-    // rooms.filter()
-  }, [])
 
   // const sorters = {
   //   leastMembers : (a,b)=>{return a.members - b.members},
@@ -51,48 +45,47 @@ export const Sidebar = () => {
   //     return (aName < bName) ? 1:-1
   //   }
   // }
-  const sorters = [
-    (a, b) => {
-      return a.unread - b.unread
-    },
-    (a, b) => {
-      return b.unread - a.unread
-    },
-    (a, b) => {
-      const aName = a.title.toUpperCase()
-      const bName = b.title.toUpperCase()
-      return aName === bName ? 0 : aName < bName ? 1 : -1
-    },
-    (a, b) => {
-      const aName = a.title.toUpperCase()
-      const bName = b.title.toUpperCase()
-      return aName === bName ? 0 : aName < bName ? -1 : 1
-    }
-  ]
+  // const sorters = [
+  //   (a, b) => {
+  //     return a.unread - b.unread
+  //   },
+  //   (a, b) => {
+  //     return b.unread - a.unread
+  //   },
+  //   (a, b) => {
+  //     const aName = a.title.toUpperCase()
+  //     const bName = b.title.toUpperCase()
+  //     return aName === bName ? 0 : aName < bName ? 1 : -1
+  //   },
+  //   (a, b) => {
+  //     const aName = a.title.toUpperCase()
+  //     const bName = b.title.toUpperCase()
+  //     return aName === bName ? 0 : aName < bName ? -1 : 1
+  //   }
+  // ]
 
-  const sidebarApi = () => {
+  const sidebarApi = async () => {
     setLoading(true)
-    axios
-      .get(
+    try {
+      const res = await axios.get(
         `https://channels.zuri.chat/api/v1/sidebar/?org=1&user=43567868&format=json`
       )
-      .then(res => {
-        // console.log(res)
-
-        let result = res.data
-        console.log(result)
-        setLoading(false)
-        setRooms(result)
-        // console.log(rooms.joinedRooms)
-        // console.log(result.joined_rooms[1].icon)
-        sorters.forEach((sortfunc, ind) => {
-          const sortedroom = rooms.public_rooms.sort(sortfunc)
-          console.log(sortedroom)
-          // rooms.joined_rooms.forEach(curr => console.log(ind,curr.sort(sortfunc)))
-        })
-      })
-      .catch(err => console.log(err))
+      // console.log(res)
+      // let result = res.data
+      // setRooms(result)
+      // console.log(rooms.joinedRooms)
+      // console.log(result.joined_rooms[1].icon)
+      // sorters.forEach((sortfunc, ind) => {
+      //   const sortedroom = rooms.public_rooms.sort(sortfunc)
+      //   console.log(sortedroom)
+      //   // rooms.joined_rooms.forEach(curr => console.log(ind,curr.sort(sortfunc)))
+      // })
+      return res.data
+    } catch (err) {
+      return console.log(err)
+    }
   }
+
   const filteredJoinedRooms = rooms.joined_rooms
     ? rooms.joined_rooms.filter(room =>
         room.title.toLowerCase().includes(query)
@@ -103,6 +96,13 @@ export const Sidebar = () => {
         room.title.toLowerCase().includes(query)
       )
     : null
+
+  useEffect(() => {
+    sidebarApi().then(data => {
+      setRooms(data)
+      setLoading(false)
+    })
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -119,7 +119,7 @@ export const Sidebar = () => {
           />
         </div>
         <Overlay isOpen={showDialog} onDismiss={close}>
-          <Content>
+          <Content aria-label="room-list">
             <CloseButton className="close-button" onClick={close}>
               {/* <VisuallyHidden>Close</VisuallyHidden> */}
               <Span aria-hidden>×</Span>
