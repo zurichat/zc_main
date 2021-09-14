33aa88220c1a8dd0cecdaffce665b2aa49557644
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2021-09-14 02:10:47 +0100

    chores for design change
    Former-commit-id: 0f0a68dd152e9ad94fbdd2f0152430c81a202851 [formerly 63ac7688b24663c61d1699d0c65c0ab973cfb6c0] [formerly 61f8fd1afd903132eee2e97a5dbc5dc1d2656f27 [formerly c722c9bdb8b20b455c5626c0b8747159dcae885e [formerly bfa8fe7a8244bf03b76a841f59df265a27f433d5] [formerly f871442ff25ba13d8a37f64aeaffb49642a0e215]]]
    Former-commit-id: da26c841c265369c1501a59fe53a8a0f7874b634 [formerly c722c9bdb8b20b455c5626c0b8747159dcae885e [formerly bfa8fe7a8244bf03b76a841f59df265a27f433d5]]
    Former-commit-id: 49c7baf586e6a288eebc1af2d0ae7630016f357f
    Former-commit-id: aca13b701671c38a35d4a8d582428bbdfb65838a
    Former-commit-id: d333b809bf68158307e98dcd5e54a94cc40bf8d3

--- zc_frontend/src/components/AuthInputBox.js
+++ zc_frontend/src/components/AuthInputBox.js
@@ -12,6 +12,7 @@ const InputBox = ({
   //className = '',
   placeholder,
   name,
+  onFocus,
   required = true
 }) => {
   const ref = useRef(null)
@@ -48,6 +49,7 @@ const InputBox = ({
             value={value}
             required={required}
             onChange={e => setValue(e.target.value)}
+            onFocus={onFocus}
           />
 
           {/password/i.test(name) && (
