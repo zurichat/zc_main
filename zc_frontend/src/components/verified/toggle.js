e5c6a823f0033e850afd163570fb7c022f879792
pidoxy <pidoxy@users.noreply.github.com>
2021-09-14 05:48:48 +0000

    same_commit
    Former-commit-id: acd2cce569989f9ba7d8587678ab5f0c5744b99f [formerly 93bb35a90d8e221d13cc9ece4c46dd8c0d95d6fd] [formerly 6a17b06dbb87b54c8d80990e56311f4af0cfbea6 [formerly 3f6af8293576faa894e799683669aebbec86b3ba [formerly 34a8f5e28b295e1b41829e4f26774fc169ebc73d] [formerly 726dfda78f5c0c1f94ef2198345b2309d8bde55c]]]
    Former-commit-id: e306fcf16b4af4adf3bae798c0d3490c81e431b8 [formerly 3f6af8293576faa894e799683669aebbec86b3ba [formerly 34a8f5e28b295e1b41829e4f26774fc169ebc73d]]
    Former-commit-id: 50ce8c50fb970145c7f19007dcd281404f46dae0
    Former-commit-id: cd54c7334144cfba2c52cf80047b40c6fd7eb667
    Former-commit-id: f8d0d140348b39ec337be67dd80324277373338e

--- zc_frontend/src/components/verified/toggle.js
+++ zc_frontend/src/components/verified/toggle.js
@@ -1,17 +1,16 @@
-import styled from 'styled-component';
+import styled from 'styled-component'
 
 const Toggle = () => {
-    return(
-        <InputWrapper>
-        <Input type = "checkbox" />
-        <Slider />
-        </InputWrapper >
-    )
+  return (
+    <InputWrapper>
+      <Input type="checkbox" />
+      <Slider />
+    </InputWrapper>
+  )
 }
 
-const InputWrapper = styled.label`
-`
+const InputWrapper = styled.label``
 const Input = styled.input``
 const Slider = styled.span``
 
-export default Toggle;
+export default Toggle
