b452a5ec849edea2fb860c230eb27fc125cac6be
Lizzietrust <ohidaelizabeth3@gmail.com>
2021-09-13 18:14:30 -0700

    uncomment preference file

--- zc_frontend/src/components/Preferences.js
+++ zc_frontend/src/components/Preferences.js
@@ -7,12 +7,7 @@ import MessagesMedia from '../pages/preferences/components/MessagesMedia'
 import PreferenceMenu from '../pages/preferences/components/PreferenceMenu'
 import styles from '../pages/preferences/styles/NotificationPreference.module.css'
 import { ProfileContext } from '../context/ProfileModal'
-<<<<<<< HEAD
-import AdvancedSettings from '../pages/preferences/components/AdvancedSettings'
-import AudioVideo from '../pages/preferences/components/AudioVideo'
-=======
 import LanguageRegion from '../pages/preferences/components/LanguageRegion'
->>>>>>> 0c1cc8bac5e63ac026dcd538af35e8915e0c1f6c
 
 const Preferences = () => {
   const { sideBar } = useContext(ProfileContext)
@@ -27,14 +22,9 @@ const Preferences = () => {
         <div>
           {sideBar === 1 && <NotificationPreference />}
           {sideBar === 2 && <SidebarPreference />}
-<<<<<<< HEAD
-          {sideBar === 8 && <AudioVideo />}
-          {sideBar === 9 && <AdvancedSettings />}
-=======
           {sideBar === 3 && <Themes />}
           {sideBar === 4 && <MessagesMedia />}
           {sideBar === 5 && <LanguageRegion />}
->>>>>>> 0c1cc8bac5e63ac026dcd538af35e8915e0c1f6c
         </div>
       </div>
     </ProfileModal>
