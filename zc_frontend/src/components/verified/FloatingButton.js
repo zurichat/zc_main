726dfda78f5c0c1f94ef2198345b2309d8bde55c
pidoxy <pidoxy@users.noreply.github.com>
2021-09-14 05:48:48 +0000

    same_commit

--- zc_frontend/src/components/verified/FloatingButton.js
+++ zc_frontend/src/components/verified/FloatingButton.js
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 /**
  * Floating Button Component
- * 
+ *
  * @param {{
  *      id?: string
  *      className?: string
@@ -13,49 +13,48 @@ import styled from "styled-components";
  * }}
  */
 const FloatingButton = ({
-    id = "",
-    name = "",
-    className = "",
-    type = "",
-    disabled = false,
-    onClick,
+  id = '',
+  name = '',
+  className = '',
+  type = '',
+  disabled = false,
+  onClick
 }) => {
-    let Component = ButtonBase;
+  let Component = ButtonBase
 
-    return (
-        <Component
-            id={`${id && id}`}
-            className={`${className && className}`}
-            name={`${name && name}`}
-            disabled={disabled}
-            onClick={onClick}
-        >
-            +
-        </Component>
-    );
-};
-
+  return (
+    <Component
+      id={`${id && id}`}
+      className={`${className && className}`}
+      name={`${name && name}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      +
+    </Component>
+  )
+}
 
 const ButtonBase = styled.button`
-    font-size: 2rem;
-    margin: 1rem;
-    padding-inline: 1rem;
-    text-align: center;
+  font-size: 2rem;
+  margin: 1rem;
+  padding-inline: 1rem;
+  text-align: center;
 
-    border: 1px solid var(--primary-color);
-    border-radius: 50%;
-    
-    background-color: var(--primary-color);
-    color: white;
+  border: 1px solid var(--primary-color);
+  border-radius: 50%;
 
-    transition: ease-in-out 150ms;
-    &:disabled {
-        opacity: 50%;
-    }
-    &:enabled:active {
-        color: var(--primary-color);
-        background-color: var(--white);
-    }
-`;
+  background-color: var(--primary-color);
+  color: white;
+
+  transition: ease-in-out 150ms;
+  &:disabled {
+    opacity: 50%;
+  }
+  &:enabled:active {
+    color: var(--primary-color);
+    background-color: var(--white);
+  }
+`
 
 export default FloatingButton
--- zc_frontend/src/components/verified/StyleGuide.js
+++ zc_frontend/src/components/verified/StyleGuide.js
@@ -1,63 +1,52 @@
-import { FiPhoneCall } from "react-icons/fi"
-import Button from "../verified"
+import { FiPhoneCall } from 'react-icons/fi'
+import Button from '../verified'
 import FloatingButton from '../verified'
 
 // import styled from 'styled-components'
 
 const StyleGuide = () => {
-    return (
-        <>
-            <h1>Primary Button</h1>
-            <Button>
-                Medium Button
-            </Button>
-            <Button>
-                Medium Button
-            </Button>
-            <Button>
-                <FiPhoneCall />{" "}Button
-            </Button>
-            <br />
+  return (
+    <>
+      <h1>Primary Button</h1>
+      <Button>Medium Button</Button>
+      <Button>Medium Button</Button>
+      <Button>
+        <FiPhoneCall /> Button
+      </Button>
+      <br />
 
-            <h1>Secondary Button</h1>
-            <Button variant={`secondary`}>
-                Medium Button
-            </Button>
-            <Button variant={`secondary`}>
-                Medium Button
-            </Button>
-            <Button variant={"secondary"}>
-                <FiPhoneCall />{" "}Button
-            </Button>
-            <br />
+      <h1>Secondary Button</h1>
+      <Button variant={`secondary`}>Medium Button</Button>
+      <Button variant={`secondary`}>Medium Button</Button>
+      <Button variant={'secondary'}>
+        <FiPhoneCall /> Button
+      </Button>
+      <br />
 
-            <h1>Tertiary Button</h1>
-            <Button variant={"tertiary"}>
-                Medium Button
-            </Button>
-            <Button variant={"tertiary"} className={"underline"}>
-                Medium Button
-            </Button>
+      <h1>Tertiary Button</h1>
+      <Button variant={'tertiary'}>Medium Button</Button>
+      <Button variant={'tertiary'} className={'underline'}>
+        Medium Button
+      </Button>
 
-            <Button variant={"tertiary"}>
-                <FiPhoneCall />{" "}Button
-            </Button>
-            <Button variant={"tertiary"} className={"underline"}>
-                <FiPhoneCall />{" "}Button
-            </Button>
-            <br />
-            <FloatingButton />
-            <FloatingButton disabled />
-            <FloatingButton />
-            {/* <P >This is the test site</P>
+      <Button variant={'tertiary'}>
+        <FiPhoneCall /> Button
+      </Button>
+      <Button variant={'tertiary'} className={'underline'}>
+        <FiPhoneCall /> Button
+      </Button>
+      <br />
+      <FloatingButton />
+      <FloatingButton disabled />
+      <FloatingButton />
+      {/* <P >This is the test site</P>
 <p className="subtext">This is the test site</p>
 <p className="small_text">This is the test site</p>
 <p className="header_1">This is the test site</p>
 <p className="header_2">This is the test site</p>
 <p className="normal_text">This is the test site</p> */}
-        </>
-    )
+    </>
+  )
 }
 
-export default StyleGuide;
-
+export default StyleGuide
