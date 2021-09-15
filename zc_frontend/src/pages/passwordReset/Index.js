8edaf978d50fac3ad0ac34780ae716ea66df75ec
Obi Fortune <gabrielobi.of@gmail.com>
2021-09-15 09:28:48 +0100

    fixed modal rerender issue
    Former-commit-id: f9759569a56c68380b20d3de051929fb6ea3dbb4

--- zc_frontend/src/pages/passwordReset/Index.js
+++ zc_frontend/src/pages/passwordReset/Index.js
@@ -13,49 +13,61 @@ const Index = () => {
   const [email, setEmail] = useState('')
   const [modalShow, setModalShow] = useState(false)
 
+  const toggleModal = () => {
+    setModalShow(!modalShow)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    toggleModal()
+  }
+
   return (
-    <main id={styles.authPageWrapper}>
-      <aside id={styles.authAsideContainer} className={styles.display_none}>
-        <div id={styles.authImageWrapper}>
-          <img src={authBg} alt="backgroundImage" />
-          <div id={styles.aside_txt}></div>
-        </div>
-      </aside>
-      <section id={``}>
-        {/* logo div  */}
-        <div className={``}>
-          <img className={styles.logo} src={Logo} alt="" />
-        </div>
-        {/* header text  */}
-        <div className={``}>
-          <h4 className={styles.headerText}>Recover Password</h4>
-          <p>
-            Enter the email address you registered with, a reset link will be
-            sent to your email!
-          </p>
-        </div>
-        {/* form section  */}
-        <form action="">
-          <AuthInputBox
-            className={`${styles.resetInput}`}
-            id="email"
-            name="Email address"
-            type="email"
-            placeholder="Johndoe@example.com"
-            value={email}
-            setValue={setEmail}
-            error=""
-          />
-          <Button className={styles.button} onClick={() => setModalShow(true)}>
-            Continue
-          </Button>
-          {/* <ResetModal 
+    <>
+      <ResetModal show={modalShow} onHide={toggleModal} />
+      <main id={styles.authPageWrapper}>
+        <aside id={styles.authAsideContainer} className={styles.display_none}>
+          <div id={styles.authImageWrapper}>
+            <img src={authBg} alt="backgroundImage" />
+            <div id={styles.aside_txt}></div>
+          </div>
+        </aside>
+        <section id={``}>
+          {/* logo div  */}
+          <div className={``}>
+            <img className={styles.logo} src={Logo} alt="" />
+          </div>
+          {/* header text  */}
+          <div className={``}>
+            <h4 className={styles.headerText}>Recover Password</h4>
+            <p>
+              Enter the email address you registered with, a reset link will be
+              sent to your email!
+            </p>
+          </div>
+          {/* form section  */}
+          <form action="">
+            <AuthInputBox
+              className={`${styles.resetInput}`}
+              id="email"
+              name="Email address"
+              type="email"
+              placeholder="Johndoe@example.com"
+              value={email}
+              setValue={setEmail}
+              error=""
+            />
+            <Button className={styles.button} onClick={handleSubmit}>
+              Continue
+            </Button>
+            {/* <ResetModal 
             show={modalShow}
             onHide={setModalShow(false)}
           /> */}
-        </form>
-      </section>
-    </main>
+          </form>
+        </section>
+      </main>
+    </>
   )
 }
 
