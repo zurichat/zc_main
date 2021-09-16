db3763041e289e252490e9e04f096ff50b80f284
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-16 18:39:44 +0100

    setup funtion to get user ID
    Former-commit-id: 227f24c00b3ac3417c2249516c4b3ddc1363df8c

--- root/src/zuri-root-config.js
+++ root/src/zuri-root-config.js
@@ -13,7 +13,14 @@ registerApplication({
   name: "@zuri/main",
   // app: () => System.import("@zuri/main"),
   app: () => System.import("//localhost:8080/zuri-main.js"),
-  // app: () => System.import("//pidoxy-react-firebase.netlify.app"),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@zuri/spa-app-2",
+  // app: () => System.import("@zuri/main"),
+  // app: () => System.import("//localhost:8080/zuri-main.js"),
+  app: () => System.import("//localhost:8082/zuri-spa-app-2.js"),
   activeWhen: ["/"],
 });
 
