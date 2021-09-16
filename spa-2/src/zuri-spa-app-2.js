faf97225d41fbdd498a48c5ddaa7090d01b531a3
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-16 18:39:44 +0100

    setup funtion to get user ID
    Former-commit-id: 0a8cb47e9161b7b2d4c51526d46adeb8154265aa [formerly 502441f81018d827c1c95f87a2ea812897291f56] [formerly 661fc2350ce4f9ed5d99f47e775409873b122288 [formerly 8b8e66f798f05fd48ec0444b7d28f79cc2bec8e3 [formerly 0585b537c7d512d73a720c9b7b2ff9dd5dfd26e8] [formerly 227f24c00b3ac3417c2249516c4b3ddc1363df8c]]]
    Former-commit-id: 5e780b49770107e18eafa2b0f9a1d943baef221d [formerly 8b8e66f798f05fd48ec0444b7d28f79cc2bec8e3 [formerly 0585b537c7d512d73a720c9b7b2ff9dd5dfd26e8]]
    Former-commit-id: b46ab270f5d42c6eb689293b25f2b50dba0403d5
    Former-commit-id: 96af10ad3e37ab8c02d7f2d85e98b7d98b30e1be
    Former-commit-id: 640df9ba153b9724980b6c871e8b66e56d667946

--- spa-2/src/zuri-spa-app-2.js
+++ spa-2/src/zuri-spa-app-2.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
-import Root from "./root2.component";
+import Root from "./root.component";
 
 const lifecycles = singleSpaReact({
   React,
@@ -9,19 +9,8 @@ const lifecycles = singleSpaReact({
   rootComponent: Root,
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
-    console.table(err, info, props);
     return null;
   },
 });
-
-let defaultUserID = `61377a21e2358b02686503c5---`;
-
-export const GetUserInfo = ({ userID }) => {
-  return (
-    <div>
-      <p>This is user...</p>
-      <div>{defaultUserID}, {userID}</div>
-    </div>);
-}
 
 export const { bootstrap, mount, unmount } = lifecycles;
