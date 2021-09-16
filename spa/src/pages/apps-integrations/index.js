ce8364e356115c4ec4e211b33e9e138f4b10f8f0
Obi Fortune <gabrielobi.of@gmail.com>
2021-09-16 14:55:52 +0100

    started spa convert
    Former-commit-id: d4168985406699814768efdd7cd5fee9a172055c

---
+++ spa/src/pages/apps-integrations/index.js
@@ -0,0 +1,17 @@
+// import Categories from './components/Categories'
+import Footer from '../../components/externalPagesComponents/Footer/Footer'
+import SearchSuggestion from '../searchSuggestion'
+import SectionOne from './components/SectionOne'
+
+function AppsAndIntegrations() {
+  return (
+    <div>
+      <SearchSuggestion />
+      <SectionOne />
+      {/* <Categories /> */}
+      <Footer />
+    </div>
+  )
+}
+
+export default AppsAndIntegrations
