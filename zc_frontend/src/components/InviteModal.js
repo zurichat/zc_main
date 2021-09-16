dc5902e1cd7af6c4dfcb5c90ba7b6d1569358e23
“zyhon96” <adedoyin27@gmail.com>
2021-09-16 12:40:15 +0100

    Team-invite rremoved TS
    Former-commit-id: b568467fa114d640c45024dc6180639ea4ba0741

--- zc_frontend/src/components/InviteModal.js
+++ zc_frontend/src/components/InviteModal.js
@@ -9,13 +9,6 @@ const Modal = props => {
     return null
   }
 
-  // const data = fetch("{}", {
-  //   method: "POST",
-  //   body: JSON.stringify(data)
-  // }).then(res => {
-  //   console.log("Request complete! response:", res);
-  // });
-
   return (
     <>
       <div className={styles.modal} onClick={props.onClose}>
@@ -25,8 +18,7 @@ const Modal = props => {
               <h4 className={styles.modaltitle}>Invite People to the Team</h4>
             </div>
             <div className={styles.inviteclose}>
-              <button onClick={props.onClose} className="button">
-                {' '}
+              <button onClick={props.onClose} className={styles.button}>
                 <svg
                   width="18"
                   height="18"
@@ -64,8 +56,7 @@ const Modal = props => {
           <form>
             <div className={styles.modalbody}>To:</div>
             <div className={styles.invitebox}>
-              <div className={styles.invitedusers}>
-              </div>
+              <h6>Email goes here</h6>
             </div>
             <div className={styles.bottominvite}>
               <div className={styles.copy}>
