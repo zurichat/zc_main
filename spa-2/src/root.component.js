3343bfa535ae8e3557e4c626d7b8de7b623fffa2
Obi Fortune <gabrielobi.of@gmail.com>
2021-09-17 12:05:52 +0100

    spa restructure
    Former-commit-id: 3ecf0710f2580741c4804bf6b3113808420b8def [formerly 0b9237086bb515aaa90dc4c9fafd4f60d25314d6] [formerly 019db42524c7a06fbd9e0a62a3c4a3f04132b7f5 [formerly 9316a86464c4636954178fcff5b9a8dd21c111e5 [formerly f964ed0a377fcaf22c16993a3717f41a7807bc5d] [formerly 49ae6a492cb8ef73fd67f4f708256874d382d679]]]
    Former-commit-id: 5f9efca1eeace8e45b5c4ab964501dfed514f70d [formerly 9316a86464c4636954178fcff5b9a8dd21c111e5 [formerly f964ed0a377fcaf22c16993a3717f41a7807bc5d]]
    Former-commit-id: b6095760cd8e4a6a7278398f04599e5dc856a6db
    Former-commit-id: b1282812e141af0638bcd7fd36ebbb9e0594ca28
    Former-commit-id: d02d579c739db64b14405d16e88a046a7c8dc641

--- spa-2/src/root.component.js
+++
@@ -1,9 +0,0 @@
-import { GetUserInfo } from "@zuri/main";
-
-export default function Root(props) {
-  // return <section>{props.name} is mounted!</section>;
-  return (<div>
-    <p>This is app 2</p>
-    <GetUserInfo userID={"Mark"} />
-  </div>);
-}
