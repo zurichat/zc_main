842b74ff75459cfea0095ff3a8a91e1505caa719
Obi Fortune <gabrielobi.of@gmail.com>
2021-09-17 12:05:52 +0100

    spa restructure
    Former-commit-id: 49ae6a492cb8ef73fd67f4f708256874d382d679

--- zc_frontend/src/redux/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-
-import { userReducer } from './userReducer/userReducer'
-
-export const store = createStore(userReducer, applyMiddleware(thunk))
