be7fd61b61cbb68ef8163b619e33fc2385ca80ef
benjamin-bala <benjamin.bala91@gmail.com>
2021-09-17 16:09:49 -0400

    Added sidebar
    Former-commit-id: 3969a3b8e5c2d4299ddb2f00a5057d6daba2d385 [formerly 3490a67cb2e74bf52e2b9f9965da4298513c4ebe] [formerly cb399b1b8cdc437d3c8bd3a09d256b88255fd00e [formerly 93c2417350de5eb76a971c875356f2806ae4c9df [formerly 8997af441a59a9a9d49423a66912f0003c996c99] [formerly 33e5262e1e05b595e7ba1019eccea32ec86a219f]]]
    Former-commit-id: 46b266f5ff4b1f94a032bab25f2bcf0697d51e78 [formerly 93c2417350de5eb76a971c875356f2806ae4c9df [formerly 8997af441a59a9a9d49423a66912f0003c996c99]]
    Former-commit-id: 57a2c934cf24d3fff6bc928dfa74c02b7f808557
    Former-commit-id: a7fe0e8335213a6928b1245a7ffc436f43834bc7
    Former-commit-id: 00aed9b644f733b871b7ed0d38aa6df57261ff44

---
+++ src/zuri-root-config.js
@@ -0,0 +1,34 @@
+import { registerApplication, start } from 'single-spa'
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+// app: () => System.import("https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"),
+//   activeWhen: ["/"],
+// });
+
+registerApplication({
+  name: '@zuri/topbar',
+  app: () => System.import('@zuri/topbar'),
+  activeWhen: ['/']
+})
+registerApplication({
+  name: '@zuri/sidebar',
+  app: () => System.import('@zuri/sidebar'),
+  activeWhen: ['/']
+})
+registerApplication({
+  name: '@zuri/control',
+  app: () => System.import('@zuri/control'),
+  activeWhen: ['/']
+})
+
+// PLUGINS HERE
+registerApplication({
+  name: '@zuri/zuri-plugin-chessboard',
+  app: () => System.import('@zuri/zuri-plugin-chessboard'),
+  activeWhen: ['/', location => location.pathname.startsWith('/chess')]
+})
+
+start({
+  urlRerouteOnly: true
+})
