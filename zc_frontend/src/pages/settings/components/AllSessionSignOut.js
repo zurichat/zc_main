58270408087beaae18ea35e8877b1de793e919ad
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-09-18 09:47:54 +0100

    Added all session signout screen

--- zc_frontend/src/pages/settings/components/AllSessionSignOut.js
+++ zc_frontend/src/pages/settings/components/AllSessionSignOut.js
@@ -1,19 +1,117 @@
-import React from 'react'
-import styles from '../styles/AllSessionSignOut.module.css'
+import React, {useState, useEffect} from 'react'
+import { useHistory } from 'react-router-dom'
 import SignOutActionList from './SignOutActionList'
 import SettingsNav from './SettingsNav'
-import boxArrow from './../box-arrow-right.svg'
+import FormMessage from './FormMessage'
+// Styles
+import styles from '../styles/AllSessionSignOut.module.css'
+// Icons
+import boxArrow from '../../images/box-arrow-right.svg'
+import errorIcon from '../../images/bx-error.svg'
+// HTTP request library
+import axios from "axios"
+
 
 function AllSessionSignOut() {
+  const [error, setError] = useState("")
+  //const [userSession, setUserSession] = useState({})
+  const [user, setUser] = useState({})
+  const [currentPassword, setCurrentPassword] = useState("")
+
+  const history = useHistory()
+
+  useEffect(() => {
+    let timerId
+    if (error) {
+      timerId = setTimeout(() => {
+        setError("")
+      }, 3000)
+    }
+
+    return () => clearTimeout(timerId)
+    //const getUserFromStorage = () => {
+    //  let userDetail;
+    //  if(sessionStorage.getItem("user") && sessionStorage.getItem("session_id")) {
+    //    userDetail = JSON.parse(sessionStorage.getItem("user"))
+    //    setUserSession(userDetail)
+    //      //fetchUser(userDetail.id, userDetail.token)
+    //  } else {
+    //    history("/login")
+    //  }  
+    //}
+    //getUserFromStorage()
+  })
+
+
+  const comparePassword = async () => {
+    
+    try {
+      const response = await axios.post("https://api.zuri.chat/auth/logout/othersessions", {
+        body: {
+          "password": currentPassword
+        }
+      })
+      
+      if(response.status !== 200) {
+        throw new Error(`Failed to fetch data, ${response.status} `)
+      }
+
+      const {data} = await response.data
+      setUser(data)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+      setError("That password is incorrect. Please try again")
+    }
+  }
+
+  //const comparePassword = async (token) => {
+  //  console.log(token)
+  //  try {
+  //    const response = await axios.post("https://api.zuri.chat/auth/confirm-password", {
+  //      headers: {
+  //        "Authorization": `Bearer ${token}`
+  //      },
+  //      body: {
+  //        "password": currentPassword,
+  //        "current_password": currentPassword
+  //      }
+  //    })
+//
+  //    const {data} = await response.data
+  //    console.log(response.data)
+  //  } catch (error) {
+  //    console.log(error)
+  //  }
+  //}  
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    comparePassword()
+    //if((currentPassWord < 5))) {
+    //  setError("That password is incorrect. Please try again")
+    //} else {
+    //  confirmPassword()
+    //  history("/session-signout/account-reset")
+    //}
+  }
+
   return (
     <>
       <SettingsNav />
       <main className={`${styles.signout_wrapper}`}>
-        <article>
+        <article className={`${styles.session_container}`}>
           <header className={`${styles.signout_header}`}>
             <img src={boxArrow} alt="" />
             <h3>Sign out all other sessions</h3>
           </header>
+          {error && (
+            <FormMessage
+              message={error}
+              icon={errorIcon}
+              color="#F40101"
+            />)}
           <section className={`${styles.session_content}`}>
             <h4>Click the button below to sign out of all other sessions.</h4>
             <div className={`${styles.signout_content}`}>
@@ -29,19 +127,17 @@ function AllSessionSignOut() {
             </div>
 
             <div className={`${styles.line}`}></div>
-            <form className={`${styles.signout_form}`}>
-              <header>
-                <h4>Confirm your password to proceed.</h4>
-              </header>
+            <form className={`${styles.signout_form}`} onSubmit={handleSubmit}>
               <div className={`${styles.form_group}`}>
                 <input
                   type="text"
                   placeholder="Password"
                   name="password"
                   className={`${styles.form_input}`}
+                  onChange={e => setCurrentPassword(e.target.value)}
                 />
 
-                <button type="submit" className={`${styles.Submit}`}>
+                <button type="submit" className={`${styles.submit_btn}`}>
                   Sign out all other sessions
                 </button>
               </div>
