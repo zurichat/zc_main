34325719ea3746d7861a62d3b8485fe48a4a3122
Christian Ndu <christiannduh@gmail.com>
2021-09-19 00:39:31 +0100

    setup rxjs globalstate

--- globalState.js
+++ globalState.js
@@ -1,1 +1,14 @@
-import { Observable } from 'rxjs'
+import { BehaviorSubject } from 'rxjs'
+
+const $behaviorSubject = new BehaviorSubject('')
+
+export default $behaviorSubject
+
+// Example
+
+//Chess Plugin
+
+// export let chessSubscription
+// $behaviorSubject.subscribe(data => {
+//   chessSubscription = data
+// })
