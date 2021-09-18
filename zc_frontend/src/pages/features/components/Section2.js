1fdba600ac40aa33479608980d19c4f0e884ac34
Michellebuchiokonicha <michelleokonicha@gmail.com>
2021-09-18 18:13:32 +0100

    my features page

--- zc_frontend/src/pages/features/components/Section2.js
+++ zc_frontend/src/pages/features/components/Section2.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import styles from '../../features/styles/section2.module.css'
-import image1 from '../../../assets/image 4.png'
-import image2 from '../../../assets/image 3.png'
-import image3 from '../../../assets/image 8.png'
-import image4 from '../../../assets/image 6.png'
-import image5 from '../../../assets/image 5.png'
+import React from 'react';
+import styles from '../../features/styles/Features.module.css';
+import image1 from '../../../assets/image1.png';
+import image2 from '../../../assets/image2.png';
+import image3 from '../../../assets/image3.png';
+import image4 from '../../../assets/image4.png';
+import image5 from '../../../assets/image5.png';
 
-const ChatFeatureSection2 = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className="row justify-content-center">
-          <div className="col-lg-2 col-sm-4 text-center">
-            <img src={image1} alt="image1" />
-          </div>
-          <div className="col-lg-2 col-sm-4 text-center">
-            <img src={image2} alt="image2" />
-          </div>
-          <div className="col-lg-2 col-sm-4 text-center">
-            <img src={image3} alt="image3" />
-          </div>
-          <div className="col-lg-2 col-sm-4 text-center">
-            <img src={image4} alt="image4" />
-          </div>
-          <div className="col-lg-2 col-sm-4 text-center">
-            <img src={image5} alt="image5" />
+
+const Section2 = () => {
+
+    return(
+        <div className={styles.real}>
+          <div className={styles.container}>
+           <div className={styles.image1}>
+             <img src={image1} alt="imageu" />
+           </div>
+           <div className={styles.image2}>
+             <img src={image2} alt="imageo" />
+           </div>
+           <div className={styles.image3}>
+             <img src={image3} alt="imagei" />
+           </div>
+           <div className={styles.image4}>
+             <img src={image4} alt="imageg" />
+           </div>
+           <div className={styles.image5}>
+             <img src={image5} alt="imagen" />
+           </div>
           </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
-export default ChatFeatureSection2
+export default Section2;
