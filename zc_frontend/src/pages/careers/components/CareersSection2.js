4f64d29366f21964b606802e02a8119e8c1702dd
Elijah Edun <edunelijah18@gmail.com>
2021-09-19 09:27:44 +0100

    Resolved the wrong path implementation  on career pages

--- zc_frontend/src/pages/careers/components/CareersSection2.js
+++ zc_frontend/src/pages/careers/components/CareersSection2.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './styles/CareersSection2.module.css'
+import styles from '../styles/CareersSection2.module.css'
 
 function CareersSection2() {
   return (
