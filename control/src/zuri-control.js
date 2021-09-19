7e5e7aa97afce422873f2b89e2d8a7b81ddd1daf
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-19 09:37:13 +0100

    fix in progress
    Former-commit-id: 2a06a0e98d7d48731175962d7ce0acb66f0437e9 [formerly 091e121068d46910d1738bf7339cfdb6232f0022] [formerly 40db619cfc0ee23f47dec9005ee7365470f2350e [formerly 15ddaee7181c7e4cea6649f509d4dd796a454ebc [formerly 3f75d0428bc6c750fa7254f8b6701ecd6f32b13b] [formerly d4ee72e6c08b0b89349fdccd009e7ca492ad7a38]]]
    Former-commit-id: 009d5dedd37884f45a844a28682bb61cb7559151 [formerly 15ddaee7181c7e4cea6649f509d4dd796a454ebc [formerly 3f75d0428bc6c750fa7254f8b6701ecd6f32b13b]]
    Former-commit-id: 3b68445d9d87b811ef5a033811c2c4d7670c25ca
    Former-commit-id: 741e3b1a947c4821b65460f2214610954ac9daf3
    Former-commit-id: 327853eea130d5ed9ffc7dc8ab5ec6ecffb2e5df

--- control/src/zuri-control.js
+++ control/src/zuri-control.js
@@ -13,15 +13,34 @@ const lifecycles = singleSpaReact({
   }
 })
 
-export const GetUserInfo = () => {
+export const GetUserInfo = ({ userID = "61437054d0284bc6a922346b", token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb29raWUiOiJNVFl6TWpBek9UWTVNbnhIZDNkQlIwUlplRTVFVFROTlJGVXdXa1JCZVU5RVVtbFplbHBvVDFSSmVVMTZVVEpaWnowOWZBbXRlXzVuVUN5b2tfdmItaURmNHhfQkZjQXpRTUhYQThDUm51LUxxRjJRIiwiZW1haWwiOiJ0ZXN0MkBnbWFpbC5jb20iLCJpZCI6IjYxNDM3MDU0ZDAyODRiYzZhOTIyMzQ2YiIsIm9wdGlvbnMiOnsiUGF0aCI6Ii8iLCJEb21haW4iOiIiLCJNYXhBZ2UiOjYzMDcyMDAwMDAsIlNlY3VyZSI6ZmFsc2UsIkh0dHBPbmx5IjpmYWxzZSwiU2FtZVNpdGUiOjB9LCJzZXNzaW9uX25hbWUiOiJmNjgyMmFmOTRlMjliYTExMmJlMzEwZDNhZjQ1ZDVjNyJ9.MLA-teUt0AN6nYzMYa6KkCTu5mKI8W1bSjsG34a0VAo"}) => {
 
-  let user = JSON.parse(sessionStorage.getItem('user'))
-  let token = sessionStorage.getItem('token')
-  let sessionId = sessionStorage.getItem('session_id')
+  // let user = JSON.parse(sessionStorage.getItem('user'))
+  // let token = sessionStorage.getItem('token')
+  // let sessionId = sessionStorage.getItem('session_id')
 
-  console.log(user)
-  console.log( token)
-  console.log(sessionId)
+  // console.log(user)
+  // console.log( token)
+  // console.log(sessionId)
+
+  const [userData, setUserData] = useState();
+
+    axios
+      .get(`https://api.zuri.chat/users/${userID}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+      .then((res) => {
+        setUserData(res.data.data);
+        console.log(userData);
+
+        let dat = res.json;
+        console.log(dat);
+
+      })
+      .catch(err => console.log(err));
 
   return token;
 
