22047ca2d5f1d96a88463ceb8b5728920de0818e
mbataprecious <mbataprecious9@gmail.com>
2021-09-19 11:51:24 +0100

    commit

--- zc_frontend/src/pages/careers/index.js
+++ zc_frontend/src/pages/careers/index.js
@@ -1,29 +1,19 @@
 import Footer from '../../components/externalPagesComponents/Footer/Footer'
 import Message from '../../components/externalPagesComponents/Footer/Message'
-<<<<<<< HEAD
-import Header from "../../components/externalPagesComponents/Header";
-=======
 import CareersSection2 from './CareersSection2'
 import Section3 from './components/Section3/Section3'
 import SectionOne from './components/section1/sectionOne'
 import SearchSuggestion from '../searchSuggestion'
->>>>>>> fcd1f67d75ed5339afa96749c66b90f95a75c1e8
 
 function Careers() {
   return (
     <div>
-<<<<<<< HEAD
-      <Header />
-      {/* <Message />
-      <Footer /> */}
-=======
       <SearchSuggestion />
       <SectionOne />
       <CareersSection2 />
       <Section3 />
       <Message />
       <Footer />
->>>>>>> fcd1f67d75ed5339afa96749c66b90f95a75c1e8
     </div>
   )
 }
