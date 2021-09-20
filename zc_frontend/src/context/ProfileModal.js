793c2cd6391f7f502a84910b026517a7540ff097
agkayster <ejike.chiboka@gmail.com>
2021-09-20 20:37:36 +0100

    added merged changes

--- zc_frontend/src/context/ProfileModal.js
+++ zc_frontend/src/context/ProfileModal.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import { createContext, useState } from 'react'
-import userAvatar from '../assets/user.svg'
-
-
-export const ProfileContext = createContext(null)
-export const ProfileProvider = ({ children }) => {
-  const [modal, setModal] = useState(false)
-  const [showProfile, setShowProfile] = useState(false)
-  const [sideBar, setSideBar] = useState(1)
-  const [user, setUser] = useState([])
-  const [orgId, setOrgId] = useState('');
-  const [userProfileImage, setUserProfileImage ] = useState(userAvatar);
-
-
-  const toggleModalState = () => {
-    setModal(!modal)
-  }
-
-  const toggleProfileState = () => {
-    setShowProfile(!showProfile)
-  }
-
-  return (
-    <ProfileContext.Provider
-      value={{
-        modal,
-        toggleModalState,
-        showProfile,
-        toggleProfileState,
-        sideBar,
-        setSideBar,
-        user,
-        setUser,
-        orgId,
-        setOrgId,
-        userProfileImage,
-        setUserProfileImage
-      }}
-    >
-      {children}
-    </ProfileContext.Provider>
-  )
-}
-=======
 import { createContext, useState } from 'react'
 import userAvatar from '../assets/user.svg'
 
@@ -84,4 +39,3 @@ export const ProfileProvider = ({ children }) => {
     </ProfileContext.Provider>
   )
 }
->>>>>>> 5ce7e1f81e5cbafc7134dd653c17981442f09083
