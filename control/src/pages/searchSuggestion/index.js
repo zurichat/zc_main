202852449691065364f55a72ea72ac8e804141c7
ehindero israel <iehindero419@stu.ui.edu.ng>
2021-09-20 02:00:00 +0100

    'converted-pages'

---
+++ control/src/pages/searchSuggestion/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderSearchSuggestion from '../../components/HeaderSearchSuggestion'
+
+const SearchSuggestion = () => {
+  return (
+    <div>
+      <HeaderSearchSuggestion />
+    </div>
+  )
+}
+
+export default SearchSuggestion
