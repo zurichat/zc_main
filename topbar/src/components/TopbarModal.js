54f1231961d5fed62044de8730c83e6104dca9ed
agkayster <ejike.chiboka@gmail.com>
2021-09-21 15:41:53 +0100

    finished polishing user icon drop down modal

--- topbar/src/components/TopbarModal.js
+++ topbar/src/components/TopbarModal.js
@@ -229,10 +229,168 @@
 
 // export default TopbarModal
 
+// import { useContext, useState } from 'react'
+// // import { authAxios } from '../util/Api'
+// import { FaCircle, FaChevronRight } from 'react-icons/fa'
+
+// import Picker, { SKIN_TONE_MEDIUM_DARK } from 'emoji-picker-react'
+
+// import styles from '../styles/Topbar.module.css'
+// import { TopbarContext } from '../context/Topbar'
+// import StatusBadgeModal from './StatusBadgeModal'
+// import { ProfileContext } from '../context/ProfileModal'
+// import Preferences from './Preferences'
+// import EditProfile from './EditProfile'
+// import MembersModal from './MembersModal'
+// import Downloads from './Downloads'
+// import PauseNotification from './PauseNotification'
+
+// // import { useContext, useState, useEffect } from 'react'
+// // import { authAxios } from '../util/Api'
+// // import { FaChevronRight } from 'react-icons/fa'
+
+// const TopbarModal = ({ members }) => {
+//   const { toggleModalState, toggleProfileState } = useContext(ProfileContext)
+
+//   const state = useContext(TopbarContext)
+//   const [showModal] = state.show
+//   const [active, setActive] = state.presence
+//   const [showStatus] = state.status
+//   const [showMembersModal] = state.modal
+//   const { onEmojiClick, openStatus, closeStatus, modalRef, closeMembersModal } =
+//     state
+//   const [modal, setModal] = useState('')
+//   const [pause, setPause] = useState(false)
+
+//   return (
+//     <>
+//       {/*The section that shows the status*/}
+//       {showStatus ? (
+//         <div
+//           ref={modalRef}
+//           className={styles.modalContainers}
+//           onClick={closeStatus}
+//         >
+//           <div className={styles.picker}>
+//             <div className={styles.smileys}>
+//               <Picker
+//                 onEmojiClick={onEmojiClick}
+//                 skinTone={SKIN_TONE_MEDIUM_DARK}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       ) : null}
+//       {/* The section that shows the members modal */}
+//       {showMembersModal ? (
+//         <div ref={modalRef} className={styles.modalContainers}>
+//           <div
+//             id="overlay"
+//             onClick={closeMembersModal}
+//             className={styles.membersModalOverlay}
+//           />
+//           <MembersModal members={members} roomTitle={'announcements'} />
+//         </div>
+//       ) : null}
+//       {/* The section that shows the topbarprofile */}
+//       {showModal ? (
+//         <section className={styles.topbarModal}>
+//           <div className={styles.sectionOne}>
+//             <div className={styles.oneLeft}>
+//               <img src="/profilepic.png" alt="profile" />
+//             </div>
+
+//             <div className={styles.oneRight}>
+//               <h4>Praise.A</h4>
+//               {active ? (
+//                 <div className={styles.online}>
+//                   <FaCircle className={styles.circle} />
+//                   <p className={styles.active}>Active</p>
+//                 </div>
+//               ) : (
+//                 <div className={styles.online}>
+//                   <FaCircle className={styles.circlegrey} />
+//                   <p className={styles.away}>Away</p>
+//                 </div>
+//               )}
+//             </div>
+//           </div>
+
+//           <div className={styles.sectionTwo}>
+//             <StatusBadgeModal />
+//           </div>
+//           <div onClick={openStatus} className={styles.sectionTwo}>
+//             <StatusBadgeModal />
+//           </div>
+
+//           <div className={styles.sectionThree}>
+//             <p onClick={openStatus}>Set a status</p>
+//             <p onClick={() => setActive(!active)}>
+//               {active ? 'Set yourself as away' : 'Set yourself as active'}
+//             </p>
+//             <div className={styles.pause}>
+//               <p onClick={() => setPause(!pause)}>Pause Notifications</p>
+//               <FaChevronRight className={styles.chevron} />
+//             </div>
+//             {pause && <PauseNotification pause={pause} setPause={setPause} />}
+//           </div>
+//           <div className={styles.sectionFour}>
+//             <p
+//               onClick={() => {
+//                 // setModal('edit profile') //needs refactoring
+//                 toggleModalState()
+//               }}
+//             >
+//               Edit profile
+//             </p>
+//             <p onClick={toggleProfileState}>View profile</p>
+//             <p
+//               onClick={() => {
+//                 // setModal('preference') //needs refactoring
+//                 toggleModalState()
+//               }}
+//             >
+//               Preferences
+//             </p>
+//           </div>
+
+//           <hr className={styles.hr} />
+
+//           <div className={styles.sectionSix}>
+//             <p
+//               onClick={() => {
+//                 setModal('downloads')
+//               }}
+//             >
+//               Downloads
+//             </p>
+//           </div>
+
+//           {modal === 'edit profile' && <EditProfile />}
+
+//           {modal === 'preference' && <Preferences />}
+
+//           {modal === 'downloads' && <Downloads setModal={setModal} />}
+
+//           <hr className={styles.hr} />
+
+//           <div className={styles.sectionFive}>
+//             <p>
+//               {' '}
+//               <a href="/signout">Sign out of Team Einstein workspace</a>{' '}
+//             </p>
+//           </div>
+//         </section>
+//       ) : null}
+//     </>
+//   )
+// }
+
+// export default TopbarModal
+
 import { useContext, useState, useEffect } from 'react'
 import { authAxios } from '../util/Api'
 import { FaChevronRight } from 'react-icons/fa'
-
 import Picker, { SKIN_TONE_MEDIUM_DARK } from 'emoji-picker-react'
 
 import styles from '../styles/Topbar.module.css'
@@ -255,9 +413,6 @@ const TopbarModal = ({ members }) => {
     orgId
   } = useContext(ProfileContext)
 
-  // const TopbarModal = ({ members }) => {
-  //   const { toggleModalState, toggleProfileState } = useContext(ProfileContext)
-
   const state = useContext(TopbarContext)
   const [showModal] = state.show
   const [presence, setPresence] = state.presence
@@ -273,16 +428,6 @@ const TopbarModal = ({ members }) => {
   } = state
   const [modal, setModal] = useState('')
   const [pause, setPause] = useState(false)
-
-  // const state = useContext(TopbarContext)
-  // const [showModal] = state.show
-  // const [active, setActive] = state.presence
-  // const [showStatus] = state.status
-  // const [showMembersModal] = state.modal
-  // const { onEmojiClick, openStatus, closeStatus, modalRef, closeMembersModal } =
-  //   state
-  // const [modal, setModal] = useState('')
-  // const [pause, setPause] = useState(false)
 
   const onSetPresence = () => {
     setPresence(() => {
@@ -363,26 +508,7 @@ const TopbarModal = ({ members }) => {
           </div>
         </div>
       ) : null}
-      {/* return (
-    <>
-      The section that shows the status
-      {showStatus ? (
-        <div
-          ref={modalRef}
-          className={styles.modalContainers}
-          onClick={closeStatus}
-        >
-          <div className={styles.picker}>
-            
-            <div className={styles.smileys}>
-              <Picker
-                onEmojiClick={onEmojiClick}
-                skinTone={SKIN_TONE_MEDIUM_DARK}
-              />
-            </div>
-          </div>
-        </div>
-      ) : null} */}
+
       {/* The section that shows the members modal */}
       {showMembersModal ? (
         <div ref={modalRef} className={styles.modalContainers}>
@@ -394,17 +520,7 @@ const TopbarModal = ({ members }) => {
           <MembersModal members={members} roomTitle={'announcements'} />
         </div>
       ) : null}
-      {/* The section that shows the members modal */}
-      {/* {showMembersModal ? (
-        <div ref={modalRef} className={styles.modalContainers}>
-          <div
-            id="overlay"
-            onClick={closeMembersModal}
-            className={styles.membersModalOverlay}
-          />
-          <MembersModal members={members} roomTitle={'announcements'} />
-        </div>
-      ) : null} */}
+
       {/* The section that shows the topbarprofile */}
       {showModal ? (
         <section className={styles.topbarModal}>
@@ -438,49 +554,6 @@ const TopbarModal = ({ members }) => {
             </div>
             {pause && <PauseNotification pause={pause} setPause={setPause} />}
           </div>
-
-          {/* The section that shows the topbarprofile */}
-          {/* {showModal ? (
-        <section className={styles.topbarModal}>
-          <div className={styles.sectionOne}>
-            <div className={styles.oneLeft}>
-              <img src="/profilepic.png" alt="profile" />
-            </div>
-
-            <div className={styles.oneRight}>
-              <h4>Praise.A</h4>
-              {active ? (
-                <div className={styles.online}>
-                  <FaCircle className={styles.circle} />
-                  <p className={styles.active}>Active</p>
-                </div>
-              ) : (
-                <div className={styles.online}>
-                  <FaCircle className={styles.circlegrey} />
-                  <p className={styles.away}>Away</p>
-                </div>
-              )}
-            </div>
-          </div>
-
-          <div className={styles.sectionTwo}>
-            <StatusBadgeModal />
-          </div>
-          <div onClick={openStatus} className={styles.sectionTwo}>
-            <StatusBadgeModal />
-          </div>
-
-          <div className={styles.sectionThree}>
-            <p onClick={openStatus}>Set a status</p>
-            <p onClick={() => setActive(!active)}>
-              {active ? 'Set yourself as away' : 'Set yourself as active'}
-            </p>
-            <div className={styles.pause}>
-              <p onClick={() => setPause(!pause)}>Pause Notifications</p>
-              <FaChevronRight className={styles.chevron} />
-            </div>
-            {pause && <PauseNotification pause={pause} setPause={setPause} />}
-          </div> */}
 
           <hr className={styles.hr} />
 
@@ -542,57 +615,5 @@ const TopbarModal = ({ members }) => {
     </>
   )
 }
-
-//           <div className={styles.sectionFour}>
-//             <p
-//               onClick={() => {
-//                 // setModal('edit profile') //needs refactoring
-//                 toggleModalState()
-//               }}
-//             >
-//               Edit profile
-//             </p>
-//             <p onClick={toggleProfileState}>View profile</p>
-//             <p
-//               onClick={() => {
-//                 // setModal('preference') //needs refactoring
-//                 toggleModalState()
-//               }}
-//             >
-//               Preferences
-//             </p>
-//           </div>
-
-//           <hr className={styles.hr} />
-
-//           <div className={styles.sectionSix}>
-//             <p
-//               onClick={() => {
-//                 setModal('downloads')
-//               }}
-//             >
-//               Downloads
-//             </p>
-//           </div>
-
-//           {modal === 'edit profile' && <EditProfile />}
-
-//           {modal === 'preference' && <Preferences />}
-
-//           {modal === 'downloads' && <Downloads setModal={setModal} />}
-
-//           <hr className={styles.hr} />
-
-//           <div className={styles.sectionFive}>
-//             <p>
-//               {' '}
-//               <a href="/signout">Sign out of Team Einstein workspace</a>{' '}
-//             </p>
-//           </div>
-//         </section>
-//       ) : null}
-//     </>
-//   )
-// }
 
 export default TopbarModal
