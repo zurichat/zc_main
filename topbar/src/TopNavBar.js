5e4c7ad84fed3b2e60ad7bc9f61882c12cd2b321
agkayster <ejike.chiboka@gmail.com>
2021-09-21 20:11:59 +0100

    Finish working on my workflow, made it connect to backend and polished it

--- topbar/src/TopNavBar.js
+++ topbar/src/TopNavBar.js
@@ -1,19 +1,59 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
+import { authAxios } from './utils/Api.js'
 import { TopbarContext } from './context/Topbar'
 import { connect } from 'react-redux'
 import zurichatlogo from './assets/images/Logo.svg'
 import { useState } from 'react'
 import styled from 'styled-components'
 import { BaseInput } from './TopBarIndex'
+import { ProfileContext } from './context/ProfileModal'
 import userAvatar from './assets/images/user.svg'
 import TopbarModal from './components/TopbarModal'
 import UserForm from '../../control/src/pages/ReportFeature/components/Form'
 
-
 const TopNavBar = ({ userProfile: { last_name, first_name } }) => {
-  const state = useContext(TopbarContext)
-  const { openModal } = state
+  const { openModal } = useContext(TopbarContext)
+  const { setUser, user, setOrgId, setUserProfileImage } =
+    useContext(ProfileContext)
+  const [organizations, setOrganizations] = useState([])
+
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    const userdef = JSON.parse(sessionStorage.getItem('user'))
+    async function getOrganizations() {
+      await authAxios
+        .get(`/users/${userdef.email}/organizations`)
+        .then(response => {
+          setOrganizations(response.data.data)
+          setOrgId(response.data.data[0]?.id)
+          authAxios
+            .get(`/organizations/${response.data.data[0]?.id}/members`)
+            .then(response => {
+              setUser(
+                response.data.data.find(
+                  member => member.email === userdef.email
+                )
+              )
+              return response.data.data.find(
+                member => member.email === userdef.email
+              )
+            })
+            .catch(err => {
+              console.log(err.response.data)
+            })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+
+    setUserProfileImage(user.image_url)
+
+    getOrganizations()
+  }, [setOrgId, setUser])
+
+  console.log(user)
 
   return (
     <TopNavBarBase>
@@ -32,10 +72,11 @@ const TopNavBar = ({ userProfile: { last_name, first_name } }) => {
         placeholder="Search here"
         border={'#99999933'}
       />
-        <UserForm />
+      <UserForm />
       <div>
         <img
-          src={userAvatar}
+          style={{ height: '30px', width: '30px', borderRadius: '10px' }}
+          src={user.image_url ? user.image_url : userAvatar}
           onClick={openModal}
           role="button"
           alt="user profile avatar"
