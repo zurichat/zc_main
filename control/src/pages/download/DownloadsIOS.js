0bfafb184d5134e75592784e8096b6ada95b6164
Tech-sis <abilawonadeola@gmail.com>
2021-09-21 14:29:43 +0100

    added download files

--- control/src/pages/download/DownloadsIOS.js
+++ control/src/pages/download/DownloadsIOS.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react'
 import downloadStyles from './styles/DownloadsIOS.module.css'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
@@ -6,10 +7,22 @@ import AppStorelogo from './assets/appstore.png'
 
 import PhoneDM from './assets/PhoneDM.png'
 import PhoneChannel from './assets/PhoneChannels.png'
+import fetchInstall from './utils/index'
 
 // import Apk from './apk/appRelease.apk'
 
 const DownloadsIOS = () => {
+   const [exe, setexe] = useState({ link: '', name: '' })
+  // const [msi, setmsi] = useState({ link: '', name: '' })
+  // const [dmg, setdmg] = useState({ link: '', name: '' })
+
+  useEffect(() => {
+    fetchInstall('exe').then(res => {
+      setexe(res)
+    })
+    // fetchInstall('msi').then(res => {setmsi(res)})
+    // fetchInstall('dmg').then(res => {setdmg(res)})
+  }, [])
   return (
     <>
       <Header />
@@ -17,12 +30,12 @@ const DownloadsIOS = () => {
         <div className={downloadStyles.container}>
           <div className={downloadStyles.containerfluid}>
             <p className={downloadStyles.zuritext}>Download Zuri Chat beta for iOS.</p>
-            {/* <a href={Apk} download="release.apk">
-              
-            </a> */}
-            <div className={downloadStyles.appstorediv}>
+            <a href={exe.link} download={exe.name}>
+               <div className={downloadStyles.appstorediv}>
                 <img src={AppStorelogo} alt="" />
               </div>
+            </a>
+           
             <p className={downloadStyles.newtext}>
               Get Zuri Chat DM & Channel
             </p>
