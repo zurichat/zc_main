22e9fdceb47621715aecce473a69dfa82d1b37ec
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-09-21 10:54:34 +0100

    Added the path to choose a workspace screen
    Former-commit-id: 46d9c97430ef2046f5039fd794cb06ac9901a5e9

--- control/src/App.js
+++ control/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './pages/login'
 import SignUp from './pages/signup'
 import LandingPage from './pages/LandingPage'
+import Workspace from './pages/workspace/components/Workspace'
 import Settings from './pages/settings'
 import ConfirmPassword from './pages/settings/components/ConfirmPassword'
 import AccDeactivated from './pages/settings/components/AccDeactivated'
@@ -43,6 +44,9 @@ const App = () => (
       </Route>
       <Route path="/signup">
         <SignUp />
+      </Route>
+      <Route path="/choose-workspace">
+        <Workspace />
       </Route>
       <Route path="/features">
         <Features />
