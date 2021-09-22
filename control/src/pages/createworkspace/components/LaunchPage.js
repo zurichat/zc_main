cdc3d952815017cd2faab3681444fb73e91e74e9
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-09-22 12:25:52 +0100

    lint

--- control/src/pages/createworkspace/components/LaunchPage.js
+++ control/src/pages/createworkspace/components/LaunchPage.js
@@ -1,21 +1,23 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import LaunchPageCSS from '../styles/LaunchPage.module.css'
 
 function LaunchPage() {
-  const [user,setUser] = useState(null)
-  const [projectname,setProjectname] = useState(localStorage.getItem('input') || "alpha")
-    useEffect(() => {
-        const user = JSON.parse(sessionStorage.getItem('user'));
-        if(user) {
-            setUser(user)
-        }  
-    },[])
+  const [user, setUser] = useState(null)
+  const [projectname, setProjectname] = useState(
+    localStorage.getItem('input') || 'alpha'
+  )
+  useEffect(() => {
+    const user = JSON.parse(sessionStorage.getItem('user'))
+    if (user) {
+      setUser(user)
+    }
+  }, [])
   return (
     <div>
       <article className={LaunchPageCSS.wrapper}>
         <div className={LaunchPageCSS.email}>
           {' '}
-          {user ? <span>Signed in as {user.email}</span>:null}
+          {user ? <span>Signed in as {user.email}</span> : null}
         </div>
         <div className={LaunchPageCSS.centerWrapper}>
           <div className={LaunchPageCSS.logoContainer}>
@@ -28,8 +30,8 @@ function LaunchPage() {
           <h1 className={LaunchPageCSS.brandhub}> Launching {projectname}</h1>
 
           <div className={LaunchPageCSS.openZuriDesktop}>
-            Click <a href="https://zuri.chat/home"> "Open Zuri Chat"</a> to launch the
-            desktop app
+            Click <a href="https://zuri.chat/home"> "Open Zuri Chat"</a> to
+            launch the desktop app
           </div>
           {/* end */}
           <a href="" className={LaunchPageCSS.openZuri}>
@@ -37,7 +39,10 @@ function LaunchPage() {
           </a>
           <p>
             Don't have the app ? You can also{' '}
-            <a href="https://zuri.chat/home" className={LaunchPageCSS.inBrowser}>
+            <a
+              href="https://zuri.chat/home"
+              className={LaunchPageCSS.inBrowser}
+            >
               use Zuri Chat in your browser
             </a>
           </p>
