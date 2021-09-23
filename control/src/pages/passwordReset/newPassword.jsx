79cf8aac692d887e3b04e137c6beba461248409a
sammiearchie77 <chinonsosamuel29@gmail.com>
2021-09-23 14:41:14 +0100

    resetpassword bug fixed && implementing modal on change password change
    Former-commit-id: 54ae7d97a6cd2d122ac124f6ef95d2234a1c4385

--- control/src/pages/passwordReset/newPassword.jsx
+++ control/src/pages/passwordReset/newPassword.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import authBg from '../../pages/images/backg.svg'
+import authBg from '../../component-assets/backg.svg'
 import { withRouter } from 'react-router-dom'
 import AuthInputBox from '../../components/AuthInputBox'
-import FormWrapper from '../../components/AuthFormWrapper'
-import styles from '../../styles/AuthFormElements.module.css'
+// import styles from '../../component-styles/ResetPassword.modules.css'
 import axios from 'axios'
+import Button from '../../components/Button'
 
-export default function NewPassword({ resetCode }) {
+const NewPassword = ({ resetCode }) => {
   const [password, setPassword] = useState('')
   const handleSubmit = async () => {
     const res = await axios.post('api.zuri.chat/account/update-password', {
@@ -19,42 +19,54 @@ export default function NewPassword({ resetCode }) {
     <main id={styles.authPageWrapper}>
       <aside id={styles.authAsideContainer} className={styles.display_none}>
         <div id={styles.authImageWrapper}>
-          <img src={authBg} alt="backgroundImage" />
+          {/* <img src={authBg} alt="backgroundImage" /> */}
           <div id={styles.aside_txt}></div>
         </div>
       </aside>
-      <section id={styles.authFormContainer}>
-        <FormWrapper
-          header="Enter your new password"
-          email={email}
-          // handleSubmit={handleSubmit}
-        >
-          <AuthInputBox
-            className={`${styles.inputElement}`}
-            id="password"
-            name="Password"
-            type="password"
-            placeholder="Enter your new password"
-            value={password}
-            setValue={setPassword}
-            onFocus={displayImage}
-            // error={error}
-          />
+      <section id={``}>
+        {/* logo div  */}
+        <div className={``}>
+            <img className={styles.logo} src={Logo} alt="" />
+          </div>
+          {/* header text  */}
+          <div className={``}>
+            <h4 className={styles.headerText}>Change Password</h4>
+            <p>
+              Enter the email address you registered with, a reset link will be
+              sent to your email!
+            </p>
+          </div>
+            {/* form section  */}
+            <form action="">
+              <AuthInputBox
+                className={`${styles.inputElement}`}
+                id="password"
+                name="Password"
+                type="password"
+                placeholder="Enter your new password"
+                value={password}
+                setValue={setPassword}
+                onFocus={displayImage}
+                // error={error}
+              />
 
-          {/* <AuthInputBox
-            className={`${styles.inputElement}`}
-            id="cpassword"
-            name="Confirm password"
-            type="password"
-            placeholder="Confirm password"
-            value={confirmPassword}
-            setValue={setConfirmPassword}
-            // error={error}
-          /> */}
-
-          <div style={{ margin: '20px' }}></div>
-        </FormWrapper>
+              {/* <AuthInputBox
+                className={`${styles.inputElement}`}
+                id="cpassword"
+                name="Confirm password"
+                type="password"
+                placeholder="Confirm password"
+                value={confirmPassword}
+                setValue={setConfirmPassword}
+                // error={error}
+              /> */}
+              <Button className={styles.button} onClick={handleSubmit}>
+                Continue
+              </Button>
+          </form>
       </section>
     </main>
   )
 }
+
+export default withRouter(NewPassword)
