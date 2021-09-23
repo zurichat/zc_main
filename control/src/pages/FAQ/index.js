9c6fb4b76d58c6a5dce37870ba1d3167a013d9f8
ThatSameBlackBoyFromAfrica <muofunanya3@gmail.com>
2021-09-23 12:38:19 +0100

    Refactoring Pricing page

--- control/src/pages/FAQ/index.js
+++ control/src/pages/FAQ/index.js
@@ -1,65 +1,27 @@
-import React from 'react'
-
 import { useState } from 'react'
-import styles from '../../component-styles/FAQ.module.css'
+import styles from './FAQ.module.css'
 
-const FAQ = () => {
+const Index = () => {
   const [selected, setSelected] = useState(null)
-
-  const data = [
-    {
-      question: 'What are my payment options - credit card and/or invoice?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
-    },
-    {
-      question:
-        "Our team has several accounts which are not being actively used, but which we don't want to disable. Do we still have to pay for them?",
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
-    },
-    {
-      question: 'My team has credits. How do we use them?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
-    },
-    {
-      question:
-        'We need to add new users to our team. How will that be billed?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
-    },
-    {
-      question:
-        'My team wants to cancel its subscription. How do we do that? Can we get a refund?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
-    },
-    {
-      question:
-        'Do you offer discounts for non-profit organizations or educational institutions?',
-      answer:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
-    }
-  ]
 
   const toggle = i => {
     if (selected === i) {
-      return setSelected(null)
+      setSelected(null)
     }
 
     setSelected(i)
   }
+
   return (
-    <div className={`${styles.faqs}`}>
-      <h2>Commonly Asked Questions</h2>
-      <div className={`${styles.wrapper}`}>
-        <div className={`${styles.accordion}`}>
+    <div className={styles.faqs}>
+      <h2 className={`{styles.h2} text-center`}>Commonly Asked Questions</h2>
+      <div className={styles.wrapper}>
+        <div className={styles.accordion}>
           {data.map(i => (
-            <li key={i.question} className={`${styles.items}`}>
-              <div className={`${styles.title}`} onClick={() => toggle(i)}>
-                <h6>{i.question}</h6>
-                <span>
+            <div className={styles.items}>
+              <div className={styles.title} onClick={() => toggle(i)}>
+                <h6 className={styles.h6}>{i.question}</h6>
+                <span className={styles.span}>
                   {selected === i ? (
                     <i className="fas fa-angle-up"></i>
                   ) : (
@@ -67,19 +29,20 @@ const FAQ = () => {
                   )}
                 </span>
               </div>
-              <div
-                className={`${selected === i ? 'answer show' : 'answer'}`}
+              {selected === i &&
+                <div
+                className={selected === i ? 'answer show' : 'answer'}
                 onClick={() => toggle(i)}
               >
                 {i.answer}
-              </div>
-            </li>
+              </div>}
+            </div>
           ))}
         </div>
       </div>
-      <div className={`${styles.billing}`}>
+      <div className={styles.billing}>
         Get more information on{' '}
-        <a className={`${styles.info}`} href="/faq">
+        <a className={styles.info} href="/faq">
           billing at Zuri Chat.
         </a>
       </div>
@@ -87,4 +50,40 @@ const FAQ = () => {
   )
 }
 
-export default FAQ
+const data = [
+  {
+    question: 'What are my payment options - credit card and/or invoice?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
+  },
+  {
+    question:
+      "Our team has several accounts which are not being actively used, but which we don't want to disable. Do we still have to pay for them?",
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
+  },
+  {
+    question: 'My team has credits. How do we use them?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
+  },
+  {
+    question: 'We need to add new users to our team. How will that be billed?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
+  },
+  {
+    question:
+      'My team wants to cancel its subscription. How do we do that? Can we get a refund?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
+  },
+  {
+    question:
+      'Do you offer discounts for non-profit organizations or educational institutions?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo pariatur recusandae maiores molestias. Tempore nemo dolor debitis velit voluptate distinctio adipisci amet, laboriosam est dignissimos sequi expedita ab at molestias.'
+  }
+]
+
+export default Index
