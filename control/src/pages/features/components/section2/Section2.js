7a7de6c7a2db1ba7906e613c20d9ffb22f930319
Michellebuchiokonicha <michelleokonicha@gmail.com>
2021-09-23 10:37:42 +0100

    feature

--- control/src/pages/features/components/section2/Section2.js
+++ control/src/pages/features/components/section2/Section2.js
@@ -1,25 +1,29 @@
 import styles from '../../styles/section2.module.css'
-import image1 from '../../assets/i4g_image.png'
-import image2 from '../../assets/hng_image.png'
-import image3 from '../../assets/nucle_image.png'
-import image4 from '../../assets/zuri_internship_image.png'
-import image5 from '../../assets/zuri_i4g_image.png'
+import image1 from '../../assets/ing_image.png'
+import image3 from '../../assets/nucleio_image.png'
+import image5 from '../../assets/train_image.png'
+import image6 from '../../assets/hotels_image.png'
+import image7 from '../../assets/camp_image.png'
 
 const Section2 = () => {
   return (
+   
     <div className={styles.section2}>
+      <p>
+      Trusted by top companies worldwide
+      </p>
       <div className={styles.section2Wrapper}>
         <div className={styles.imageWrapper}>
-          <img src={image1} alt="imageu" />
+          <img src={image6} alt="imageh" />
         </div>
         <div className={styles.imageWrapper}>
-          <img src={image2} alt="imageo" />
+          <img src={image1} alt="imageu" />
         </div>
         <div className={styles.imageWrapper}>
           <img src={image3} alt="imagei" />
         </div>
         <div className={styles.imageWrapper}>
-          <img src={image4} alt="imageg" />
+          <img src={image7} alt="imagec" />
         </div>
         <div className={styles.imageWrapper}>
           <img src={image5} alt="imagen" />
