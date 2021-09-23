4eef2b3c0b8914bc9c413f58b99e504987a3bc3e
Obi Fortune <gabrielobi.of@gmail.com>
2021-09-23 06:19:52 +0100

    ran yarn format

--- control/src/pages/LandingPage/components/Subscribe.js
+++ control/src/pages/LandingPage/components/Subscribe.js
@@ -4,69 +4,65 @@ import sub_img1 from './assets/email-sub-img.svg'
 import axios from 'axios'
 
 const Subscribe = () => {
+  const [email, setEmail] = useState('')
 
-      const [email, setEmail] = useState('')
+  const handleSubmit = async event => {
+    event.preventDefault()
 
+    await axios
+      .post('https://api.zuri.chat/external/subscribe', { email })
+      .then(response => {
+        const { data, message } = response.data
+        console.log(response.data)
+        alert(message)
+      })
+      .catch(error => {
+        const { data } = error.response
+      })
+  }
 
-    const handleSubmit = async event => {
-        event.preventDefault()
+  const handleChange = event => setEmail(event.target.value)
 
-        await axios
-        .post('https://api.zuri.chat/external/subscribe', {email})
-        .then(response => {
-            const { data, message } = response.data
-            console.log(response.data)        
-            alert(message) 
-
-        })
-        .catch(error => {
-            const { data } = error.response
-          })
-        }
-    
-      const handleChange = event => setEmail(event.target.value);
-    
-    return (
-        <section className={`container my-5 ${css.sectionContainer}`}>
-            <div className={`row`}>
-                <div className={`col-md-6 col-sm-12 ${css.leftColumn}`}>
-                    <div className={`mr-5 ${css.image}`}>
-                        <img src={sub_img1}
-                            alt="Hero Top"
-                            className={`img-fluid`}
-                        />
-                    </div>                   
-                </div>
-                <div className={`col-md-6 col-sm-12 d-flex flex-column ${css.rightColumn}`}>
-                    <div className={`mx-3 ${css.text}`}>
-                        <h2 className={` ${css.heading2}`}>Be the First to get updates and exclusive offers from Zuri Chat</h2>
-                        <p className={`py-4 mb-4 px-0 ${css.subtext}`}>
-                            Enrich your Zuri Chat experience by signing up here to get the latest news and special deals on Zuri Chat
-                        </p>
-                        <div className={`${css.formContainer}`} >
-                    <form onSubmit={handleSubmit} >
-                        <input                         
-                            type="email"
-                            name="email"
-                            placeholder="jazeelandteam@yes.com"
-                            value={email}
-                            onChange={handleChange}
-                            required
-                            className={` ${css.inputField}`}
-                        />                        
-                        <button
-                            type="submit"
-                            className={`${css.subscribeBtn}`}
-                        >
-                            Subscribe
-                        </button>
-                        </form>
-                    </div>
-                    </div>                                   
-                </div> 
-            </div>                     
-        </section>
-    )
+  return (
+    <section className={`container my-5 ${css.sectionContainer}`}>
+      <div className={`row`}>
+        <div className={`col-md-6 col-sm-12 ${css.leftColumn}`}>
+          <div className={`mr-5 ${css.image}`}>
+            <img src={sub_img1} alt="Hero Top" className={`img-fluid`} />
+          </div>
+        </div>
+        <div
+          className={`col-md-6 col-sm-12 d-flex flex-column ${css.rightColumn}`}
+        >
+          <div className={`mx-3 ${css.text}`}>
+            <h2 className={` ${css.heading2}`}>
+              Be the First to get updates and exclusive offers from Zuri Chat
+            </h2>
+            <p className={`py-4 mb-4 px-0 ${css.subtext}`}>
+              Enrich your Zuri Chat experience by signing up here to get the
+              latest news and special deals on Zuri Chat
+            </p>
+            <div className={`${css.formContainer}`}>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="jazeelandteam@yes.com"
+                  value={email}
+                  onChange={handleChange}
+                  required
+                  className={` ${css.inputField}`}
+                />
+                <button type="submit" className={`${css.subscribeBtn}`}>
+                  Subscribe
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Subscribe
