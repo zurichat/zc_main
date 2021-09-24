8c6b0f646ca8adc5f0305ebec27a5a03898703dc
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-09-24 03:47:42 +0100

    Adjusted the logic for rendering the workspace header info

--- control/src/pages/workspace/components/WorkspaceCard.js
+++ control/src/pages/workspace/components/WorkspaceCard.js
@@ -8,9 +8,9 @@ const WorkspaceCard = () => {
 
   return (
     <section className={`${styles.workspace_card}`}>
-      <header className={`${styles.card_header}`}>
+      <header className={`${styles.card_header}`} style={{borderBottom: (!organizations.length && 'none')}}>
         <h4 className={`${styles.card_title}`}>
-          { !user.email ? 'Please login to view your workspaces' : !organizations.length ? 'No workspace found' : `Workspaces for ${user.email}`}
+          { !user.email ? 'Please login to view your workspaces' : !organizations.length ? 'No workspace found or session expired' : `Workspaces for ${user.email}`}
         </h4>
       </header>
       <div className={`${styles.singleworkspace_wrapper}`}>
