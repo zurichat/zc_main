2fb0f41f2c25cecd89949a73e757629899fe1648
Bori7 <oluwaboriife@gmail.com>
2021-09-24 20:40:20 +0100

    styling update for report feature

--- control/src/pages/ReportFeature/Admin/AdminPage.js
+++ control/src/pages/ReportFeature/Admin/AdminPage.js
@@ -1,44 +1,48 @@
 import React from 'react'
 import styles from './reports.module.css'
+import {Container} from 'react-bootstrap'
 
-export const AdminPage = props => {
-  const Continue = e => {
-    e.preventDefault()
-    props.nextStep(1)
-  }
+export const AdminPage = (props) => {
+    const Continue = e => {
+        e.preventDefault();
+        props.nextStep(1);
+    };
 
-  return (
-    <div className={styles.formcontainer}>
-      <div>
-        <span style={{ padding: '12px', marginLeft: '10px' }}>
-          <h1>View Complaint</h1>
-        </span>
+ 
+        return (
 
-        {props.len ? (
-          <b>Dear Admin, you have {props.len} complaint(s) to review</b>
-        ) : (
-          <p>Dear Admin, you have no complaints to review</p>
-        )}
-      </div>
+            <Container className={styles.formcontainer}>
+                <div>
+                    <span className='text-success' style={{ padding: "12px", marginLeft: "10px" }} ><h1>View Complaint</h1>
+                    </span>
 
-      <br />
-      <div className="text-right">
-        <button
-          className="btn btn-primary"
-          onClick={Continue}
-          style={{
-            color: 'white',
-            borderRadius: '10%',
-            backgroundColor: '#00B87C',
-            padding: '12px',
-            marginLeft: '15rem'
-          }}
-        >
-          View Complaint
-        </button>
-      </div>
-    </div>
-  )
-}
+                    {props.len ? 
+                    <b>
+                    Dear Admin, you have {props.len} complaint(s) to review
+                    </b>
+                    :
+                    <p>
+                    Dear Admin, you have no complaints to review
+                    </p>
+                
+                    }
+                    
+                </div>
+
+                <br />
+                <div className="text-right">
+                    <button className="btn btn-success" onClick={Continue}
+                        style={{
+                            color: "white", borderRadius: "5%",
+                            // backgroundColor: "#00B87C",
+                            // padding: "12px",
+                            marginLeft: "45%"
+                        }}  >View Complaint</button>
+                </div>
+            </Container >
+
+
+        )
+    }
 
 export default AdminPage
