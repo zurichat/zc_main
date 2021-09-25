c1b5bbc96580912c4ff714739c84ec9433af0110
Tech-sis <abilawonadeola@gmail.com>
2021-09-25 18:46:15 +0100

    new design of the download page
    Former-commit-id: deafc1413606d9a4eb611dbc58ef7389de40d9e9 [formerly c6e1ce04a22d11fc485301e758fdb7fcf6115b4d] [formerly 687411d01bf86864b13fe1feaa1194d08a393093 [formerly 228b3ca236f9d18d7a553e2768d4cc039c150afb [formerly 1e022e6ce0fe9b1009576c885b80b3e9c5f3e43c] [formerly 15b5c611c9f8be2137f1f8a66878cba24cb3a0dd]]]
    Former-commit-id: 05de9613e1e3846013fe2216b1460f04fec8a848 [formerly 228b3ca236f9d18d7a553e2768d4cc039c150afb [formerly 1e022e6ce0fe9b1009576c885b80b3e9c5f3e43c]]
    Former-commit-id: 8347aa0db8d0b2405a351c4174005320ef665ac1
    Former-commit-id: 8f9ddc246b99e27dce71b0ffe0e1bd49a193853b
    Former-commit-id: 313cfc34411fa1c328689f6f1d0e94a07c2db034

--- control/src/pages/download/DownloadsMac.js
+++ control/src/pages/download/DownloadsMac.js
@@ -29,76 +29,89 @@ const DownloadsMac = () => {
     <>
       <Header />
       <section className={``}>
-        <div
-          className={`${styles.container} `}>
+        <div className={`${styles.container} `}>
           <div className={`text-center p-lg-4`}>
-            <h1 className={`${styles.h1}`}>
-              Experience Zuri Chat on MacOS
-            </h1>
+            <h1 className={`${styles.h1}`}>Experience Zuri Chat on MacOS</h1>
             <p className={styles.p}>
               Connecting with your team has never been easier with Zuri Chat.
             </p>
           </div>
-          <div className={ `${styles.images} text-center p-lg-4 pb-lg-0`}>
+          <div className={`${styles.images} text-center p-lg-4 pb-lg-0`}>
             <img className={styles.circle} src={Ellipse159} alt="circle"></img>
             <img
               className={`${styles.screenshot}`}
               src={laptop}
               alt="app screenshot"
             ></img>
-            <div className={`${styles.buttondiv} p-lg-5 m-lg-5 mb-lg-0 p-md-3 m-md-3`}>
-               <a href={exe.link} download={exe.name} className={styles.button1}>
-                 <img className={`px-2`} src={icon} alt="download icon"></img>
-              DOWNLOAD
-            </a>
-             <Link className={`${styles.plink} mt-lg-4 d-inline`} to="./download-app">Download for Windows?</Link>
+            <div
+              className={`${styles.buttondiv} p-lg-5 m-lg-5 mb-lg-0 p-md-3 m-md-3`}
+            >
+              <a href={exe.link} download={exe.name} className={styles.button1}>
+                <img className={`px-2`} src={icon} alt="download icon"></img>
+                DOWNLOAD
+              </a>
+              <Link
+                className={`${styles.plink} mt-lg-4 d-inline`}
+                to="./download-app"
+              >
+                Download for Windows?
+              </Link>
             </div>
           </div>
         </div>
       </section>
 
-        <section className={`${styles.bg}`}>
-          <div
-            className={`row p-5 m-5 mt-0`}>
-            <div className={` ${styles.zuri} col`}>
-              <h6 className={`${styles.h6zuri}`}>
-                Why Zuri Chat App?
-              </h6>
-              <p className={`${styles.p1}`}>
-                Never miss the seamless collaboration
-                experience even on the go with the
-                zuri chat pc, <a href="#" className={`${styles.mobile}`}>Android</a> and <a href="#" className={`${styles.mobile}`}>iOS</a> apps.
-              </p>
-            </div>
-            <div className={`col`}>
-               <img
+      <section className={`${styles.bg}`}>
+        <div className={`row p-5 m-5 mt-0`}>
+          <div className={` ${styles.zuri} col`}>
+            <h6 className={`${styles.h6zuri}`}>Why Zuri Chat App?</h6>
+            <p className={`${styles.p1}`}>
+              Never miss the seamless collaboration experience even on the go
+              with the zuri chat pc,{' '}
+              <a href="#" className={`${styles.mobile}`}>
+                Android
+              </a>{' '}
+              and{' '}
+              <a href="#" className={`${styles.mobile}`}>
+                iOS
+              </a>{' '}
+              apps.
+            </p>
+          </div>
+          <div className={`col`}>
+            <img
               className={`${styles.screen}`}
               src={screen}
               alt="app screenshot"
             ></img>
-            </div>
           </div>
-        </section>
+        </div>
+      </section>
 
-        <section className={`row m-5 p-5`}>
-          <div className={` ${styles.zuri} col container`}>
-              <h1 className={ `${styles.h1} py-3`}>
-                Same feel, same team on Zuri App!
-              </h1>
-               <a href={exe.link} download={exe.name} className={styles.buttonW}>
-                  <img className={`px-2`} src={icon} alt="download icon"></img>
-              DOWNLOAD FOR MAC
-            </a>
-             <Link className={`${styles.plink} mt-lg-4 d-inline`} to="./download-app">Download for Windows?</Link>
-          </div>
-          <div className={`col px-0`}>
-              <img
-                className={`${styles.laptop}`}
-                src={laptop}
-                alt= "app screenshot"
-              ></img>
-          </div>
-        </section>
+      <section className={`row m-5 p-5`}>
+        <div className={` ${styles.zuri} col container`}>
+          <h1 className={`${styles.h1} py-3`}>
+            Same feel, same team on Zuri App!
+          </h1>
+          <a href={exe.link} download={exe.name} className={styles.buttonW}>
+            <img className={`px-2`} src={icon} alt="download icon"></img>
+            DOWNLOAD FOR MAC
+          </a>
+          <Link
+            className={`${styles.plink} mt-lg-4 d-inline`}
+            to="./download-app"
+          >
+            Download for Windows?
+          </Link>
+        </div>
+        <div className={`col px-0`}>
+          <img
+            className={`${styles.laptop}`}
+            src={laptop}
+            alt="app screenshot"
+          ></img>
+        </div>
+      </section>
 
       <Footer />
     </>
