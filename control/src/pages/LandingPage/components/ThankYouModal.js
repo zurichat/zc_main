cc9c7e0cadb437e16946917009f37ec3b07556d4
WumiDev <wumi2021@gmail.com>
2021-09-25 23:51:27 +0100

    implemented thank you modal display for email subscription

--- control/src/pages/LandingPage/components/ThankYouModal.js
+++ control/src/pages/LandingPage/components/ThankYouModal.js
@@ -3,38 +3,45 @@ import css from './styles/thankYouModal.module.css'
 import modal_img1 from './assets/modal-email-icon.svg'
 // import axios from 'axios'
 
-const ThankYouModal = ({displayModal, setDisplayModal}) => {
-    // const modalRef = useRef()
+const ThankYouModal = ({ displayModal, setDisplayModal }) => {
+  // const modalRef = useRef()
 
-    // const closeModal = event => {
-    //     if (modalRef.current !== event.target) {
-    //         setDisplayModal(false)
-    //     }
-    // }
-    
-    return (
-        <>
-            {displayModal ? (        
-                    <div className={`col-sm-12 d-flex flex-column ${css.modalBox}`}>
-                        <div className={`p-5 ${css.modalWrap}`}>
-                            <div className={`row ${css.modalRow}`}>
-                                <div className={`col-sm-12 d-flex flex-column ${css.modalColumn}`}>
-                                    <div className={`mx-auto ${css.modalIcon}`}>
-                                        <img src={modal_img1} alt="email icon" className={`img-fluid`} />
-                                    </div>
-                                    <h3 className={`text-center my-4 ${css.modalHeading}`}>
-                                        Thank you for subscribing! 
-                                    </h3>
-                                    <p className={`text-center ${css.modalText}`}>
-                                        Now, you'll be the first to receive updates and unique offers from the Zuri Chat team. 
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            ) : null}
-        </>    
-    )
+  // const closeModal = event => {
+  //     if (modalRef.current !== event.target) {
+  //         setDisplayModal(false)
+  //     }
+  // }
+
+  return (
+    <>
+      {displayModal ? (
+        <div className={`col-sm-12 d-flex flex-column ${css.modalBox}`}>
+          <div className={`p-5 ${css.modalWrap}`}>
+            <div className={`row ${css.modalRow}`}>
+              <div
+                className={`col-sm-12 d-flex flex-column ${css.modalColumn}`}
+              >
+                <div className={`mx-auto ${css.modalIcon}`}>
+                  <img
+                    src={modal_img1}
+                    alt="email icon"
+                    className={`img-fluid`}
+                  />
+                </div>
+                <h3 className={`text-center my-4 ${css.modalHeading}`}>
+                  Thank you for subscribing!
+                </h3>
+                <p className={`text-center ${css.modalText}`}>
+                  Now, you'll be the first to receive updates and unique offers
+                  from the Zuri Chat team.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  )
 }
 
 export default ThankYouModal
