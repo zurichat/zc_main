8bf6bc72030993896612aba540cafb5328ab88fb
Adams Lee <officialman776@gmail.com>
2021-09-25 22:40:38 +0100

    added section three of app and integrations page

--- control/src/pages/apps-integration/index.js
+++ control/src/pages/apps-integration/index.js
@@ -1,13 +1,16 @@
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 // import Categories from './components/Categories'
+
 import SectionOne from './components/SectionOne'
+import SectionThree from './components/SectionThree'
 
 function AppsAndIntegrations() {
   return (
     <div>
       <Header />
       <SectionOne />
+      <SectionThree />
       {/* <Categories /> */}
       <Footer />
     </div>
