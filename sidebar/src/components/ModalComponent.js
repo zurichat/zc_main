c21a03c0b65c87b6e8c9b64da80cb82053a81bfa
ehindero israel <iehindero419@stu.ui.edu.ng>
2021-09-28 17:41:36 +0100

    'startedWorkspaceModal'

--- sidebar/src/components/ModalComponent.js
+++ sidebar/src/components/ModalComponent.js
@@ -1,42 +1,32 @@
 import React, { useState } from 'react'
 import styles from '../styles/ModalComponentStyles.module.css'
 
+const ModalComponent = ({ isOpen }) => {
+  return (
+    <section className={`${isOpen ? styles.open : styles.topbarModal}`}>
+      <div className={styles.sectionOne}>The BrandHub</div>
 
-const ModalComponent = ({isOpen}) => {
-    
+      <div className={styles.sectionOne}>
+        Invite people to HNGi8 <br />
+        Create a new Workspace
+      </div>
 
-    return (
-        
-        <section 
-        className={`${isOpen ? styles.open : styles.topbarModal }`}
-        >
-        <div className={styles.sectionOne}>
-         The BrandHub
-        </div>
+      <div className={styles.sectionOne}>
+        Prefrence <br />
+        Customize HNGi8
+      </div>
 
-        <div className={styles.sectionOne}>
-            Invite people to HNGi8 <br />
-            Create a new Workspace
-        </div>
-
-        <div className={styles.sectionOne}>
-            Prefrence <br />
-            Customize HNGi8
-          </div>
-         
-        <div className={styles.sectionOne}>
-            Tools
-         </div>
-        <div className={styles.sectionOne}>
-             Sign Out
-        </div>
-         <div className={` d-flex flex-column align-items-start ${styles.sectionOne}`}>
-           <span> Add workspace </span>
-           <span>Switch workspace</span>
-            <span>Open the Zuri Chat App </span>
-         </div>
-      </section>
-    )
+      <div className={styles.sectionOne}>Tools</div>
+      <div className={styles.sectionOne}>Sign Out</div>
+      <div
+        className={` d-flex flex-column align-items-start ${styles.sectionOne}`}
+      >
+        <span> Add workspace </span>
+        <span>Switch workspace</span>
+        <span>Open the Zuri Chat App </span>
+      </div>
+    </section>
+  )
 }
 
 export default ModalComponent
--- sidebar/src/sidebar.component.js
+++ sidebar/src/sidebar.component.js
@@ -45,8 +45,8 @@ const Sidebar = props => {
   const [inviteEmail, setInviteEmail] = useState('')
   const [owner, setOwner] = useState(false)
   const [InviteSuccess, setInviteSuccess] = useState(false)
-  const [homeModal, toggleHomeModal] = useState(false) 
-  const toggle = () => toggleHomeModal(!homeModal);
+  const [homeModal, toggleHomeModal] = useState(false)
+  const toggle = () => toggleHomeModal(!homeModal)
 
   let currentWorkspace = localStorage.getItem('currentWorkspace')
   console.log(currentWorkspace)
@@ -192,234 +192,240 @@ const Sidebar = props => {
   return (
     <div className={`container-fluid ${styles.sb__container}`}>
       <div className={`${styles.subCon1}`}>
-      <div className={`row ${styles.orgDiv}`}>
-        <div className={`col-12 px-3 ${styles.orgInfo}`}>
-          <div onClick={toggle} className={`row p-0 ${styles.orgHeader}`}>
-            <p className={`col-6 mb-0 ${styles.orgTitle}`}>HNGi8</p>
-            <img
-              className={`col-6 mx-auto ${styles.arrowDown}`}
-              src={shapekeyboardarrowdown}
-              alt="HNGi8"
-            />
-          </div>
-          <div className={`row ${styles.newMessage}`}>
-            <img
-              className={`col-3 img-fluid w-100 ${styles.newMsgIcon}`}
-              src={newMsgIcon}
-              alt="message"
-            />
-          </div>
-        </div>  
-        <div className={`col-12 px-3 ${styles.odalContainer}`}>
-        <ModalComponent isOpen ={homeModal}/>
-        </div>
-  
-
-
-        <Modall showDialog={showDialog} closeDialog={close} />
-
-        <Overlay isOpen={showDialog} onDismiss={close}>
-          <Content aria-label="room-list">
-            <CloseButton className="close-button" onClick={close}>
-              <Span aria-hidden>×</Span>
-            </CloseButton>
-            <AuthInputBox
-              value={query}
-              setValue={setQuery}
-              placeholder="🔍 Search for plugins"
-            />
-            <Wrapper>
-              {loading && <p>Loading..</p>}
-              <p>
-                {links.map((plugs, id) => {
-                  return (
-                    <div key={id}>
-                      <Link to={plugs.href} onClick={navigateToUrl}>
-                        <p>{plugs.name}</p>
-                      </Link>
-                    </div>
-                  )
-                })}
-              </p>
-            </Wrapper>
-          </Content>
-        </Overlay>
-        <Overlay isOpen={openInvite} onDismiss={closeInviteModal}>
-          <Content
-            style={{ width: '55%', height: '55%' }}
-            aria-label="room-list"
-          >
-            <CloseButton className="close-button" onClick={closeInviteModal}>
-              <Span aria-hidden>×</Span>
-            </CloseButton>
-            <div>
-              <h3>Invite people to The Workspace</h3>
+        <div className={`row ${styles.orgDiv}`}>
+          <div className={`col-12 px-3 ${styles.orgInfo}`}>
+            <div onClick={toggle} className={`row p-0 ${styles.orgHeader}`}>
+              <p className={`col-6 mb-0 ${styles.orgTitle}`}>HNGi8</p>
+              <img
+                className={`col-6 mx-auto ${styles.arrowDown}`}
+                src={shapekeyboardarrowdown}
+                alt="HNGi8"
+              />
             </div>
-            {InviteSuccess && (
-              <div className={`alert alert-success`}>
-                Invite was sent to {inviteEmail}
-              </div>
-            )}
-            <div>
-              <label for="email_invite">To:</label>
+            <div className={`row ${styles.newMessage}`}>
+              <img
+                className={`col-3 img-fluid w-100 ${styles.newMsgIcon}`}
+                src={newMsgIcon}
+                alt="message"
+              />
             </div>
-            <Wrapper>
-              <div>
-                <input
-                  type="email"
-                  placeholder="example@gmail.com"
-                  multiple
-                  value={inviteEmail}
-                  onChange={e => setInviteEmail(e.target.value)}
-                  name="email_invite"
-                  className={`pb-4 form-control`}
-                />
-              </div>
-              <div
-                className={`mt-5 pt-3 d-flex my-auto justify-content-between`}
-              >
-                <p
-                  onClick={() => {
-                    window.navigator.clipboard.writeText(
-                      `https://zuri.chat/invite?organization=${currentWorkspace}`
+          </div>
+          <div className={`col-12 px-3 ${styles.odalContainer}`}>
+            <ModalComponent isOpen={homeModal} />
+          </div>
+
+          <Modall showDialog={showDialog} closeDialog={close} />
+
+          <Overlay isOpen={showDialog} onDismiss={close}>
+            <Content aria-label="room-list">
+              <CloseButton className="close-button" onClick={close}>
+                <Span aria-hidden>×</Span>
+              </CloseButton>
+              <AuthInputBox
+                value={query}
+                setValue={setQuery}
+                placeholder="🔍 Search for plugins"
+              />
+              <Wrapper>
+                {loading && <p>Loading..</p>}
+                <p>
+                  {links.map((plugs, id) => {
+                    return (
+                      <div key={id}>
+                        <Link to={plugs.href} onClick={navigateToUrl}>
+                          <p>{plugs.name}</p>
+                        </Link>
+                      </div>
                     )
-                    alert('link has been copied')
-                  }}
-                  className={`mb-0 align-items-center`}
-                  style={{ color: '#00B87C', fontSize: '13px' }}
-                >
-                  <img className={`pe-3`} src={linkIcon} />
-                  Copy invite link{' '}
+                  })}
                 </p>
-                <button
-                  onClick={() => inviteUser()}
-                  style={{ color: 'white', backgroundColor: '#00B87C' }}
-                  type="button"
-                  disabled={inviteEmail === '' ? true : false}
-                  className={`btn my-auto `}
-                >
-                  Send
-                </button>
+              </Wrapper>
+            </Content>
+          </Overlay>
+          <Overlay isOpen={openInvite} onDismiss={closeInviteModal}>
+            <Content
+              style={{ width: '55%', height: '55%' }}
+              aria-label="room-list"
+            >
+              <CloseButton className="close-button" onClick={closeInviteModal}>
+                <Span aria-hidden>×</Span>
+              </CloseButton>
+              <div>
+                <h3>Invite people to The Workspace</h3>
               </div>
-            </Wrapper>
-          </Content>
-        </Overlay>
-      </div>
+              {InviteSuccess && (
+                <div className={`alert alert-success`}>
+                  Invite was sent to {inviteEmail}
+                </div>
+              )}
+              <div>
+                <label for="email_invite">To:</label>
+              </div>
+              <Wrapper>
+                <div>
+                  <input
+                    type="email"
+                    placeholder="example@gmail.com"
+                    multiple
+                    value={inviteEmail}
+                    onChange={e => setInviteEmail(e.target.value)}
+                    name="email_invite"
+                    className={`pb-4 form-control`}
+                  />
+                </div>
+                <div
+                  className={`mt-5 pt-3 d-flex my-auto justify-content-between`}
+                >
+                  <p
+                    onClick={() => {
+                      window.navigator.clipboard.writeText(
+                        `https://zuri.chat/invite?organization=${currentWorkspace}`
+                      )
+                      alert('link has been copied')
+                    }}
+                    className={`mb-0 align-items-center`}
+                    style={{ color: '#00B87C', fontSize: '13px' }}
+                  >
+                    <img className={`pe-3`} src={linkIcon} />
+                    Copy invite link{' '}
+                  </p>
+                  <button
+                    onClick={() => inviteUser()}
+                    style={{ color: 'white', backgroundColor: '#00B87C' }}
+                    type="button"
+                    disabled={inviteEmail === '' ? true : false}
+                    className={`btn my-auto `}
+                  >
+                    Send
+                  </button>
+                </div>
+              </Wrapper>
+            </Content>
+          </Overlay>
+        </div>
       </div>
       <div className={`${styles.subCon2}`}>
-      <div className={`row mt-2 ${styles.sb__item}`}>
-        {owner && (
+        <div className={`row mt-2 ${styles.sb__item}`}>
+          {owner && (
+            <div
+              className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
+            >
+              <img
+                style={{ width: '10%' }}
+                className={`${styles.item__img}`}
+                role="button"
+                onClick={openInviteModal}
+                src={addIcon}
+                alt="icon"
+              />
+              <p
+                role="button"
+                onClick={openInviteModal}
+                className={`mb-0 ${styles.item_p}`}
+              >
+                Invite people to workspace
+              </p>
+            </div>
+          )}
+        </div>
+        <div className={`row mt-2 ${styles.sb__item}`}>
           <div
             className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
           >
             <img
-              style={{ width: '10%' }}
               className={`${styles.item__img}`}
-              role="button"
-              onClick={openInviteModal}
-              src={addIcon}
+              src={threadIcon}
               alt="icon"
             />
-            <p
-              role="button"
-              onClick={openInviteModal}
-              className={`mb-0 ${styles.item_p}`}
-            >
-              Invite people to workspace
-            </p>
+            <p className={`mb-0 ${styles.item_p}`}>Threads</p>
           </div>
-        )}
-      </div>
-      <div className={`row mt-2 ${styles.sb__item}`}>
-        <div
-          className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
-        >
-          <img className={`${styles.item__img}`} src={threadIcon} alt="icon" />
-          <p className={`mb-0 ${styles.item_p}`}>Threads</p>
         </div>
-      </div>
-      <div className={`row ${styles.sb__item}`}>
-        <div
-          className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
-        >
-          <img className={`${styles.item__img}`} src={dmIcon} alt="icon" />
-          <p className={`mb-0 ${styles.item_p}`}>All DMs</p>
+        <div className={`row ${styles.sb__item}`}>
+          <div
+            className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
+          >
+            <img className={`${styles.item__img}`} src={dmIcon} alt="icon" />
+            <p className={`mb-0 ${styles.item_p}`}>All DMs</p>
+          </div>
         </div>
-      </div>
-      <div className={`row ${styles.sb__item}`}>
-        <div
-          className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
-        >
-          <img className={`${styles.item__img}`} src={draftIcon} alt="icon" />
-          <p className={`mb-0 ${styles.item_p}`}>Drafts</p>
+        <div className={`row ${styles.sb__item}`}>
+          <div
+            className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
+          >
+            <img className={`${styles.item__img}`} src={draftIcon} alt="icon" />
+            <p className={`mb-0 ${styles.item_p}`}>Drafts</p>
+          </div>
         </div>
-      </div>
-      <div className={`row ${styles.sb__item}`}>
-        <div
-          className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
-        >
-          <img className={`${styles.item__img}`} src={filesIcon} alt="icon" />
-          <p className={`mb-0 ${styles.item_p}`}>Files</p>
+        <div className={`row ${styles.sb__item}`}>
+          <div
+            className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
+          >
+            <img className={`${styles.item__img}`} src={filesIcon} alt="icon" />
+            <p className={`mb-0 ${styles.item_p}`}>Files</p>
+          </div>
         </div>
-      </div>
-      <div className={`row ${styles.sb__item}`}>
-        <div
-          className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
-        >
-          <img className={`${styles.item__img}`} src={pluginIcon} alt="icon" />
-          <p className={`mb-0 ${styles.item_p}`}>Plugins</p>{' '}
-          <img
-            onClick={open}
-            className={`${styles.addButton}`}
-            src={addIcon}
-            alt="Add button"
-            role="button"
-          />
-        </div>
-      </div>
-
-      {/* <DropDown /> */}
-
-      {/* SIDE BAR DATA */}
-      {sidebarData &&
-        Object.keys(sidebarData).map((plugin, index) => {
-          return (
-            <DropDown
-              itemName={sidebarData[plugin].name}
-              id={sidebarData[plugin].name}
-              key={index}
-              items={sidebarData[plugin]}
+        <div className={`row ${styles.sb__item}`}>
+          <div
+            className={`col-12 ps-3 d-flex align-items-center ${styles.sb__col}`}
+          >
+            <img
+              className={`${styles.item__img}`}
+              src={pluginIcon}
+              alt="icon"
             />
-            // console.log()
+            <p className={`mb-0 ${styles.item_p}`}>Plugins</p>{' '}
+            <img
+              onClick={open}
+              className={`${styles.addButton}`}
+              src={addIcon}
+              alt="Add button"
+              role="button"
+            />
+          </div>
+        </div>
 
-            // <div key={index}>
-            //   <h5>{plugin.name}</h5>
+        {/* <DropDown /> */}
 
-            //   <ul>
-            //     {plugin.joined_rooms &&
-            //       plugin.joined_rooms.map((room, index) => {
-            //         if (room.room_name !== undefined) {
-            //           return (
-            //             <li key={index}>
-            //               <a
-            //                 style={{
-            //                   marginLeft: '5px',
-            //                   color: 'red'
-            //                 }}
-            //                 href={room.room_url}
-            //                 onClick={navigateToUrl}
-            //               >
-            //                 {room.room_name}
-            //               </a>
-            //             </li>
-            //           )
-            //         }
-            //       })}
-            //   </ul>
-            // </div>
-          )
-        })}
-      {/*
+        {/* SIDE BAR DATA */}
+        {sidebarData &&
+          Object.keys(sidebarData).map((plugin, index) => {
+            return (
+              <DropDown
+                itemName={sidebarData[plugin].name}
+                id={sidebarData[plugin].name}
+                key={index}
+                items={sidebarData[plugin]}
+              />
+              // console.log()
+
+              // <div key={index}>
+              //   <h5>{plugin.name}</h5>
+
+              //   <ul>
+              //     {plugin.joined_rooms &&
+              //       plugin.joined_rooms.map((room, index) => {
+              //         if (room.room_name !== undefined) {
+              //           return (
+              //             <li key={index}>
+              //               <a
+              //                 style={{
+              //                   marginLeft: '5px',
+              //                   color: 'red'
+              //                 }}
+              //                 href={room.room_url}
+              //                 onClick={navigateToUrl}
+              //               >
+              //                 {room.room_name}
+              //               </a>
+              //             </li>
+              //           )
+              //         }
+              //       })}
+              //   </ul>
+              // </div>
+            )
+          })}
+        {/*
         {roomInfo.rooms !== undefined &&
                 roomInfo.rooms.map(room => {
                   return (
@@ -463,9 +469,9 @@ const Sidebar = props => {
       )
           */}
 
-      {/* button for adding invites */}
+        {/* button for adding invites */}
 
-      {/* <Button
+        {/* <Button
         style={{
           width: '80%',
           margin: '0 auto',
@@ -475,7 +481,7 @@ const Sidebar = props => {
       >
         <LinkStyled to={'/createworkspace'}>Create Workspace</LinkStyled>{' '}
       </Button> */}
-    </div>
+      </div>
     </div>
   )
 }
