0c153fde66781915c554499aad99e2d15c24b3eb
DevSaalih <soliualley@gmail.com>
2021-09-29 10:26:19 +0100

    New SignOut Fix
    Former-commit-id: 478c7a1114abf78f78c3fad7cbb90f68787ff8c0 [formerly 189fb4a5c58def081410cb4bd47540ec6aa64731] [formerly f49a3d6047b2e32ae7c913d591a41ac75a03dfd1 [formerly 73ab804d5c2adb055ca7f00275d9e932519b2211 [formerly d357d6426be7591a89036ae5dcfe3b671def20fd] [formerly 69c2c7c91a2cadc778d9597c3208cb2738181eb7]]]
    Former-commit-id: 2e08aeb24efcfd352d948aa34f98ea0a86143fcd [formerly 73ab804d5c2adb055ca7f00275d9e932519b2211 [formerly d357d6426be7591a89036ae5dcfe3b671def20fd]]
    Former-commit-id: 93afb1b525da719ad124ec5c4dd2ba6fca3d2c46
    Former-commit-id: 3ec22af60b7e3d6b635d8151b1e0ec033fd34353
    Former-commit-id: e7126cc470c3902659e03661eaaf881e7a1aeaca

--- control/src/App.js
+++ control/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './pages/login'
 import SignUp from './pages/signup'
 import SignOut from './pages/signout/index'
-import SuccessSignout from './pages/signout/SuccessSignout'
 import Workspace from './pages/workspace/components/Workspace'
 import LandingPage from './pages/LandingPage'
 // password block
@@ -66,9 +65,6 @@ const App = () => {
         </Route>
         <Route path="/signout">
           <SignOut />
-        </Route>
-        <Route  path="/signout-success">
-          <SuccessSignout />
         </Route>
         <Route path="/choose-workspace">
           <Workspace />
--- control/src/pages/signout/index.js
+++ control/src/pages/signout/index.js
@@ -5,7 +5,7 @@ import styles from '../../component-styles/Signout.module.css'
 import { SignoutStyleWrapper } from '../../component-styles/SignoutStyle'
 import logo from '../../component-assets/zuri.svg'
 
-const Signout = () => {
+const Signout = ({history}) => {
   return (
     <>
       <SignoutStyleWrapper>
@@ -17,9 +17,9 @@ const Signout = () => {
           <h6>Signed out of Team Einstein Workspace</h6>
           <signoutMessage>You have been signed out of Team Einstein Workspace</signoutMessage>
 
-          <button>Login</button>
+          <button onClick={history.push('/home')}>Login</button>
           <p>
-            Or <a href="/login"> Login</a> to another workspace
+            or <a href="/login"> Login</a> to another workspace
           </p>
         </div>
       </SignoutStyleWrapper>
