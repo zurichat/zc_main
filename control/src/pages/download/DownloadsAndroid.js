077cb01ff53a15a6e12ef9128cb5516538542e98
Robert <orazurobert@yahoo.com>
2021-09-30 20:08:50 +0100

    mobile view of download page
    Former-commit-id: a66e3ea7468242a57bb1f5c0b062f9e75091b044

--- control/src/pages/download/DownloadsAndroid.js
+++ control/src/pages/download/DownloadsAndroid.js
@@ -4,47 +4,73 @@ import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import icon from './assets/icon.svg'
 import laptop from './assets/laptop.svg'
+import apple from './assets/apple.svg'
+import windows from './assets/windows.svg'
 
-import PlayStorelogo from './assets/playstore.png'
-
-import PhoneDM from './assets/PhoneDM.png'
-import PhoneChannel from './assets/PhoneChannels.png'
-
-// import Apk from './apk/appRelease.apk'
 
 const DownloadsAndroid = () => {
   const [exe, setexe] = useState({ link: '', name: '' })
-  // const [msi, setmsi] = useState({ link: '', name: '' })
-  // const [dmg, setdmg] = useState({ link: '', name: '' })
 
   return (
     <>
       <Header />
       <div className={downloadStyles.containermain}>
-        <div className={downloadStyles.container}>
-          <p className={downloadStyles.herotext}>
-            Experience the Zuri Chat’s desktop and mobile apps
+        <section className={downloadStyles.hero}>
+          <div className={downloadStyles.herotext}>
+            <p className={downloadStyles.herotextHeader}>
+              Experience the Zuri Chat’s desktop and mobile apps
+            </p>
+            <p className='mb-4'>Connecting with your team has never been easier with Zuri Chat.</p>
+            <a
+              href={exe.link}
+              download={exe.name}
+              className={downloadStyles.button1}
+            >
+              <img className={`px-2`} src={icon} alt="download icon"></img>
+              DOWNLOAD
+            </a>
+          </div>
+        </section>
+        <section className={`${downloadStyles.info}`}>
+          <p className={`h2 text-center font-weight-bold text-white mb-4`}>
+            Download and enjoy the Zuri Chat experience on any these devices
           </p>
-          <p>Connecting with your team has never been easier with Zuri Chat.</p>
+          <div className={`${downloadStyles.downloadButtonContainer}`}>
+            <a
+              href={exe.link}
+              download={exe.name}
+              className={downloadStyles.downloadButton}
+            >
+              Windows
+              <img className={`px-2`} src={windows} alt="download icon" />
+            </a>
+            <a
+              href={exe.link}
+              download={exe.name}
+              className={downloadStyles.downloadButton}
+            >
+              macOS
+              <img className={`px-2`} src={apple} alt="download icon" />
+            </a>
+         </div>
+         <div className={`${downloadStyles.downloadButtonContainer}`}>
           <a
             href={exe.link}
             download={exe.name}
-            className={downloadStyles.button1}
+            className={downloadStyles.downloadButton}
           >
-            <img className={`px-2`} src={icon} alt="download icon"></img>
-            DOWNLOAD
+            Android
+            <img className={`px-2`} src={windows} alt="download icon" />
+          </a>
+          <a
+            href={exe.link}
+            download={exe.name}
+            className={downloadStyles.downloadButton}
+          >
+            iOS
+            <img className={`px-2`} src={apple} alt="download icon" />
           </a>
         </div>
-        <section className={`${downloadStyles.info}`}>
-          <p className={`${downloadStyles.p1} text-center`}>
-            Download and enjoy the Zuri Chat experience on any these devices
-          </p>
-          <div>
-            <button>Windows</button>
-            <button>macOS</button>
-            <button>iOs</button>
-            <button>Android</button>
-          </div>
           <div className={`d-flex justify-content-center`}>
             <img
               className={`${downloadStyles.screen}`}
@@ -54,22 +80,22 @@ const DownloadsAndroid = () => {
           </div>
         </section>
         <section>
-          <div className={` ${downloadStyles.zuri} col container`}>
-            <h1 className={`${downloadStyles.heroheading} py-3`}>
+          <div className={`d-flex flex-column justify-content-center`}>
+            <h1 className={`${downloadStyles.ctaText}`}>
               Same feel, same team on Zuri App!
             </h1>
             <a
               href={exe.link}
               download={exe.name}
-              className={downloadStyles.button1}
+              className={`${downloadStyles.button2} text-center`}
             >
               <img className={`px-2`} src={icon} alt="download icon"></img>
               DOWNLOAD FOR WINDOWS
             </a>
           </div>
-          <div className={`col px-0`}>
+          <div className={`d-flex justify-content-center `}>
             <img
-              className={`${downloadStyles.laptop}`}
+              className={`${downloadStyles.screenshot}`}
               src={laptop}
               alt="app screenshot"
             ></img>
