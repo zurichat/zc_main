e50fe27487e968c22d8679e5548447193a1d4da8
deku <adedokunoluwaferanmi@gmail.com>
2021-09-30 07:42:30 +0100

    feat: changed marketplace cards, new success flow

--- control/src/pages/marketplace/components/marketplace-container/MarketplaceHeader.js
+++ control/src/pages/marketplace/components/marketplace-container/MarketplaceHeader.js
@@ -8,26 +8,31 @@ const MarketplaceHeader = () => {
   let user = JSON.parse(sessionStorage.getItem('user'))
   console.log(user?.first_name.slice(0, 2))
   return (
-    <nav
-      className={`navbar navbar-expand-lg navbar-light ${styles.navbar}`}
-    >
+    <nav className={`navbar navbar-expand-lg navbar-light ${styles.navbar}`}>
       <Link to="/" className={`navbar-brand`}>
         <img
           src={ZuriChatLogo}
           alt="zuri-logo"
           className={`d-inline-block align-top ${headerStyles.image}`}
-        />        
-      </Link>   
-        <div className={styles.accountOwnerAvatar}>  
-          { !user?.id && !currentWorkspace && <Link to="/login" className={styles.joinButton}>Login to An Account</Link>  }
-          { user?.id && !currentWorkspace && <Link to="/choose-workspace" className={styles.joinButton}>Join an Organization</Link> }
-          {
-            user?.id && currentWorkspace && 
-            <div className={styles.accountOwnerAvatarContainer}>
-              {user?.first_name.slice(0, 2).toUpperCase()}
-            </div>
-          }
-        </div>      
+        />
+      </Link>
+      <div className={styles.accountOwnerAvatar}>
+        {!user?.id && !currentWorkspace && (
+          <Link to="/login" className={styles.joinButton}>
+            Login to An Account
+          </Link>
+        )}
+        {user?.id && !currentWorkspace && (
+          <Link to="/choose-workspace" className={styles.joinButton}>
+            Join an Organization
+          </Link>
+        )}
+        {user?.id && currentWorkspace && (
+          <div className={styles.accountOwnerAvatarContainer}>
+            {user?.first_name.slice(0, 2).toUpperCase()}
+          </div>
+        )}
+      </div>
     </nav>
   )
 }
