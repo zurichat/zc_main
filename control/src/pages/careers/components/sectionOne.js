f5df0c69b9ae168f88e61b40f0afc6560661e5b9
agkayster <ejike.chiboka@gmail.com>
2021-10-01 11:29:02 +0100

    replaced group image, changed wordings in line with Figma styling, changed font color for middle border and removed last section

--- control/src/pages/careers/components/sectionOne.js
+++ control/src/pages/careers/components/sectionOne.js
@@ -2,21 +2,22 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 import styles from '../styles/sectionOne.module.css'
-import image from '../assets/section_one_image.svg'
+// import image from '../assets/section_one_image.svg'
+import groupImage from '../assets/group_image_sectionOne.svg'
 
 const SectionOne = () => (
   <div className={`${styles.Section1}`}>
     <div className={`${styles.section1Left}`}>
-      <h1>Work with us</h1>
+      <h1>Careers at Zuri Chat</h1>
       <p>
-        Explore flexible and remote-friendly opportunities, join our mission to
-        make work life easier, more convenient, and more productive.
+        Passionate about making work life simpler, and more productive? Join our
+        remote-friendly and flexible team.
       </p>
       <Link to="/signup">
         <a className={styles.JoinBtn}>Join us</a>
       </Link>
     </div>
-    <img src={image} alt="people" />
+    <img src={groupImage} alt="people" />
   </div>
 )
 
