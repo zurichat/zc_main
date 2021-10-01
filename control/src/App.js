f2e600497a7f1f0ba4080d2e93e239560630b8ed
Emmanuel Yusuf <seek9ja@gmail.com>
2021-10-01 20:01:26 +0100

    added update
    Former-commit-id: 6afa80aab125f84a281966bb6b71279cd962b3d3 [formerly d79ae2f86a18a3b486616d5bedaa13b98343d599] [formerly ea6fd724b424513338f4b9d1595b32a72f62ceea [formerly 6b37372ff94fb2d1bbe8e94a7fd156fb36c8e56a [formerly b14f3d22818b0d2e06513fdc7aba774b004e00ba] [formerly 5080e34e90e9085091e51de32c2a01660c6d61ef]]]
    Former-commit-id: 03ede082531fe30ac60bca02a2ae28bf6710d453 [formerly 6b37372ff94fb2d1bbe8e94a7fd156fb36c8e56a [formerly b14f3d22818b0d2e06513fdc7aba774b004e00ba]]
    Former-commit-id: 3b06a047e58182b0b6e1384e8828fa76cb516dc6
    Former-commit-id: 0028b2a82a5852308b69f0a064fdc55fec99b2dd
    Former-commit-id: 8185ee6a6640ed9b6bcb670694e32d183672f3b1

--- control/src/App.js
+++ control/src/App.js
@@ -35,6 +35,8 @@ import PrivacyPolicy from './pages/privacy-policy/index'
 import NotFoundPage from './pages/404'
 import About from './pages/about/index'
 import Billing from './pages/admin/Billing'
+import Permissions from './pages/admin/Permissions'
+import SettingsHome from './pages/admin/Home'
 
 // useEffect(() => {
 //     localStorage.setItem('input',input);
@@ -126,7 +128,9 @@ const App = () => {
         />
         <PrivateRoute path="/session-signout" component={AllSessionSignOut} />
         <PrivateRoute path="/settings" exact component={Settings} />
-        <PrivateRoute path="/admin/settings" exact component={Billing} />
+        <PrivateRoute path="/admin/settings" exact component={SettingsHome} />
+        <PrivateRoute path="/admin/settings/billings" exact component={Billing} />
+        <PrivateRoute path="/admin/settings/permission" exact component={Permissions} />
         <PrivateRoute path="/settings/:id" component={ConfirmPassword} />
         {/* ----------------settings routes closed----------------- */}
         <Route path="/reset-password">
