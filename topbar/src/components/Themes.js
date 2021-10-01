6c367ea3146fd7a0c314159860bb7ea65716aad8
DevSaalih <soliualley@gmail.com>
2021-10-01 05:19:44 +0100

    Axios import Fix
    Former-commit-id: f4b05337cc6f0bafb429af289726cfb9abdf181b [formerly eefd479f03a68ca9824d64694a1d289e1a948645] [formerly bf9da7b7c52c37f4aa41ee47e92589a1d77eb531 [formerly 7faf4f0fb997a8cf04608f6609747608dc8a2853 [formerly 695633f1687fbf1b658ea5e5bbf4efba67763143] [formerly 85e5d2b6575293ad2bbc8afa4dcb1de311a11273]]]
    Former-commit-id: e8e26380da30db1bb3a0ee2c3c5f54ac58a487f4 [formerly 7faf4f0fb997a8cf04608f6609747608dc8a2853 [formerly 695633f1687fbf1b658ea5e5bbf4efba67763143]]
    Former-commit-id: 3715635e9971cb38c614d96a0ecb717d39a854e1
    Former-commit-id: 796bc07ff9d33e1f15eff05c29bfd60296419cf5
    Former-commit-id: ad47f9b459b3759085f4bb1b0144396e38900191

--- topbar/src/components/Themes.js
+++ topbar/src/components/Themes.js
@@ -14,24 +14,22 @@ import { useState, useContext, useEffect } from 'react'
 import { authAxios } from '../utils/Api'
 import { ProfileContext } from '../context/ProfileModal'
 
-
 const Themes = () => {
-  const [mode, setMode] = useState(() => localStorage.getItem("mode"));
+  const [mode, setMode] = useState(() => localStorage.getItem('mode'))
 
   const [color, setColor] = useState('light')
 
-    useEffect(() => {
-       if(mode === "dark"){
-          document.body.classList.add("darkmode");
-          localStorage.setItem("mode", "dark");
-       }else{
-          document.body.classList.remove("darkmode");
-          localStorage.setItem("mode", "light");
-       }
-    }, [mode])
+  useEffect(() => {
+    if (mode === 'dark') {
+      document.body.classList.add('darkmode')
+      localStorage.setItem('mode', 'dark')
+    } else {
+      document.body.classList.remove('darkmode')
+      localStorage.setItem('mode', 'light')
+    }
+  }, [mode])
 
-
-  const { user} = useContext(ProfileContext)
+  const { user } = useContext(ProfileContext)
 
   const [dataState, setDataState] = useState({
     // channel_hurdle_notification: channel_hurdle,
@@ -41,11 +39,14 @@ const Themes = () => {
 
   const setData = () => {
     authAxios
-      .patch(`/organizations/6137d69b21d3c78fc9a84bdf/members/6137d69b21d3c78fc9a84bdf/settings`, {
-        settings: {
-          themes: dataState
+      .patch(
+        `/organizations/6137d69b21d3c78fc9a84bdf/members/6137d69b21d3c78fc9a84bdf/settings`,
+        {
+          settings: {
+            themes: dataState
+          }
         }
-      })
+      )
       .then(res => {
         console.log(res)
         setState({ loading: false })
@@ -89,102 +90,105 @@ const Themes = () => {
 
   return (
     <body className={styles.darkmode}>
-    <div className={styles.themeCont}>
-      <div className={styles.title}>
-        <div className={styles.text}>
-          Change the appearance of Slack across all of your workspaces.
+      <div className={styles.themeCont}>
+        <div className={styles.title}>
+          <div className={styles.text}>
+            Change the appearance of Slack across all of your workspaces.
+          </div>
         </div>
-      </div>
-      <div className={styles.sync}>
+        <div className={styles.sync}>
           <div className={styles.checkbox}>
             <input
               type="checkbox"
               name="sync"
               onClick={() => {
-                setDataState({sync_with_os: 'yes'})
+                setDataState({ sync_with_os: 'yes' })
                 setData()
               }}
             />
           </div>
-        <div className={styles.os}>Sync with OS setting</div>
-      </div>
-      <div className={styles.direct}>
+          <div className={styles.os}>Sync with OS setting</div>
+        </div>
+        <div className={styles.direct}>
           <div className={styles.radio}>
             <input
               type="checkbox"
               name="sync"
               onClick={() => {
-                setDataState({direct_messages_mentions_and_network: 'yes'})
+                setDataState({ direct_messages_mentions_and_network: 'yes' })
                 setData()
               }}
             />
           </div>
-        <div className={styles.mention}>
-          Direct messages, mentions &amp; network
+          <div className={styles.mention}>
+            Direct messages, mentions &amp; network
+          </div>
         </div>
-      </div>
-      <div className={styles.text2}>
-        Automatically switch between light and dark themes
-      </div>
-      <div className={styles.text2b}>when your system does.</div>
-        <a className="cursor-pointer" onClick={() => setMode(mode => (mode === "dark" ? "light" : "dark"))}>
-          <small>{mode === "dark" ? "Light" : "Dark"} Mode</small>
+        <div className={styles.text2}>
+          Automatically switch between light and dark themes
+        </div>
+        <div className={styles.text2b}>when your system does.</div>
+        <a
+          className="cursor-pointer"
+          onClick={() => setMode(mode => (mode === 'dark' ? 'light' : 'dark'))}
+        >
+          <small>{mode === 'dark' ? 'Light' : 'Dark'} Mode</small>
         </a>
         <div className={styles.img}>
           <img src={theme1} alt="theme1" className={styles.theme1} />
         </div>
-      <div className={styles.img2}>
-        <img src={theme2} alt="theme2" className={styles.theme2} />
-      </div>
-      <div className={styles.customize}>
-        <div className={styles.text3}>Colors</div>
-        <div className={styles.custom}>
-          Customize the look of your workspace. Feeling
+        <div className={styles.img2}>
+          <img src={theme2} alt="theme2" className={styles.theme2} />
         </div>
-        <div className={styles.custom2}>adventurous?</div>
-        <div className={styles.create}>Create a custom theme</div>
-        <div className={styles.true}>Tried and true</div>
-      </div>
-      <div className={styles.set1}>
-        <div className={styles.img3}>
-          <img src={theme3} alt="theme3" className={styles.theme3} />
-        </div>
-        <div className={styles.img4}>
-          <img src={theme4} alt="theme4" className={styles.theme4} />
-        </div>
-      </div>
-      <div className={styles.all}>
-        <div className={styles.arrow}>↓</div>
-        <div className={styles.show}>Show all classic themes</div>
-      </div>
-      <div className={styles.clean}>Clean and minimal</div>
-      <div className={styles.set2}>
-        <div className={styles.img5}>
-          <img src={theme5} alt="theme5" className={styles.theme5} />
-        </div>
-        <div className={styles.img6}>
-          <img src={theme6} alt="theme6" className={styles.theme6} />
-        </div>
-      </div>
-      <div className={styles.set3}>
-        <div className={styles.img7}>
-          <img src={theme7} alt="theme7" className={styles.theme7} />
-        </div>
-        <div className={styles.img8}>
-          <img src={theme8} alt="theme8" className={styles.theme8} />
-        </div>
-      </div>
-      <div className={styles.set4}>
-        <div className={styles.bottom}>
-          <div className={styles.img9}>
-            <img src={theme9} alt="theme9" className={styles.theme9} />
+        <div className={styles.customize}>
+          <div className={styles.text3}>Colors</div>
+          <div className={styles.custom}>
+            Customize the look of your workspace. Feeling
           </div>
-          <div className={styles.img10}>
-            <img src={theme10} alt="theme10" className={styles.theme10} />
+          <div className={styles.custom2}>adventurous?</div>
+          <div className={styles.create}>Create a custom theme</div>
+          <div className={styles.true}>Tried and true</div>
+        </div>
+        <div className={styles.set1}>
+          <div className={styles.img3}>
+            <img src={theme3} alt="theme3" className={styles.theme3} />
+          </div>
+          <div className={styles.img4}>
+            <img src={theme4} alt="theme4" className={styles.theme4} />
           </div>
         </div>
+        <div className={styles.all}>
+          <div className={styles.arrow}>↓</div>
+          <div className={styles.show}>Show all classic themes</div>
+        </div>
+        <div className={styles.clean}>Clean and minimal</div>
+        <div className={styles.set2}>
+          <div className={styles.img5}>
+            <img src={theme5} alt="theme5" className={styles.theme5} />
+          </div>
+          <div className={styles.img6}>
+            <img src={theme6} alt="theme6" className={styles.theme6} />
+          </div>
+        </div>
+        <div className={styles.set3}>
+          <div className={styles.img7}>
+            <img src={theme7} alt="theme7" className={styles.theme7} />
+          </div>
+          <div className={styles.img8}>
+            <img src={theme8} alt="theme8" className={styles.theme8} />
+          </div>
+        </div>
+        <div className={styles.set4}>
+          <div className={styles.bottom}>
+            <div className={styles.img9}>
+              <img src={theme9} alt="theme9" className={styles.theme9} />
+            </div>
+            <div className={styles.img10}>
+              <img src={theme10} alt="theme10" className={styles.theme10} />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </body>
   )
 }
