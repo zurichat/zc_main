db2ff8e666f55bc0ab343665cf2ecf2f8da9b757
DevSaalih <soliualley@gmail.com>
2021-10-02 17:52:08 +0100

    Code Refactoring
    Former-commit-id: e823a77276bd3218b12de51a4ea55b4b83a7c5ca

--- control/src/pages/signout/index.js
+++ control/src/pages/signout/index.js
@@ -1,13 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import axios from 'axios'
-// import {GetUserInfo} from '../../zuri-control'
 import { SignoutStyleWrapper } from '../../component-styles/SignoutStyle'
 import logo from '../../component-assets/zuri.svg'
 import { Helmet } from 'react-helmet'
 
 const Signout = ({ history }) => {
   const orgName = localStorage.getItem('orgName')
+
+
+  const currentWorkspace = localStorage.getItem('currentWorkspace')
+  let token = sessionStorage.getItem('token')
+
+  const logout = () => {
+    axios({
+      method: 'post',
+      url: `https://api.zuri.chat/auth/logout`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+  useEffect(() => {
+    let user = JSON.parse(sessionStorage.getItem('user'))
+
+    if ((user && token) !== null) {
+       logout();
+    
+    } else {
+      console.log('YOU ARE NOT LOGGED IN, PLEASE LOG IN')
+    }
+  }, [orgName])
+
+ 
+ 
 
   return (
     <>
