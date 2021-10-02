110edf4a47766bc928420141444c7d712ead504b
Michellebuchiokonicha <michelleokonicha@gmail.com>
2021-10-02 22:58:58 +0100

    browser notifications
    Former-commit-id: 867e289aefac9a117acbbea83da2a746acc0b7e9

---
+++ control/src/browser-notifications/browserNotification2.js
@@ -0,0 +1,30 @@
+import React from "react";
+import usePushNotifications from "./browser-notifications/browserNotification3";
+
+
+
+export default function PushNotificationDemo() {
+  const {
+    userConsent,
+    pushNotificationSupported,
+    // userSubscription,
+    onClickAskUserPermission,
+    // onClickSusbribeToPushNotification,
+    // onClickSendSubscriptionToPushServer,
+    // pushServerSubscriptionId,
+    // onClickSendNotification
+  } = usePushNotifications();
+
+  const isConsentGranted = userConsent === "granted";
+
+  return (
+    <main>
+
+      <button enabled={!pushNotificationSupported || isConsentGranted} onClick={onClickAskUserPermission}>
+        {isConsentGranted ? "Show Notifications" : "Show Notifications"} 
+      </button>
+
+     
+    </main>
+  );
+}
