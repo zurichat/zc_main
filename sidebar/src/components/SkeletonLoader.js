c526b14f96be5d88268f9394d6646fd8e5163837
Godson <imiebo.godson@gmail.com>
2021-10-03 00:15:05 +0100

    Changed the loading feature of the sidebar
    Former-commit-id: 1d658ad3d20cb8481218f64cdc9281bff5891212 [formerly b338a52121d38e17d0a5b0813fd24e150492a098] [formerly b8f855e9cca8b59653f258f06d92a44deab2a946 [formerly 17eafe4585f5f791763e01f2163d371c165b9611 [formerly e6d574c76d4c570ae3237bf56f80b9c80348db6f] [formerly 832c7d7a62fc9cbc9cf2aeeefe40ff0a0336d1a0]]]
    Former-commit-id: 436469b5bdc8c346e9c282b080725c87f412e6ce [formerly 17eafe4585f5f791763e01f2163d371c165b9611 [formerly e6d574c76d4c570ae3237bf56f80b9c80348db6f]]
    Former-commit-id: 035da3cf35ba59961fbf43812035caca1c408824
    Former-commit-id: e8756b64fb95b7a714b3d4c1fefd9dcfffe96d8d
    Former-commit-id: b50e0a56512ecef8c1dc68221a1747f266b30686

--- sidebar/src/components/SkeletonLoader.js
+++ sidebar/src/components/SkeletonLoader.js
@@ -2,9 +2,14 @@ import React from 'react'
 import styles from '../styles/skeletonLoader.module.css'
 
 export default function SkeletonLoader() {
-  return (
-    <div className={styles.skeleton_loader}>
-      <div className={styles.skeleton_animation}></div>
+   const COUNTER = 8;
+
+   const SideBarSkeleton = () => (
+    <div className={styles.skeletonLoaderWrapper}>
+      <div className={styles.skeletonLoaderLeftDiv}></div>
+      <div className={styles.skeletonLoaderRightDiv}></div>
     </div>
-  )
+   );
+
+  return Array(COUNTER).fill(<SideBarSkeleton />);
 }
