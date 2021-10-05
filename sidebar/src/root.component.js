4479157dfa6616ca46c419812a9033c5ea37f6d0
benjamin-bala <benjamin.bala91@gmail.com>
2021-10-05 08:31:10 -0400

    Refactored get plugins on sidebar
    Former-commit-id: 4b359d8132f30d5b5056d01d9aad63dff545b128

--- sidebar/src/root.component.js
+++ sidebar/src/root.component.js
@@ -1,10 +1,56 @@
 import Sidebar from './sidebar.component'
 import { BrowserRouter } from 'react-router-dom'
+import { useReducer } from 'react'
+
+export const ACTIONS = {
+  ADD_USER_INFO: 'add-user-info',
+  ADD_ITEM: 'add',
+  UPDATE_ITEM: 'update',
+  ADD_ORGANIZATION_EMAIL: 'add-org-email'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_ITEM:
+      //Add items to sidebar
+      let sidebar_data = action.payload
+      return {
+        ...state,
+        sidebar: {
+          ...state.sidebar,
+          ...sidebar_data
+        }
+      }
+    case ACTIONS.UPDATE_ITEM:
+      //Update sidebar
+      console.log('can update')
+      return state
+
+    case ACTIONS.ADD_USER_INFO:
+      //Add user info
+      let userInfo = action.payload
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...userInfo
+        }
+      }
+    case ACTIONS.ADD_ORGANIZATION_EMAIL:
+      //set organization info
+      let organizationEmail = action.payload
+      return { ...state, organization_email: organizationEmail }
+    default:
+      return state
+  }
+}
 
 export default function Root(props) {
+  const [state, dispatch] = useReducer(reducer, {})
+
   return (
     <BrowserRouter>
-      <Sidebar />
+      <Sidebar state={state} dispatch={dispatch} />
     </BrowserRouter>
   )
 }
