bf45744bfc47c10607cf21f0168e0a90399541cf
Obi Fortune <gabrielobi.of@gmail.com>
2021-10-06 22:04:42 +0300

    linted in control
    Former-commit-id: ae7cb674c0b48ca0585b5d528039a186ccdf6fba

--- control/src/pages/admin/Settings/components/DefaultChannel.js
+++ control/src/pages/admin/Settings/components/DefaultChannel.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react"
+import axios from "axios"
 
 const myStyle = {
   inputfield: {
-    margin: '5px 0',
-    width: '50%'
+    margin: "5px 0",
+    width: "50%"
   },
   btn: {
-    backgroundColor: '#00B87C',
-    color: '#ffffff',
-    border: 'none'
+    backgroundColor: "#00B87C",
+    color: "#ffffff",
+    border: "none"
   }
 }
 
 const modifyOrganizationSettings = () => {
-  let organizationid = '6158c1ac3a1e49b1e5165df3'
-  let defaultchannels = ''
-  let token = localStorage.getItem('token')
-  axios.defaults.baseURL = 'https://api.zuri.chat'
+  let organizationid = "6158c1ac3a1e49b1e5165df3"
+  let defaultchannels = ""
+  let token = localStorage.getItem("token")
+  axios.defaults.baseURL = "https://api.zuri.chat"
   axios.defaults.headers.common = { Authorization: `bearer ${token}` }
   axios({
-    method: 'patch',
-    url: '/organizations/' + organizationid + '/settings',
+    method: "patch",
+    url: "/organizations/" + organizationid + "/settings",
     data: {
       defaultchannels: defaultchannels
     }
@@ -32,7 +33,7 @@ const DefaultChannels = () => {
     <>
       <input
         type="text"
-        class="form-control"
+        className="form-control"
         id=""
         placeholder="keep all files"
         style={myStyle.inputfield}
