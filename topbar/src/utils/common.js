6c363c90d9149b8f8eac675c70c1fed91e686c7b
Obi Fortune <gabrielobi.of@gmail.com>
2021-10-06 19:09:43 +0300

    js linting on topbar
    Former-commit-id: 295c82abdcc885713a7baf9de16f4d17df19fd3c

--- topbar/src/utils/common.js
+++ topbar/src/utils/common.js
@@ -1,18 +1,18 @@
-import { authAxios } from './Api'
+import { authAxios } from "./Api"
 
 // return the token from the session storage
 export const getToken = () => {
-  return sessionStorage.getItem('token') || null
+  return sessionStorage.getItem("token") || null
 }
 
 export const getUser = () => {
-  const result = JSON.parse(sessionStorage.getItem('user') || null)
+  const result = JSON.parse(sessionStorage.getItem("user") || null)
   return result
 }
 
 // get Current Workspace
 export const getCurrentWorkspace = () => {
-  const currentWorkspace = localStorage.getItem('currentWorkspace') || null
+  const currentWorkspace = localStorage.getItem("currentWorkspace") || null
   return currentWorkspace
 }
 
@@ -22,11 +22,11 @@ export const getCurrentWorkspaceData = () => {
     authAxios
       .get(`/organizations/${getCurrentWorkspace()}`)
       .then(res => {
-        console.log(res.data.data)
+        // console.log(res.data.data)
         return res.data.data
       })
       .catch(err => {
-        console.log(err)
+        console.error(err)
       })
   }
 }
