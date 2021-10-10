06c08c1a36fde17636e74fdce8178c80efe024f4
Tech-sis <abilawonadeola@gmail.com>
2021-10-10 17:49:31 +0100

    created terms of service page and linked to create account

--- control/src/App.js
+++ control/src/App.js
@@ -39,6 +39,7 @@ import Permissions from './pages/admin/Permissions'
 import SettingsHome from './pages/admin/Settings/index'
 import About from './pages/about/index'
 import AdminSettingsTab from './pages/admin/Settings/components/AdminSettingsTab'
+import TermsOfService from './pages/termsOfService'
 
 // useEffect(() => {
 //     localStorage.setItem('input',input);
@@ -109,6 +110,9 @@ const App = () => {
         </Route>
         <Route path="/privacy">
           <PrivacyPolicy />
+        </Route>
+        <Route path="/terms">
+          <TermsOfService />
         </Route>
         <Route path="/marketplace" exact>
           <MarketPlace />
