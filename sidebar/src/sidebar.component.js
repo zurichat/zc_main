bb0e1d73cea0ea2102add7765fee2e6939ba769a
ehindero israel <iehindero419@stu.ui.edu.ng>
2021-10-11 11:49:31 +0100

    ''
    Former-commit-id: 2139d3459e65fecd9ed0a4d5ee2c16a669481a7a

--- sidebar/src/sidebar.component.js
+++ sidebar/src/sidebar.component.js
@@ -79,8 +79,6 @@ const Sidebar = props => {
   //organization
   const [org, setOrg] = useState({})
   
-  //set user worksapces
-  const [orgs, setOrgs] = useState([])
 
   //toggle
   const toggle = () => {
@@ -112,7 +110,7 @@ const Sidebar = props => {
   //   // console.log('userinfo', userInfo)
 
   const [nullValue, setnullValue] = useState(0)
-  const [nullValue2, setnullValue2] = useState(0)
+  
   
   const [organizationInfo, setOrganizationInfo] = useState(null)
   const [sidebarData, setSidebarData] = useState({})
@@ -150,22 +148,6 @@ const Sidebar = props => {
     })
   }, [])
 
-  //get all user workspaces
-  useEffect(()=>{
-
-    axios.get(
-      `https://api.zuri.chat/users/${user_id_session.email}/organizations`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }
-    ).then(res => {
-      const orgs = res.data.data
-      setOrgs(orgs)
-    })   
-     setnullValue2(1)
-    },[])
 
 
   useEffect(() => {
@@ -234,7 +216,6 @@ const Sidebar = props => {
 
   {
     nullValue === 1 &&
-    nullValue2 === 1 &&
       currentWorkspace &&
       userInfo.userId &&
       SubscribeToChannel(
@@ -281,7 +262,6 @@ const Sidebar = props => {
           <div className={`col-12 px-3 ${styles.modalContainer}`}>
             <div className={`col-12 px-3 ${styles.odalContainer}`}>
               <ModalComponent
-                orgs={orgs}
                 workSpace={org}
                 isOpen={homeModal}
                 toggleOpenInvite={toggleOpenInvite}
