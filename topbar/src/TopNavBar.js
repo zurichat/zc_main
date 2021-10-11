623432936eaac2dc319bb31844004d4ad81c70b0
Job Oaikhenah <oaikhenahjob@gmail.com>
2021-10-12 00:48:16 +0100

    update

--- topbar/src/TopNavBar.js
+++ topbar/src/TopNavBar.js
@@ -241,7 +241,7 @@ const TopNavBar = ({ userProfile: { last_name, first_name } }) => {
         className="d-flex justify-content-end align-items-center pe-3"
         style={{ width: "20%" }}
       >
-        <IoLanguageOutline size={30} style={{ border: "1px #A1A1A1 solid", borderRadius: "50%", cursor: "pointer", padding: '.15rem', marginRight: "10%"}} />
+        <IoLanguageOutline size={30} style={{ border: "1px #A1A1A1 solid", borderRadius: "50%", cursor: "pointer", padding: '.15rem', marginRight: "10%", marginTop: ".1rem" }} />
         {toggleStatus}
         <ProfileImg
           src={userProfileImage ? userProfileImage : defaultAvatar}
