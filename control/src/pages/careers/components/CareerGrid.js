053beda3d9e04a4e3d8b5f36ddb3f5aa3f617b14
Muwaffaq <71173373+mowfaqqa@users.noreply.github.com>
2021-10-12 09:45:46 +0100

    Update CareerGrid.js
    Former-commit-id: ad14e5fed56ae6edee2c075403fdde3e18d4c397 [formerly 55a4d6f286b1f2c8838b624875f60f20398cba89] [formerly 5ef8c2b976373b61190d01a081f41102b9c11a19 [formerly f0cf5ead6631b061f98d827c4da12b0f554cf81b [formerly 495919acc547a959dcfa3c75292f40301c4a3a2a] [formerly 978c959e13636be97f596b175cd2237c47fb5f20]]]
    Former-commit-id: 7e7304111b08c7a7c40d0fea984093d3e8833589 [formerly f0cf5ead6631b061f98d827c4da12b0f554cf81b [formerly 495919acc547a959dcfa3c75292f40301c4a3a2a]]
    Former-commit-id: c44a9eeded9abd6a39d62dc9d3e71d8049980647
    Former-commit-id: db229afebf66dc04cdb9b7834b3d9239b82ed1d2
    Former-commit-id: a9f7bae9d31dd12ab396628eecaafcb563a0390d

--- control/src/pages/careers/components/CareerGrid.js
+++ control/src/pages/careers/components/CareerGrid.js
@@ -11,7 +11,7 @@ const CareerGrid = () => {
          <div className={`${styles.categories}`}>
             <div className={`${styles.category}`}>
                <div className={`${styles.subcategory}`}>
-                  <h4>Frontend Development</h4>
+                  <h3>Frontend Development</h3>
                   <p>This track deals with the aspect of your application that the users interact with. A front-end developer will be able to correctly interpret a given design to the user interface.</p>
                </div>
                <div className={`${styles.flex}`}>
@@ -22,7 +22,7 @@ const CareerGrid = () => {
             </div>
             <div className={`${styles.category}`}>
                <div className={`${styles.subcategory}`}>
-                  <h4>Backend Development</h4>
+                  <h3>Backend Development</h3>
                   <p>This track deals with the aspect of the application the user does not directly interact with. It allows the frontend function either by running some back-end code or connecting to the database.</p>
                </div>
                <div className={`${styles.flex}`}>
@@ -34,7 +34,7 @@ const CareerGrid = () => {
             </div>
             <div className={`${styles.category}`}>
                <div className={`${styles.subcategory}`}>
-                  <h4}>Design</h4>
+                  <h3>Design</h3>
                   <p>This track deals with creating a graphic plan for an application. You will learn to convert project documentation into viewable and understandable graphic design for the developers to work with</p>
                </div>
                <div className={`${styles.flex}`}>
@@ -43,7 +43,7 @@ const CareerGrid = () => {
             </div>
             <div className={`${styles.category}`}>
                <div className={`${styles.subcategory}`}>
-                  <h4>Mobile Development</h4>
+                  <h3>Mobile Development</h3>
                   <p>The mobile track contains a list of tools and languages needed for a developer to create mobile applications. A mobile application can be IOS or Android.</p>
                </div>
                <div className={`${styles.flex}`}>
@@ -53,7 +53,7 @@ const CareerGrid = () => {
             </div>
             <div className={`${styles.category}`}>
                <div className={`${styles.subcategory}`}>
-                  <h4>Digital Marketing</h4>
+                  <h3>Digital Marketing</h3>
                   <p>It encomprises the use of digital means and utilities to boost discoverability of products, services and brands.</p>
                </div>
                <div className={`${styles.flex}`}>
@@ -62,7 +62,7 @@ const CareerGrid = () => {
             </div>
             <div className={`${styles.category}`}>
                <div className={`${styles.subcategory}`}>
-                  <h4>Frameworks</h4>
+                  <h3>Frameworks</h3>
                   <p>During the program we will be exploring several useful frameworks to give you an edge in the industry and help you develop complex applications much faster</p>
                </div>
                <div className={`${styles.flex}`}>
@@ -75,7 +75,7 @@ const CareerGrid = () => {
          <div className={`${styles.cohort}`}>
          Be a part of our next cohort starting summer of 2022
          </div>
-         <div>
+         <div className={`${styles.register}`}>
             <button className={`${styles.btngreen}`}>Register Now</button>
          </div>
       </div>
