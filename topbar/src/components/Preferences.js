9fea7637281d36714b0fe5079b35b5c6e7f07566
AY-BAMZ <aybamz181@gmail.com>
2021-10-12 20:46:20 +0100

    done
    Former-commit-id: 63387b358a5659cb718dd0cb05e1812b8030f9f9

--- topbar/src/components/Preferences.js
+++ topbar/src/components/Preferences.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react"
 import ProfileModal from "./ProfileModal"
 import NotificationPreference from "../components/NotificationPreference"
-import SidebarPreference from "../components/SidebarPreference"
 import Themes from "../components/Themes"
 import MessagesMedia from "../components/MessagesMedia"
 import PreferenceMenu from "../components/PreferenceMenu"
@@ -10,7 +9,6 @@ import MarkAsRead from "../components/MarkAsRead"
 import LanguageAndRegion from "../components/LanguageAndRegion"
 import styles from "../styles/NotificationPreference.module.css"
 import { ProfileContext } from "../context/ProfileModal"
-import AudioVideo from "../components/AudioVideo"
 import AdvancedSettings from "../components/AdvancedSettings"
 import Accessibility from "./Accessibility"
 import { HiMenuAlt2 } from "react-icons/hi"
@@ -31,13 +29,15 @@ const Preferences = () => {
       <div className={styles.body}>
         <div className={styles.buttonToggle}>
           <PreferenceMenu />
-          <div ><button className={styles.toggle} onClick={handleToggle}>
-            {navbarOpen ? (
-              <IoMdClose />
-            ) : (
-              <HiMenuAlt2 onClick={() => closeMenu()} />
-            )}
-          </button></div>
+          <div>
+            <button className={styles.toggle} onClick={handleToggle}>
+              {navbarOpen ? (
+                <IoMdClose />
+              ) : (
+                <HiMenuAlt2 onClick={() => closeMenu()} />
+              )}
+            </button>
+          </div>
 
           <hr className={styles.hrLine} />
           <MobilePreferenceMenu
@@ -49,18 +49,13 @@ const Preferences = () => {
 
         <div>
           {sideBar === 1 && <NotificationPreference />}
-          {/*
-{sideBar === 2 && <SidebarPreference />}
-*/}
 
           {sideBar === 3 && <Themes />}
           {sideBar === 4 && <MessagesMedia />}
           {sideBar === 5 && <LanguageAndRegion />}
           {sideBar === 6 && <Accessibility />}
           {sideBar === 7 && <MarkAsRead />}
-          {/*
-            {sideBar === 8 && <AudioVideo />}
-          */}
+
           {sideBar === 9 && <AdvancedSettings />}
         </div>
       </div>
