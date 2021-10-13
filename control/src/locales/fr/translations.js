f30ef58055a6ee6fab98409a9b999676b88f414a
drchibs <richarddennis789@gmail.com>
2021-10-13 01:28:25 +0100

    landing page translation to english, chinese and german complete
    Former-commit-id: b1e8c4b3a21685ba12f5f63e8ea2dbcf0ba1096b

---
+++ control/src/locales/fr/translations.js
@@ -0,0 +1,91 @@
+export const TRANSLATIONS_FR = {
+  landing: {
+    title: {
+      landing: "Connectez-vous et interagissez"
+    },
+
+    nav: {
+      pricing: "Prix",
+      about: "Sur",
+      downloads: "Téléchargements",
+      contact: "Contact",
+      signup: "S'inscrire",
+      login: "Connexion"
+    },
+
+    hero: {
+      pre_headline: "Pour les organisations et institutions",
+      headline: "Un moyen entièrement intégré de se connecter et d'interagir",
+      post_headline:
+        "Créez votre avenir avec nous, réseautez, collaborez, éduquez et apprenez à distance",
+      call_to_action: "Essayez Zuri.chat gratuitement"
+    },
+
+    featureRowOne: {
+      title: "Collaboration transparente",
+      headline: "Communiquez et collaborez où que vous soyez",
+      description:
+        "Invitez votre équipe dans votre espace de travail. Restez connecté, restez synchronisé et explorez des idées ensemble où que vous soyez.",
+      link: "Créez votre propre espace de travail"
+    },
+
+    featureRowTwo: {
+      title: "Enseignement à distance",
+      headline: "Apprendre en déplacement",
+      description:
+        "Emmenez votre classe partout, rendez l'apprentissage amusant, restez engagé et inspiré avec le salon virtuel et les jeux.",
+      link: "En savoir plus sur le salon virtuel"
+    },
+
+    featureRowThree: {
+      title: "Espace de travail tout-en-un",
+      headline: "Personnalisez votre espace de travail",
+      description:
+        "Un outil, plusieurs plugins. Suivez les dépenses de l'entreprise, envoyez des informations rapidement et en douceur, gérez les fichiers et intégrez des outils à Zuri.chat.",
+      link: "Explorer les plugins"
+    },
+
+    featureRowFour: {
+      title: "Recherche Avancée",
+      headline:
+        "Accédez aux fichiers et aux messages de votre espace de travail sans effort",
+      description:
+        "Avec l'outil de recherche, trouvez facilement des messages, des fichiers et des liens précédemment partagés.",
+      link: "En savoir plus sur la recherche"
+    },
+
+    cardOne: {
+      title:
+        "Un moyen flexible d'éduquer, de collaborer et de faire équipe où que vous soyez",
+      button: "Inscrivez-vous maintenant gratuitement"
+    },
+
+    cardTwo: {
+      title:
+        "Soyez le premier à recevoir des mises à jour et des offres exclusives de Zuri Chat",
+      description: "Enrichissez votre expérience Zuri Chat en vous inscrivant ici pour obtenir les dernières nouvelles et offres spéciales sur Zuri Chat",
+      button: "S'abonner"
+    },
+    footer: {
+      company: "Société",
+      careers: "Carrières",
+      about: "Sur",
+      info: "Info",
+      privacy: "Confidentialité et modalités",
+      market: "Place du marché",
+      knowUs: "Connaissez-nous",
+      downloads: "Téléchargements",
+      contact: "Contact",
+      documentation: "Documentation",
+      socialMedia: "Des médias sociaux",
+      rights: "Tous les droits sont réservés"
+    },
+    cookies:{
+      textOne: "Nous utilisons des",
+      textTwo: "cookies",
+      textThree: "tiers afin de personnaliser votre expérience sur le site.",
+      accept: "Permettre",
+      decline: "Déclin"
+    }
+  }
+}
