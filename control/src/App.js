0d0457fe93ca7de39e5e792bf18e406049d9eb37
Lotanna Kyrian A <kyrian.lotanna@gmail.com>
2021-10-13 15:19:35 +0100

    Implemented SideBar to Pixel Perfection: ZC-2684
    Former-commit-id: d256b8f231ebbdfe55e952ad0d9b232039467755

--- control/src/App.js
+++ control/src/App.js
@@ -135,11 +135,11 @@ const App = () => {
         <Route path="/careers">
           <Careers />
         </Route>
-        
+
         <Route path="/legal">
           <Legal />
         </Route>
-        
+
         <Route path="/privacy">
           <PrivacyPolicy />
         </Route>
