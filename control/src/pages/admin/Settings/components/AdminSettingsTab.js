69ffe36d7b02b0390c3079f5b006d949bb681641
Boluwatt <boluwatt@gmail.com>
2021-10-13 10:33:40 +0000

    minor bug fix
    Former-commit-id: 2c170a74d7ac75634fea6d97782fad97daca0a12

--- control/src/pages/admin/Settings/components/AdminSettingsTab.js
+++ control/src/pages/admin/Settings/components/AdminSettingsTab.js
@@ -4,9 +4,9 @@ import styles from '../styles/AdminSettingsTab.module.css'
 
 // components
 import AdminSettings from '../adminSettings'
+import PermissionsTab from '../Permissions'
 import AuthTab from './AuthTab'
 import MembersTab from '../../components/MembersTab'
-import HistoryTab from '../../components/HistoryTab'
 
 // icon
 import { FiMenu } from 'react-icons/fi'
@@ -74,7 +74,7 @@ const AdminSettingsTab = () => {
             openTab={openTab}
           />
         )}
-        {active === 2 && <HistoryTab />}
+        {active === 2 && <PermissionsTab />}
         {active === 3 && <AuthTab />}
         {active === 4 && <MembersTab />}
       </div>
