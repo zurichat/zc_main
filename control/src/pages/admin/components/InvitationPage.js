4df36a848f4e8d8a6ed5f0083ec8273d98d04772
Ariiie <oluwafunmilayodada2018@gmail.com>
2021-10-13 16:56:06 +0100

    updates
    Former-commit-id: c15b09fbf18237a5ae38bb68088b60e0b564be07

--- control/src/pages/admin/components/InvitationPage.js
+++ control/src/pages/admin/components/InvitationPage.js
@@ -1,8 +1,14 @@
+import { Link } from " react-router-dom"
+import AdminHeader from "./AdminHeader"
+import AdminSidebar from "./AdminSidebar"
+
 import styles from "../styles/InvitationPage.module.css"
 
 const InvitationPage = () => {
   return (
     <>
+      <AdminHeader />
+      <AdminSidebar />
       <div>Invite.js</div>
     </>
   )
