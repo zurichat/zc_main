f3683c9b108faf878605b6efeddbf433575083cd
billmal071 <billmal071@gmail.com>
2021-10-13 17:59:12 +0100

    feat:user preference accessibilty settings
    Former-commit-id: 3b0851d4ce1602918dd72829f2226387acd6df08

--- topbar/src/components/Accessibility.js
+++ topbar/src/components/Accessibility.js
@@ -1,9 +1,61 @@
-import React, { useState } from 'react'
-import styles from '../styles/Accessibility.module.css'
+import React, { useState, useEffect } from "react"
+import styles from "../styles/Accessibility.module.css"
 import image1 from "../assets/images/up_icon.jpeg"
-
+import { GetUserInfo } from "../../../control/src/zuri-control"
+import axios from "axios"
+import { useSelector, useDispatch } from "react-redux"
+import { setUserAccessibilitySettings } from "../../src/redux/userAction/userActions"
 
 function Accessibility() {
+  const dispatch = useDispatch()
+  const { accessibility } = useSelector(state => state.rootReducer)
+  const [accessbilitySettings, setAccessibilitySettings] = useState({
+    animation: false,
+    receivedSound: false,
+    sentSound: false,
+    readOutLoud: true,
+    messageControl: { bool: true, message: "focus_on_last_message" }
+  })
+  const [info, setInfo] = useState()
+  const [error, setError] = useState()
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const userInfo = await GetUserInfo()
+        if (userInfo === {}) throw new Error("No user info")
+        const orgId = userInfo[0].org_id
+        const memId = userInfo[0]._id
+        const userAccessibiltySettings = {
+          links: true,
+          animation: accessbilitySettings.animation,
+          direct_message_announcement: {
+            receive_sound: accessbilitySettings.receivedSound,
+            send_sound: accessbilitySettings.sentSound,
+            read_message: accessbilitySettings.readOutLoud
+          },
+          press_empty_message_field: accessbilitySettings.messageControl.message
+        }
+        const userHeader = {
+          headers: {
+            Authorization: `Bearer ${userInfo.token}`
+          }
+        }
+        dispatch(
+          setUserAccessibilitySettings(
+            `https://api.zuri.chat/organizations/${orgId}/members/${memId}/settings/accessibility`,
+            userAccessibiltySettings,
+            userHeader
+          )
+        )
+        setInfo(userInfo)
+      } catch (err) {
+        setError(err)
+      }
+    })()
+    return () => {}
+  }, [accessbilitySettings])
+
   return (
     <div>
       <div className={styles.containerAccessibility}>
@@ -12,13 +64,21 @@ function Accessibility() {
           <form action="">
             <div className={styles.imagesAndEmojis}>
               <input
+                id="animation"
                 className={styles.accessibilityCheckbox}
                 type="checkbox"
-                checked="checked"
-                name=""
+                checked={accessbilitySettings.animation}
+                name="animation"
+                onChange={() =>
+                  setAccessibilitySettings({
+                    ...accessbilitySettings,
+                    animation: !accessbilitySettings.animation
+                  })
+                }
                 value="Allow animated images and emojis"
               />
-              <label className={styles.accessibilityLabel}>
+              <span className={styles.customCheckbox}></span>
+              <label htmlFor="animation" className={styles.accessibilityLabel}>
                 Allow animated images and emojis
               </label>
             </div>
@@ -30,42 +90,69 @@ function Accessibility() {
           <h3 className={styles.directH3}>Direct message announcement</h3>
           <p className={styles.accessibilityP}>
             Choose which sounds and announcements you'd like to receive while
-            using a
-            <br />
-            screen reader inside a direct message.
+            using a screen reader inside a direct message.
           </p>
           <form action="">
             <div className={styles.imagesAndEmojis}>
               <input
                 className={styles.accessibilityCheckbox}
+                id="receivedSound"
                 type="checkbox"
-                name=""
+                checked={accessbilitySettings.receivedSound}
+                onChange={() =>
+                  setAccessibilitySettings({
+                    ...accessbilitySettings,
+                    receivedSound: !accessbilitySettings.receivedSound
+                  })
+                }
+                name="receivedSound"
                 value="Play a sound when a message is received"
               />
-              <label className={styles.accessibilityLabel}>
+              <label
+                htmlFor="receivedSound"
+                className={styles.accessibilityLabel}
+              >
                 Play a sound when a message is received
               </label>
             </div>
             <div className={styles.imagesAndEmojis}>
               <input
                 className={styles.accessibilityCheckbox}
+                id="sentSound"
                 type="checkbox"
-                name=""
+                name="sentSound"
+                checked={accessbilitySettings.sentSound}
+                onChange={() =>
+                  setAccessibilitySettings({
+                    ...accessbilitySettings,
+                    sentSound: !accessbilitySettings.sentSound
+                  })
+                }
                 value="Play a sound when a message is sent"
               />
-              <label className={styles.accessibilityLabel}>
+              <label htmlFor="sentSound" className={styles.accessibilityLabel}>
                 Play a sound when a message is sent
               </label>
             </div>
             <div className={styles.imagesAndEmojis}>
               <input
                 className={styles.accessibilityCheckbox}
+                id="readOutLoud"
                 type="checkbox"
-                checked="checked"
-                name=""
+                checked={accessbilitySettings.readOutLoud}
+                onChange={() =>
+                  setAccessibilitySettings({
+                    ...accessbilitySettings,
+                    readOutLoud: !accessbilitySettings.readOutLoud
+                  })
+                }
+                name="readOutLoud"
                 value="Read incoming message out loud"
               />
-              <label className={styles.accessibilityLabel}>
+              <label
+                htmlFor="readOutLoud"
+                className={styles.accessibilityLabel}
+              >
                 Read incoming message out loud
               </label>
             </div>
@@ -76,58 +163,95 @@ function Accessibility() {
         <div>
           <h3 className={styles.keyboardH3}>Keyboard</h3>
           <p className={styles.accessibilityP}>
-            You can improve the way you use Zurichat by learning our{' '}
+            You can improve the way you use Zurichat by learning our{" "}
             <span id={styles.handyKeyboard}>handy keyboard shortcuts.</span>
           </p>
 
           <hr className={styles.hrLine} />
 
           <h3 className={styles.pressH3}>
-            Press <img src={image1} alt="upicon" className={styles.image1} /> in the empty message field to:
+            Press <img src={image1} alt="upicon" className={styles.image1} /> in
+            the empty message field to:
           </h3>
           <form action="">
             <div className={styles.imagesAndEmojis}>
               <input
                 className={styles.accessibilityRadio}
+                id="messageList"
                 type="radio"
-                checked="checked"
+                checked={
+                  accessbilitySettings.messageControl.message ===
+                  "focus_on_last_message"
+                }
+                onChange={() => {
+                  setAccessibilitySettings({
+                    ...accessbilitySettings,
+                    messageControl: {
+                      bool: true,
+                      message: "focus_on_last_message"
+                    }
+                  })
+                }}
                 name="accessradio"
-                value="Move focus to the message list"
+                value="focus_on_last_message"
               />
-              <label className={styles.accessibilityRadioLabel}>
-                Move focus to the message list
+              <label
+                htmlFor="messageList"
+                className={styles.accessibilityRadioLabel}
+              >
+                <span>Move focus to the message list</span>
+                <br />
+                <span className={styles.accessbilityText}>
+                  The last visible message in the list will be selected
+                </span>
               </label>
-              <p className={styles.accessibilityP} id={styles.accessibilityPId}>
-                The last visible message in the list will be selected
-              </p>
             </div>
             <div className={styles.imagesAndEmojis}>
               <input
                 className={styles.accessibilityRadio}
+                id="editMessage"
                 type="radio"
+                checked={
+                  accessbilitySettings.messageControl.message ===
+                  "edit_last_message"
+                }
+                onChange={() => {
+                  setAccessibilitySettings({
+                    ...accessbilitySettings,
+                    messageControl: { bool: true, message: "edit_last_message" }
+                  })
+                }}
                 name="accessradio"
-                value="Edit your last message"
+                value="edit_last_message"
               />
-              <label className={styles.accessibilityRadioLabel}>
-                Edit your last message
-              </label>
-              <p
-                className={styles.accessibilityP}
-                id={styles.accessibilityPId2}
+              <label
+                htmlFor="editMessage"
+                className={styles.accessibilityRadioLabel}
               >
-                The last visible message you sent will be selected and in the
-                editing mode
-              </p>
+                <span>Edit your last message</span>
+                <br />
+                <span className={styles.accessbilityText}>
+                  The last visible message you sent will be selected and in the
+                  editing mode
+                </span>
+              </label>
             </div>
           </form>
 
-          <p className={styles.accessibilityQ} id={styles.noteP}>
-            Note: press
-            <span className={styles.accessibilityEsc}> Ctrl </span>
-            <span className={styles.accessibilityEsc} id={styles.exclamation}>
-              {' '}
-              !{' '}
-            </span>{' '}
+          <p
+            className={styles.accessibilityQ}
+            id={styles.noteP}
+            style={{ marginTop: "10px" }}
+          >
+            Note: press <span className={styles.accessibilityEsc}> Ctrl </span>
+            <span
+              className={styles.accessibilityEsc}
+              id={styles.exclamation}
+              style={{ marginRight: "5px" }}
+            >
+              {" "}
+              !{" "}
+            </span>{" "}
             to edit your last message, with either option.
           </p>
         </div>
--- topbar/src/redux/userAction/userActions.js
+++ topbar/src/redux/userAction/userActions.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { LOGIN_USER_SUCCESS } from '../userConstants/userConstants'
+import axios from "axios"
+import { LOGIN_USER_SUCCESS } from "../userConstants/userConstants"
 
 const loginUserSuccess = payload => ({
   type: LOGIN_USER_SUCCESS,
@@ -12,12 +12,33 @@ export const loginUser = (url, loginInfo, setCookies, history) => {
       .post(url, loginInfo)
       .then(({ data }) => {
         dispatch(loginUserSuccess(data.data.user))
-        setCookies('Zuri_Chat', data.data.token, { path: '/' })
-        history.push('/home')
+        setCookies("Zuri_Chat", data.data.token, { path: "/" })
+        history.push("/home")
       })
       .catch(e => {
-        alert('Error: Wrong Email or password ')
-        history.push('/login')
+        alert("Error: Wrong Email or password ")
+        history.push("/login")
       })
+  }
+}
+
+export function setUserAccessibilitySettings(link, accessibilitySettings, headers) {
+  return async function (dispatch) {
+    try {
+      const response = await axios.patch(
+        link,
+        accessibilitySettings,
+        headers
+      )
+      dispatch({
+        type: "SET_ACCESSIBILITY_SETTINGS_SUCCESS",
+        payload: response.data.message
+      })
+    } catch (err) {
+      dispatch({
+        type: "SET_ACCESSIBILITY_SETTINGS_FAILURE",
+        payload: err.message
+      })
+    }
   }
 }
