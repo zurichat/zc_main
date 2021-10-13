2d095df159d0ac729fcf9e627a7ed7e124de7839
Quado2 <engchikwas404@gmail.com>
2021-10-14 00:21:33 +0100

    Implemented displaying and reaction with emoji

--- zuriUi/src/components/MessageBoard/MessageBoard.js
+++ zuriUi/src/components/MessageBoard/MessageBoard.js
@@ -4,7 +4,7 @@ import { MsgBoard } from "./MsgBoardStyle"
 import MoreMenu from "./Subs/MessageContainer/MoreMenu/MoreMenu"
 import Overlay from "./Subs/MessageContainer/Overlay/Overlay"
 
-import EmojiReaction from '../EmojiReaction/EmojiReaction'
+import EmojiReaction from "../EmojiReaction/EmojiReaction"
 
 import { useState } from "react"
 
@@ -12,61 +12,101 @@ function Uitest() {
   const MsgList = [
     {
       name: "Mark essien",
+      id: 4,
       time: "7:05PM",
+      emojis: [],
       message:
         "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
     },
     {
       name: "Xylux",
       time: "8:05PM",
+      emojis: [
+        { name: "smiling", count: 2, emoji: "👹" },
+        { name: "grining", count: 1, emoji: "😊" }
+      ],
       message:
         "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
     },
     {
       name: "Naza",
+      id: 5,
       time: "10:05PM",
+      emojis: [
+        { name: "smiling", count: 2, emoji: "👹" },
+        { name: "grining", count: 1, emoji: "😊" }
+      ],
       message:
         "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
     },
     {
       name: "Nonso",
+      id: 6,
       time: "7:05PM",
+      emojis: [
+        { name: "smiling", count: 2, emoji: "👹" },
+        { name: "grining", count: 1, emoji: "😊" }
+      ],
       message:
         "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
     },
     {
       name: "Chanda",
+      id: 7,
       time: "7:05PM",
+      emojis: [
+        { name: "smiling", count: 4, emoji: "😋" },
+        { name: "grining", count: 1, emoji: "😊" }
+      ],
+      message:
+        "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
+    },
+    {
+      name: "Ufon",
+      id: 8,
+      time: "2:00PM",
+      emojis: [
+        { name: "smiling", count: 2, emoji: "👹" },
+        { name: "grining", count: 1, emoji: "😊" }
+      ],
+      message:
+        "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
+    },
+    {
+      name: "Ufon",
+      id: 9,
+      time: "2:00PM",
+      emojis: [
+        { name: "smiling", count: 2, emoji: "👹" },
+        { name: "grining", count: 10, emoji: "😊" }
+      ],
       message:
         "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
     },
     {
       name: "Ufon",
       time: "2:00PM",
+      id: 10,
+      emojis: [{ name: "grining", count: 1, emoji: "😊" }],
       message:
         "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
     },
     {
       name: "Ufon",
+      id: 9,
       time: "2:00PM",
+      emojis: [
+        { name: "smiling", count: 2, emoji: "👹" },
+        { name: "grining", count: 1, emoji: "😊" }
+      ],
       message:
         "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
     },
     {
+      id: 10,
       name: "Ufon",
       time: "2:00PM",
-      message:
-        "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
-    },
-    {
-      name: "Ufon",
-      time: "2:00PM",
-      message:
-        "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
-    },
-    {
-      name: "Ufon",
-      time: "2:00PM",
+      emojis: [],
       message:
         "Donec rutrum congue leo eget malesuada. Nulla porttitor accumstincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit ametaliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam iddui posuere blandit. Vestibulum ac diam sit amet quam vehicula"
     }
@@ -74,13 +114,15 @@ function Uitest() {
 
   const [showMoreOptions, setShowMoreOptions] = useState(false)
   const [showEmoji, setShowEmoji] = useState(false)
+  const [messageList, setMessageList] = useState(MsgList)
 
   const [top, setTop] = useState(null)
   const [right, setRight] = useState(null)
+  const [currentMessageId, setCurrentMessageId] = useState(null)
 
   function handleOverlayClicked() {
     setShowMoreOptions(false)
-    
+    setShowEmoji(false)
   }
 
   const handleShowMoreOptions = (id, event) => {
@@ -90,24 +132,60 @@ function Uitest() {
   }
 
   const handleShowEmoji = (id, event) => {
+    setCurrentMessageId(id)
     setShowEmoji(!showEmoji)
     setTop(event.clientY)
     setRight(window.innerWidth - event.clientX)
   }
 
+  function handleEmojiClicked(event, emojiObject) {
+    // extract the data
+    const emoji = emojiObject.emoji
+    const newEmojiName = emojiObject.names[1]
+
+    //copy the message
+    const messageListCopy = [...messageList]
+    const messageIndex = messageListCopy.findIndex(
+      message => message.id === currentMessageId
+    )
+
+    if (messageIndex < 0) {
+      return
+    }
+
+    const message = messageListCopy[messageIndex]
+    const emojiIndex = message.emojis.findIndex(
+      emoji => emoji.name === newEmojiName
+    )
+
+    if (emojiIndex >= 0) {
+      //the emoji exist
+      //increase the count
+      message.emojis[emojiIndex].count = message.emojis[emojiIndex].count + 1
+    } else {
+      //the emoji does not exist
+      //create the emoji object and push
+      const newEmojiObject = { name: newEmojiName, count: 1, emoji: emoji }
+      message.emojis.push(newEmojiObject)
+    }
+
+    //now we replace the message with the new one
+    messageListCopy[messageIndex] = message
+    setMessageList(messageListCopy)
+  }
+
   return (
     <>
-     
       <MsgBoard>
-        {MsgList.map((messageData, i) => (
-          <MessageContainer
-            handleShowMoreOptions={handleShowMoreOptions}
-            handleShowEmoji={handleShowEmoji}
-            key={i}
-            id={i}
-            messageData={messageData}
-          />
-        ))}
+        {messageList &&
+          messageList.map((messageData, i) => (
+            <MessageContainer
+              handleShowMoreOptions={handleShowMoreOptions}
+              handleShowEmoji={handleShowEmoji}
+              key={i}
+              messageData={messageData}
+            />
+          ))}
       </MsgBoard>
 
       {showMoreOptions ? (
@@ -119,7 +197,11 @@ function Uitest() {
 
       {showEmoji ? (
         <div>
-          <EmojiReaction top={top} right={right} />
+          <EmojiReaction
+            top={top}
+            right={right}
+            handleEmojiClicked={handleEmojiClicked}
+          />
           <Overlay handleOverlayClicked={handleOverlayClicked} />
         </div>
       ) : null}
