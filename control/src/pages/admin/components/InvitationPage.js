993e3fe74202b7334291f87eb5d526daf4d5a723
Ariiie <oluwafunmilayodada2018@gmail.com>
2021-10-13 15:49:00 +0100

    invitation_page

---
+++ control/src/pages/admin/components/InvitationPage.js
@@ -0,0 +1,11 @@
+import styles from "../styles/InvitationPage.module.css"
+
+const InvitationPage = () => {
+  return (
+    <>
+      <div>Invite.js</div>
+    </>
+  )
+}
+
+export default InvitationPage
