1ee359976d8da9303d80625fa770296acb0fce84
Damilola Akinlade <akinlade.co@gmail.com>
2021-10-13 12:35:26 +0100

    emoji setting page in progress
    Former-commit-id: 46b4fd64e629fd7d82cd73331d491da0ab6f3899 [formerly 08a2c3455ba081d15670c2fd01787c33ceca1bc0] [formerly 4737d3a4bc6507dc8836269df8c4d29581405455 [formerly ee07cf661ee5e1df5347590a3de3679fd12074cb [formerly 7a850cf0f4659db99db931200b618f2d8a80295a] [formerly 7646852ed1c27201509d1aee5647b4b32daa5d95]]]
    Former-commit-id: 97d4d6eab204d810212473a087761591fec6dacc [formerly ee07cf661ee5e1df5347590a3de3679fd12074cb [formerly 7a850cf0f4659db99db931200b618f2d8a80295a]]
    Former-commit-id: 27a4e222b326c16ed04405f15e4bb8627b5bf7df
    Former-commit-id: 246468862b6778a4f496c590640a63f02e43ac4d
    Former-commit-id: 6b8113d70813c6b9dcadc8e2a9569c569a17eb1d

--- control/src/pages/admin/Settings/components/Emoji.js
+++ control/src/pages/admin/Settings/components/Emoji.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState } from "react"
+import classes from "../styles/EmojiTab.module.css"
 
 const Emoji = () => {
-    return (
-        <div>
-            Emoji
+  const [display, setDisplay] = useState(false)
+  return (
+    <div className={classes.container}>
+      <div className={classes.top}>
+        <div className={classes.header}>One-click reactions</div>
+        <div className={classes.text}>
+          Choose the default emoji people will see when they enable one-click
+          reactions
         </div>
-    )
+        <div className={classes.emojis}></div>
+        <div className={classes.example}></div>
+      </div>
+      <div className={classes.bottom}>
+        <div className={classes.title}>
+          There aren’t any custom emoji in your workspace yet
+        </div>
+        <div className={classes.text}>
+          You don’t have to be limited to the standard emoji set. Make your
+          workspace your own with custom emoji that mean something to your team.
+        </div>
+        <div className={classes.btn}>
+          <button>Add Alias</button>
+          <button onClick={() => setDisplay(true)}>Add Custom Emoji</button>
+        </div>
+      </div>
+
+      {display && (
+        <>
+          <div onClick={() => setDisplay(false)} className={classes.overlay}></div>
+          <div className={classes.modal}>
+            <div className={classes.container}>
+              <div className={classes.title}>Add custom emoji</div>
+              <div className={classes.text}>
+                Your custom emoji will be available to everyone in your
+                workspace. You’ll find it in the custom tab of the emoji picker.
+                (Hint: it’s the one with the Slack icon!)
+              </div>
+              <div className={classes.list}>
+                <ul>
+                  <li>
+                    <div className={classes.title}>Upload an image</div>
+                    <div className={classes.text}>
+                      Square images under 128KB and with transparent backgrounds
+                      work best. If your image is too large, we’ll try to resize
+                      it for you.
+                    </div>
+                  </li>
+                  <li>
+                    <div className={classes.title}>Give it a name</div>
+                    <div className={classes.text}>
+                      This is also what you’ll type to add this emoji to your
+                      messages.
+                    </div>
+                    <div className={classes.input}>
+                      <input type="text" />
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
 
 export default Emoji
