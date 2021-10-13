f37e0cc60e0e7533713bc67484b5ff0a97e332e6
Yemi-dev <akinyemitiana77@gmail.com>
2021-10-14 00:59:11 +0100

    Revert "done"
    This reverts commit c7722621e5312976c689b7f9a47adb0c9d506322.

--- sidebar/src/components/SingleRoom.js
+++ sidebar/src/components/SingleRoom.js
@@ -1,22 +1,19 @@
-import {React, useState} from "react"
+import React from "react"
 import { navigateToUrl } from "single-spa"
 import styles from "../styles/Sidebar.module.css"
 import Badge from "./badge"
-import useThemeMode from "../../../topbar/customHooks/useThemeMode";
 
 export default function SingleRoom({ image, name, link }) {
-  const [nullValue, setnullValue] = useState(0)
-  const {theme} = useThemeMode();
   return (
-    <div className={`row mt-2 ${styles.sb__item}  ${theme}`}>
+    <div className={`row mt-2 ${styles.sb__item}`}>
       <a
-        className={`row mt-2 ${styles.sb__item} ${theme}`}
+        className={`row mt-2 ${styles.sb__item}`}
         href={link && link}
         onClick={navigateToUrl}
       >
-        <div className={`col-10 d-flex align-items-center ${styles.sb__col}  ${theme}`}>
+        <div className={`col-10 d-flex align-items-center ${styles.sb__col}`}>
           <img className={`${styles.item__img}`} src={image} alt="icon" />
-          <p className={`mb-0 ${styles.item_p}  ${theme}`}>{name}</p>
+          <p className={`mb-0 ${styles.item_p}`}>{name}</p>
         </div>
         <div className={`col-2 d-flex align-items-center`}>
           {}
