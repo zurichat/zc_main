7bce2527a37a0ee8e4013745201146bc0c75153a
Wavist <sommy.okafor@yahoo.com>
2021-10-14 22:59:22 +0100

    Setup and create translations for [Footer & Pricing Page - WEB ] and fix default language of Zuri Main
    Former-commit-id: d72f5e41eb8f12118aeb9d831b32344c8a587564

--- control/src/components/Header.js
+++ control/src/components/Header.js
@@ -78,8 +78,8 @@ const HeaderSearchSuggestion = () => {
           <option value="en">English</option>
           <option value="fr">French</option>
           <option value="zh">Chinese</option>
+          <option value="de">German</option>
           <option value="ar">Arabic</option>
-          <option value="de">Deutsch</option>
         </select>
 
         <button
--- control/src/locales/en/translations.js
+++ control/src/locales/en/translations.js
@@ -67,6 +67,7 @@ export const TRANSLATIONS_EN = {
         "Enrich your Zuri Chat experience by signing up here to get the latest news and special deals on Zuri Chat",
       button: "Subscribe"
     },
+
     footer: {
       company: "Company",
       careers: "Careers",
@@ -74,6 +75,82 @@ export const TRANSLATIONS_EN = {
       info: "Info",
       privacy: "Privacy & Terms",
       market: "Market Place",
+      blog: "Blog",
+      security: "Security",
+      web3: "Web3/DAO",
+      knowUs: "Know us",
+      downloads: "Downloads",
+      contact: "Contact",
+      documentation: "Documentation",
+      socialMedia: "Social Media",
+      rights: " All Rights Reserved"
+    },
+    cookies: {
+      textOne: "We use third-party",
+      textTwo: "cookies",
+      textThree: "in order to personalize your site experience.",
+      accept: "Allow",
+      decline: "Decline"
+    }
+  },
+
+
+  pricing: 
+  {
+    title: {
+      pricing: "Pricing - Zuri Chat"
+    },
+
+    nav: {
+      pricing: "Pricing",
+      about: "About",
+      downloads: "Downloads",
+      contact: "Contact",
+      signup: "Sign Up",
+      login: "Login"
+    },
+
+    cardOne: {
+      headline: "Features",
+      post_headline:
+        "Create your future with us, network, collaborate, educate and learn remotely",
+      call_to_action: "Try Zuri.chat for free"
+    },
+
+    cardThree: {
+      duration_one: "Annual",
+      duration_two: "Monthly",
+      button: "Get Started",
+    },
+
+    sectionThree: {
+      headline: "Leading the way in enterprise security",
+      description: "In addition to encryption in transit and at rest, we operate comprehensive compliance ans assurance programs.",
+      link: "Learn more about security..."
+    },
+
+    sectionFour: {
+      headline: "Whatever work you do, you can do it in Zuri Chat",
+      button_one: "Talk to Sales",
+      button_two: "TRY IT FREE"
+    },
+
+    FAQ: {
+      headline: "Commonly Asked Questions",
+      link_info: "Get more information on",
+      link: "billing at Zuri Chat."
+    },
+
+    footer: {
+      company: "Company",
+      careers: "Careers",
+      about: "About",
+      info: "Info",
+      privacy: "Privacy & Terms",
+      market: "Market Place",
+      blog: "Blog",
+      security: "Security",
+      web3: "Web3/DAO",
       knowUs: "Know us",
       downloads: "Downloads",
       contact: "Contact",
