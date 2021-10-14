6544e5441f4d007033210cc14566c1093a5d1e2c
Ekeyekwu Oscar Ikechukwu <oscarekeyekwu@gmail.com>
2021-10-14 04:43:42 +0100

    translated career grid component
    Former-commit-id: d6c5a80f18a3147150142aaf6e906d571428ff2a

--- control/src/pages/careers/components/CareerGrid.js
+++ control/src/pages/careers/components/CareerGrid.js
@@ -4,8 +4,7 @@ import { useTranslation} from "react-i18next";
 
 const CareerGrid = () => {
   const { t } = useTranslation();
-
-
+  
    return (
       <div>
          <div className={`${styles.headerContent}`}>
