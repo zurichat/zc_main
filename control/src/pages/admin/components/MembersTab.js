1d2c14fbd19d802f03b94fd3cfbf0e0185eab655
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-10-14 11:14:42 +0300

    Implement Analytic  member tab
    Former-commit-id: ebeb816beafa6d453b8deb9da436b966d70d1a3c [formerly 865127bd74ef6438d4c76455536444092db6116e] [formerly 7243fa2c23cf493f4b6a29c525a14795a29339c4 [formerly dcd81594d3dc7b7d5f1126776cc2487f96fc2fd9 [formerly 155ff23047ac3d8cfbeb77836469da9dc0660d86] [formerly 4987ef32f5d4da2ee81d02762a023ac2a950eaaf]]]
    Former-commit-id: 631514eac2908d88a579af814ed0a4334627d6a8 [formerly dcd81594d3dc7b7d5f1126776cc2487f96fc2fd9 [formerly 155ff23047ac3d8cfbeb77836469da9dc0660d86]]
    Former-commit-id: c23efcba190d09bbe0990714876fbd49d7b5a35c
    Former-commit-id: 6f0602766b4e6832cf4fc7fac1f13274b1c3b149
    Former-commit-id: 64493e35bf00fa08c8ab96983ca6c54a733bc1e8

--- control/src/pages/admin/components/MembersTab.js
+++ control/src/pages/admin/components/MembersTab.js
@@ -1,26 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { joiningPeriod } from '../data/membersData'
-import JoiningMembers from './JoiningMembers'
-
-import styles from '../styles/adminMembersTab.module.css'
+import React, { useContext, useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { joiningPeriod } from "../data/membersData"
+import JoiningMembers from "./JoiningMembers"
+import { authAxios } from "../Utils/Api"
+import styles from "../styles/adminMembersTab.module.css"
+import { GetWorkspaceUsers } from "../../../zuri-control"
+import { getUser, getCurrentWorkspace } from "../Utils/Common"
+import DataTable from "react-data-table-component"
 
 const MembersTab = () => {
+  const currentWorkspace = getCurrentWorkspace()
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch(
+        "https://api.zuri.chat/organizations/614679ee1a5607b13c00bcb7/members"
+      )
+        .then(res => res.json())
+        .then(data => {
+          setItems(data.data)
+          console.log(data.data, "why null")
+        })
+        .catch(err => console.log(err.message))
+    }, 1000)
+  }, [])
+  const columns = [
+    {
+      name: "Account created",
+      selector: row => row.joined_at,
+      sortable: true
+    },
+    {
+      name: "userID",
+      selector: row => row._id,
+      sortable: true
+    },
+    {
+      name: "Email",
+      selector: row => row.email,
+      sortable: true
+    }
+  ]
+
   return (
     <section className={styles.members_tab_container}>
-      <div className={styles.tab_info}>
-        <p>
-          This page lists each change in your workspace that affects your
-          account billing (for example, adding a member, or noting that a member
-          has become inactive). It does not show non-billable changes, like
-          adding bots. For more information visit our{' '}
-          <Link to="/">Guide to billing at Zurichat.</Link>
-        </p>
-      </div>
-      {joiningPeriod.map(membersData => {
-        const { id, date, members, url } = membersData
-        return <JoiningMembers key={id} date={date} users={members} url={url} />
-      })}
+      <DataTable
+        title="Joining Members"
+        columns={columns}
+        data={items}
+        pagination
+        paginationPerPage={5}
+        paginationRowsPerPageOptions={[5, 10, 15]}
+        paginationComponentOptions={{
+          rowsPerPageText: "Members per page:",
+          rangeSeparatorText: "of"
+        }}
+        rowClassName={row => {
+          if (row.status === "active") {
+            return "active"
+          } else if (row.status === "inactive") {
+            return "inactive"
+          } else if (row.status === "pending") {
+            return "pending"
+          }
+        }}
+      />
     </section>
   )
 }
