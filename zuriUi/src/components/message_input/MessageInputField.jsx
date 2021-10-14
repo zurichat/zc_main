7f41982eb6e2b09d884cbe3e33f9e7f33c485db3
toydaboss <toyeseaderibigbe@gmail.com>
2021-10-14 06:57:09 +0100

    attach file
    Former-commit-id: cdbdbea6b63595751c4c52b29a5241908907dff4

--- zuriUi/src/components/message_input/MessageInputField.jsx
+++ zuriUi/src/components/message_input/MessageInputField.jsx
@@ -10,14 +10,18 @@ import Clip from "../../assets/comments/clip.svg"
 import Bigborder from "../../assets/comments/bigborder.svg"
 import Dropdown from "../../assets/comments/dropdown.svg"
 import Send from "../../assets/comments/send.svg"
+import Google from '../InputBox/assets/comments/google.svg'
+import Computer from '../InputBox/assets/comments/computer.svg'
 import UnstyledButton from "./UnstyledButton"
+import axios from 'axios'
+import React,{ useState,useEffect } from "react"
 
-import { useState } from "react"
-const MessageInputBox = ({ sendMessageHandler, addToMessages }) => {
+const MessageInputBox = ({ sendMessageHandler, addToMessages,org_id,room_id,attachedFileApi }) => {
   const [text, setText] = useState("")
   const [textIsBold, setTextBold] = useState(false)
   const [textIsItalic, setTextItalic] = useState(false)
   const [attachedFile, setAttachedFile] = useState(null)
+  const [preview, setPreview] = useState('')
   const [inputKey, setInputKey] = useState('any-key-press')
   const [showAttachInputBox, setshowAttachInputBox] = useState(false)
 
@@ -41,14 +45,33 @@ const MessageInputBox = ({ sendMessageHandler, addToMessages }) => {
       sendMessageHandler(newMessage)
     }
   }
-
-  const handleAttachMedia=(e)=>{
-    if (e.target.files && e.target.files[0]) {
-      const fd = new FormData()
-      fd.append('media',e.target.files[0],e.target.files[0].name )
-      setAttachedFile(fd)
+    const inputRef = React.createRef()
+  
+  const handleAttachMedia=(e)=>{{ 
+      e.preventDefault()
+      //const data = attachedFile
+      axios.post(`${attachedFileApi}${org_id}/rooms/${room_id}/messagemedia`,attachedFile).then((res)=>{
+        alert(res)
+        }
+      )
+      clearAttached()
     }
-
+  }
+  useEffect(() => {
+    if(attachedFile){
+      const reader = new FileReader
+      reader.onloadend=()=>{
+        setPreview(reader.result)
+      }
+      reader.readAsDataURL(attachedFile)
+    }else{
+      setPreview('')
+    }
+  }, [attachedFile])
+  const handleSelectMedia=(e)=>{
+    //console.log(e.target.files[0])
+    setAttachedFile(e.target.files[0])
+    setshowAttachInputBox(false)
   }
 
   // on click clear attached file
@@ -60,7 +83,16 @@ const MessageInputBox = ({ sendMessageHandler, addToMessages }) => {
 
   return (
     <Wrapper>
+      
       <InputWrapper>
+      {
+            preview?
+            <Preview>
+              <img src={preview} style={{ position: "relative" }} alt="Image Preview"/>
+              <button style={{ position: "absolute", top: '-12px', left:'8', height:'30px', width:'30px', borderRadius:'50%'  }} onClick={clearAttached}>X</button>
+            </Preview>
+            :null
+          }
         <Input
           type="text"
           placeholder="Send a message"
@@ -69,17 +101,31 @@ const MessageInputBox = ({ sendMessageHandler, addToMessages }) => {
         />
         {/* Attached File Input field */}
        { showAttachInputBox ?
-       <div>
-         <input 
-              onChange={attachedFile}
-              key={inputKey || ''}
-              type='file'
-            />
-          <button onClick={handleAttachMedia}>Send</button>
-          <button 
-            onClick={clearAttached}
-          >Clear Attached File</button>
-       </div>
+       <AttachFile>
+         <div>
+           <div>
+           <img src={Google} alt="" />
+             Google Drive
+           </div>
+           <label>
+           <img src={Computer} alt=""  onClick={handleSelectMedia}/>
+           Upload from your computer
+            <input 
+                style={{
+                  display: "none",
+                }}
+                onChange={handleSelectMedia}
+                key={inputKey || ''}
+                type='file'
+                ref= {inputRef}
+                //onClick={handleAttachMedia}
+              />
+           </label>
+         </div>
+         
+         
+          <button onClick={handleAttachMedia} >Send</button>
+       </AttachFile>
          :null
           }
         <SendWrapper>
@@ -123,10 +169,12 @@ const MessageInputBox = ({ sendMessageHandler, addToMessages }) => {
               <img src={AtSign} alt="" />
             </UnstyledButton>
             <UnstyledButton>
-              <img src={Clip} alt="" onClick={()=>setshowAttachInputBox(true)}/>
+              <img src={Clip} alt="" 
+              className="attach_file"
+              onClick={()=>setshowAttachInputBox(true)}/>
             </UnstyledButton> 
             <UnstyledButton>
-              <img src={Send} alt="" onClick={handleClickSendMessage} />
+              <img src={Send} alt="" onClick={handleClickSendMessage && handleAttachMedia } />
             </UnstyledButton>
             <UnstyledButton>
               <img src={Bigborder} alt="" />
@@ -153,6 +201,7 @@ const Wrapper = styled.div`
 const InputWrapper = styled.section`
   border: 1px solid hsla(0, 0%, 92%, 1);
   border-radius: 3px;
+  width:100%;
   /* padding-left: 8px;
 padding-top: 8px;
 padding-bottom: 8px; */
@@ -165,6 +214,7 @@ const SendWrapper = styled.section`
   align-items: center;
   padding-right: 18px;
   gap: 4px;
+  position: relative;
 `
 const Input = styled.input`
   display: block;
@@ -191,3 +241,18 @@ const SendButton = styled.button`
   font-weight: 700;
   font-size: 1rem;
 `
+const AttachFile = styled.div`
+  width: 324px;
+  border-radius: 8px;
+  background-color: #F8F8F8;
+  padding: 15px 35px;
+  position: absolute;
+  right: 104px;
+  bottom: 46px;
+`
+const Preview = styled.div`
+  width:100px;
+  height: 100px
+  border-radius: 2px;
+`
+
