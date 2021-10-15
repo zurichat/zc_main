36b14aa1b0b1d451928fee3c1edc8d3af2f11f0d
kenechukwu offokansi <koffokansi@gmail.com>
2021-10-15 21:43:12 +0100

    topBarUpdated
    Former-commit-id: 1fb4f645330347129785393f9b9f0dec6fa2d38c [formerly 451562bde9cc4f7658b1871bfbabd37bcfad4284] [formerly ca3ee5d801c7aba42c78844a7da997a175200d5d [formerly 91badf4b13621d72e45ddee1ec258384d380fe3b [formerly 63dcbbf7d6d1b712d3c2df76fe655401abe44e91] [formerly 68edc525c9c40b462063d0c22980b16b964f757f]]]
    Former-commit-id: baa7ed3013fbacd16fe2df9f3f4183d4c6540c06 [formerly 91badf4b13621d72e45ddee1ec258384d380fe3b [formerly 63dcbbf7d6d1b712d3c2df76fe655401abe44e91]]
    Former-commit-id: ec40b1598ff20f7b04383da8edae37f5d428ba91
    Former-commit-id: e454f356e6c1c4db2c21d473feca52e000798a2d
    Former-commit-id: a803cd5abb5fc40b2489efab419fa7a7e6396d90

--- topbar/src/components/TopBarSearchModal.js
+++ topbar/src/components/TopBarSearchModal.js
@@ -1,83 +1,96 @@
 import styles from "../styles/TopBarSearchModal.module.css"
-import { useState } from "react"
+import { useState, useEffect, useContext } from "react"
 import axios from "axios"
 import SearchModalResult from "./ModalAutoCompleteResult"
 import { BigModal } from "./bigModal"
+import { FilterItem } from "./filterItem"
+import { plugins } from "../utils/topbarApi"
+import { ProfileContext } from "../context/ProfileModal"
 
 const base_URL = "https://jsonplaceholder.typicode.com/todos"
 
 const TopBarSearchModal = () => {
   const [value, setValue] = useState("")
-  const [items, setItems] = useState("")
+  const [items, setItems] = useState([])
   const [filter, setFilter] = useState("")
   const [isSearchOpen, setOpenSearch] = useState(false)
+  const [isLoading, setLoading] = useState(false)
 
-  const onSearchSubmit = e => {
+  const { user } = useContext(ProfileContext)
+
+  const pluginName = window.location.href
+  const newName = pluginName.split("/")
+
+  const exactPlugin = plugins.find(
+    plugin => newName[3] === (plugin.name || plugin.name + "#")
+  )
+  const onSearchSubmit = async e => {
     if (e.keyCode === 13 && value.length >= 1) {
       setOpenSearch(true)
+      setLoading(true)
+      let response = await exactPlugin.apiCall(
+        user.org_id,
+        user._id,
+        value,
+        filter
+      )
+      if (response.status >= 200 || response.status <= 299) {
+        //d
+      }
     }
   }
 
   const onInputChange = e => {
     setValue(e.target.value)
-    getData()
   }
-  function getData() {
-    axios.get(base_URL).then(res => {
-      const result = Object.values(res.data)
-        .map(item => {
-          return item
-        })
-        .filter(item => {
-          return item.title.includes(value)
-        })
-
-      const n = 10 //get the first 15 items
-      setItems(result.slice(0, n))
-    })
-  }
-
-  const pluginName = window.location.href
-  const newName = pluginName.split("/")
-  const plugins = [
-    {
-      name: "todo",
-      baseUrl: "todo.zuri.chat"
-    },
-    {
-      name: "music",
-      baseUrl: "music.zuri.chat"
-    },
-    {
-      name: "channels",
-      baseUrl: "channels.zuri.chat"
-    },
-    {
-      name: "dm",
-      baseUrl: "dm.zuri.chat"
-    },
-    {
-      name: "files",
-      baseUrl: "companyfiles.zuri.chat"
+  useEffect(() => {
+    function getData() {
+      axios.get(base_URL).then(res => {
+        const result = Object.values(res.data)
+        const n = 10 //get the first 15 items
+        setItems(result.slice(0, n))
+      })
     }
-  ]
-  const exactPlugin = plugins.find(
-    plugin => plugin.name === newName[3] || newName[3] + "#"
-  )
-  // console.log(newName)
-  // console.log(exactPlugin)
+    getData()
+  }, [])
 
+  const FilterList = items.map(item => (
+    <li key={item.id} className={styles.List}>
+      <button
+        onClick={e => {
+          e.stopPropagation()
+          setFilter(item.title)
+        }}
+        style={{ width: "100%", textAlign: "left" }}
+      >
+        <SearchModalResult title={item.title} />
+      </button>
+    </li>
+  ))
   return (
     <div className={styles.topBarSearchModal}>
-      <input
-        type="text"
-        className={styles._input}
-        placeholder="Search Here"
-        value={value}
-        onChange={onInputChange}
-        onKeyUp={onSearchSubmit}
-      />
-      <div className={styles.MainBox}>
+      <div className={styles._input}>
+        {filter === "" ? null : (
+          <FilterItem
+            onRemove={() => {
+              setFilter("")
+            }}
+            filter={filter}
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Search Here"
+          value={value}
+          onChange={onInputChange}
+          onKeyUp={onSearchSubmit}
+          className={styles._input2}
+        />
+      </div>
+      <div
+        className={styles.MainBox}
+        style={filter === "" ? {} : { display: "none" }}
+      >
         <div>
           <div className={styles.input_box}>
             <div className={styles.grow}>
@@ -111,20 +124,9 @@ const TopBarSearchModal = () => {
             </div>
           </div>
         </div>
-        <div></div>
         <ul className={styles.ListWrapper}>
-          {Array.isArray(items) ? (
-            items.map(item => (
-              <li
-                key={item.id}
-                className={styles.List}
-                onClick={() => {
-                  setFilter(item.title)
-                }}
-              >
-                <SearchModalResult title={item.title} />
-              </li>
-            ))
+          {items.length > 1 ? (
+            FilterList
           ) : (
             <li>
               <SearchModalResult title="" />
@@ -143,116 +145,3 @@ const TopBarSearchModal = () => {
     </div>
   )
 }
-
-export default TopBarSearchModal
-
-// import styles from "../styles/TopBarSearchModal.module.css"
-// import { useState } from "react"
-// import axios from "axios"
-// import SearchModalResult from "./SearchModalResults"
-// import { BigModal } from "./bigModal"
-
-// const base_URL = "https://jsonplaceholder.typicode.com/todos"
-
-// const TopBarSearchModal = () => {
-//   const [value, setValue] = useState("")
-//   const [items, setItems] = useState("")
-//   const [isSearchOpen, setOpenSearch] = useState(false)
-
-//   const onSearchSubmit = e => {
-//     if (e.keyCode === 13 && value.length >= 1) {
-//       setOpenSearch(true)
-//     }
-//   }
-
-//   const onInputChange = e => {
-//     setValue(e.target.value)
-//     getData()
-//   }
-
-//   function getData() {
-//     axios.get(base_URL).then(res => {
-//       const result = Object.values(res.data)
-//         .map(item => {
-//           return item
-//         })
-//         .filter(item => {
-//           return item.title.includes(value)
-//         })
-
-//       const n = 10 //get the first 15 items
-//       setItems(result.slice(0, n))
-//     })
-//   }
-//   return (
-//     <div className={styles.topBarSearchModal}>
-//       <input
-//         type="text"
-//         className={styles._input}
-//         placeholder="Search Here"
-//         value={value}
-//         onChange={onInputChange}
-//         onKeyUp={onSearchSubmit}
-//       />
-//       <div className={styles.MainBox}>
-//         <div>
-//           <div className={styles.input_box}>
-//             <div className={styles.grow}>
-//               <div className={styles.close_icon}>
-//                 <svg
-//                   focusable="false"
-//                   xmlns="http://www.w3.org/2000/svg"
-//                   viewBox="0 0 24 24"
-//                 >
-//                   <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-//                 </svg>
-//               </div>
-
-//               <input
-//                 type="text"
-//                 className={styles._MainInput}
-//                 placeholder="Search Here"
-//                 value={value}
-//                 onChange={onInputChange}
-//                 onKeyUp={onSearchSubmit}
-//               />
-//             </div>
-//             <div className={styles.close_icon}>
-//               <svg
-//                 focusable="false"
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 viewBox="0 0 24 24"
-//               >
-//                 <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-//               </svg>
-//             </div>
-//           </div>
-//         </div>
-//         <div></div>
-//         <ul className={styles.ListWrapper}>
-//           {Array.isArray(items) ? (
-//             items.map(item => (
-//               <li key={item.id} className={styles.List}>
-//                 <SearchModalResult title={item.title} />
-//               </li>
-//             ))
-//           ) : (
-//             <li>
-//               <SearchModalResult title="" />
-//             </li>
-//           )}
-//         </ul>
-//       </div>
-//       {isSearchOpen ? (
-//         <BigModal
-//           onClose={() => {
-//             setOpenSearch(false)
-//           }}
-//           inputValue={value}
-//         />
-//       ) : null}
-//     </div>
-//   )
-// }
-
-// export default TopBarSearchModal
