815a13756a49a8d32f70a79838c7d244b375195b
Paul Keno <paulkenoofficial.com>
2021-10-15 12:18:32 +0100

    change workspace name implementation
    Former-commit-id: ce1c131be78546e8ed151abf1a0f6e1f87e6e823 [formerly ea7e717b803b10aebeb6bbd69da0250a9e2ff90e] [formerly 89f0eec0895ce60592e6592f9523518ff16bb266 [formerly f21815e1991309ca523f0880fbc84036d2479c5f [formerly 035eec517030c0ea50b9f09b8e57e074230598e8] [formerly 42515eb6e5152e639f848ebd642e4fa3578e137d]]]
    Former-commit-id: ab5fac12f09efdd98aa68da29a7206e024783041 [formerly f21815e1991309ca523f0880fbc84036d2479c5f [formerly 035eec517030c0ea50b9f09b8e57e074230598e8]]
    Former-commit-id: 7e80d261f07fe0e3a2d3b0a108e5d7ac893afd61
    Former-commit-id: 159f7aff58c0a9e117aaa518dc5eea8866284547
    Former-commit-id: 0e09c77fea5cfb8c6cde72cc12a2cb5e86255629

--- control/src/pages/admin/Settings/components/ChangeWorkspaceName.js
+++ control/src/pages/admin/Settings/components/ChangeWorkspaceName.js
@@ -6,18 +6,25 @@ import Logo  from '../../assets/default.svg'
 import axios from 'axios'
 import { Link } from "react-router-dom"
 
-let currentWorkspace = localStorage.getItem("currentWorkspace")
+
 
 
 const ChangeWorkspaceName = () => {
 
     const [user, setUser] = useState(null)
     const [orgUrl, setOrgUrl] = useState(null)
-    const [orgName, setOrgName] = useState(orgName)
+    const [orgName, setOrgName] = useState("")
   
+
+    let currentWorkspace = localStorage.getItem("currentWorkspace")
+    
     useEffect(() => {
+
+
+      
+    
       const user = JSON.parse(sessionStorage.getItem("user"))
-  
+      
       if (user) {
         setUser(user)
       }
@@ -109,19 +116,20 @@ const ChangeWorkspaceName = () => {
                         </h2>
 
                             <div className={styles["change_workspace_text"]}>
-
-                            <p className={styles["change_workspace_p"]} >
-                            Your workspace name is displayed in menus and headings. It will
-                            usually be (or include) the name of your company.
+                              <div className={styles["change_workspace_flexp"]}>
+                                <p className={styles["change_workspace_p"]} >
+                                Your workspace name is displayed in menus and headings. It will
+                                usually be (or include) the name of your company.            
                         </p>
-
+                        </div>
+                        <div className={styles["inputdiv"]}>
                         <input
                                 type="text"
-                                placeholder="HNGI8"
+                               
                                 className={styles["change_workspace_input1"]}
                                 onChange={e => setOrgName(e.target.value)}
                             />
-
+                        </div>
                           </div>
                     </div>
 
@@ -151,15 +159,23 @@ const ChangeWorkspaceName = () => {
 
                            <div className={styles["change_workspace_flexinput"]}>
 
-                            <input
-                            type="text"
-                            name=""
-                            id=""
-                            placeholder="HNG"
-                            className={styles["change_workspace_input2"]}
-                            onChange={e => setOrgUrl(e.target.value)}
-                            />
-                            <span className="px-2">.zuri.chat</span>{' '}
+                            <div className={styles["change_workspace_flexsmallinput"]}>
+                                <input
+                                type="text"
+                                name=""
+                                id=""
+                                placeholder="HNG"
+                                className={styles["change_workspace_input2"]}
+                                onChange={e => setOrgUrl(e.target.value)}
+                                />
+                                <span className={styles["change_workspace_span"]}>.zuri.chat</span>{' '}
+                            </div>
+                            <div>
+                                <p className={styles["change_workspace_small"]}>
+                            Your workspace URL can only contain lowercase letters, numbers
+                            and dashes (and must start with a letter or number).
+                            </p>
+                            </div>
 
                                
 
@@ -171,18 +187,25 @@ const ChangeWorkspaceName = () => {
               </section>
 
                <section className={styles["change_workspace_section_last"]}>
-                        <p className={styles["change_workspace_small"]}>
-                        Your workspace URL can only contain lowercase letters, numbers
-                        and dashes (and must start with a letter or number).
+                        <div className={styles["Note_div"]}>
+                        <p className={styles["Note"]}>
+                            Note: If you change your workspace’s URL, Zurichat will
+                            automatically redirect from the old to the new address. However,
+                            you should still make sure everyone in your workspace knows about
+                            the change because the old name will be placed back into the pool
+                             and could be used by some other workspace in the future.
                         </p>
+                        </div>
 
 
+                        <div className={styles["Link"]} >
+                          <Link to='/admin/settings'>
+                          <button type="submit" onClick={updateName} className={styles["Button"]}>
+                          Save Changes
+                          </button>
+                          </Link>
 
-                        <Link to='/admin/settings'>
-                        <button type="submit" onClick={updateName} className={styles["Button"]}>
-                        Save Changes
-                        </button>
-                        </Link>
+                        </div>
 
                 
 
@@ -202,138 +225,3 @@ export default ChangeWorkspaceName;
 
 
 
-// import React, { useState, useEffect } from "react"
-// import { Helmet } from 'react-helmet'
-// import AdminSettings from '../..'
-// import styles[""] from "../styles[""]/changeworkspaceName.module.css";
-// import Logo  from '../../assets/default.svg'
-// import axios from 'axios'
-// import { Link } from "react-router-dom"
-
-// let currentWorkspace = localStorage.getItem("currentWorkspace")
-
-
-// const ChangeWorkspaceName = () => {
-
-//     const [user, setUser] = useState(null)
-//     const [orgUrl, setOrgUrl] = useState(null)
-//     const [orgName, setOrgName] = useState(orgName)
-  
-//     useEffect(() => {
-//       const user = JSON.parse(sessionStorage.getItem("user"))
-  
-//       if (user) {
-//         setUser(user)
-//       }
-//     }, []);
-
-    
-
-
-//     const updateName = () => {
-//         axios
-//           .patch(
-//             `https://api.zuri.chat/organizations/${currentWorkspace}/name`,
-//             {
-//                 organization_name: orgName
-//               },
-          
-//             {
-//               headers: {
-//                 Authorization: "Bearer " + user.token
-//               }
-//             }
-//           )
-//           .then(res => {
-            // Clears User Extracted Details from LS during Registraion
-    
-            
-            // setOrgId(res.data.data.organization_id)
-    
-            // Automatic Org Name Renaming From Default to new Org Name
-    //         setTimeout(() => {
-    //           axios.patch(
-    //             `https://api.zuri.chat/organizations/${currentWorkspace}/url`,
-    //             {
-    //               url: orgUrl
-    //             },
-    //             {
-    //               headers: {
-    //                 Authorization: "Bearer " + user.token
-    //               }
-    //             }
-    //           )
-    //           // .then(res => console.log(res))
-    //         }, 500)
-    
-           
-    //       })
-    //       .catch(err => {
-    //         console.error(err.message)
-    //       })
-    //   }
-
-    //   
-
-//     return (
-
-        
-//         <AdminSettings>
-//       <div>
-//         <Helmet>
-//           <title>Settings - Zuri Chat</title>
-//         </Helmet>
-//       </div>
-//         <div className="app bg-light pb-5">
-//             <div className="container py-5 d-flex">
-//               <img src={Logo} alt="" />
-//                 <h2 className="px-3" styles[""]={{ fontWeight: '700' }}>
-//                     Change Workspace Name
-//                 </h2>
-//             </div>
-
-//             <div className="container bg-white mb-5 px-5 rounded">
-//                 <div className="row">
-//                     <div className="col-sm-6">
-//                         <h2 className="my-3" styles[""]={{ fontWeight: '700' }}>
-//                             Workspace Name
-//                         </h2>
-//                         <p className="mb-5">
-//                             Your workspace name is displayed in mrnus and headings. It will
-//                             usually be (or include) the name of your company.
-//                         </p>
-
-//                         <h2 styles[""]={{ fontWeight: '700' }}>Workspace URL</h2>
-//                         <p className="mb-5">
-//                             You can change your workspace’s URL (web address), but out of
-//                             courtesy to your fellow Zurichat users, please don’t do this often
-//                             :)
-//                         </p>
-
-//                         <p className={styles[""].Note}>
-//                             Note: If you change your workspace’s URL, Zurichat will
-//                             automatically redirect from the old to the new address. However,
-//                             you should still make sure everyone in your workspace knows about
-//                             the change because the old name will be placed back into the pool
-//                             and could be used by some other workspace in the future.
-//                         </p>
-//                     </div>
-
-//                     <div className="col-sm-6">
-//                         <form action="" method="post" className="pt-5">
-                           
-                           
-                           
-                           
-//                         </form>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//         </AdminSettings>
-//     );
-// }
-
-
-
-// export default ChangeWorkspaceName
