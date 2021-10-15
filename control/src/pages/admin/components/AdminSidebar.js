3b0ec33aac43a4568c8179c210b62f37aa6ae302
Paul Keno <paulkenoofficial.com>
2021-10-15 20:50:20 +0100

    proper routing of workspace settings sidebar links
    Former-commit-id: 4d3247e3b4c0a32931442de473b78bfdb613c9aa

--- control/src/pages/admin/components/AdminSidebar.js
+++ control/src/pages/admin/components/AdminSidebar.js
@@ -37,6 +37,7 @@ import { getUser } from "../Utils/Common"
 const AdminSidebar = ({ setModal, openModal }) => {
   const user = getUser()
 
+ 
   return (
     <div
       className={
@@ -256,6 +257,14 @@ const AdminSidebar = ({ setModal, openModal }) => {
           <Link
             onClick={() => setModal(!openModal)}
             className={styles.sidebarLink}
+            to="/help"
+          >
+            Help
+            
+          </Link>
+          <Link
+            onClick={() => setModal(!openModal)}
+            className={styles.sidebarLink}
             to="/"
           >
             API
@@ -271,21 +280,21 @@ const AdminSidebar = ({ setModal, openModal }) => {
           <Link
             onClick={() => setModal(!openModal)}
             className={styles.sidebarLink}
-            to="/"
+            to="/pricing"
           >
             Pricing
           </Link>
           <Link
-            onClick={() => setModal(!openModal)}
+            
             className={styles.sidebarLink}
-            to="/"
+            to="/blog"
           >
             Our blog
           </Link>
           <Link
-            onClick={() => setModal(!openModal)}
+            
             className={styles.sidebarLink}
-            to="/"
+            to="/signout"
           >
             Sign out
             <FiLogOut className={styles.iconRight} />
