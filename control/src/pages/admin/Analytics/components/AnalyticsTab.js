1ef92307a1841e1ac30f7605022ddeb6d56f84d5
Babsmutini <scotteyawo@gmail.com>
2021-10-15 17:33:53 +0100

    Styling Refactoring for workspace settings
    Edited the Header to display just the workspace name and home icon
    Changed the path of the overview css file
    Added the icon for the analytics page title
    
    
    Former-commit-id: e3e0fe4493c3d2944b4f19135259bd6861c08f7a [formerly 83bc5e6ab2c00461eeeff663e2086f6ce5e4588c] [formerly 50f0955cbbcbf9736861a064176f2126dba8abd8 [formerly 38debb0207466c6e6544a9b37674fee46d32a56f [formerly 7721cc1e7a3cbd26a13813d22265ea67821d50a7] [formerly bb9d025c85072c36f9131d741c776d8de31f1787]]]
    Former-commit-id: 017a144a26dd8a76a8fb41a24f36932b83f5d36b [formerly 38debb0207466c6e6544a9b37674fee46d32a56f [formerly 7721cc1e7a3cbd26a13813d22265ea67821d50a7]]
    Former-commit-id: 6d0e8561dcdb0864712b85922565e67614c4e9c8
    Former-commit-id: 272b2cf85df776cf0e55b09914668818d423bed1
    Former-commit-id: 980a33870ffc8baf5f10090657f5ff382e9c1f03

--- control/src/pages/admin/Analytics/components/AnalyticsTab.js
+++ control/src/pages/admin/Analytics/components/AnalyticsTab.js
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 
 import styles from "../styles/AdminAnalyticsTab.module.css"
 
+import { IoSpeedometerOutline} from "react-icons/io5"
+
 // components
 // import AuthTab from './AuthTab'
 // import MembersTab from '../../components/MembersTab'
@@ -17,7 +19,10 @@ const AdminSettingsTab = () => {
   const [active, setActive] = useState(1)
 
   return (
+    <>
+    
     <div className={`${styles.tabWrapper} mt-4`}>
+    <h2 className={`mb-2 pt-3 ${styles.analytics_title}`}><IoSpeedometerOutline color="#489379" /> {""} Analytics</h2>
       <div onClick={() => setOpenTab(!openTab)} className={styles.mobileToggle}>
         <FiMenu className={styles.menuIcon} />
       </div>
@@ -70,6 +75,7 @@ const AdminSettingsTab = () => {
         {/* {active === 3 && <AuthTab />} */}
       </div>
     </div>
+    </>
   )
 }
 
