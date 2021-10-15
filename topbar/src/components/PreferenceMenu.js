9a767a8cbe798579cf1c66e1ca7912d99c35a9db
agkayster <ejike.chiboka@gmail.com>
2021-10-15 18:11:41 +0100

    committing my local code before pulling

--- topbar/src/components/PreferenceMenu.js
+++ topbar/src/components/PreferenceMenu.js
@@ -20,13 +20,13 @@ const PreferenceMenu = () => {
           <p>Notifications</p>
         </div>
 
-        <div
+        {/* <div
           onClick={() => setSideBar(3)}
           className={sideBar === 3 ? styles.active : styles.one}
         >
           <AiOutlineEye className={styles.icon} />
           <p>Themes</p>
-        </div>
+        </div> */}
         <div
           onClick={() => setSideBar(4)}
           className={sideBar === 4 ? styles.active : styles.one}
