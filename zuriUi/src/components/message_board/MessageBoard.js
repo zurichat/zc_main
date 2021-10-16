c566db6d41867e56f4d3eef51c3181ee55437609
Eniola Osabiya <eosabiya@gmail.com>
2021-10-16 19:50:15 +0100

    fixed messageBoard reuseable component and entry point to dms for all dms
    Former-commit-id: e6a284117b69a9eac80726d642f77c3ebdb740ea [formerly b50ef569de4e1ad8688e2039c949fc2f2b86166c] [formerly fac1bc37ab633c6315ea69128ac7b06f92d048b6 [formerly 627f84c63b1bb15c566f4bd7f8290fef66e5ebaf [formerly 2a22e2a688c9dc65d9070bf387be3baa5dd9828d] [formerly 45acc0c426923c2741b0d71c480adecb851f9417]]]
    Former-commit-id: a30b984b7824ed9b9a6c5feb1d0389bdf2513fb5 [formerly 627f84c63b1bb15c566f4bd7f8290fef66e5ebaf [formerly 2a22e2a688c9dc65d9070bf387be3baa5dd9828d]]
    Former-commit-id: 41d05796d36375ec7ab391a48b5140e6753552af
    Former-commit-id: c709632758f4c3b766865b635e0da4b0e28dc5f4
    Former-commit-id: cc5a73de4c8a641c6260f1beff162cdadb036a86

--- zuriUi/src/components/message_board/MessageBoard.js
+++ zuriUi/src/components/message_board/MessageBoard.js
@@ -128,6 +128,7 @@ function MessageBoard({ chatsConfig }) {
           <MessageInputBox
             currentUserData={chatsConfig.currentUserData}
             addToMessages={addToMessages}
+            sendMessageHandler={chatsConfig.sendChatMessageHandler}
           />
         </div>
       </ChatContainer>
