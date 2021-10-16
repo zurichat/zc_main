9187429f2ad7417f12691be9f80360e3f4bb49f1
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-10-16 01:33:11 +0100

    upload workspace icon functionality
    Former-commit-id: 13b550a1e93d6d0711eef48b720497e821439ed1

--- control/src/pages/admin/Settings/components/LogoCrop.js
+++ control/src/pages/admin/Settings/components/LogoCrop.js
@@ -1,47 +1,58 @@
 import React, { useState } from "react"
+import axios from "axios"
 import ReactCrop from "react-image-crop"
 import "react-image-crop/dist/ReactCrop.css"
-import { WorkSPaceLogoContainer, WorkSpaceName, CropButtons, Button } from "./workSpaceIconChange"
+import {
+  WorkSPaceLogoContainer,
+  WorkSpaceName,
+  CropButtons,
+  Button
+} from "./workSpaceIconChange"
 
-const LogoCrop = ({ logo, setToggle }) => {
-  const [crop, setCrop] = useState({ unit: "%", width: 30, aspect: 16 / 9 })
+const LogoCrop = ({ logo, setToggle, setAlertToggle }) => {
+  const [crop, setCrop] = useState({ unit: "%", width: 100, aspect: 16 / 9 })
 
   const organisation_id = localStorage.getItem("currentWorkspace")
+  let token = sessionStorage.getItem("token")
 
   const handleIconUpload = () => {
-    const formData = new formData()
-    formData.append("file", updateLogo)
-    axios
-      .patch(
-        `https://api.zuri.chat/organisations/${organisation_id}/logo`,
-        formData
-      )
-      .then(res => {
-        console.log(res.message)
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    // axios
+    //   .patch(
+    //     `https://api.zuri.chat/organisations/${organisation_id}/logo`,
+    //     crop,
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${token}`,
+    //         "content-type": "multipart/form-data"
+    //       }
+    //     }
+    //   )
+    //   .then(res => {
+    //     // eslint-disable-next-line no-console
+    //     console.log(res.message)
+    //     setAlertToggle(true)
+    //   })
+    //   .catch(error => {
+    //     // eslint-disable-next-line no-console
+    //     console.log(error.message)
+    //   })
+      setAlertToggle(true)
+      setToggle(false)
   }
 
   const cancelUpload = () => {
     setToggle(false)
   }
-
-  <WorkSPaceLogoContainer>
-    <WorkSpaceName>Crop Workspace Icon</WorkSpaceName>
-    <ReactCrop
-      src={logo}
-      onImageLoaded={onLoad}
-      crop={crop}
-      onChange={c => setCrop(c)}
-      onComplete={c => setCompletedCrop(c)}
-    />
-    <CropButtons>
-      <Button onClick={handleIconUpload}>Crop Icon</Button>
-      <Button onClick={cancelUpload}>Cancel</Button>
-    </CropButtons>
-  </WorkSPaceLogoContainer>
+  return (
+    <WorkSPaceLogoContainer>
+      <WorkSpaceName>Crop Workspace Icon</WorkSpaceName>
+      <ReactCrop src={logo} crop={crop} onChange={newCrop => setCrop(newCrop)} />
+      <CropButtons>
+        <Button onClick={handleIconUpload}>Crop Icon</Button>
+        <Button onClick={cancelUpload}>Cancel</Button>
+      </CropButtons>
+    </WorkSPaceLogoContainer>
+  )
 }
 
 export default LogoCrop
