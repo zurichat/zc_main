c0b682e59b26173c445396909ad310478baf78fe
Otuokere Chibuchi Elizabeth <chibuchio@gmail.com>
2021-10-16 14:34:07 +0000

    implemented deprecation page
    Former-commit-id: d4e258c6661123f787095b2cd76b8dadd2737186

--- control/src/pages/admin/Deprecation.js
+++ control/src/pages/admin/Deprecation.js
@@ -6,7 +6,7 @@ import styles from './styles/deprecation.module.css'
 
 const Deprecation = () => {
   return (
-    <div className={styles.Deprecation}>
+    <div className={styles.deprecation}>
         <div  className={styles.upper}>
           <h1 className={styles.caps}>Apps & Custom Integrations Impacted by Deprecations
           </h1>
@@ -33,15 +33,25 @@ const Deprecation = () => {
       <div className={styles.export}><button>Export</button></div>
       </div>
       </div>
+
         <div className={styles.secondbox}>
-      <h1 className={styles.caps}>Legacy Workspace Apps
+            <div className={styles.legacy}>
+            <h1 className={styles.caps}>Legacy Workspace Apps
           </h1>
           <div className={styles.firstline}><p>We’re retiring a rare form of Zuri app that was in developer preview and discontinued in October 2018. These apps will completely stop functioning on August 24, 2022.</p></div>
            <div className={styles.secondline}><p> This list includes all legacy workspace apps, whether they are actively used or not.</p></div>
-          <div><p className={styles.first}>Date Announced</p>
-              <p>March 15, 2021</p>
-          <p className={styles.first}>Deprecation Date</p>
-              <p>August 24, 2022</p></div>
+             </div>
+
+             <div className={styles.updates}>
+                 <div>
+                <p className={styles.bold}>Date Announced</p>
+                    <p>March 15, 2021</p></div>
+                    <div>
+                <p className={styles.bold}>Deprecation Date</p> 
+                <p>August 24, 2022</p>
+                </div>
+              </div>
+            </div>
          <div className={styles.link_announcement}> <a href="#">view announcement</a></div>
         <div className={styles.list}>
           <p>Impacted App</p>
@@ -51,7 +61,7 @@ const Deprecation = () => {
          <div className={styles.apps_found}> 
            No apps found 
           </div>
-          </div> 
+           
 
 
 
