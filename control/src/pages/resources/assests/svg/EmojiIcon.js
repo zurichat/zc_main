dd1a2f887cfade5637f4de9e16710c62c973449c
Damilola Akinlade <akinlade.co@gmail.com>
2021-10-16 11:50:32 +0100

    cleanup

---
+++ control/src/pages/resources/assests/svg/EmojiIcon.js
@@ -0,0 +1,73 @@
+import React from "react"
+
+const EmojiIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      id="add-reaction"
+    >
+      <rect
+        id="Rectangle_3"
+        data-name="Rectangle 3"
+        width="24"
+        height="24"
+        fill="none"
+      />
+      <path
+        id="Oval"
+        d="M8.4,0a8.4,8.4,0,0,0,0,16.8h0a8.4,8.4,0,0,0,8.4-8.4"
+        transform="translate(2.4 4.8)"
+        fill="none"
+        stroke="#000"
+      />
+      <path
+        id="Oval-2"
+        data-name="Oval"
+        d="M0,0A4.807,4.807,0,0,0,1.042,1.557,4.785,4.785,0,0,0,4.436,2.963h0A4.785,4.785,0,0,0,7.83,1.557,4.807,4.807,0,0,0,8.872,0"
+        transform="translate(6.364 15.037)"
+        fill="none"
+        stroke="#000"
+      />
+      <circle
+        id="Oval-3"
+        data-name="Oval"
+        cx="1.2"
+        cy="1.2"
+        r="1.2"
+        transform="translate(7.2 9.6)"
+        fill="#fff"
+        stroke="#000"
+      />
+      <circle
+        id="Oval-4"
+        data-name="Oval"
+        cx="1.2"
+        cy="1.2"
+        r="1.2"
+        transform="translate(12 9.6)"
+        fill="#fff"
+        stroke="#000"
+      />
+      <path
+        id="Line"
+        d="M1.2,0V7.2"
+        transform="translate(18 1.2)"
+        fill="none"
+        stroke="#000"
+      />
+      <path
+        id="Line-2"
+        data-name="Line"
+        d="M0,1.2H7.2"
+        transform="translate(15.6 3.6)"
+        fill="none"
+        stroke="#000"
+      />
+    </svg>
+  )
+}
+
+export default EmojiIcon
---
+++ control/src/pages/resources/assests/svg/OptionsIcon.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const OptionsIcon = () => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        id="svg10"
+      >
+        <title id="title2">ellipsis vertical</title>
+        <g>
+          <circle cx="-10" cy="10" r="2" id="circle4" transform="rotate(-90)" />
+          <circle cx="-17" cy="10" r="2" id="circle6" transform="rotate(-90)" />
+          <circle cx="-3" cy="10" r="2" id="circle8" transform="rotate(-90)" />
+        </g>
+      </svg>
+    </div>
+  )
+}
+
+export default OptionsIcon
