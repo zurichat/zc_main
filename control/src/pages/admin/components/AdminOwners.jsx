30d3e76299a755e38a3c0beba9087e39fd1178ce
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-10-15 21:56:11 -0700

    workaround for adminandowners tab
    Former-commit-id: 0eff63a396e316f3a1a1d007954cc4b951a170a0

--- control/src/pages/admin/components/AdminOwners.jsx
+++ control/src/pages/admin/components/AdminOwners.jsx
@@ -1,10 +1,31 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styles from "../styles/adminowners.module.css"
 import { Link } from "react-router-dom"
 import { FiSearch } from "react-icons/fi"
 import { authAxios } from "../Utils/Api"
 
-const AdminOwners = () => {
+const AdminOwners = ({ admins }) => {
+  const [adminObj, setAdminObj] = useState(admins)
+  const [filtered, setFiltered] = useState(null)
+
+  const handleChange = e => {
+    filterAdmins(e.target.value)
+    //eslint-disable-next-line
+    console.log(e)
+  }
+
+  const filterAdmins = text => {
+    const filteredAdmins = adminObj.filter(eachObj => {
+      const regex = new RegExp(`${text}`, "gi")
+      return (
+        eachObj.email.match(regex) ||
+        eachObj.first_name.match(regex) ||
+        eachObj.last_name.match(regex)
+      )
+    })
+    setFiltered(filteredAdmins)
+  }
+
   return (
     <div className={styles.adminowners}>
       <h3 className={styles.heading}>Sort By</h3>
@@ -21,10 +42,15 @@ const AdminOwners = () => {
           <input
             placeholder="Search Admins and Owners"
             className={styles.input}
+            onChange={handleChange}
+            type="text"
           />
         </div>
       </div>
       {/* end of role and search */}
+      {filtered !== null
+        ? filtered.map(person => <div key={person._id}>{person.email}</div>)
+        : adminObj.map(person => <div key={person._id}>{person.email}</div>)}
     </div>
   )
 }
