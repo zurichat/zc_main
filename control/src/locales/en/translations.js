618e31c11b357c1ba499c51211d754c6c8a6c8d4
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2021-10-16 16:25:36 +0100

    Update translations.js
    Former-commit-id: 00edcb97c4b01781fb61802f93ffd43514354d37

--- control/src/locales/en/translations.js
+++ control/src/locales/en/translations.js
@@ -2,6 +2,9 @@ export const TRANSLATIONS_EN = {
   header: {
     language_select: "Change Language"
   },
+
+
+  // Landing Page
   landing: {
     title: {
       landing: ""
@@ -87,6 +90,7 @@ export const TRANSLATIONS_EN = {
       socialMedia: "Social Media",
       rights: " All Rights Reserved"
     },
+    
     cookies: {
       textOne: "We use third-party",
       textTwo: "cookies",
@@ -96,7 +100,10 @@ export const TRANSLATIONS_EN = {
     }
   },
 
-  pricing: {
+
+// Pricing Page
+  pricing: 
+  {
     title: {
       pricing: "Pricing - Zuri Chat"
     },
@@ -111,13 +118,12 @@ export const TRANSLATIONS_EN = {
     cardThree: {
       duration_one: "Annual",
       duration_two: "Monthly",
-      button: "Get Started"
+      button: "Get Started",
     },
 
     sectionThree: {
       headline: "Leading the way in enterprise security",
-      description:
-        "In addition to encryption in transit and at rest, we operate comprehensive compliance ans assurance programs.",
+      description: "In addition to encryption in transit and at rest, we operate comprehensive compliance ans assurance programs.",
       link: "Learn more about security..."
     },
 
@@ -134,6 +140,8 @@ export const TRANSLATIONS_EN = {
     }
   },
 
+
+// Plugin Page
   plugin: {
     title: {
       plugin: "Connect and Interact"
@@ -165,7 +173,8 @@ export const TRANSLATIONS_EN = {
 
     games: {
       title: "Chess Plugin",
-      headline: "Make learning fun, stay engaged with the virtual games",
+      headline:
+        "Make learning fun, stay engaged with the virtual games",
       description:
         "Zuri chesss plugin is a web based recreational and competitive board game played between two players.",
       link: "Explore Games Plugin"
@@ -173,7 +182,8 @@ export const TRANSLATIONS_EN = {
 
     goal: {
       title: "Company Goal's Plugin",
-      headline: "Keep track of Goals",
+      headline:
+        "Keep track of Goals",
       description:
         "A plugin that allows you to keep track of company's goals and achievements",
       link: "Explore Companys Goal Plugin"
@@ -181,7 +191,8 @@ export const TRANSLATIONS_EN = {
 
     todo: {
       title: "Todo Plugin",
-      headline: "Keeps track of important task",
+      headline:
+        "Keeps track of important task",
       description:
         "A plugin that allows you list everything that you have to do and make sure that your tasks are written down all in one place so you don't forget anything important.",
       link: "Explore Todo's plugin"
@@ -189,7 +200,8 @@ export const TRANSLATIONS_EN = {
 
     channels: {
       title: "Channels Plugin",
-      headline: "Organizes conversations into dedicated spaces",
+      headline:
+        "Organizes conversations into dedicated spaces",
       description:
         "A plugin that lets a group of indivduals in a channel know about timely, relevant informations in order to achieve a certain goal",
       link: "Explore Channels plugin"
@@ -197,7 +209,8 @@ export const TRANSLATIONS_EN = {
 
     noticeboard: {
       title: "Noticeboard Plugin",
-      headline: "Share Information Effectively",
+      headline:
+        "Share Information Effectively",
       description:
         "Its an Effective plugin for displaying information and communicating with collaborators",
       link: "Explore Noticeboard Plugin"
@@ -205,7 +218,8 @@ export const TRANSLATIONS_EN = {
 
     company_files: {
       title: "Company Files Plugin",
-      headline: "Access and Manages Company's Files Effectively",
+      headline:
+        "Access and Manages Company's Files Effectively",
       description:
         "An effective file management system that improves business workflow, organizes important data and provides a searchable database for quick retrieval.",
       link: "Explore Company Files Plugins"
@@ -213,15 +227,17 @@ export const TRANSLATIONS_EN = {
 
     company_sales: {
       title: "Company Sales Plugin",
-      headline: "Keeps track of Sales record",
+      headline:
+        "Keeps track of Sales record",
       description:
         "An effective sales system that keeps track of company's sales records.",
       link: "Explore Company Sales Plugins"
     },
-
+    
     deadline: {
       title: "Deadline Plugins",
-      headline: "Deadline Tracker",
+      headline:
+        "Deadline Tracker",
       description:
         "A plugin that keeps track of deadlines of task, events, projects assigned to members of an organisation",
       link: "Explore Deadline Plugins"
@@ -229,7 +245,8 @@ export const TRANSLATIONS_EN = {
 
     expenses: {
       title: "Expenses Plugins",
-      headline: "Keeps track of Expenses",
+      headline:
+        "Keeps track of Expenses",
       description:
         "A plugin that helps to keep an accurate record of a company's expenses",
       link: "Explore Expenses Plugins"
@@ -237,7 +254,8 @@ export const TRANSLATIONS_EN = {
 
     duty_shift: {
       title: "Duty Shift Plugin",
-      headline: "Tracking your shifts has never been easier!",
+      headline:
+        "Tracking your shifts has never been easier!",
       description:
         "A plugin that helps indiviuals track their working hours and communicate with other employees.",
       link: "Explore Duty Shift Plugin"
@@ -245,15 +263,17 @@ export const TRANSLATIONS_EN = {
 
     external_tools: {
       title: "External Tools Plugins",
-      headline: "Usage of 3rd Party Tools",
+      headline:
+        "Usage of 3rd Party Tools",
       description:
         "The External Tools plugin is a feature which allows users to interact with third party tools like Google Drive, Figma, Github, etc. to make work flow for themselves or their team faster and easier",
       link: "Explore Tools Plugin"
     },
-
+    
     whiteboard: {
       title: "Whiteboard Plugin",
-      headline: "Idea sharing and Team collaborations",
+      headline:
+        "Idea sharing and Team collaborations",
       description:
         "The whiteboard plugin is used for visualizing thoughts, concepts, write down ideas, explain and teach, to plan and create within a certain group of individuals",
       link: "Explore Whiteboard Plugins"
@@ -261,7 +281,8 @@ export const TRANSLATIONS_EN = {
 
     holiday: {
       title: "Holiday Calender Plugin",
-      headline: "Schedules Events or holidays for staffs",
+      headline:
+        "Schedules Events or holidays for staffs",
       description:
         "Company Holiday Calendar Plugin is a plugin that enables calendar functions mainly to organize or schedule holidays or events for a company",
       link: "Explore Company's Calender Plugin"
@@ -269,7 +290,8 @@ export const TRANSLATIONS_EN = {
 
     company_shift: {
       title: "Company Shift Plugin",
-      headline: "Monitors a Company's Working Hours",
+      headline:
+        "Monitors a Company's Working Hours",
       description:
         "A plugin that keeps track of a company's working hour and how it is distributed among employees of the company.",
       link: "Explore Company Shift Plugin"
@@ -277,9 +299,8 @@ export const TRANSLATIONS_EN = {
 
     signup: {
       headline: "Get plugins that you would enjoy",
-      description:
-        "Integrate your favorite plugins and get more exciting experience",
-      link: "Discover more"
+      description: "Integrate your favorite plugins and get more exciting experience",
+      link: "Discover more",
     },
 
     subscribe: {
@@ -292,6 +313,8 @@ export const TRANSLATIONS_EN = {
     }
   },
 
+  
+  // About Page
   about: {
     title: {
       about: ""
@@ -383,80 +406,11 @@ export const TRANSLATIONS_EN = {
       link_sales: "Talk to sales",
       link_try: "Try it for free"
     },
-
-    careers: {
-      title: {
-        careers: "Careers"
-      },
-      section_one: {
-        headline: "Careers at Zuri Chat",
-        post_headline:
-          "Passionate about making work life simpler, and more productive? Join our remote-friendly and flexible team.",
-        join_link: "Join Zuri Chat"
-      },
-      career_grid: {
-        headline: "Join Our Internship",
-        post_headline:
-          "Zuri Internship is a 100% free, remote and fast paced training for young professionals seeking to hon their tech skills and boost their resume.",
-        categories: {
-          category_frontend: {
-            headline: "Frontend Development",
-            post_headline:
-              "This track deals with the aspect of your application that the users interact with. A front-end developer will be able to correctly interpret a given design to the user interface."
-          },
-          category_backend: {
-            headline: "Backend Development",
-            post_headline:
-              "This track deals with the aspect of the application the user does not directly interact with. It allows the frontend function either by running some back-end code or connecting to the database."
-          },
-          category_design: {
-            headline: "Design",
-            post_headline:
-              "This track deals with creating a graphic plan for an application. You will learn to convert project documentation into viewable and understandable graphic design for the developers to work with."
-          },
-          category_mobile: {
-            headline: "Mobile Development",
-            post_headline:
-              "The mobile track contains a list of tools and languages needed for a developer to create mobile applications. A mobile application can be IOS or Android."
-          },
-          category_digital_marketing: {
-            headline: "Digital Marketing",
-            post_headline:
-              "It encomprises the use of digital means and utilities to boost discoverability of products, services and brands."
-          },
-          category_frameworks: {
-            headline: "Frameworks",
-            post_headline:
-              "During the program we will be exploring several useful frameworks to give you an edge in the industry and help you develop complex applications much faster."
-          }
-        },
-        join_cohort: "Be a part of our next cohort starting summer of 2022",
-        register_now: "Register Now"
-      },
-      section_two: {
-        headline: "Working and Thriving",
-        post_headline:
-          "We offer benefits that help you feel your best and do your best.",
-        digital_wellness: {
-          headline: "Digital-First Office",
-          post_headline:
-            "With remote roles, office-free onboarding and flexible working practices, we are set up for ever-changing realities, schedules and responsibilities."
-        },
-        just_for_you: {
-          headline: "Just For You",
-          post_headline:
-            "Receive an annual allowance for professional development, plus a monthly stipend to cover your needs; A new work look, a pet, whatever matters most to you."
-        },
-        health_and_wellness: {
-          headline: "Health and Wellness",
-          post_headline:
-            "Zuri Pays 100% of your premiums. We also offer access to a range of free mental health and well-being resources."
-        }
-      }
-    }
   },
 
-  login: {
+
+  // Login Page
+  login:{
     title: "Login - Zuri Chat",
     form: {
       header: "Login",
@@ -483,6 +437,71 @@ export const TRANSLATIONS_EN = {
     }
   },
 
+  
+  // Career page
+  careers: {
+    title: {
+      careers: "Careers"
+    },
+    section_one: {
+      headline: "Careers at Zuri Chat",
+      post_headline: "Passionate about making work life simpler, and more productive? Join our remote-friendly and flexible team.",
+      join_link: "Join Zuri Chat"
+    },
+    career_grid: {
+      headline: "Join Our Internship",
+      post_headline:
+        "Zuri Internship is a 100% free, remote and fast paced training for young professionals seeking to hon their tech skills and boost their resume.",
+      categories: {
+        category_frontend: {
+          headline: "Frontend Development",
+          post_headline: "This track deals with the aspect of your application that the users interact with. A front-end developer will be able to correctly interpret a given design to the user interface."
+        },
+        category_backend: {
+          headline: "Backend Development",
+          post_headline: "This track deals with the aspect of the application the user does not directly interact with. It allows the frontend function either by running some back-end code or connecting to the database."
+        },
+        category_design: {
+          headline: "Design",
+          post_headline: "This track deals with creating a graphic plan for an application. You will learn to convert project documentation into viewable and understandable graphic design for the developers to work with."
+        },
+        category_mobile: {
+          headline: "Mobile Development",
+          post_headline: "The mobile track contains a list of tools and languages needed for a developer to create mobile applications. A mobile application can be IOS or Android."
+        },
+        category_digital_marketing: {
+          headline: "Digital Marketing",
+          post_headline: "It encomprises the use of digital means and utilities to boost discoverability of products, services and brands."
+        },
+        category_frameworks: {
+          headline: "Frameworks",
+          post_headline: "During the program we will be exploring several useful frameworks to give you an edge in the industry and help you develop complex applications much faster."
+        }
+
+      },
+      join_cohort: "Be a part of our next cohort starting summer of 2022",
+      register_now: "Register Now"
+    },
+    section_two: {
+      headline: "Working and Thriving",
+      post_headline: "We offer benefits that help you feel your best and do your best.",
+      digital_wellness: {
+        headline: "Digital-First Office",
+        post_headline: "With remote roles, office-free onboarding and flexible working practices, we are set up for ever-changing realities, schedules and responsibilities."
+      },
+      just_for_you: {
+        headline: "Just For You",
+        post_headline: "Receive an annual allowance for professional development, plus a monthly stipend to cover your needs; A new work look, a pet, whatever matters most to you."
+      },
+      health_and_wellness: {
+        headline: "Health and Wellness",
+        post_headline: "Zuri Pays 100% of your premiums. We also offer access to a range of free mental health and well-being resources."
+      }
+    }
+  },
+
+
+// All download pages
   download_desktop: {
     title: {
       download: ""
@@ -490,8 +509,7 @@ export const TRANSLATIONS_EN = {
 
     section_one: {
       headline: "Experience the Zuri Chat’s desktop and mobile apps",
-      post_headline:
-        "Connecting with your team has never been easier with Zuri Chat.",
+      post_headline: "Connecting with your team has never been easier with Zuri Chat.",
       desktop_line: "Zuri for Desktop",
       desktop_app: "Download on Mac App Store",
       desktop_soft: "Get it from Microsoft",
@@ -501,16 +519,15 @@ export const TRANSLATIONS_EN = {
     },
 
     section_two: {
-      headline:
-        "Download and enjoy the Zuri Chat experience on any these devices",
+      headline: "Download and enjoy the Zuri Chat experience on any these devices",
       windows: "Windows",
       mac: "macOS",
       ios: "iOS",
-      android: "Android"
+      android: "Android",
     },
 
     section_three: {
-      headline: "Same feel, same team on Zuri App!"
+      headline: "Same feel, same team on Zuri App!",
     }
   },
   download_ios: {
@@ -522,9 +539,8 @@ export const TRANSLATIONS_EN = {
       headline: "Download Zuri Chat beta for iOS.",
       post_headline: "Get Zuri Chat DM & Channel",
       mac_line: "We’re on Mac, too",
-      mac_description:
-        "Keep up with the conversation with our apps for Mac, Windows and Linux. We can email you a quick download link to get you started!",
-      button: "GET DOWNLOAD LINK"
+      mac_description: "Keep up with the conversation with our apps for Mac, Windows and Linux. We can email you a quick download link to get you started!",
+      button: "GET DOWNLOAD LINK",
     }
   },
   download_mac: {
@@ -534,16 +550,14 @@ export const TRANSLATIONS_EN = {
 
     section_one: {
       headline: "Experience Zuri Chat on MacOS",
-      post_headline:
-        "Connecting with your team has never been easier with Zuri Chat.",
+      post_headline: "Connecting with your team has never been easier with Zuri Chat.",
       download: "DOWNLOAD",
       download_soft: "Download for Windows?"
     },
 
     section_two: {
       headline: "Why Zuri Chat App?",
-      post_headline:
-        "Never miss the seamless collaboration experience even on the go with the zuri chat pc",
+      post_headline: "Never miss the seamless collaboration experience even on the go with the zuri chat pc",
       android: "Android",
       and_break: "and",
       ios: "iOS",
@@ -563,13 +577,11 @@ export const TRANSLATIONS_EN = {
 
     section_one: {
       headline: "Experience the Zuri Chat’s desktop and mobile apps",
-      post_headline:
-        "Connecting with your team has never been easier with Zuri Chat."
+      post_headline: "Connecting with your team has never been easier with Zuri Chat.",
     },
 
     section_two: {
-      headline:
-        "Download and enjoy the Zuri Chat experience on any these devices",
+      headline: "Download and enjoy the Zuri Chat experience on any these devices",
       windows: "Windows",
       mac: "macOS",
       android: "Android",
@@ -577,7 +589,7 @@ export const TRANSLATIONS_EN = {
     },
 
     section_three: {
-      headline: "Same feel, same team on Zuri App!"
+      headline: "Same feel, same team on Zuri App!",
     }
   },
   download_windows: {
@@ -587,16 +599,14 @@ export const TRANSLATIONS_EN = {
 
     section_one: {
       headline: "Experience Zuri Chat on Windows",
-      post_headline:
-        "Connecting with your team has never been easier with Zuri Chat.",
+      post_headline: "Connecting with your team has never been easier with Zuri Chat.",
       download: "DOWNLOAD",
       download_mac: "Download for Mac?"
     },
 
     section_two: {
       headline: "Why Zuri Chat App?",
-      post_headline:
-        "Never miss the seamless collaboration experience even on the go with the zuri chat pc",
+      post_headline: "Never miss the seamless collaboration experience even on the go with the zuri chat pc",
       android: "Android",
       and_break: "and",
       ios: "iOS",
@@ -608,5 +618,30 @@ export const TRANSLATIONS_EN = {
       download: "DOWNLOAD FOR WINDOWS",
       download_ios: "Download for Mac?"
     }
-  }
+  },
+
+
+// Workspace Page
+  workspace:{
+    title: {
+      workspace :"Workspace - Zuri Chat"
+    },
+
+    space:{
+      headline: "Choose a workspace",
+      post_headline: "Welcome back! Pick one to get back to working with your team",
+      join_line: "Want to work with another group? or Take charge and start your own team",
+      joinbtn: "Create a workspace",
+      foot_head: "Not seeing your workspace?",
+      foot_link: "Try a different email",
+      open: "Open",
+      selected: "selected",
+      errno: "401",
+      errmsg: "You're not logged in!",
+      errlink: "Login here.",
+      err_no: "404",
+      err_msg: "Oops something went wrong! Go to",
+      err_link: "zuri.chat"
+    }
+  }  
 }
