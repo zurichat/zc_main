80c832c8028eb380266030ba9f1f03a5c464036c
billmal071 <billmal071@gmail.com>
2021-10-16 11:27:47 +0100

    fix:styleing issues

--- topbar/src/components/SearchNotFound.js
+++ topbar/src/components/SearchNotFound.js
@@ -1,7 +1,7 @@
 import React from "react"
 import style from "../styles/SearchNotFound.module.css"
 import emoji from "../assets/images/thinking.png"
-import startNewSearch from "../topbar/src/components/StartNewSearchButton.jsx"
+import startNewSearch from "../components/StartNewSearchBotton"
 
 export const NoResult = () => {
   return (
