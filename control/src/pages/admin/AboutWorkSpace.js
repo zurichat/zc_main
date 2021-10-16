5d5f40b574322c340ac6be631a47e793256f702f
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-10-15 21:56:11 -0700

    workaround for adminandowners tab
    Former-commit-id: 4496f5cb74b75f501039daf733f2cfb32e1fa9aa [formerly 610bad54aac01ad231e03e83521859fd3c077361] [formerly 0774bd2dba547b3d01cdc3d843018cb0d3a3c388 [formerly bd8f650ee25348707a0d810965aba57fcf63e153 [formerly 0b913ae260806d59dffacd2a915a4d2de41a18d8] [formerly 0eff63a396e316f3a1a1d007954cc4b951a170a0]]]
    Former-commit-id: f795abe1f1d4ac61520495213ee1385a37bb47b3 [formerly bd8f650ee25348707a0d810965aba57fcf63e153 [formerly 0b913ae260806d59dffacd2a915a4d2de41a18d8]]
    Former-commit-id: 359749006a3713229a3eeb27c216b0a1f9f7a195
    Former-commit-id: 32b6334891442272fec5ceec639a6a08cbcf0701
    Former-commit-id: 6f24092c7c0972bf91a71607ea57efb11382a141

--- control/src/pages/admin/AboutWorkSpace.js
+++ control/src/pages/admin/AboutWorkSpace.js
@@ -7,7 +7,9 @@ import { authAxios } from "./Utils/Api"
 
 const AboutWorkSpace = () => {
   const [orgDetails, setOrgdetails] = useState({})
+  const [items, setItems] = useState(null)
   const currentWorkspace = localStorage.getItem("currentWorkspace")
+  const [filtered, setFiltered] = useState(null)
 
   if (!currentWorkspace) {
     return null
@@ -25,12 +27,40 @@ const AboutWorkSpace = () => {
       setOrgdetails(organization.data.data)
     } catch (err) {
       if (err) throw err
-
     }
   }
 
-  useEffect(async () => {
+  const workspaceuser = async () => {
+    try {
+      const response = await authAxios.get(
+        `/organizations/${currentWorkspace}/members`
+      )
+
+      if (!response) {
+        return null
+      }
+
+      const users = response.data.data
+
+      setItems(users)
+
+      //eslint-disable-next-line
+      console.log(users)
+    } catch (error) {
+      if (error) throw error
+    }
+  }
+
+  const filterOwner = () => {
+    const filter = items.filter(function (item) {
+      return item.role === "owner" || item.role === "member"
+    })
+    setFiltered(filter)
+  }
+
+  useEffect(() => {
     getOrgDetails()
+    workspaceuser()
     //eslint-disable-next-line
   }, [])
 
@@ -40,11 +70,16 @@ const AboutWorkSpace = () => {
         <div className={styles.contentWrapper}>
           <h5 className={styles.contentHeading}>
             {" "}
-            <FiAlertOctagon className={styles.icon} /> About This Workspace
+            <FiAlertOctagon
+              className={styles.icon}
+              onClick={filterOwner}
+            />{" "}
+            About This Workspace
           </h5>
           <AboutWorkSpaceTabs
             organizationDetails={orgDetails}
             currentWorkspace={currentWorkspace}
+            admins={filtered}
           />
         </div>
       </div>
