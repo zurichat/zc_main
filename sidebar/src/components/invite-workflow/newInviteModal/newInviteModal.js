f6356125d761076022bf612a3205adc0795dcb0d
Toluwalase <toluwalase@Toluwalases-MacBook-Air.local>
2021-10-17 14:43:53 +0100

    - I added an invite modal that pop on click - I enabled invite to be sent to an email
    Former-commit-id: c8c026a2a9c3df3972ce2213420ab86bed2eb064

--- sidebar/src/components/invite-workflow/newInviteModal/newInviteModal.js
+++ sidebar/src/components/invite-workflow/newInviteModal/newInviteModal.js
@@ -5,6 +5,7 @@ import useClickOutside from "../customHooks/useClickOutside"
 import cancel from "./assets/cancel.svg"
 import { sendInviteAPI } from "./new-invite.utils"
 
+
 const Container = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap");
   display: block !important;
