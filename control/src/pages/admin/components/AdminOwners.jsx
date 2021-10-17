3d1ba5ee0b81c8cfdf93a55f302e54c784918202
User <taofiqaiyelabegan45@gmail.com>
2021-10-17 16:50:10 +0800

    styling fix
    Former-commit-id: b1a774d46e0c0f5bfe3e55c8d1f87486422c28fe [formerly 4b275f465963bd20ce0d7510101c666795a5b979] [formerly 01bbcc5a065f650cba9431841be671ce0754ac7a [formerly bc56fec49e616c87ed06b9528be282909d77609e]]
    Former-commit-id: 09f4528bc70ef83f314617ceed2220e83e8947bc [formerly ef4594f1e7288511737b1dc981ba523c9c72120d]
    Former-commit-id: 3077bd6bac7fcce558fa06e4745f1ceef0236b6b
    Former-commit-id: c607424da5e3407eda127fff970771107ab732a4
    Former-commit-id: 7ee1cd6eb45862951d2c0e907b869480bb6072ec

--- control/src/pages/admin/components/AdminOwners.jsx
+++ control/src/pages/admin/components/AdminOwners.jsx
@@ -6,6 +6,7 @@ import AdminOwnersItem from "./AdminOwnersItem"
 const AdminOwners = ({ admins }) => {
   const [adminObj, setAdminObj] = useState(admins)
   const [filtered, setFiltered] = useState(null)
+  const [selectValue, setSelectValue] = useState("role")
 
   const filterAdmins = text => {
     const filteredAdmins = adminObj.filter(eachObj => {
@@ -25,15 +26,29 @@ const AdminOwners = ({ admins }) => {
     console.log(e)
   }
 
-  // const handleSort = e => {
-  //   let sort
-  //   if (e.target.value === "Role") {
-  //     sort = adminObj.sort((a, b) => a.email - b.email)
-  //   } else if (e.target.value === "FullName") {
-  //     sort = adminObj.sort((a, b) => a.first_name - b.firstName)
-  //   }
-  // }
+  const handleSort = role => {
+    setSelectValue(role)
 
+    if (selectValue === "role") {
+      adminObj.sort(function (a, b) {
+        let nameA = a.first_name.toLowerCase()
+        let nameB = b.first_name.toLowerCase()
+        return nameA < nameB ? -1 : nameA > nameB ? 1 : 0
+      })
+      //eslint-disable-next-line
+      console.log(adminObj)
+    }
+
+    if (selectValue === "name") {
+      adminObj.sort(function (a, b) {
+        let roleA = a.role.toLowerCase()
+        let roleB = b.role.toLowerCase()
+        return roleA < roleB ? 1 : roleA > roleB ? -1 : 0
+      })
+      //eslint-disable-next-line
+      console.log(adminObj)
+    }
+  }
   return (
     <div className={styles.container}>
       <div className={styles.tabContainer}>
@@ -41,7 +56,10 @@ const AdminOwners = ({ admins }) => {
           {/* part 1 */}
           <label>
             <span>Sort By</span>
-            <select>
+            <select
+              defaultValue={selectValue}
+              onChange={event => handleSort(event.target.value)}
+            >
               <option value="Role">Role</option>
               <option value="FullName">FullName</option>
             </select>
