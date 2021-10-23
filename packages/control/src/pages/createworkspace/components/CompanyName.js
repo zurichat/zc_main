8321cf10dbf16bed47233029efc39815aa17aee7
Obi Fortune <gabrielobi.of@gmail.com>
2021-10-23 22:06:30 +0300

    moved control into packages folder
    Former-commit-id: 053d625848fca9912b0f6fe37f3019147b3de36f [formerly 3f51daf4ac8dcdbf1cdae5d649c33c99ea8e8552] [formerly 4edd6c9a40c4fedd45e7ecebed5a5d87078838bb [formerly 3fc1d4a3fb916a9af4b09e08f0e06406b717f4fc]]
    Former-commit-id: 805e8fe9a40387988f743067138a986f0a83741a [formerly 9b4a6615bf636e3c98eea6e47e62d74c808c5bd4]
    Former-commit-id: 1e24163a783e3e7179f49818cc3c70a0f460055d
    Former-commit-id: d2b795cf48285e018c6df0c7c714c3d9453224bb
    Former-commit-id: 1fce95239b90b0f50a1493f5d8bb73b5e873148f

--- packages/control/src/pages/createworkspace/components/CompanyName.js
+++ packages/control/src/pages/createworkspace/components/CompanyName.js
@@ -3,16 +3,16 @@ import CompanyNameCSS from "../styles/CompanyName.module.css"
 import { Link, useRouteMatch } from "react-router-dom"
 import axios from "axios"
 import { Helmet } from "react-helmet"
-import { createDefaultChannel} from "../../../api/channels"
+import { createDefaultChannel } from "../../../api/channels"
 
-function CompanyName ({ input }) {
+function CompanyName({ input }) {
   const [user, setUser] = useState(null)
   const [orgId, setOrgId] = useState(null)
   const [orgName, setOrgName] = useState("")
   let match = useRouteMatch()
-  let newOrgId 
+  let newOrgId
   useEffect(() => {
-    const user = JSON.parse(sessionStorage.getItem('user'))
+    const user = JSON.parse(sessionStorage.getItem("user"))
 
     if (user) {
       setUser(user)
@@ -23,11 +23,11 @@ function CompanyName ({ input }) {
   const createUserOrg = () => {
     axios
       .post(
-        'https://api.zuri.chat/organizations',
+        "https://api.zuri.chat/organizations",
         { creator_email: user.email },
         {
           headers: {
-            Authorization: 'Bearer ' + user.token
+            Authorization: "Bearer " + user.token
           }
         }
       )
@@ -37,7 +37,7 @@ function CompanyName ({ input }) {
         localStorage.removeItem("userUserPassword")
         localStorage.removeItem("newUserEmail")
         newOrgId = res.data.data.organization_id
-        
+
         // Automatic Org Name Renaming From Default to new Org Name
         setTimeout(() => {
           axios.patch(
@@ -47,18 +47,21 @@ function CompanyName ({ input }) {
             },
             {
               headers: {
-                Authorization: 'Bearer ' + user.token
+                Authorization: "Bearer " + user.token
               }
             }
           )
           // .then(res => console.log(res))
-          axios.get(`https://api.zuri.chat/organizations/${newOrgId}/members/?query=${user.email}`)
-          .then(res => {
-            console.log("this is res: ", res);
-          let member_id = res.data.data[0]._id 
-          console.log(member_id);
-          localStorage.setItem('member_id', member_id)
-        } )
+          axios
+            .get(
+              `https://api.zuri.chat/organizations/${newOrgId}/members/?query=${user.email}`
+            )
+            .then(res => {
+              // console.log("this is res: ", res);
+              let member_id = res.data.data[0]._id
+              // console.log(member_id);
+              localStorage.setItem("member_id", member_id)
+            })
         }, 500)
 
         localStorage.setItem("currentWorkspace", newOrgId)
@@ -87,9 +90,9 @@ function CompanyName ({ input }) {
             team will recognise
           </h4>
           <input
-            type='text'
-            placeholder='Ex: The Brand Hub'
-            maxLength='50'
+            type="text"
+            placeholder="Ex: The Brand Hub"
+            maxLength="50"
             onChange={e => setOrgName(e.target.value)}
             className={CompanyNameCSS.inputBox}
           />
@@ -98,24 +101,22 @@ function CompanyName ({ input }) {
           </span>
           <div className={CompanyNameCSS.buttonContainer}>
             <Link to={`${match.url}/step2`}>
-              {' '}
+              {" "}
               <button
                 disabled={orgName.length < 3}
                 style={
-                orgName.length > 1
-                  ? { backgroundColor: '#00b87c', color: 'white' }
-                  : { backgroundColor: 'revert', cursor: 'not-allowed' }
-              }
+                  orgName.length > 1
+                    ? { backgroundColor: "#00b87c", color: "white" }
+                    : { backgroundColor: "revert", cursor: "not-allowed" }
+                }
                 onClick={createUserOrg}
               >
                 Continue
               </button>
             </Link>
-            <Link to='/createworkspace'>
-              {' '}
-              <button
-                style={{ backgroundColor: '#f40101', color: 'white' }}
-              >
+            <Link to="/createworkspace">
+              {" "}
+              <button style={{ backgroundColor: "#f40101", color: "white" }}>
                 Cancel
               </button>
             </Link>
