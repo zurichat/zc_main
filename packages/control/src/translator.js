c77c712bca69fe95fd521ee4916b5c38dc50cbc0
airondev <airondev@gmail.com>
2021-10-27 12:29:39 +0100

    Traslation strategy refactored to support external translators Landing page Footer Navbar
    Former-commit-id: 206ae467cb596d664938661bed81c0f49b7f705a [formerly 4a2627d8a88ad6fa3bf7ff06a5e0f08b01d0c53a] [formerly 7e16b432e32e0b9df0bd18527385d84a4842f02e [formerly 311db52f8c0291f7a195856f10d725dbb4d7c340]]
    Former-commit-id: 07434a3443391927d17ed79d46c70823cfd72ae5 [formerly 97f4b699736c4e31de2d4b52d9f4b8aa008a3c1b]
    Former-commit-id: ab820757291cc47eaed8d84b4b918d6ce5affc2e
    Former-commit-id: 5f8d6b24c762b051cd894bc65894cf42d84171ae
    Former-commit-id: aa020a8b2222bb2fc1e7109975c92a9af8a9d12e

--- packages/control/src/translator.js
+++ packages/control/src/translator.js
@@ -1,8 +1,13 @@
-let currentWorkspace = localStorage.getItem("currentWorkspace")
-let token = sessionStorage.getItem("token")
+import { LandingPage } from "./translations/pages/landing"
+import { NavBar } from "./translations/nav"
+import { Footer } from "./translations/footer"
+
+
+
+
+
 
 export const googleTranslateElementInit = async () => {
-  
   const lang = localStorage.getItem("myLanguage")
   ? localStorage.getItem("myLanguage")
   : "en"
@@ -13,7 +18,31 @@ export const googleTranslateElementInit = async () => {
   let expires = new Date();
   expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
   document.cookie = 'googtrans' + '=' + locale + ';expires=' + expires.toUTCString();
+  // new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false }, 'google_translate_element')
 
-  //new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false }, 'google_translate_element')
+}
+
+
+export const getTranslations =  () => {
+  const resources = {};
+  let langData = [ ...LandingPage, ...NavBar, ...Footer ]
+  langData.forEach((o) => {
+    Object.keys(o).forEach((key) => {
+      var langCode = String, langKey =  String, langKeyVal = String;
+      if (key !== "key") {
+        langCode = key;
+        langKey = o.key;
+        langKeyVal = o[key];
+        if (resources[langCode] === undefined) {
+          resources[langCode] = {
+            translation: {},
+          };
+        }
+        resources[langCode].translation[langKey] = langKeyVal;
+      }
+    });
+  });
+
+  return resources
 
 }
