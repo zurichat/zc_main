f3505d2b243f22d491df7a18613e615a17e0b096
Bori <mac@macs-MacBook-Pro.local>
2021-10-28 10:29:17 +0100

    ZC-3540: Choose workspace page (try a different email)
    Former-commit-id: 85579c22283a78b747e7b003f7825f746aee5ae7

--- packages/control/src/pages/workspace/components/WorkspaceFooter.js
+++ packages/control/src/pages/workspace/components/WorkspaceFooter.js
@@ -7,10 +7,17 @@ import { useTranslation} from "react-i18next"
 const WorkspaceFooter = () => {
   const { t } = useTranslation()
 
+  const deleteSessionId = ()=>{
+    // console.log(sessionStorage.getItem("session_id"))
+    sessionStorage.removeItem("session_id")
+    // console.log(sessionStorage.getItem("session_id"))
+  }
+
   return (
     <footer className={`${styles.workspace_footer}`}>
       <p>{t("workspace.space.foot_head")}</p>
-      <Link to="/login">{t("workspace.space.foot_link")}</Link>
+      <Link to="/login" onClick={deleteSessionId}>
+        {t("workspace.space.foot_link")}</Link>
     </footer>
   )
 }
