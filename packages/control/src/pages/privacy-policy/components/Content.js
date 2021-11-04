9dac48a33eb463b950db274137fcd8642f1af052
Davidlloyd8 <extendavidba4@gmail.com>
2021-11-04 12:22:50 +0100

    Refactoring And CSS clean up for privacy page

--- packages/control/src/pages/privacy-policy/components/Content.js
+++ packages/control/src/pages/privacy-policy/components/Content.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react"
 import Link from "../../../components/Link"
 import Style from "../styles/content.module.css"
-
 import imageTut from "../assets/image1.png"
 import bulbiImage from "../assets/bulb.png"
 import shakeImage from "../assets/shake.png"
@@ -9,7 +8,6 @@ import bigLock from "../assets/bigLock.svg"
 import yellowBulb from "../assets/yellowBulb.svg"
 import smallLock from "../assets/smallLock.svg"
 import handShake from "../assets/handShake.svg"
-import veryBigLock from "../assets/veryBigLock.svg"
 
 const Content = () => {
   return (
@@ -73,13 +71,6 @@ const Content = () => {
                     />
                   </p>
                 </div>
-              </div>
-              <div className={Style.bigLockImage}>
-                <img
-                  src={veryBigLock}
-                  className={Style.veryBigLock}
-                  alt="very big lock"
-                />
               </div>
             </div>
           </div>
