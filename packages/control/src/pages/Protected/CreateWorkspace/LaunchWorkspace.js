321e650249dbe9479c8e26a3fe4b54a265c206de
Bori7 <oluwaboriife@gmail.com>
2021-11-28 20:35:25 +0100

    Install Messaging Plugin by Default

--- packages/control/src/pages/Protected/CreateWorkspace/LaunchWorkspace.js
+++ packages/control/src/pages/Protected/CreateWorkspace/LaunchWorkspace.js
@@ -51,7 +51,8 @@ export default function Index({ createWorkspaceData }) {
     //   { organisation_id: workspaceId, user_id: creatorMemberId },
     //   { headers: { Authorization: "Bearer " + user.token } }
     // );
-    // Install Messaging Plugin
+
+    // Install Messaging Plugin in ZC Core
     const installMessagingPluginInCore = await axios.post(
       `https://api.zuri.chat/organizations/${workspaceId}/plugins`,
       {
@@ -61,6 +62,8 @@ export default function Index({ createWorkspaceData }) {
       },
       { headers: { Authorization: "Bearer " + user.token } }
     );
+
+    // Install Messaging Plugin in ZC Main
     const installMessagingPluginApiCall = await axios.post(
       `https://chat.zuri.chat/api/v1/install`,
       { organisation_id: workspaceId, user_id: creatorMemberId },
