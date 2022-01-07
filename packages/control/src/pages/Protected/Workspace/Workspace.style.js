ea0ce8351a349800bf18ab6ea4768594dc41622d
odelolajosh <oladapoodelola@gmail.com>
2022-01-07 23:12:14 +0100

    Clean code, removed comments

--- packages/control/src/pages/Protected/Workspace/Workspace.style.js
+++ packages/control/src/pages/Protected/Workspace/Workspace.style.js
@@ -9,16 +9,9 @@ export const GlobalWorkSpaceStyle = createGlobalStyle`
 export const TopBarWrapperStyle = styled.div`
   width: 100%;
   height: 48px;
-  ${
-    "" /* height: 6vh;
-  position: fixed;
-  top: 0;
-  left: 0; */
-  }
   display: flex;
   align-items: center;
   background-color: var(--bg-color);
-  ${"" /* z-index: 1; */}
   overflow: unset;
 
   @media (min-width: 1441px) {
@@ -32,13 +25,8 @@ export const SidebarWrapperStyle = styled.div`
   max-width: 300px;
   min-width: 230px;
   height: 100%;
-  ${"" /* position: fixed; */}
   -ms-overflow-style: none;
   scrollbar-width: none;
-  ${
-    "" /* top: 6vh;
-  left: 0; */
-  }
   background-color: var(--bg-color);
 
   &::-webkit-scrollbar {
@@ -51,29 +39,12 @@ export const SidebarWrapperStyle = styled.div`
 `;
 
 export const WorkspaceWrapperStyle = styled.div`
-  ${"" /* width: 80%; */}
   flex: 1;
   height: 100%;
-  ${
-    "" /* position: fixed;
-  top: 6vh;
-  left: 20%; */
-  }
   background-color: var(--bg-color);
   overflow: auto;
-  ${"" /* margin-left: 5px; */}
-  z-index: -1;
 
   & > #zuri-plugin-load-section {
     height: 100%;
   }
-
-  ${
-    "" /* @media (max-width: 768px) {
-    left: 0;
-    width: 100%;
-    ${"" /* position: relative; */
-  }
-    z-index: unset;
-  } */}
 `;
