01c64ad673b23746e5d5872fe33627c02e2420fc
Tunde Alabi <tundealabi4780@gmail.com>
2022-01-18 20:50:13 +0100

    write tests for topbar

---
+++ packages/control/src/components/Protected/TopBar/src/mocks/org-members.data.js
@@ -0,0 +1,272 @@
+const organizationMembersMockResponse = {
+  data: [
+    {
+      _id: "719ba4671a5f54782939d385",
+      bio: "",
+      deleted: false,
+      deleted_at: "0001-01-01T00:53:28+00:53",
+      display_name: "",
+      email: "tester@gmail.com",
+      files: null,
+      first_name: "",
+      id: "",
+      image_url:
+        "https://api.zuri.chat/files/profile_image/619ba4671a5f54782939d384/619ba4671a5f54782939d385/20211213192712_0.png",
+      joined_at: "2021-11-22T15:08:39.876+01:00",
+      language: "",
+      last_name: "",
+      org_id: "819ba4671a5f54782939d385",
+      phone: "",
+      presence: "true",
+      pronouns: "",
+      role: "owner",
+      settings: {
+        accessibility: {
+          animation: false,
+          direct_message_announcement: {
+            read_message: false,
+            receive_sound: false,
+            send_sound: false
+          },
+          links: false,
+          press_empty_message_field: ""
+        },
+        advanced: {
+          excluded_channels: null,
+          input_option: {
+            dont_send_with_enter: false,
+            format_messages: false
+          },
+          other_option: {
+            key_scroll_messages: false,
+            send_survey: false,
+            toggle_away_status: false,
+            warn_against_files: false,
+            warn_against_links: false
+          },
+          press_enter_to: "",
+          search_option: {
+            start_quick_switcher: false,
+            start_slack_search: false
+          }
+        },
+        audio_and_video: {
+          enable_automatic_gain_control: false,
+          integrated_webcam: "",
+          microphone: "",
+          speaker: "",
+          when_joining_a_huddle: null,
+          when_joining_a_zuri_chat_call: null,
+          when_slack_is_in_the_background: null
+        },
+        chat_settings: {
+          enter_is_send: false,
+          font_size: "",
+          media_visibility: false,
+          theme: "",
+          wallpaper: ""
+        },
+        languages_and_regions: {
+          language: "",
+          languages_zuri_should_spell_check: null,
+          set_time_zone_automatically: false,
+          spell_check: false,
+          time_zone: '""'
+        },
+        messages_and_media: {
+          additional_options: {
+            clock: false,
+            color_swatches: false,
+            currently_typing: false
+          },
+          bring_emails_into_zuri: "",
+          convert_emoticons_to_emoji: false,
+          custom: false,
+          emoji: "",
+          emoji_as_text: false,
+          frequently_used_emoji: false,
+          inline_media_and_links: {
+            larger_than_2_mb: false,
+            show_images_and_files_from_linked_websites: false,
+            show_images_and_files_uploaded_to_zurichat: false,
+            show_text_previews_of_linked_websites: false
+          },
+          messages_one_click_reaction: null,
+          names: "",
+          show_jumbomoji: false,
+          theme: ""
+        },
+        notifications: {
+          channel_hurdle_notification: false,
+          custom_notification_schedule: null,
+          message_preview_in_each_notification: false,
+          mute_all_sounds: false,
+          notification_schedule: {
+            day: "",
+            from: "",
+            to: ""
+          },
+          set_lounge_notifications_right: "",
+          set_message_notifications_right: ""
+        },
+        plugin_settings: null,
+        sidebar: {
+          always_show_in_the_sidebar: null,
+          list_private_channels_separately: false,
+          organize_external_conversations: false,
+          show_all_the_following: "",
+          show_conversations: "",
+          show_profile_picture_next_to_dm: false,
+          sidebar_sort: ""
+        },
+        themes: {
+          colors: "",
+          mode: ""
+        }
+      },
+      socials: null,
+      status: {
+        expiry_time: "",
+        status_history: null,
+        tag: "",
+        text: ""
+      },
+      time_zone: "",
+      user_name: "tester"
+    },
+    {
+      _id: "719baa5c1a5f54782939d387",
+      bio: "",
+      deleted: false,
+      deleted_at: "0001-01-01T00:53:28+00:53",
+      display_name: "",
+      email: "tester1@gmail.com",
+      files: null,
+      first_name: "",
+      image_url: "",
+      joined_at: "2021-11-22T15:34:04.287+01:00",
+      language: "",
+      last_name: "",
+      org_id: "619ba4671a5f54782939d384",
+      phone: "",
+      presence: "true",
+      pronouns: "",
+      role: "member",
+      settings: {
+        accessibility: {
+          animation: false,
+          direct_message_announcement: {
+            read_message: false,
+            receive_sound: false,
+            send_sound: false
+          },
+          links: false,
+          press_empty_message_field: ""
+        },
+        advanced: {
+          excluded_channels: null,
+          input_option: {
+            dont_send_with_enter: false,
+            format_messages: false
+          },
+          other_option: {
+            key_scroll_messages: false,
+            send_survey: false,
+            toggle_away_status: false,
+            warn_against_files: false,
+            warn_against_links: false
+          },
+          press_enter_to: "",
+          search_option: {
+            start_quick_switcher: false,
+            start_slack_search: false
+          }
+        },
+        audio_and_video: {
+          enable_automatic_gain_control: false,
+          integrated_webcam: "",
+          microphone: "",
+          speaker: "",
+          when_joining_a_huddle: null,
+          when_joining_a_zuri_chat_call: null,
+          when_slack_is_in_the_background: null
+        },
+        chat_settings: {
+          enter_is_send: false,
+          font_size: "",
+          media_visibility: false,
+          theme: "",
+          wallpaper: ""
+        },
+        languages_and_regions: {
+          language: "",
+          languages_zuri_should_spell_check: null,
+          set_time_zone_automatically: false,
+          spell_check: false,
+          time_zone: ""
+        },
+        messages_and_media: {
+          additional_options: {
+            clock: false,
+            color_swatches: false,
+            currently_typing: false
+          },
+          bring_emails_into_zuri: "",
+          convert_emoticons_to_emoji: false,
+          custom: false,
+          emoji: "",
+          emoji_as_text: false,
+          frequently_used_emoji: false,
+          inline_media_and_links: {
+            larger_than_2_mb: false,
+            show_images_and_files_from_linked_websites: false,
+            show_images_and_files_uploaded_to_zurichat: false,
+            show_text_previews_of_linked_websites: false
+          },
+          messages_one_click_reaction: null,
+          names: "",
+          show_jumbomoji: false,
+          theme: ""
+        },
+        notifications: {
+          channel_hurdle_notification: false,
+          custom_notification_schedule: null,
+          message_preview_in_each_notification: false,
+          mute_all_sounds: false,
+          notification_schedule: {
+            day: "",
+            from: "",
+            to: ""
+          },
+          set_lounge_notifications_right: "",
+          set_message_notifications_right: ""
+        },
+        plugin_settings: null,
+        sidebar: {
+          always_show_in_the_sidebar: null,
+          list_private_channels_separately: false,
+          organize_external_conversations: false,
+          show_all_the_following: "",
+          show_conversations: "",
+          show_profile_picture_next_to_dm: false,
+          sidebar_sort: ""
+        },
+        themes: {
+          colors: "",
+          mode: ""
+        }
+      },
+      socials: null,
+      status: {
+        expiry_time: "",
+        status_history: null,
+        tag: "",
+        text: ""
+      },
+      time_zone: "",
+      user_name: "tester1"
+    }
+  ]
+};
+
+export default organizationMembersMockResponse;
