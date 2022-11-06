60c22c4fc4792eae1ee344ae2c3a94a65c0ed287
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-11-06 21:39:02 +0100

    chore: redirects logged out users to the logout page instead of the login page

--- packages/main/src/App.js
+++ packages/main/src/App.js
@@ -13,7 +13,7 @@ import {
   HomePage,
   Login,
   SignUp,
-  SignOut,
+  NewSignOut,
   AboutPage,
   ContactUsPage,
   DownloadsPage,
@@ -35,12 +35,12 @@ const ProtectedRoute = ({ children, ...rest }) => {
     <Route
       {...rest}
       render={({ location }) =>
-        auth.user ? (
+        auth.user !== true ? (
           children
         ) : (
           <Redirect
             to={{
-              pathname: "/login",
+              pathname: "/signout",
               state: { from: location }
             }}
           />
@@ -94,7 +94,7 @@ const App = () => (
             <SignUp />
           </ProtectFromAuthRoute>
           <ProtectedRoute exact path="/signout">
-            <SignOut />
+            <NewSignOut />
           </ProtectedRoute>
           <ProtectedRoute exact path="/choose-workspace">
             {withSuspense(ChooseWorkspace)}
