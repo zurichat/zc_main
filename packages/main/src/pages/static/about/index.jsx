7c8c97463593ba23afa062cf4a745e6db8c02a77
Praise Obende <davidobende@gmail.com>
2022-11-06 23:32:42 +0100

    Fix the About page issues with the styling

--- packages/main/src/pages/static/about/index.jsx
+++ packages/main/src/pages/static/about/index.jsx
@@ -66,15 +66,15 @@ const About = () => {
           <div className={`col-lg-6  ${styles.team}`}>
             <img src={team} alt={"image of teams"} srcSet="" />
           </div>
-          <div className={`col-lg-6 text-start ${styles.join_us}`}>
+          <div className={`col-lg-6 ${styles.join_us}`}>
             <h3 className={` ${styles.h3m}`}>
               {t("about.whoWeAre.title")} <br />
             </h3>
-            <p className="text-start">
+            <p>
               {t("about.whoWeAre.description")} <br />
             </p>
             <p>
-              <span className={`text-start ${styles.span}`}>ZuriChat</span>{" "}
+              <span className={` ${styles.span}`}>ZuriChat</span>{" "}
               {t("about.whoWeAre.description_")}
             </p>
           </div>
