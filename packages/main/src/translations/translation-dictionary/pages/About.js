62fbd8d7de374e7738bb5aeec195b30b932a8d87
Udoka <udochukwukaonyela@gmail.com>
2022-11-07 20:11:28 +0100

    added french translations

--- packages/main/src/translations/translation-dictionary/pages/About.js
+++ packages/main/src/translations/translation-dictionary/pages/About.js
@@ -1,10 +1,10 @@
 export default [
   {
     key: "about.banner.headline_one",
-    en: "Make work simple, pleasant and"
+    en: "Make work simple, pleasant and",
+    fr: "Rendre le travail simple, agréable et"
     // de: "Einloggen - Zuri Chat",
     // enus: "Login - Zuri Chat",
-    // fr: "Connexion - Chat Zuri",
     // zh: "登录 -  zuri聊天",
     // ar: "تسجيل الدخول - zuri chat",
     // es: "Iniciar sesión - Chat de Zuri",
@@ -14,174 +14,218 @@ export default [
   },
   {
     key: "about.banner.headline_two",
-    en: "more productive."
+    en: "more productive.",
+    fr: "plus productif."
   },
   {
     key: "about.banner.description",
-    en: "is a collaboration hub that brings the right people and tools together to get work done. From Fortune 100 companies to corner markets, millions of people around the world use Zuri Chat to connect their teams, unify their systems, and drive their business forward."
+    en: "is a collaboration hub that brings the right people and tools together to get work done. From Fortune 100 companies to corner markets, millions of people around the world use Zuri Chat to connect their teams, unify their systems, and drive their business forward.",
+    fr: "est un hub de collaboration qui rassemble les bonnes personnes et les bons outils pour faire avancer le travail. Des entreprises du Fortune 100 aux marchés de quartier, des millions de personnes dans le monde entier utilisent Zuri Chat pour connecter leurs équipes, unifier leurs systèmes et faire avancer leur entreprise."
   },
   {
     key: "about.counter.customers",
-    en: "Paid"
+    en: "Paid",
+    fr: "Payé"
   },
   {
     key: "about.counter.customers_",
-    en: "Customers"
+    en: "Customers",
+    fr: "Clients"
   },
   {
     key: "about.counter.fortune_companies",
-    en: "of the Fortune 100"
+    en: "of the Fortune 100",
+    fr: "du Fortune 100",
+
   },
   {
     key: "about.counter.fortune_companies_",
-    en: "use"
+    en: "use",
+    fr: "utilisent"
   },
   {
     key: "about.counter.countries",
-    en: "countries have daily"
+    en: "countries have daily",
+    fr: "pays ont quotidiennement"
   },
   {
     key: "about.counter.countries_",
-    en: "active users in"
+    en: "active users in",
+    fr: "utilisateurs actifs dans"
   },
   {
     key: "about.whoWeAre.title",
-    en: "Who we are and how to join us"
+    en: "Who we are and how to join us",
+    fr: "Qui nous sommes et comment nous rejoindre"
   },
   {
     key: "about.whoWeAre.description",
-    en: "Our mission is to improve people's working life by providing a platform that can host a strong diverse team , of creative professionals who want to do the best work of their lives"
+    en: "Our mission is to improve people's working life by providing a platform that can host a strong diverse team , of creative professionals who want to do the best work of their lives",
+    fr: "Notre mission est d'améliorer la vie professionnelle des gens en fournissant une plateforme qui peut accueillir une équipe forte et diversifiée de professionnels créatifs qui veulent faire le meilleur travail de leur vie"
   },
   {
     key: "about.whoWeAre.description_",
-    en: "promises all kinds of opportunities and a simple, pleasant and more productive working life."
+    en: "promises all kinds of opportunities and a simple, pleasant and more productive working life.",
+    fr: "promet toutes sortes d'opportunités et une vie professionnelle simple, agréable et plus productive."
   },
   {
     key: "about.highlights.title",
-    en: "Working remotely more intriguing than ever"
+    en: "Working remotely more intriguing than ever",
+    fr: "Travailler à distance plus intrigant que jamais"
   },
   {
     key: "about.highlights.description",
-    en: "Zuri Chat is where work meets fun."
+    en: "Zuri Chat is where work meets fun.",
+    fr: "Zuri Chat est là où le travail rencontre le plaisir."
   },
   {
     key: "about.highlights.description_",
-    en: "Connect and interact with your team efficiently from anywhere around the globe"
+    en: "Connect and interact with your team efficiently from anywhere around the globe",
+    fr: "Connectez-vous et interagissez avec votre équipe efficacement de n'importe où dans le monde"
   },
   {
     key: "about.highlights.channels.title",
-    en: "Channels"
+    en: "Channels",
+    fr: "Canaux"
   },
   {
     key: "about.highlights.channels.description",
-    en: "Organize conversations into dedicated"
+    en: "Organize conversations into dedicated",
+    fr: "Organisez les conversations dans des"
   },
   {
     key: "about.highlights.channels.description_",
-    en: "spaces called channels"
+    en: "spaces called channels",
+    fr: "espaces appelés canaux"
   },
   {
     key: "about.highlights.holidayCalender.title",
-    en: "Holiday Calender"
+    en: "Holiday Calender",
+    fr: "Calendrier des vacances"
   },
   {
     key: "about.highlights.holidayCalender.description",
-    en: "Add holidays, set reminders"
+    en: "Add holidays, set reminders",
+    fr: "Ajoutez des vacances, définissez des rappels"
   },
   {
     key: "about.highlights.holidayCalender.description_",
-    en: "and see all events at a glance"
+    en: "and see all events at a glance",
+    fr: "et voyez tous les événements d'un coup d'œil"
   },
   {
     key: "about.highlights.companyDeadline.title",
-    en: "Company Deadline"
+    en: "Company Deadline",
+    fr: "Date limite de l'entreprise"
   },
   {
     key: "about.highlights.companyDeadline.description",
-    en: "Keep track of deadlines with"
+    en: "Keep track of deadlines with",
+    fr: "Suivez les délais avec"
   },
   {
     key: "about.highlights.companyDeadline.description_",
-    en: "the company deadline"
+    en: "the company deadline",
+    fr: "la date limite de l'entreprise"
   },
   {
     key: "about.highlights.companyDeadline.description__",
-    en: "feature"
+    en: "feature",
+    fr: "fonctionnalité"
   },
   {
     key: "about.highlights.quickReply.title",
-    en: "Quick Reply App"
+    en: "Quick Reply App",
+    fr: "Application de réponse rapide"
   },
   {
     key: "about.highlights.quickReply.description",
-    en: "Get swift replies to Direct"
+    en: "Get swift replies to Direct",
+    fr: "Obtenez des réponses rapides à Direct"
   },
   {
     key: "about.highlights.quickReply.description_",
-    en: "Messages"
+    en: "Messages",
+    fr: "Messages"
   },
   {
     key: "about.highlights.todoList.title",
-    en: "To do list"
+    en: "To do list",
+    fr: "Liste de choses à faire"
   },
   {
     key: "about.highlights.todoList.description",
-    en: "Organize tasks with the to-do-list"
+    en: "Organize tasks with the to-do-list",
+    fr: "Organisez les tâches avec la liste de choses à faire"
   },
   {
     key: "about.highlights.todoList.description_",
-    en: "feature"
+    en: "feature",
+    fr: "fonctionnalité"
   },
   {
     key: "about.features.title",
-    en: "Special Features"
+    en: "Special Features",
+    fr: "Fonctionnalités spéciales"
   },
   {
     key: "about.features.description",
-    en: "Increase your productivity by assembling all your work tools into one place"
+    en: "Increase your productivity by assembling all your work tools into one place",
+    fr: "Augmentez votre productivité en rassemblant tous vos outils de travail dans un seul endroit"
   },
   {
     key: "about.features.title",
-    en: "Special Features"
+    en: "Special Features",
+    fr: "Fonctionnalités spéciales"
   },
   {
     key: "about.features.avatar.title",
-    en: "Animated Avatar Loungue"
+    en: "Animated Avatar Loungue",
+    fr: "Avatar animé Loungue"
   },
   {
     key: "about.features.avatar.description",
-    en: "Create and animate your own avatar in the avatar lounge"
+    en: "Create and animate your own avatar in the avatar lounge",
+    fr: "Créez et animez votre propre avatar dans le salon d'avatar"
   },
   {
     key: "about.features.files.title",
-    en: "Company Files"
+    en: "Company Files",
+    fr: "Fichiers d'entreprise"
   },
   {
     key: "about.features.files.description",
-    en: "Access your files on google drive while on Zuri Chat."
+    en: "Access your files on google drive while on Zuri Chat.",
+    fr: "Accédez à vos fichiers sur Google Drive pendant que vous êtes sur Zuri Chat."
   },
   {
     key: "about.features.accessibility.title",
-    en: "Accessibility"
+    en: "Accessibility",
+    fr: "Accessibilité"
   },
   {
     key: "about.features.accessibility.description",
-    en: "Be in control, Use Zuri Chat and enjoy"
+    en: "Be in control, Use Zuri Chat and enjoy",
+    fr: "Être en contrôle, utiliser Zuri Chat et profiter"
   },
   {
     key: "about.features.accessibility.description_",
-    en: "the best accessibility features"
+    en: "the best accessibility features",
+    fr: "des meilleures fonctionnalités d'accessibilité"
   },
   {
     key: "about.features.goals.title",
-    en: "Company Goals"
+    en: "Company Goals",
+    fr: "Objectifs de l'entreprise"
   },
   {
     key: "about.features.goals.description",
-    en: "Keep up with company goals"
+    en: "Keep up with company goals",
+    fr: "Rester à jour avec les objectifs de l'entreprise"
   },
   {
     key: "about.features.goals.description_",
-    en: "right inside Zuri Chat"
+    en: "right inside Zuri Chat",
+    fr: "tout à l'intérieur de Zuri Chat"
   }
 ];
