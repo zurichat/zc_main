72675fc69509745fb072ce7c302428a75b79558e
Oladimeji Abdrahman Adebayo <abdrahmanoladimeji02@gmail.com>
2022-11-14 14:56:58 +0100

    rm: removed other languages from the translation/pages/Home file

--- packages/main/src/translations/translation-dictionary/pages/Home.js
+++ packages/main/src/translations/translation-dictionary/pages/Home.js
@@ -4,195 +4,105 @@ export default [
     en: "For Organizations & Institutions",
     de: "Für Organisationen & Institutionen",
     enus: "For Organizations & Institutions",
-    fr: "Pour les organisations et les institutions",
-    zh: "适用于组织和机构",
-    ar: "للمنظمات والمؤسسات",
-    es: "Para organizaciones e instituciones.",
-    it: "Per organizzazioni e istituzioni",
-    he: "עבור ארגונים ומוסדות",
-    pt: "Para organizações e instituições"
+    fr: "Pour les organisations et les institutions"
   },
   {
     key: "landing_hero_headline",
     en: "A fully integrated way to connect and interact",
     de: "Eine vollständig integrierte Weise, um eine Verbindung herzustellen und zu interagieren",
     enus: "A fully integrated way to connect and interact",
-    fr: "Une manière entièrement intégrée de se connecter et d'interagir",
-    zh: "一个完全综合的连接和交互方式",
-    ar: "طريقة متكاملة تماما للاتصال والتفاعل",
-    es: "Una forma totalmente integrada de conectar e interactuar.",
-    it: "Un modo completamente integrato per connettersi e interagire",
-    he: "דרך משולבת לחלוטין להתחבר ולא אינטראקציה",
-    pt: "Uma maneira totalmente integrada de se conectar e interagir"
+    fr: "Une manière entièrement intégrée de se connecter et d'interagir"
   },
   {
     key: "landing_hero_post_headline",
     en: "Create your future with us, network, collaborate, educate and learn remotely",
     de: "Erstellen Sie Ihre Zukunft mit uns, Netzwerk, zusammenarbeiten, erziehen und aus der Ferne erlernen",
     enus: "Create your future with us, network, collaborate, educate and learn remotely",
-    fr: "Créez votre avenir avec nous, réseau, collaborer, éduquer et apprendre à distance",
-    zh: "与我们，网络，合作，教育和远程学习创建未来",
-    ar: "قم بإنشاء مستقبلك معنا والشبكة والتعاون والتعليم والتعلم عن بعد",
-    es: "Crea tu futuro con nosotros, la red, colabora, educa y aprende de forma remota.",
-    it: "Crea il tuo futuro con noi, rete, collaborare, educare e imparare da remoto",
-    he: "צור את העתיד שלך איתנו, רשת, לשתף פעולה, לחנך וללמוד מרחוק",
-    pt: "Crie seu futuro conosco, rede, colabore, eduque e aprenda remotamente"
+    fr: "Créez votre avenir avec nous, réseau, collaborer, éduquer et apprendre à distance"
   },
   {
     key: "landing_hero_call_to_action",
     en: "Try Zuri Chat for free",
     de: "Probieren Sie den Zuri-Chat kostenlos aus",
     enus: "Try Zuri Chat for free",
-    fr: "Essayez zuri chat gratuitement",
-    zh: "尝试zuri免费聊天",
-    ar: "جرب zuri chat مجانا",
-    es: "Prueba zuri chat gratis",
-    it: "Prova Zuri Chat gratuitamente",
-    he: "נסה צורי צ'אט בחינם",
-    pt: "Tente zuri chat gratuitamente"
+    fr: "Essayez zuri chat gratuitement"
   },
   {
     key: "landing_feature_row_one_title",
     en: "Seamless collaboration",
     de: "Nahtlose Zusammenarbeit",
     enus: "Seamless collaboration",
-    fr: "Collaboration sans soudure",
-    zh: "无缝协作",
-    ar: "تعاون سلس",
-    es: "Colaboración inconsútil",
-    it: "Collaborazione senza soluzione di continuità",
-    he: "שיתוף פעולה חלקה",
-    pt: "Colaboração sem emenda"
+    fr: "Collaboration sans soudure"
   },
   {
     key: "landing_feature_row_one_headline",
     en: "Communicate and collaborate from wherever you are",
     de: "Kommunizieren und zusammenarbeiten, wo immer Sie sind",
     enus: "Communicate and collaborate from wherever you are",
-    fr: "Communiquer et collaborer où que vous soyez",
-    zh: "从任何地方沟通和协作",
-    ar: "التواصل والتعاون من أينما كنت",
-    es: "Comunicarse y colaborar desde donde sea que estés",
-    it: "Comunicare e collaborare da dove sei",
-    he: "לתקשר ולשתף פעולה מכל מקום שבו אתה נמצא",
-    pt: "Comunicar e colaborar de onde quer que você esteja"
+    fr: "Communiquer et collaborer où que vous soyez"
   },
   {
     key: "landing_feature_row_one_description ",
     en: "Invite your team to your workspace. Stay connected,stay in sync, and explore ideas together from anywhere.",
     de: "Laden Sie Ihr Team in Ihren Arbeitsbereich ein. Bleiben Sie in Verbindung, bleiben Sie synchron und erkunden Sie Ideen zusammen von überall.",
     enus: "Invite your team to your workspace. Stay connected,stay in sync, and explore ideas together from anywhere.",
-    fr: "Invitez votre équipe à votre espace de travail. Restez connecté, restez en synchronisation et explorez des idées ensemble de n'importe où.",
-    zh: "邀请您的团队到您的工作区。保持联系，保持同步，并在任何地方探索思考。",
-    ar: "دعوة فريقك إلى مساحة العمل الخاصة بك. ابق على اتصال، والبقاء في المزامنة، واستكشف الأفكار معا من أي مكان.",
-    es: "Invite a su equipo a su espacio de trabajo. Manténgase conectado, permanezca en sincronización y explore ideas juntos desde cualquier lugar.",
-    it: "Invita la tua squadra al tuo spazio di lavoro. Rimani connesso, rimani in sincronizzazione ed esplorare idee insieme da qualsiasi luogo.",
-    he: "הזמן את הצוות שלך לסביבת העבודה שלך. הישאר מחובר, להישאר בסנכרון, ולחקור רעיונות יחד מכל מקום.",
-    pt: "Convide sua equipe para o seu espaço de trabalho. Fique conectado, fique em sincronia e explore as ideias em qualquer lugar."
+    fr: "Invitez votre équipe à votre espace de travail. Restez connecté, restez en synchronisation et explorez des idées ensemble de n'importe où."
   },
   {
     key: "landing_feature_row_one_link",
     en: "Create your own workspace",
     de: "Erstellen Sie Ihren eigenen Arbeitsbereich",
     enus: "Create your own workspace",
-    fr: "Créez votre propre espace de travail",
-    zh: "创建自己的工作区",
-    ar: "إنشاء مساحة العمل الخاصة بك",
-    es: "Crea tu propio espacio de trabajo",
-    it: "Crea il tuo spazio di lavoro",
-    he: "צור סביבת עבודה משלך",
-    pt: "Crie seu próprio espaço de trabalho"
+    fr: "Créez votre propre espace de travail"
   },
   {
     key: "landing_feature_row_two_title",
     en: "Remote Education",
     de: "Remote-Bildung",
     enus: "Remote Education",
-    fr: "Éducation à distance",
-    zh: "远程教育",
-    ar: "التربية النائية",
-    es: "Educación remota",
-    it: "Educazione remota",
-    he: "חינוך מרחוק",
-    pt: "Educação remota"
+    fr: "Éducation à distance"
   },
   {
     key: "landing_feature_row_two_headline",
     en: "Learn on the go",
     de: "Lernen",
     enus: "Learn on the go",
-    fr: "Apprendre sur le coup",
-    zh: "随时随地学习",
-    ar: "تعلم على الذهاب",
-    es: "Aprender en la marcha",
-    it: "Impara in movimento",
-    he: "למד בדרכים",
-    pt: "Aprenda em movimento"
+    fr: "Apprendre sur le coup"
   },
   {
     key: "landing_feature_row_two_description",
     en: 'Take your classroom everywhere, make learning fun, stay engaged and inspired with the virtual lounge and games.",',
     de: 'Nehmen Sie Ihr Klassenzimmer überall, machen Sie Lernspaß, bleiben Sie mit dem virtuellen Lounge und Spielen inspiriert. ",',
     enus: 'Take your classroom everywhere, make learning fun, stay engaged and inspired with the virtual lounge and games.",',
-    fr: "Prenez votre salle de classe partout, faites de l'apprentissage amusant, restez engagé et inspiré du salon virtuel et des jeux. »,",
-    zh: "随处携带你的教室，让学习乐趣，住宿和灵感在虚拟休息室和游戏。“，",
-    ar: 'خذ الفصل الدراسي الخاص بك في كل مكان، وجعل التعلم متعة، والبقاء يشاركون ويستلهم مع الصالة الافتراضية والألعاب. "',
-    es: 'Tome su salón de clases en todas partes, haga que el aprendizaje sea divertido, permanezca comprometido y inspirado con el salón virtual y los juegos ".',
-    it: 'Prendi la tua Aula ovunque, rendi divertimento in modo da imparare, rimani fidanzato e ispirato con la lounge e i giochi virtuali. ",',
-    he: 'קח את הכיתה שלך בכל מקום, לעשות כיף למידה, להישאר עוסקת השראה עם טרקלין וירטואלי ומשחקים. ",',
-    pt: 'Leve sua sala de aula em todos os lugares, torne a aprendizagem divertida, fique envolvida e inspirada com o lounge virtual e jogos. ",'
+    fr: "Prenez votre salle de classe partout, faites de l'apprentissage amusant, restez engagé et inspiré du salon virtuel et des jeux. »,"
   },
   {
     key: "landing_feature_row_two_link",
     en: "Learn more about the virtual lounge",
     de: "Erfahren Sie mehr über die virtuelle Lounge",
     enus: "Learn more about the virtual lounge",
-    fr: "En savoir plus sur le salon virtuel",
-    zh: "了解有关虚拟休息室的更多信息",
-    ar: "تعرف على المزيد حول الصالة الافتراضية",
-    es: "Aprenda más sobre el salón virtual",
-    it: "Scopri di più sulla sala virtuale",
-    he: "למידע נוסף על טרקלין הווירטואלי",
-    pt: "Saiba mais sobre o Lounge Virtual"
+    fr: "En savoir plus sur le salon virtuel"
   },
   {
     key: "landing_feature_row_three_title",
     en: "All-in-one Workspace",
     de: "All-in-One-Arbeitsbereich",
     enus: "All-in-one Workspace",
-    fr: "Espace de travail tout-en-un",
-    zh: "一体化工作空间",
-    ar: "مساحة العمل الكل في واحد",
-    es: "Espacio de trabajo todo en uno",
-    it: "Area di lavoro all-in-one",
-    he: "כל סביבת עבודה",
-    pt: "Espaço de Trabalho All-in-One"
+    fr: "Espace de travail tout-en-un"
   },
   {
     key: "landing_feature_row_three_headline",
     en: "Customise your Workspace",
     de: "Passen Sie Ihren Arbeitsbereich an",
     enus: "Customise your Workspace",
-    fr: "Personnalisez votre espace de travail",
-    zh: "自定义工作区",
-    ar: "تخصيص مساحة العمل الخاصة بك",
-    es: "Personaliza tu espacio de trabajo",
-    it: "Personalizza il tuo spazio di lavoro",
-    he: "התאם אישית את סביבת העבודה שלך",
-    pt: "Personalize seu espaço de trabalho"
+    fr: "Personnalisez votre espace de travail"
   },
   {
     key: "landing_feature_row_three_description",
     en: "One tool, several plugins. Track company expenses, send information fast and smoothly, manage files and integrate tools all with Zuri Chat.",
     de: "Ein Werkzeug, mehrere Plugins. Track Company-Ausgaben, Senden Sie Informationen schnell und reibungslos, verwalten Sie Dateien und integrieren Sie Tools alle mit dem ZURI-Chat.",
     enus: "One tool, several plugins. Track company expenses, send information fast and smoothly, manage files and integrate tools all with Zuri Chat.",
-    fr: "Un outil, plusieurs plugins. Suivre les frais de l'entreprise, envoyez des informations rapidement et en douceur, gérez les fichiers et intégrez les outils avec le chat Zuri.",
-    zh: "一个工具，几个插件。跟踪公司费用，快速且顺利地发送信息，管理文件并使用ZURI Chat集成所有工具。",
-    ar: "أداة واحدة، عدة إضافات. تتبع نفقات الشركة، وإرسال المعلومات بسرعة وسلاسة، وإدارة الملفات ودمج الأدوات جميع مع دردشة Zuri.",
-    es: "Una herramienta, varios complementos. Rastrear los gastos de la compañía, enviar información rápida y sin problemas, administrar archivos e integrar las herramientas con Zuri Chat.",
-    it: "Uno strumento, diversi plugin. Traccia le spese aziendali, inviare informazioni veloci e senza intoppi, gestire i file e integrare gli strumenti tutti con la chat Zuri.",
-    he: "כלי אחד, כמה תוספים. עקוב אחר הוצאות החברה, שלח מידע מהיר וחלק בצורה חלקה, לנהל קבצים ולשלב כלים עם צורי Zuri.",
-    pt: "Uma ferramenta, vários plugins. Acompanhe as despesas da empresa, envie informações rápidas e suavemente, gerencie arquivos e integre ferramentas todas com o bate-papo Zuri."
+    fr: "Un outil, plusieurs plugins. Suivre les frais de l'entreprise, envoyez des informations rapidement et en douceur, gérez les fichiers et intégrez les outils avec le chat Zuri."
   },
   {
     key: "landing_feature_row_three_link",
@@ -212,116 +122,62 @@ export default [
     en: "Advanced Search",
     de: "Erweiterte Suche",
     enus: "Advanced Search",
-    fr: "Recherche Avancée",
-    zh: "高级搜索",
-    ar: "البحث المتقدم",
-    es: "Búsqueda Avanzada",
-    it: "Ricerca Avanzata",
-    he: "חיפוש מתקדם",
-    pt: "Busca Avançada"
+    fr: "Recherche Avancée"
   },
   {
     key: "landing_feature_row_four_headline",
     en: "Access Files and Messages in your Workspace Effortlessly",
     de: "Zugriff auf Dateien und Nachrichten in Ihrem Arbeitsbereich mühelos",
     enus: "Access Files and Messages in your Workspace Effortlessly",
-    fr: "Accès des fichiers et des messages dans votre espace de travail sans effort",
-    zh: "毫不费力地访问工作区中的文件和消息",
-    ar: "الوصول إلى الملفات والرسائل في مساحة العمل الخاصة بك دون عناء",
-    es: "Acceda a archivos y mensajes en su espacio de trabajo sin esfuerzo.",
-    it: "Accedi file e messaggi nel tuo spazio di lavoro senza sforzo",
-    he: "גישה קבצים והודעות בסביבת העבודה שלך ללא מאמץ",
-    pt: "Acesse arquivos e mensagens em seu espaço de trabalho sem esforço"
+    fr: "Accès des fichiers et des messages dans votre espace de travail sans effort"
   },
   {
     key: "landing_feature_row_four_description",
     en: "With the search tool, find previously shared messages, files and links with ease.",
     de: "Finden Sie mit dem Suchwerkzeug zuvor freigegebene Nachrichten, Dateien und Links mit Leichtigkeit.",
     enus: "With the search tool, find previously shared messages, files and links with ease.",
-    fr: "Avec l'outil de recherche, trouvez facilement des messages, des fichiers et des liens précédemment partagés.",
-    zh: "使用“搜索”工具，以轻松查找以前共享的消息，文件和链接。",
-    ar: "مع أداة البحث، ابحث عن الرسائل والملفات المشتركة مسبقا مع سهولة.",
-    es: "Con la herramienta de búsqueda, encuentre mensajes, archivos y enlaces compartidos previamente con facilidad.",
-    it: "Con lo strumento di ricerca, trova messaggi, file e collegamenti precedentemente condivisi con facilità.",
-    he: "עם כלי החיפוש, מצא הודעות משותפות בעבר, קבצים וקישורים בקלות.",
-    pt: "Com a ferramenta de pesquisa, encontre mensagens, arquivos e links anteriormente compartilhados com facilidade."
+    fr: "Avec l'outil de recherche, trouvez facilement des messages, des fichiers et des liens précédemment partagés."
   },
   {
     key: "landing_feature_row_four_link",
     en: "Learn more about Search",
     de: "Erfahren Sie mehr über die Suche",
     enus: "Learn more about Search",
-    fr: "En savoir plus sur la recherche",
-    zh: "了解有关搜索的更多信息",
-    ar: "تعرف على المزيد حول البحث",
-    es: "Aprende más sobre la búsqueda",
-    it: "Ulteriori informazioni sulla ricerca",
-    he: "למידע נוסף על חיפוש",
-    pt: "Saiba mais sobre pesquisa"
+    fr: "En savoir plus sur la recherche"
   },
   {
     key: "landing_card_one_title",
     en: "A flexible Way to Educate, Collaborate and Team Up From Wherever You Are",
     de: "Eine flexible Art, sich auszuziehen, zusammenzuarbeiten, zusammenzuarbeiten, wo immer Sie sind",
     enus: "A flexible Way to Educate, Collaborate and Team Up From Wherever You Are",
-    fr: "Un moyen flexible d'éduquer, de collaborer et de faire équipe de partout où vous êtes",
-    zh: "从你所在的任何地方教育，协作和团队的灵活方式",
-    ar: "طريقة مرنة للتثقيف والتعاون والفريق لأعلى من أينما كنت",
-    es: "Una forma flexible de educar, colaborar y equipar desde donde sea que estés.",
-    it: "Un modo flessibile per educare, collaborare e lavorare da dove sei",
-    he: "דרך גמישה לחנך, לשתף פעולה ולצוות למעלה מכל מקום שאתה נמצא",
-    pt: "Uma maneira flexível de educar, colaborar e se juntar a onde quer que você esteja"
+    fr: "Un moyen flexible d'éduquer, de collaborer et de faire équipe de partout où vous êtes"
   },
   {
     key: "landing_card_one_button",
     en: "Sign up now for Free",
     de: "Jetzt kostenlos registrieren",
     enus: "Sign up now for Free",
-    fr: "Inscrivez-vous maintenant gratuitement",
-    zh: "现在免费注册",
-    ar: "اشترك الآن مجانا",
-    es: "Regístrate ahora gratis",
-    it: "Registrati subito gratuitamente",
-    he: "הרשם עכשיו בחינם",
-    pt: "Registe-se agora gratuitamente"
+    fr: "Inscrivez-vous maintenant gratuitement"
   },
   {
     key: "landing_card_two_title",
     en: "Be the First to get updates and exclusive offers from Zuri Chat",
     de: "Seien Sie der Erste, der Updates und exklusive Angebote vom ZURI-Chat erhalten",
     enus: "Be the First to get updates and exclusive offers from Zuri Chat",
-    fr: "Soyez le premier à obtenir des mises à jour et des offres exclusives de Zuri Chat",
-    zh: "成为第一个获取ZURI CHAT的更新和独家优惠",
-    ar: "كن أول من يحصل على التحديثات والعروض الحصرية من Chat Zuri",
-    es: "Sé el primero en obtener actualizaciones y ofertas exclusivas de Zuri Chat",
-    it: "Sii il primo a ricevere aggiornamenti ed offerte esclusive della chat zuri",
-    he: "היה הראשון לקבל עדכונים והצעות בלעדיות מ Zuri צ'אט",
-    pt: "Seja o primeiro a receber atualizações e ofertas exclusivas de Zuri Chat"
+    fr: "Soyez le premier à obtenir des mises à jour et des offres exclusives de Zuri Chat"
   },
   {
     key: "landing_card_two_description ",
     en: "Enrich your Zuri Chat experience by signing up here to get the latest news and special deals on Zuri Chat",
     de: "Bereichern Sie Ihr Zuri-Chat-Erlebnis, indem Sie sich hier anmelden, um die neuesten Nachrichten und Sonderangebote für ZURI-Chat zu erhalten",
     enus: "Enrich your Zuri Chat experience by signing up here to get the latest news and special deals on Zuri Chat",
-    fr: "Enrichissez votre expérience de chat Zuri en vous inscrivant ici pour obtenir les dernières nouvelles et offres spéciales sur Zuri Chat",
-    zh: "通过在这里注册来丰富您的ZURI聊天体验，以获得ZURI CHAT的最新消息和特价",
-    ar: "إثراء تجربة دردشة Zuri الخاصة بك عن طريق التسجيل هنا للحصول على آخر الأخبار والعروض الخاصة على دردشة Zuri",
-    es: "Enriquecer su experiencia de chat de Zuri al registrarse aquí para obtener las últimas noticias y ofertas especiales en Zuri Chat",
-    it: "Arricchisci la tua esperienza di chat Zuri registrando qui per ottenere le ultime notizie e offerte speciali sulla chat di zuri",
-    he: "להעשיר את חוויית הצאצאים שלך על ידי הרשמה לכאן כדי לקבל את החדשות האחרונות ועסקאות מיוחדות על צורי צ'אט",
-    pt: "Enriqueça sua experiência de bate-papo Zuri, inscrevendo-se aqui para obter as últimas notícias e ofertas especiais em Chat de Zuri"
+    fr: "Enrichissez votre expérience de chat Zuri en vous inscrivant ici pour obtenir les dernières nouvelles et offres spéciales sur Zuri Chat"
   },
   {
     key: "landing_card_two_button ",
     en: "Subscribe",
     de: "Abonnieren",
     enus: "Subscribe",
-    fr: "S'abonner",
-    zh: "订阅",
-    ar: "الإشتراك",
-    es: "Suscribir",
-    it: "sottoscrivi",
-    he: "להירשם כמנוי",
-    pt: "Se inscrever"
+    fr: "S'abonner"
   }
 ];
