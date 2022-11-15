3f0380c37e33a904f737a7b20bcfc2ba76b051b1
robiat <zakariyahrobiat2018@gmail.com>
2022-11-15 09:43:35 +0000

    change text and border radius of text on active

--- packages/main/src/pages/static/contact-us/components/FaqSelected.jsx
+++ packages/main/src/pages/static/contact-us/components/FaqSelected.jsx
@@ -1,7 +1,11 @@
 import ContactFormContainer from "./ContactFormContainer";
 
 const FaqSelected = () => {
-  return <div className="FAQselected">{/* <ContactFormContainer /> */}</div>;
+  return (
+    <div className="FAQselected">
+      <ContactFormContainer />
+    </div>
+  );
 };
 
 export default FaqSelected;
