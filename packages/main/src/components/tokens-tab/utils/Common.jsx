b7f698421f3a821cf5f0de60280f9ce9cedf39d0
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-11-23 17:23:07 +0100

    fix: fix import errors of common.jsx file of tokens tab in new src

--- packages/main/src/components/tokens-tab/utils/Common.jsx
+++ packages/main/src/components/tokens-tab/utils/Common.jsx
@@ -1,5 +1,5 @@
 import { authAxios } from "./Api";
-import styles from "../styles/paymentMethod.module.css";
+import styles from "../styles/PaymentMethod.module.css";
 
 // images
 import mastercard from "../assets/Mastercard.svg";
