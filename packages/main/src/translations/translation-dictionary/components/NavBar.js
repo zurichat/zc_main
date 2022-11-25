824ede539a4584b03fc7e71747c89c6811a98cc3
Kofo <kajotonis@gmail.com>
2022-11-25 11:46:38 +0100

    feat:enabled the words on the navbar to be translated

--- packages/main/src/translations/translation-dictionary/components/NavBar.js
+++ packages/main/src/translations/translation-dictionary/components/NavBar.js
@@ -3,56 +3,113 @@ export default [
     key: "nav_pricing",
     en: "Pricing",
     de: "Preisgestaltung",
+    nl: "prijzen",
     engb: "Pricing",
-    fr: "Tarification"
+    fr: "tarification",
+    zh: "价钱",
+    ar: "التسعير",
+    es: "fijación de precios",
+    it: "prezzi",
+    iw: "תמחור",
+    pt: "preços"
   },
   {
     key: "nav_about",
     en: "About",
-    de: "Über",
+    de: "um",
+    nl: "over",
     engb: "About",
-    fr: "Sur"
+    fr: "sur",
+    zh: "關於",
+    ar: "حول",
+    es: "sobre",
+    it: "di",
+    iw: "על אודות",
+    pt: "cerca de"
   },
   {
     key: "nav_plugin",
     en: "Plugin",
     de: "Plugin",
+    nl: "inpluggen",
     engb: "Plugin",
-    fr: "Brancher"
+    fr: "brancher",
+    zh: "插入",
+    ar: "توصيل في",
+    es: "enchufar",
+    it: "collegare",
+    iw: "חיבור",
+    pt: "plugar"
   },
   {
     key: "nav_downloads",
     en: "Downloads ",
     de: "Downloads",
-    engb: "Downloads "
+    nl: "downloaden",
+    engb: "Downloads ",
+    fr: "téléchargements",
+    zh: "下載",
+    ar: "التحميلات",
+    es: "descargas",
+    it: "download",
+    iw: "הורדות",
+    pt: "Transferências"
   },
   {
     key: "nav_contact",
     en: "Contact",
     de: "Kontakt",
+    nl: "contact",
     engb: "Contact",
-    fr: "Contact"
+    fr: "Contactez",
+    zh: "接觸",
+    ar: "اتصل",
+    es: "contacto",
+    it: "contatto",
+    iw: "איש קשר",
+    pt: "contato"
   },
   {
     key: "nav_signup",
     en: "Sign Up",
-    de: "Anmeldung",
+    de: "Anmelden",
+    nl: "aanmelden",
     engb: "Sign Up",
-    fr: "S'inscrire"
+    fr: "S'inscrire",
+    zh: "報名",
+    ar: "اشتراك",
+    es: "inscribirse",
+    it: "iscrizione",
+    iw: "הירשם",
+    pt: "inscrever-se"
   },
   {
     key: "nav_login",
     en: "Login",
     de: "Anmeldung",
+    nl: "Log in",
     engb: "Login",
-    fr: "Connexion"
+    fr: "connexion",
+    zh: "登錄",
+    ar: "تسجيل الدخول",
+    es: "acceso",
+    it: "Accedere",
+    iw: "התחברות",
+    pt: "Conecte-se"
   },
   {
     key: "nav_signout",
     en: "Sign Out",
-    de: "Austragen",
+    de: "austragen",
+    nl: "afmelden",
     engb: "Sign Out",
-    fr: "Se déconnecter"
+    fr: "se déconnecter",
+    zh: "登出",
+    ar: "خروج",
+    es: "desconectar",
+    it: "disconnessione",
+    iw: "התנתק",
+    pt: "sair"
   },
   {
     key: "search.placeholder",
