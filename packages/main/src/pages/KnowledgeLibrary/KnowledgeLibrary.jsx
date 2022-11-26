fd5551de135e11f4cc60f9f5df6f4ee7130ec4a3
Ejim Favour <favourejim56@gmail.com>
2022-11-26 23:44:07 +0100

    feat: knowledge library page created

--- packages/main/src/pages/KnowledgeLibrary/KnowledgeLibrary.jsx
+++ packages/main/src/pages/KnowledgeLibrary/KnowledgeLibrary.jsx
@@ -1,9 +1,34 @@
-import { TopNavigationBar } from "../../components";
+import { Footer, TopNavigationBar } from "../../components";
+import Navigation from "./Navigation/Navigation";
+import classes from "./KnowledgeLibrary.module.css";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
+import Home from "./Home/Home";
+import Categories from "./Categories/Categories";
 
 function KnowledgeLibrary() {
+  let { path, url } = useRouteMatch();
+
   return (
-    <div>
+    <div className={classes.container}>
       <TopNavigationBar />
+      <div className={classes["container-body"]}>
+        <Navigation />
+        <main className={classes.main}>
+          <h2>Welcome to Zuri chat Knowledge library</h2>
+          <p className={classes["head-p"]}>
+            Find useful company information in one place
+          </p>
+          <Switch>
+            <Route path={`${path}`} component={() => <Home />} exact />
+            <Route
+              path={`${path}/categories`}
+              component={() => <Categories />}
+              exact
+            />
+          </Switch>
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 }
