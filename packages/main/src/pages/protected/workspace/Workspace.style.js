e2fb285f9b343590cd19b4866c3c9f31db2d1beb
bazz <clement.f.bazz@gmail.com>
2022-11-27 15:27:43 +0100

    Fix:  Reduced Side Bar Width from 300 to 260

--- packages/main/src/pages/protected/workspace/Workspace.style.js
+++ packages/main/src/pages/protected/workspace/Workspace.style.js
@@ -21,8 +21,8 @@ export const TopBarWrapperStyle = styled.div`
 `;
 
 export const SidebarWrapperStyle = styled.div`
-  flex-basis: 20%;
-  max-width: 300px;
+  width: 100%;
+  max-width: 260px;
   min-width: 230px;
   height: 100%;
   -ms-overflow-style: none;
