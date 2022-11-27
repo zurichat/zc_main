83194cb56ac1daded41188500e8188904b9bf147
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-11-27 18:39:08 +0100

    fix: fix all the import errors of Language.jsx file from old to new src

--- packages/main/src/components/language/Language.jsx
+++ packages/main/src/components/language/Language.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Languages from "../constants/Language";
-import styles from "../styles/SettingsTab.module.css";
+import styles from "../language/SettingsTab.module.css";
 
 const Language = () => {
   return (
