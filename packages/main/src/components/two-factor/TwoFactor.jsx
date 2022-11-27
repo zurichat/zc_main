5dfa833d96b4894a24d86d620a73614b916b86dd
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-11-27 18:43:44 +0100

    fix: fix all import errors of TwoFactor.jsx file from old to new src

--- packages/main/src/components/two-factor/TwoFactor.jsx
+++ packages/main/src/components/two-factor/TwoFactor.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "../styles/SettingsTab.module.css";
+import styles from "../two-factor/SettingsTab.module.css";
 
 const TwoFactor = () => {
   return (
