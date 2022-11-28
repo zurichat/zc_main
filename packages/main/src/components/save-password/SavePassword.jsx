7a0b9bd3de6abffa0844b63031b6e565b392865a
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-28 10:37:58 +0100

    feat: (FRO-77) Set up form submission in SavePassword

--- packages/main/src/components/save-password/SavePassword.jsx
+++ packages/main/src/components/save-password/SavePassword.jsx
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useRef } from "react";
+import useForm from "../use-form-hook";
 import styles from "../save-password/SettingsTab.module.css";
 
 const SavePassword = () => {
+  const formElement = useRef(null);
+  const user = sessionStorage.getItem(`user`);
+  const FORM_ENDPOINT = `https://staging.api.zuri.chat/users/${user.id}`;
+  const { message, handleSubmit } = useForm({
+    form: formElement.current,
+    options: {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${user.token}`,
+        "Content-Type": "application/json"
+      }
+    },
+    endpointUrl: FORM_ENDPOINT
+  });
+
+  /* TODO: Display message after form submission */
+
   return (
     <div className={styles.passwordsection}>
-      <form className="row d-flex flex-column">
+      <form
+        className="row d-flex flex-column"
+        ref={formElement}
+        action={FORM_ENDPOINT}
+        onSubmit={handleSubmit}
+      >
         <div className="col-md-5">
-          <label htmlFor="firstname" className="form-label">
+          <label htmlFor="current_password" className="form-label">
             Current password
           </label>
-          <input type="password" className="form-control" required />
+          <input
+            name="current_password"
+            id="current_password"
+            type="password"
+            className="form-control"
+            required
+          />
         </div>
         <div className="col-md-5">
-          <label htmlFor="lastname" className="form-label">
+          <label htmlFor="password" className="form-label">
             New password
           </label>
-          <input type="password" className="form-control" required />
+          <input
+            name="password"
+            id="password"
+            type="password"
+            className="form-control"
+            required
+          />
         </div>
         <div className="col-md-4 mb-3 mt-3" id={styles.p_section}>
           <button className="btn" id="submit">
