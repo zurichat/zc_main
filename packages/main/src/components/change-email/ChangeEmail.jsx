1f586d5f7be25e51140e8f499d5b7ac672328615
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-28 10:51:21 +0100

    feat: (FRO-78) Set up form submission in ChangeEmail

--- packages/main/src/components/change-email/ChangeEmail.jsx
+++ packages/main/src/components/change-email/ChangeEmail.jsx
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useRef } from "react";
+import useForm from "../use-form-hook";
 import styles from "../change-email/SettingsTab.module.css";
 
 const ChangeEmail = () => {
+  const formElement = useRef(null);
+  const user = sessionStorage.getItem(`user`);
+  const FORM_ENDPOINT = `https://staging.api.zuri.chat/users/${user.id}`;
+  const { message, handleSubmit } = useForm({
+    form: formElement,
+    options: {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${user.token}`,
+        "Content-Type": "application/json"
+      }
+    },
+    endpointUrl: FORM_ENDPOINT
+  });
+
+  /* TODO: Display message after form submission */
+
   return (
     <div className={styles.passwordsection}>
-      <form className="row d-flex flex-column">
+      <form
+        className="row d-flex flex-column"
+        ref={formElement}
+        action={FORM_ENDPOINT}
+        onSubmit={handleSubmit}
+      >
         <div className="col-md-5">
-          <label htmlFor="firstname" className="form-label">
+          <label htmlFor="current_email" className="form-label">
             Current Email
           </label>
-          <input type="text" className="form-control" required />
+          <input
+            id="current_email"
+            name="current_email"
+            type="email"
+            className="form-control"
+            required
+          />
         </div>
         <div className="col-md-5">
-          <label htmlFor="lastname" className="form-label">
+          <label htmlFor="email" className="form-label">
             New email address
           </label>
-          <input type="text" className="form-control" required />
+          <input
+            id="email"
+            name="email"
+            type="email"
+            className="form-control"
+            required
+          />
         </div>
         <div className="col-md-4 mb-3 mt-3">
           <button className="btn" id="submit">
