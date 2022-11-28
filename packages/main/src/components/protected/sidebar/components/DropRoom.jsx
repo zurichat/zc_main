7491254dc6f6f0d0927bca63b4fccf8d31a9dedf
queen-x-vee <valentinabuoro4@gmail.com>
2022-11-28 17:28:17 +0100

    fix: removed unwanted packages

--- packages/main/src/components/protected/sidebar/components/DropRoom.jsx
+++ packages/main/src/components/protected/sidebar/components/DropRoom.jsx
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Drop.module.css";
 import ToggleSwitch from "./ToggleSwitch";
 import { BsToggleOff } from "react-icons/bs";
 import GrCircleAlert from "react-icons/gr";
+import axios from "axios";
 
 const DropRoom = props => {
+  //{ updatePrivacy, createChannel } = useChannel(props.data?.room_id);
+  const [state, setState] = useState({});
+
+  const handleEvent = ({ target }) => {
+    setState({
+      ...state,
+      [target.name]: target.type === "checkbox" ? target.checked : target.value
+    });
+  };
+
+  const handleSubmission = e => {
+    e.preventDefault();
+    //createChannel(state);
+    console.log(state);
+  };
+  const org_id = localStorage.getItem("currentWorkspace");
+  const user = JSON.parse(sessionStorage.getItem("organisations"));
+  const room = window.location.href.split("/").at(6);
+  const BASE_URL = "https://chat.zuri.chat";
+  const tina = user?.find(x => x.id == org_id)?.member_id;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/api/v1/org/${org_id}/rooms/${room}`)
+      .then(res => {
+        setData(res.data.data);
+      })
+      .catch(e => console.log(e));
+  }, [5]);
+
+  const updatePrivacy = () => {
+    const tina = user?.find(x => x.id === org_id)?.member_id;
+    if (data !== null) {
+      const newData = { ...data, is_private: JSON.stringify(state.Accept) };
+      console.log(`this is a new ${newData}`);
+      axios
+        .put(
+          `${BASE_URL}/api/v1/org/${org_id}/members/${tina}/rooms/${room}`,
+          newData
+        )
+        .then(res => {
+          console.log(res);
+        })
+        .catch(e => console.log(e));
+    }
+  };
+
   return props.trigger ? (
     <div
       className={`w-100 h-100 d-flex align-items-center justify-content-center ${styles.droproom__overlay} `}
@@ -25,23 +74,33 @@ const DropRoom = props => {
         </p>
         <form
           action=""
+          onSubmit={handleSubmission}
           className={`w-100 mt-3 mb-3 d-flex flex-column justify-content-between align-items-start`}
         >
           <div className={`w-100 d-flex flex-column  `}>
             <span className={`w-100`}>
               <label>Name</label>
               <input
+                name="name"
                 className={`w-100`}
                 type="text"
                 placeholder="# e.g, plan-budget"
                 required
+                onChange={handleEvent}
+                value={state["name"]}
               />
             </span>
             <span className={`w-100`}>
               <label>
                 Description <span>(optional)</span>
               </label>
-              <input className={`w-100`} type="text" />
+              <input
+                className={`w-100`}
+                type="text"
+                name="description"
+                onChange={handleEvent}
+                value={state["description"]}
+              />
               <label>
                 <span>What's this channel about?</span>
               </label>
@@ -57,7 +116,11 @@ const DropRoom = props => {
                   When a channel is set to private, it can only be viewed or
                   joined by invitation
                 </p>
-                <ToggleSwitch label="Accept" />
+                <ToggleSwitch
+                  label="Accept"
+                  name="private"
+                  onChange={handleEvent}
+                />
               </span>
             </div>
             <div
@@ -70,7 +133,10 @@ const DropRoom = props => {
                 </label>
               </span>
               {/* <GrCircleAlert /> */}
-              <button type="submit"> Create</button>
+              <button type="submit" onClick={updatePrivacy}>
+                {" "}
+                Create
+              </button>
             </div>
           </div>
         </form>
--- packages/main/src/components/protected/sidebar/components/ToggleSwitch.jsx
+++ packages/main/src/components/protected/sidebar/components/ToggleSwitch.jsx
@@ -2,44 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "../styles/Drop.module.css";
 import axios from "axios";
 
-const ToggleSwitch = ({ label, onChange }) => {
-  const org_id = localStorage.getItem("currentWorkspace");
-  const user = JSON.parse(sessionStorage.getItem("organisations"));
-  const room = window.location.href.split("/").at(6);
-  const BASE_URL = "https://chat.zuri.chat";
-
-  const [data, setData] = useState(null);
-  const [state, setState] = useState(true);
-  const handleChange = () => {
-    setState(!state);
-  };
-
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}/api/v1/org/${org_id}/rooms/${room}`)
-      .then(res => {
-        setData(res.data.data);
-      })
-      .catch(e => console.log(e));
-  }, [7]);
-
-  const privateChannel = () => {
-    const tina = user?.find(x => x.id == org_id)?.member_id;
-    console.log("loading");
-    if (data !== null) {
-      const newData = { ...data, is_private: true };
-      axios
-        .put(
-          `${BASE_URL}/api/v1/org/${org_id}/members/${tina}/rooms/${room}`,
-          newData
-        )
-        .then(res => {
-          console.log(res);
-        })
-        .catch(e => console.log(e));
-    }
-  };
-
+const ToggleSwitch = ({ label, value, onChange }) => {
   return (
     <div className={`${styles.container}`}>
       {label}{" "}
@@ -49,13 +12,11 @@ const ToggleSwitch = ({ label, onChange }) => {
           className={`${styles.checkbox}`}
           name={label}
           id={label}
+          checked={value}
+          onChange={onChange}
         />
         <label className={`${styles.label}`} htmlFor={label}>
-          <span
-            className={`${styles.inner}`}
-            onChange={handleChange}
-            onClick={state ? privateChannel : undefined}
-          />
+          <span className={`${styles.inner}`} />
         </label>
       </div>
     </div>
