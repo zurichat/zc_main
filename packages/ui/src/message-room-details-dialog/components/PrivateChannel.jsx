82947cf7e60f0d043eaeab619e6cb73af88852e3
queen-x-vee <valentinabuoro4@gmail.com>
2022-11-28 18:52:18 +0100

    fix: removed unwanted package

--- packages/ui/src/message-room-details-dialog/components/PrivateChannel.jsx
+++ packages/ui/src/message-room-details-dialog/components/PrivateChannel.jsx
@@ -2,8 +2,37 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ChannelModal from "./ChannelModal";
 import styles from "./archive-channel.module.css";
+import {
+  NotificationContainer,
+  NotificationManager
+} from "react-notifications";
 
 const PrivateChannel = ({ closeEdit }) => {
+  const createNotification = type => {
+    return () => {
+      switch (type) {
+        case "info":
+          NotificationManager.info("Info message");
+          break;
+        case "success":
+          NotificationManager.success("Success message", "Title here");
+          break;
+        case "warning":
+          NotificationManager.warning(
+            "Warning message",
+            "Close after 3000ms",
+            3000
+          );
+          break;
+        case "error":
+          NotificationManager.error("Error message", "Click me!", 5000, () => {
+            alert("callback");
+          });
+          break;
+      }
+    };
+  };
+
   const org_id = localStorage.getItem("currentWorkspace");
   const user = JSON.parse(sessionStorage.getItem("organisations"));
   const room = window.location.href.split("/").at(6);
@@ -31,33 +60,37 @@ const PrivateChannel = ({ closeEdit }) => {
         )
         .then(res => {
           console.log(res);
+          createNotification("success");
         })
         .catch(e => console.log(e));
     }
   };
 
   return (
-    <ChannelModal
-      closeEdit={closeEdit}
-      full
-      archiveTitle="Make this channel Private?"
-    >
-      <div className={styles.content}>
-        <p className={styles.p}>When you make a channel private</p>
-        <p className={styles.p}>It means that:</p>
-        <ul className={styles.ul}>
-          <li className={styles.li}>
-            only you can invite members to this channel
-          </li>
-        </ul>
-        <div className={styles.button}>
-          <button className={styles.button4}>Cancel</button>
-          <button className={styles.button2} onClick={privateChannel}>
-            Private Channel
-          </button>
+    <>
+      <NotificationContainer />
+      <ChannelModal
+        closeEdit={closeEdit}
+        full
+        archiveTitle="Make this channel Private?"
+      >
+        <div className={styles.content}>
+          <p className={styles.p}>When you make a channel private</p>
+          <p className={styles.p}>It means that:</p>
+          <ul className={styles.ul}>
+            <li className={styles.li}>
+              only you can invite members to this channel
+            </li>
+          </ul>
+          <div className={styles.button}>
+            <button className={styles.button4}>Cancel</button>
+            <button className={styles.button2} onClick={privateChannel}>
+              Private Channel
+            </button>
+          </div>
         </div>
-      </div>
-    </ChannelModal>
+      </ChannelModal>
+    </>
   );
 };
 
