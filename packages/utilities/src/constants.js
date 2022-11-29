7adaa9e0bdda69c2112166196c9287bf251607e4
Obi Fortune <gabrielobi.of@gmail.com>
2022-11-29 23:18:47 +0100

    Updated agora envs

--- packages/utilities/src/constants.js
+++ packages/utilities/src/constants.js
@@ -16,3 +16,7 @@ export const BASE_CLIENT_URL =
 export const WS_CONNECTION_URL =
   process.env.WS_CONNECTION_URL ||
   "wss://realtime.zuri.chat/connection/websocket";
+
+export const AGORA_APP_ID = process.env.REACT_APP_AGORA_APP_ID;
+
+export const AGORA_TOKEN = process.env.REACT_APP_AGORA_TOKEN;
