ac7d50bbba597fe6fb9bba6de27c062d3582d02c
Obi Fortune <gabrielobi.of@gmail.com>
2022-11-29 23:11:24 +0100

    added fallback urls

--- packages/utilities/src/constants.js
+++ packages/utilities/src/constants.js
@@ -1,17 +1,18 @@
 /**
  * @description base API URL for request making
  */
-
-export const BASE_API_URL = process.env.BASE_API_URL;
+export const BASE_API_URL =
+  process.env.BASE_API_URL || "https://staging.api.zuri.chat";
 
 /**
  * @description base URL for the client
  */
-
-export const BASE_CLIENT_URL = process.env.BASE_CLIENT_URL;
+export const BASE_CLIENT_URL =
+  process.env.BASE_CLIENT_URL || "https://staging.zuri.chat";
 
 /**
  * @description centrifugo websocket connection URL
  */
-
-export const WS_CONNECTION_URL = process.env.WS_CONNECTION_URL;
+export const WS_CONNECTION_URL =
+  process.env.WS_CONNECTION_URL ||
+  "wss://realtime.zuri.chat/connection/websocket";
