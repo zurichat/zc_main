56dae496308c21eec02fe9a11c82aaa058de84a7
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-12-09 23:59:20 +0100

    fix: fix all the import errors of SettingsNav file for AcctDeactivation file from old to new src

--- packages/main/src/components/settings-nav/SettingsNav.jsx
+++ packages/main/src/components/settings-nav/SettingsNav.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from "react";
-import { TopbarContext } from "../../../context/Topbar";
+// import { TopbarContext } from "../../../context/Topbar";
 
-import styles from "../styles/settingsNav.module.css";
+import styles from "../settings-nav/SettingsNav.module.css";
 import { BiMenuAltRight } from "react-icons/bi";
 import { Link } from "react-router-dom";
-import zuri from "../assets/zuri.svg";
-import navImage from "../assets/navImage.png";
+import zuri from "../settings-nav/assets/zuri.svg";
+import navImage from "../settings-nav/assets/navImage.png";
 
 const SettingsNav = () => {
   const [menu, setMenu] = useState(false);
