c2a375427f20af1947527c78c4f053b90ce4b4fd
CM <etchiedanie1@gmail.com>
2022-12-09 09:05:19 +0100

    feat: verify current password and fix signout of all session leading to an error page

--- packages/main/src/components/save-password/SavePassword.jsx
+++ packages/main/src/components/save-password/SavePassword.jsx
@@ -1,27 +1,87 @@
-import React, { useRef } from "react";
-import useForm from "../use-form-hook";
+import React, { useRef, useState } from "react";
 import styles from "../save-password/SettingsTab.module.css";
 import { BASE_API_URL } from "@zuri/utilities";
 
+let errorOut = null;
+
 const SavePassword = () => {
   const formElement = useRef(null);
+  const [state, setState] = useState({ status: "idle", message: null });
   const user = JSON.parse(sessionStorage.getItem("user"));
   const FORM_ENDPOINT = `${BASE_API_URL}/users/${user.id}`;
-  const { message, handleSubmit } = useForm({
-    form: formElement.current,
-    options: {
-      method: "PATCH",
-      credentials: "include",
+
+  const checkPassword = async password => {
+    const confirmPassword = {
+      password,
+      confirm_password: password
+    };
+    const response = await fetch(`${BASE_API_URL}/auth/confirm-password`, {
+      method: "POST",
       headers: {
         Accept: "application/json",
         Authorization: `Bearer ${user.token}`,
         "Content-Type": "application/json"
-      }
-    },
-    endpointUrl: FORM_ENDPOINT
-  });
+      },
+      body: JSON.stringify(confirmPassword)
+    });
 
-  /* TODO: Display message after form submission */
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
+    return "success";
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    clearTimeout(errorOut);
+
+    setState({ status: "loading", message: "Please wait..." });
+
+    const form = formElement.current;
+
+    const data = Array.from(form.elements)
+      .filter(input => input.name)
+      .reduce(
+        (obj, input) => Object.assign(obj, { [input.name]: input.value }),
+        {}
+      );
+
+    checkPassword(data.current_password)
+      .then(response => {
+        /* TODO SEND REQUEST TO CHANGE USERS PASSWORD */
+
+        setState({
+          status: "info",
+          message: "Change Password feature is currently unavailable"
+        });
+      })
+      .catch(err => {
+        setState({ status: "error", message: err.message });
+      });
+
+    errorOut = setTimeout(() => {
+      setState({ status: "idle", message: null });
+    }, 30000);
+  };
+
+  const buttonState = state.status === "loading" ? { disabled: true } : {};
+  const message = () => {
+    const status = state.status;
+    switch (status) {
+      case "success":
+        return <p className={styles.success}>{state.message}</p>;
+      case "error":
+        return <p className={styles.error}>{state.message}</p>;
+      case "info":
+        return <p className={styles.info}>{state.message}</p>;
+      default:
+        return "";
+    }
+  };
 
   return (
     <div className={styles.passwordsection}>
@@ -35,14 +95,19 @@ const SavePassword = () => {
           <label htmlFor="current_password" className="form-label">
             Current password
           </label>
-          <input type="password" className="form-control" required />
+          <input
+            name="current_password"
+            type="password"
+            className="form-control"
+            required
+          />
         </div>
         <div className="col-md-5">
           <label htmlFor="password" className="form-label">
             New password
           </label>
           <input
-            name="password"
+            name="new_password"
             id="password"
             type="password"
             className="form-control"
@@ -50,8 +115,16 @@ const SavePassword = () => {
           />
         </div>
         <div className="col-md-4 mb-3 mt-3" id={styles.p_section}>
-          <button className="btn" id="submit">
-            Save password
+          {message()}
+          <button className="btn" id="submit" {...buttonState}>
+            {state.status === "loading" ? (
+              <>
+                <span className={styles.loader}></span>
+                {state.message}
+              </>
+            ) : (
+              "Save Password"
+            )}
           </button>
           <br />
           <p>
