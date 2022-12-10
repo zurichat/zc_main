dd555d781890c2b05445dd1fa9965802fbd44531
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-12-10 03:48:25 +0100

    fix: fix all the import errors of ClearTime file of Statuses file

--- packages/main/src/components/clear-time/ClearTIme.jsx
+++ packages/main/src/components/clear-time/ClearTIme.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "../styles/ClearTime.module.css";
+import style from "../clear-time/ClearTime.module.css";
 
 const ClearTime = ({ defaultOptionValue }) => {
   return (
